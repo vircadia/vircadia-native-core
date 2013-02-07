3c96f4d3a43702be3cf32411812b8f556cc16921
Stephen Birarda <commit@birarda.com>
2013-02-07 15:40:17 -0800

    remove thirdparty directory in interface project

--- interface/thirdparty/glm/interface.build/Debug/glm.build/Script-82940E848B4C4A3D899023FC.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/birarda/code/worklist/interface/interface/thirdparty/glm -f /Users/birarda/code/worklist/interface/interface/thirdparty/glm/CMakeScripts/glm_postBuildPhase.make$CONFIGURATION all
