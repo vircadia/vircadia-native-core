71713cf2653fc71859891d6182dbce875ad7726a
Stephen Birarda <commit@birarda.com>
2013-02-08 13:28:36 -0800

    conditional inclusion of glext.h for non-apple OS

--- interface/src/Cloud.cpp
+++ interface/src/Cloud.cpp
@@ -10,6 +10,9 @@
 #include <InterfaceConfig.h>
 #include "Cloud.h"
 #include "Util.h"
+#ifndef __APPLE__
+#include <GL/glext.h>
+#endif
 
 #define COLOR_MIN 0.2f // minimum R/G/B value at 0,0,0 - also needs setting in field.cpp
 
