6c70d264ac60c3cc745022e46cf1cb6e126714ad
Stephen Birarda <commit@birarda.com>
2013-02-19 12:50:19 -0800

    fix company name in header comments

--- interface/src/Audio.cpp
+++ interface/src/Audio.cpp
@@ -3,7 +3,7 @@
 //  interface
 //
 //  Created by Stephen Birarda on 1/22/13.
-//  Copyright (c) 2013 High Fidelity, Inc.. All rights reserved.
+//  Copyright (c) 2013 High Fidelity, Inc. All rights reserved.
 //
 
 #include <iostream>
