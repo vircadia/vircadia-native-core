47e2e6651a47b7f1df68f9c7d64997467fd0de98
Stephen Birarda <commit@birarda.com>
2013-02-21 12:57:45 -0800

    add AgentData.cpp with defined abstract destructor

---
+++ shared/src/AgentData.cpp
@@ -0,0 +1,11 @@
+//
+//  AgentData.cpp
+//  hifi
+//
+//  Created by Stephen Birarda on 2/19/13.
+//  Copyright (c) 2013 High Fidelity, Inc. All rights reserved.
+//
+
+#include "AgentData.h"
+
+AgentData::~AgentData() {}
