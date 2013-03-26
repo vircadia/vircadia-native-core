761db13e90483ccce07cd43241eeb98046dfdf1a
Stephen Birarda <commit@birarda.com>
2013-03-26 14:06:34 -0700

    add the MarkerNode class to maintain markers during tree traversal

---
+++ voxel/src/MarkerNode.cpp
@@ -0,0 +1,9 @@
+//
+//  MarkerNode.cpp
+//  hifi
+//
+//  Created by Stephen Birarda on 3/26/13.
+//
+//
+
+#include "MarkerNode.h"
---
+++ voxel/src/MarkerNode.h
@@ -0,0 +1,14 @@
+//
+//  MarkerNode.h
+//  hifi
+//
+//  Created by Stephen Birarda on 3/26/13.
+//
+//
+
+#ifndef __hifi__MarkerNode__
+#define __hifi__MarkerNode__
+
+#include <iostream>
+
+#endif /* defined(__hifi__MarkerNode__) */
