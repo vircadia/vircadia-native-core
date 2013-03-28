f469ec8e2e257475f5b5b2cacf394147cc1c4053
Stephen Birarda <commit@birarda.com>
2013-03-28 14:33:26 -0700

    remove the notion of last sent level from VoxelAgentData

--- voxel/src/VoxelAgentData.cpp
+++ voxel/src/VoxelAgentData.cpp
@@ -11,7 +11,6 @@
 #include <cstdio>
 
 VoxelAgentData::VoxelAgentData() {
-    lastSentLevel = 0;
     rootMarkerNode = new MarkerNode();
 }
 
@@ -20,7 +19,6 @@ VoxelAgentData::~VoxelAgentData() {
 }
 
 VoxelAgentData::VoxelAgentData(const VoxelAgentData &otherAgentData) {
-    lastSentLevel = otherAgentData.lastSentLevel;
     memcpy(position, otherAgentData.position, sizeof(float) * 3);
     rootMarkerNode = new MarkerNode(*otherAgentData.rootMarkerNode);
 }
