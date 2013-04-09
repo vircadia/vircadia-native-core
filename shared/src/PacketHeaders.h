b636536ab715ce48762c27884c11ff3808956f01
Stephen Birarda <commit@birarda.com>
2013-04-08 18:23:57 -0700

    remove interface define in PacketHeader.h

--- shared/src/PacketHeaders.h
+++ shared/src/PacketHeaders.h
@@ -9,7 +9,7 @@
 #ifndef hifi_PacketHeaders_h
 #define hifi_PacketHeaders_h
 
-const char PACKET_HEADER_INTERFACE = 'I';
+const char PACKET_HEADER_DOMAIN = 'D';
 const char PACKET_HEADER_PING = 'P';
 
 #endif
