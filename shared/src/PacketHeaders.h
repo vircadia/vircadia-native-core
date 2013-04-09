06a5e4d8a95c6fd7b393dca2b9d77448feff3649
Philip Rosedale <philip@coffeeandpower.com>
2013-04-08 19:03:49 -0700

    Adding more packet headers

--- shared/src/PacketHeaders.h
+++ shared/src/PacketHeaders.h
@@ -5,6 +5,9 @@
 //  Created by Stephen Birarda on 4/8/13.
 //
 //
+//  The packet headers below refer to the first byte of a received UDP packet transmitted between
+//  any two Hifi components.  For example, a packet whose first byte is 'P' is always a ping packet.
+//  
 
 #ifndef hifi_PacketHeaders_h
 #define hifi_PacketHeaders_h
@@ -12,5 +15,6 @@
 const char PACKET_HEADER_DOMAIN = 'D';
 const char PACKET_HEADER_PING = 'P';
 const char PACKET_HEADER_PING_REPLY = 'R';
+const char PACKET_HEADER_HEAD = 'H';
 
 #endif
