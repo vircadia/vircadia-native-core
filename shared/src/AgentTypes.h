712997451bda7398730660279dfa6ac76c5a1936
ZappoMan <bradh@konamoxt.com>
2013-04-09 13:00:20 -0700

    Adding friendly names to AgentType display.

--- shared/src/AgentTypes.h
+++ shared/src/AgentTypes.h
@@ -12,6 +12,12 @@
 #ifndef hifi_AgentTypes_h
 #define hifi_AgentTypes_h
 
+// NOTE: If you add a new AGENT_TYPE_XXX then you also should add a new AGENT_TYPE_NAME_XXX and a new "case" to the
+//       switch statement in Agent.cpp specifically Agent::getTypeName().
+//       If you don't then it will make things harder on your co-developers in debugging because the Agent
+//       class won't know the name and will report it as "Unknown".
+
+// Agent Type Codes
 const char AGENT_TYPE_DOMAIN = 'D';
 const char AGENT_TYPE_VOXEL = 'V';
 const char AGENT_TYPE_INTERFACE = 'I'; // could also be injector???
