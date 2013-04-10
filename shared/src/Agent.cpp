7742d3114e100ff39aa805ed0f918f6bf76c94cb
Stephen Birarda <commit@birarda.com>
2013-04-09 17:03:10 -0700

    add avatar mixer to agent type names, remove head

--- shared/src/Agent.cpp
+++ shared/src/Agent.cpp
@@ -99,8 +99,8 @@ char Agent::getType() const {
 const char* AGENT_TYPE_NAME_DOMAIN = "Domain";
 const char* AGENT_TYPE_NAME_VOXEL = "Voxel Server";
 const char* AGENT_TYPE_NAME_INTERFACE = "Client Interface";
-const char* AGENT_TYPE_NAME_HEAD = "Avatar Head"; // Is this needed???
 const char* AGENT_TYPE_NAME_AUDIO_MIXER = "Audio Mixer";
+const char* AGENT_TYPE_NAME_AVATAR_MIXER = "Avatar Mixer";
 const char* AGENT_TYPE_NAME_UNKNOWN = "Unknown";
 
 const char* Agent::getTypeName() const {
@@ -115,12 +115,12 @@ const char* Agent::getTypeName() const {
 		case AGENT_TYPE_INTERFACE:
 			name = AGENT_TYPE_NAME_INTERFACE;
 			break;
-		case AGENT_TYPE_HEAD:
-			name = AGENT_TYPE_NAME_HEAD;
-			break;
 		case AGENT_TYPE_AUDIO_MIXER:
 			name = AGENT_TYPE_NAME_AUDIO_MIXER;
 			break;
+        case AGENT_TYPE_AVATAR_MIXER:
+            name = AGENT_TYPE_NAME_AVATAR_MIXER;
+            break;
 	}
 	return name;
 }
