093f489520f9024c5f6a88682f7c809021d6a8b8
ZappoMan <bradh@konamoxt.com>
2013-04-13 13:07:02 -0700

    make menu columns render properly for menu items wider than 100 pixels

--- interface/src/MenuColumn.h
+++ interface/src/MenuColumn.h
@@ -27,6 +27,7 @@ public:
     void render(int yOffset, int menuHeight, int lineHeight);
     void renderMouseOver(int yOffset);
     int addRow(const char* rowName, MenuRowCallback callback);
+	int getMaxRowWidth();
 private:
     char columnName[MAX_COLUMN_NAME];
     int columnWidth;
