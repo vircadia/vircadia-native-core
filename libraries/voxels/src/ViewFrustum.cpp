8ad51b6c0833ca2112f65162c6c086ccc6760901
ZappoMan <bradh@konamoxt.com>
2013-04-19 19:56:03 -0700

    got rid of a bunch of this-> at Stephen's request. Added debuging for box in view

--- libraries/voxels/src/ViewFrustum.cpp
+++ libraries/voxels/src/ViewFrustum.cpp
@@ -53,92 +53,92 @@ void ViewFrustum::calculate() {
 	glm::vec3 front    = _direction;
 	
 	// Calculating field of view.
-	float fovInRadians = this->_fieldOfView * PI_OVER_180;
+	float fovInRadians = _fieldOfView * PI_OVER_180;
 	
 	float twoTimesTanHalfFOV = 2.0f * tan(fovInRadians/2.0f);
 
     // Do we need this?
 	//tang = (float)tan(ANG2RAD * angle * 0.5) ;
 
-    float nearClip = this->_nearClip;
-    float farClip  = this->_farClip;
+    float nearClip = _nearClip;
+    float farClip  = _farClip;
 	
-	this->_nearHeight = (twoTimesTanHalfFOV * nearClip);
-	this->_nearWidth  = this->_nearHeight * this->_aspectRatio;
-	this->_farHeight  = (twoTimesTanHalfFOV * farClip);
-	this->_farWidth   = this->_farHeight * this->_aspectRatio;
+	_nearHeight = (twoTimesTanHalfFOV * nearClip);
+	_nearWidth  = _nearHeight * _aspectRatio;
+	_farHeight  = (twoTimesTanHalfFOV * farClip);
+	_farWidth   = _farHeight * _aspectRatio;
 
-	float farHalfHeight    = (this->_farHeight * 0.5f);
-	float farHalfWidth     = (this->_farWidth  * 0.5f);
-	this->_farCenter       = this->_position+front * farClip;
-	this->_farTopLeft      = this->_farCenter  + (this->_up * farHalfHeight)  - (this->_right * farHalfWidth); 
-	this->_farTopRight     = this->_farCenter  + (this->_up * farHalfHeight)  + (this->_right * farHalfWidth); 
-	this->_farBottomLeft   = this->_farCenter  - (this->_up * farHalfHeight)  - (this->_right * farHalfWidth); 
-	this->_farBottomRight  = this->_farCenter  - (this->_up * farHalfHeight)  + (this->_right * farHalfWidth); 
+	float farHalfHeight    = (_farHeight * 0.5f);
+	float farHalfWidth     = (_farWidth  * 0.5f);
+	_farCenter       = _position+front * farClip;
+	_farTopLeft      = _farCenter  + (_up * farHalfHeight)  - (_right * farHalfWidth); 
+	_farTopRight     = _farCenter  + (_up * farHalfHeight)  + (_right * farHalfWidth); 
+	_farBottomLeft   = _farCenter  - (_up * farHalfHeight)  - (_right * farHalfWidth); 
+	_farBottomRight  = _farCenter  - (_up * farHalfHeight)  + (_right * farHalfWidth); 
 
-	float nearHalfHeight   = (this->_nearHeight * 0.5f);
-	float nearHalfWidth    = (this->_nearWidth  * 0.5f);
-	this->_nearCenter      = this->_position+front * nearClip;
-	this->_nearTopLeft     = this->_nearCenter + (this->_up * nearHalfHeight) - (this->_right * nearHalfWidth); 
-	this->_nearTopRight    = this->_nearCenter + (this->_up * nearHalfHeight) + (this->_right * nearHalfWidth); 
-	this->_nearBottomLeft  = this->_nearCenter - (this->_up * nearHalfHeight) - (this->_right * nearHalfWidth); 
-	this->_nearBottomRight = this->_nearCenter - (this->_up * nearHalfHeight) + (this->_right * nearHalfWidth); 
+	float nearHalfHeight   = (_nearHeight * 0.5f);
+	float nearHalfWidth    = (_nearWidth  * 0.5f);
+	_nearCenter      = _position+front * nearClip;
+	_nearTopLeft     = _nearCenter + (_up * nearHalfHeight) - (_right * nearHalfWidth); 
+	_nearTopRight    = _nearCenter + (_up * nearHalfHeight) + (_right * nearHalfWidth); 
+	_nearBottomLeft  = _nearCenter - (_up * nearHalfHeight) - (_right * nearHalfWidth); 
+	_nearBottomRight = _nearCenter - (_up * nearHalfHeight) + (_right * nearHalfWidth); 
 
 	// compute the six planes
 	// the function set3Points assumes that the points
 	// are given in counter clockwise order
-	this->_planes[TOPP].set3Points(this->_nearTopRight,this->_nearTopLeft,this->_farTopLeft);
-	this->_planes[BOTTOMP].set3Points(this->_nearBottomLeft,this->_nearBottomRight,this->_farBottomRight);
-	this->_planes[LEFTP].set3Points(this->_nearTopLeft,this->_nearBottomLeft,this->_farBottomLeft);
-	this->_planes[RIGHTP].set3Points(this->_nearBottomRight,this->_nearTopRight,this->_farBottomRight);
-	this->_planes[NEARP].set3Points(this->_nearTopLeft,this->_nearTopRight,this->_nearBottomRight);
-	this->_planes[FARP].set3Points(this->_farTopRight,this->_farTopLeft,this->_farBottomLeft);
+	_planes[TOPP].set3Points(_nearTopRight,_nearTopLeft,_farTopLeft);
+	_planes[BOTTOMP].set3Points(_nearBottomLeft,_nearBottomRight,_farBottomRight);
+	_planes[LEFTP].set3Points(_nearTopLeft,_nearBottomLeft,_farBottomLeft);
+	_planes[RIGHTP].set3Points(_nearBottomRight,_nearTopRight,_farBottomRight);
+	_planes[NEARP].set3Points(_nearTopLeft,_nearTopRight,_nearBottomRight);
+	_planes[FARP].set3Points(_farTopRight,_farTopLeft,_farBottomLeft);
 
 }
 
 void ViewFrustum::dump() {
 
-    printLog("position.x=%f, position.y=%f, position.z=%f\n", this->_position.x, this->_position.y, this->_position.z);
-    printLog("direction.x=%f, direction.y=%f, direction.z=%f\n", this->_direction.x, this->_direction.y, this->_direction.z);
-    printLog("up.x=%f, up.y=%f, up.z=%f\n", this->_up.x, this->_up.y, this->_up.z);
-    printLog("right.x=%f, right.y=%f, right.z=%f\n", this->_right.x, this->_right.y, this->_right.z);
+    printLog("position.x=%f, position.y=%f, position.z=%f\n", _position.x, _position.y, _position.z);
+    printLog("direction.x=%f, direction.y=%f, direction.z=%f\n", _direction.x, _direction.y, _direction.z);
+    printLog("up.x=%f, up.y=%f, up.z=%f\n", _up.x, _up.y, _up.z);
+    printLog("right.x=%f, right.y=%f, right.z=%f\n", _right.x, _right.y, _right.z);
 
-    printLog("farDist=%f\n", this->_farClip);
-    printLog("farHeight=%f\n", this->_farHeight);
-    printLog("farWidth=%f\n", this->_farWidth);
+    printLog("farDist=%f\n", _farClip);
+    printLog("farHeight=%f\n", _farHeight);
+    printLog("farWidth=%f\n", _farWidth);
 
-    printLog("nearDist=%f\n", this->_nearClip);
-    printLog("nearHeight=%f\n", this->_nearHeight);
-    printLog("nearWidth=%f\n", this->_nearWidth);
+    printLog("nearDist=%f\n", _nearClip);
+    printLog("nearHeight=%f\n", _nearHeight);
+    printLog("nearWidth=%f\n", _nearWidth);
 
     printLog("farCenter.x=%f,      farCenter.y=%f,      farCenter.z=%f\n",
-    	this->_farCenter.x, this->_farCenter.y, this->_farCenter.z);
+    	_farCenter.x, _farCenter.y, _farCenter.z);
     printLog("farTopLeft.x=%f,     farTopLeft.y=%f,     farTopLeft.z=%f\n",
-    	this->_farTopLeft.x, this->_farTopLeft.y, this->_farTopLeft.z);
+    	_farTopLeft.x, _farTopLeft.y, _farTopLeft.z);
     printLog("farTopRight.x=%f,    farTopRight.y=%f,    farTopRight.z=%f\n",
-    	this->_farTopRight.x, this->_farTopRight.y, this->_farTopRight.z);
+    	_farTopRight.x, _farTopRight.y, _farTopRight.z);
     printLog("farBottomLeft.x=%f,  farBottomLeft.y=%f,  farBottomLeft.z=%f\n",
-    	this->_farBottomLeft.x, this->_farBottomLeft.y, this->_farBottomLeft.z);
+    	_farBottomLeft.x, _farBottomLeft.y, _farBottomLeft.z);
     printLog("farBottomRight.x=%f, farBottomRight.y=%f, farBottomRight.z=%f\n",
-    	this->_farBottomRight.x, this->_farBottomRight.y, this->_farBottomRight.z);
+    	_farBottomRight.x, _farBottomRight.y, _farBottomRight.z);
 
     printLog("nearCenter.x=%f,      nearCenter.y=%f,      nearCenter.z=%f\n",
-    	this->_nearCenter.x, this->_nearCenter.y, this->_nearCenter.z);
+    	_nearCenter.x, _nearCenter.y, _nearCenter.z);
     printLog("nearTopLeft.x=%f,     nearTopLeft.y=%f,     nearTopLeft.z=%f\n",
-    	this->_nearTopLeft.x, this->_nearTopLeft.y, this->_nearTopLeft.z);
+    	_nearTopLeft.x, _nearTopLeft.y, _nearTopLeft.z);
     printLog("nearTopRight.x=%f,    nearTopRight.y=%f,    nearTopRight.z=%f\n",
-    	this->_nearTopRight.x, this->_nearTopRight.y, this->_nearTopRight.z);
+    	_nearTopRight.x, _nearTopRight.y, _nearTopRight.z);
     printLog("nearBottomLeft.x=%f,  nearBottomLeft.y=%f,  nearBottomLeft.z=%f\n",
-    	this->_nearBottomLeft.x, this->_nearBottomLeft.y, this->_nearBottomLeft.z);
+    	_nearBottomLeft.x, _nearBottomLeft.y, _nearBottomLeft.z);
     printLog("nearBottomRight.x=%f, nearBottomRight.y=%f, nearBottomRight.z=%f\n",
-    	this->_nearBottomRight.x, this->_nearBottomRight.y, this->_nearBottomRight.z);
+    	_nearBottomRight.x, _nearBottomRight.y, _nearBottomRight.z);
 }
 
 
 int ViewFrustum::pointInFrustum(glm::vec3 &p) {
 	int result = INSIDE;
 	for(int i=0; i < 6; i++) {
-		if (this->_planes[i].distance(p) < 0)
+		if (_planes[i].distance(p) < 0)
 			return OUTSIDE;
 	}
 	return(result);
@@ -148,7 +148,7 @@ int ViewFrustum::sphereInFrustum(glm::vec3 &center, float radius) {
 	int result = INSIDE;
 	float distance;
 	for(int i=0; i < 6; i++) {
-		distance = this->_planes[i].distance(center);
+		distance = _planes[i].distance(center);
 		if (distance < -radius)
 			return OUTSIDE;
 		else if (distance < radius)
@@ -159,12 +159,37 @@ int ViewFrustum::sphereInFrustum(glm::vec3 &center, float radius) {
 
 
 int ViewFrustum::boxInFrustum(AABox &b) {
+
+printf("ViewFrustum::boxInFrustum() box.corner=%f,%f,%f x=%f\n",b.corner.x,b.corner.y,b.corner.z,b.x);
 	int result = INSIDE;
 	for(int i=0; i < 6; i++) {
-		if (this->_planes[i].distance(b.getVertexP(this->_planes[i].normal)) < 0)
+
+        printf("plane[%d] -- point(%f,%f,%f) normal(%f,%f,%f) d=%f \n",i,
+            _planes[i].point.x, _planes[i].point.y, _planes[i].point.z, 
+            _planes[i].normal.x, _planes[i].normal.y, _planes[i].normal.z, 
+            _planes[i].d
+        );
+
+	    glm::vec3 normal = _planes[i].normal;
+	    glm::vec3 boxVertexP = b.getVertexP(normal);
+	    float planeToBoxVertexPDistance = _planes[i].distance(boxVertexP);
+
+	    glm::vec3 boxVertexN = b.getVertexN(normal);
+	    float planeToBoxVertexNDistance = _planes[i].distance(boxVertexN);
+	    
+	    
+	    
+        printf("plane[%d] normal=(%f,%f,%f) bVertexP=(%f,%f,%f) planeToBoxVertexPDistance=%f  boxVertexN=(%f,%f,%f) planeToBoxVertexNDistance=%f\n",i, 
+            normal.x,normal.y,normal.z,
+            boxVertexP.x,boxVertexP.y,boxVertexP.z,planeToBoxVertexPDistance,
+            boxVertexN.x,boxVertexN.y,boxVertexN.z,planeToBoxVertexNDistance
+            );
+
+		if (planeToBoxVertexPDistance < 0) {
 			return OUTSIDE;
-		else if (this->_planes[i].distance(b.getVertexN(this->_planes[i].normal)) < 0)
+		} else if (planeToBoxVertexNDistance < 0) {
 			result =  INTERSECT;
+		}
 	}
 	return(result);
  }
