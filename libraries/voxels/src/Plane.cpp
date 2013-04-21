8a150d27b8ad84754739a570011294b7a8e6a3b4
ZappoMan <bradh@konamoxt.com>
2013-04-21 15:18:17 -0700

    Cleanup for coding standard fix bug in creation of plane from points - cleaned up code to match HiFi Coding standard - fixed bug in creation of plane related to cross product

--- libraries/voxels/src/Plane.cpp
+++ libraries/voxels/src/Plane.cpp
@@ -1,6 +1,12 @@
-// Plane.cpp
 //
-//////////////////////////////////////////////////////////////////////
+//  Plane.h
+//  hifi
+//
+//  Created by Brad Hefta-Gaub on 04/11/13.
+//  Originally from lighthouse3d. Modified to utilize glm::vec3 and clean up to our coding standards
+//
+//  Simple plane class.
+//
 
 #include "Plane.h"
 #include <stdio.h>
@@ -10,78 +16,83 @@
 using voxels_lib::printLog;
 
 // These are some useful utilities that vec3 is missing
-float vec3_length(const glm::vec3& v) {
-	return((float)sqrt(v.x*v.x + v.y*v.y + v.z*v.z));
+float length(const glm::vec3& v) {
+    return((float)sqrt(v.x * v.x + v.y * v.y + v.z * v.z));
 }
 
-void vec3_normalize(glm::vec3& v) {
-
-	float len;
-
-	len = vec3_length(v);
-	if (len) {
-		v.x /= len;;
-		v.y /= len;
-		v.z /= len;
-	}
+void normalize(glm::vec3& v) {
+    float len;
+    len = length(v);
+    if (len) {
+        v.x /= len;;
+        v.y /= len;
+        v.z /= len;
+    }
 }
 
-float vec3_innerProduct(const glm::vec3& v1,const glm::vec3& v2) {
-    
-	return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
-}
-
-
-
-Plane::Plane(const glm::vec3 &v1, const glm::vec3 &v2, const glm::vec3 &v3) {
-
-	set3Points(v1,v2,v3);
+// cross product
+glm::vec3 crossProduct(const glm::vec3& lhs, const glm::vec3& rhs) {
+    glm::vec3 result;
+    result.x = lhs.y * rhs.z - lhs.z * rhs.y;
+    result.y = lhs.z * rhs.x - lhs.x * rhs.z;
+    result.z = lhs.x * rhs.y - lhs.y * rhs.x;
+    return result;
 }
 
 
-Plane::Plane() {}
+float innerProduct(const glm::vec3& v1,const glm::vec3& v2) {
+    return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
+}
 
-Plane::~Plane() {}
-
+void printVec3(const char* name, const glm::vec3& v) {
+    printf("%s x=%f y=%f z=%f\n", name, v.x, v.y, v.z);
+}
 
 void Plane::set3Points(const glm::vec3 &v1, const glm::vec3 &v2, const glm::vec3 &v3) {
+    glm::vec3 linev1v2, linev1v3;
 
+    linev1v2 = v2 - v1;
+    linev1v3 = v3 - v1;
 
-	glm::vec3 aux1, aux2;
+    // this will be perpendicular to both lines
+    _normal = crossProduct(linev1v2,linev1v3);
+    normalize(_normal);
 
-	aux1 = v1 - v2;
-	aux2 = v3 - v2;
+    // this is a point on the plane
+    _point = v2;
 
-	normal = aux2 * aux1;
-
-	vec3_normalize(normal);
-	point = v2;
-	d = -(vec3_innerProduct(normal,point));
+    // the D coefficient from the form Ax+By+Cz=D
+    _dCoefficient = -(innerProduct(_normal,_point));
 }
 
 void Plane::setNormalAndPoint(const glm::vec3 &normal, const glm::vec3 &point) {
+    _point = point;
+    _normal = normal;
+    normalize(_normal);
 
-	this->normal = normal;
-	vec3_normalize(this->normal);
-	d = -(vec3_innerProduct(this->normal,point));
+    // the D coefficient from the form Ax+By+Cz=D
+    _dCoefficient = -(innerProduct(_normal,_point));
 }
 
 void Plane::setCoefficients(float a, float b, float c, float d) {
+    // set the normal vector
+    _normal = glm::vec3(a,b,c);
 
-	// set the normal vector
-	normal = glm::vec3(a,b,c);
-	//compute the lenght of the vector
-	float l = normal.length();
-	// normalize the vector
-	normal = glm::vec3(a/l,b/l,c/l);
-	// and divide d by th length as well
-	this->d = d/l;
+    //compute the lenght of the vector
+    float l = length(_normal);
+
+    // normalize the vector
+    _normal = glm::vec3(a/l,b/l,c/l);
+
+    // and divide d by th length as well
+    _dCoefficient = d/l;
 }
 
-float Plane::distance(const glm::vec3 &p) {
-    return (d + vec3_innerProduct(normal,p));
+float Plane::distance(const glm::vec3 &point) const {
+    return (_dCoefficient + innerProduct(_normal,point));
 }
 
-void Plane::print() {
-	//printLog("Plane(");normal.print();printLog("# %f)",d);
+void Plane::print() const {
+    printf("Plane - point (x=%f y=%f z=%f) normal (x=%f y=%f z=%f) d=%f\n",
+        _point.x, _point.y, _point.z, _normal.x, _normal.y, _normal.z, _dCoefficient);
 }
