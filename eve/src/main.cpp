38f8769ff648a09650a280d0cc48cdfbe76a06cf
Stephen Birarda <commit@birarda.com>
2013-04-22 11:39:38 -0700

    stub setup for eve

---
+++ eve/src/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  eve
+//
+//  Created by Stephen Birarda on 4/22/13.
+//  Copyright (c) 2013 High Fidelity, Inc. All rights reserved.
+//
+
+int main(int argc, char* argv[]) {
+
+}
+
+
