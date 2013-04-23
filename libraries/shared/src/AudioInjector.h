ed5afb353795156623c600c504b6a4caaa0c69d1
Stephen Birarda <commit@birarda.com>
2013-04-23 13:57:41 -0700

    add const to injectAudio method

--- libraries/shared/src/AudioInjector.h
+++ libraries/shared/src/AudioInjector.h
@@ -23,7 +23,7 @@ public:
     void setBearing(float bearing) { _bearing = bearing; }
     void setAttenuationModifier(unsigned char attenuationModifier) { _attenuationModifier = attenuationModifier; }
     
-    void injectAudio(UDPSocket* injectorSocket, sockaddr* destinationSocket);
+    const void injectAudio(UDPSocket* injectorSocket, sockaddr* destinationSocket);
 private:
     int16_t* _audioSampleArray;
     int _numTotalBytesAudio;
