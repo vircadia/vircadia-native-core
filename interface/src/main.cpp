bc6512600479c346f3417757d816cdce6793e6b7
Philip Rosedale <philip@coffeeandpower.com>
2013-04-24 16:58:52 -0700

    Wider log window, log panel on by default, menu->tools->log to control.

--- interface/src/main.cpp
+++ interface/src/main.cpp
@@ -157,6 +157,8 @@ VoxelDetail paintingVoxel;          //	The voxel we're painting if we're paintin
 unsigned char dominantColor = 0;    //	The dominant color of the voxel we're painting
 bool perfStatsOn = false;			//  Do we want to display perfStats?
 
+bool logOn = true;                  //  Whether to show on-screen log
+
 int noiseOn = 0;					//  Whether to add random noise 
 float noise = 1.0;                  //  Overall magnitude scaling for random noise levels 
 
@@ -919,7 +921,7 @@ void display(void)
         if ( !::lookingInMirror ) balls.render();
     
         //  Render the world box
-        if (!::lookingInMirror && statsOn) render_world_box();
+        if (!::lookingInMirror && ::statsOn) { render_world_box(); }
         
         // brad's frustum for debugging
         if (::frustumOn) renderViewFrustum(::viewFrustum);
@@ -961,13 +963,12 @@ void display(void)
     //  Show detected levels from the serial I/O ADC channel sensors
     if (displayLevels) serialPort.renderLevels(WIDTH,HEIGHT);
     
-    //  Display miscellaneous text stats onscreen
-    if (statsOn) {
-        glLineWidth(1.0f);
-        glPointSize(1.0f);
-        displayStats();
-        logger.render(WIDTH, HEIGHT);
-    }
+    //  Display stats and log text onscreen
+    glLineWidth(1.0f);
+    glPointSize(1.0f);
+    
+    if (::statsOn) { displayStats(); }
+    if (::logOn) { logger.render(WIDTH, HEIGHT); }
         
     //  Show menu
     if (::menuOn) {
@@ -1056,6 +1057,11 @@ int setNoise(int state) {
     return iRet;
 }
 
+int setLog(int state) {
+    int iRet = setValue(state, &::logOn);
+    return iRet;
+}
+
 int setGyroLook(int state) {
     int iRet = setValue(state, &::gyroLook);
     return iRet;
@@ -1070,7 +1076,7 @@ int setStars(int state) {
 }
 
 int setStats(int state) {
-    return setValue(state, &statsOn);
+    return setValue(state, &::statsOn);
 }
 
 int setMenu(int state) {
@@ -1195,7 +1201,8 @@ void initMenu() {
     
     //  Tools
     menuColumnTools = menu.addColumn("Tools");
-    menuColumnTools->addRow("Stats (/)", setStats); 
+    menuColumnTools->addRow("Stats (/)", setStats);
+    menuColumnTools->addRow("Log ", setLog);
     menuColumnTools->addRow("(M)enu", setMenu);
 
     // Frustum Options
@@ -1353,7 +1360,7 @@ void key(unsigned char k, int x, int y)
     
 	//  Process keypresses 
  	if (k == 'q' || k == 'Q')  ::terminate();
-    if (k == '/')  statsOn = !statsOn;		// toggle stats
+    if (k == '/')  ::statsOn = !::statsOn;		// toggle stats
     if (k == '*')  ::starsOn = !::starsOn;		// toggle stars
     if (k == 'V' || k == 'v')  ::showingVoxels = !::showingVoxels;		// toggle voxels
     if (k == 'F')  ::frustumOn = !::frustumOn;		// toggle view frustum debugging
