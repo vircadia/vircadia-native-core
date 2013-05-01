87f48b5bc4d275027c2f8a62b392dd1dc25a4735
Stephen Birarda <commit@birarda.com>
2013-05-01 15:40:31 -0700

    add a PariableDevice struct that will hold device info in vector

--- pairing-server/src/main.cpp
+++ pairing-server/src/main.cpp
@@ -6,8 +6,34 @@
 //  Copyright (c) 2013 High Fidelity, Inc. All rights reserved.
 //
 
+#include <iostream>
+#include <vector>
+
+#include <UDPSocket.h>
+
+const int PAIRING_SERVER_LISTEN_PORT = 52934;
+const int MAX_PACKET_SIZE_BYTES = 1400;
+
+struct PairableDevice {
+    char identifier[64];
+    char name[64];
+    sockaddr localSocket;
+};
+
 int main(int argc, const char* argv[]) {
+    UDPSocket serverSocket(PAIRING_SERVER_LISTEN_PORT);
     
+    sockaddr deviceAddress;
+    char deviceData[MAX_PACKET_SIZE_BYTES] = {};
+    ssize_t receivedBytes = 0;
+    
+    std::vector<PairableDevice> devices;
+    
+    while (true) {
+        if (serverSocket.receive(&deviceAddress, &deviceData, &receivedBytes)) {
+            
+        }
+    }
 }
 
 
