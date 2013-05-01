01816ccda3f31c78144ea00095cd2e02f50eb660
Stephen Birarda <commit@birarda.com>
2013-05-01 14:23:25 -0700

    add a base config for a pairing server

---
+++ pairing-server/src/main.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  pairing-server
+//
+//  Created by Stephen Birarda on 5/1/13.
+//  Copyright (c) 2013 High Fidelity, Inc. All rights reserved.
+//
+
+int main(int argc, const char* argv[]) {
+    
+}
+
+
