bd6f0d9885054862c62b1a9497853a1ee495fa5b
Stephen Birarda <commit@birarda.com>
2013-05-03 10:58:57 -0700

    couple of line deletions

--- interface/src/main.cpp
+++ interface/src/main.cpp
@@ -1593,8 +1593,6 @@ void mouseoverFunc( int x, int y)
     {}
 }
 
-
-
 void attachNewHeadToAgent(Agent *newAgent) {
     if (newAgent->getLinkedData() == NULL) {
         newAgent->setLinkedData(new Avatar(false));
