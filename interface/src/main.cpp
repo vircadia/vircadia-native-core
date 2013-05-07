92daa8e6e05f31f9c3413ba7f9bb8c20ccd6c8f0
Stephen Birarda <commit@birarda.com>
2013-05-07 13:43:07 -0700

    revert the type squish

--- interface/src/main.cpp
+++ interface/src/main.cpp
@@ -701,7 +701,7 @@ void displaySide(Camera& whichCamera) {
     }
 	
     //  Render avatars of other agents
-    AgentList *agentList = AgentList::getInstance();
+    AgentList* agentList = AgentList::getInstance();
     agentList->lock();
     for (AgentList::iterator agent = agentList->begin(); agent != agentList->end(); agent++) {
         if (agent->getLinkedData() != NULL && agent->getType() == AGENT_TYPE_AVATAR) {
