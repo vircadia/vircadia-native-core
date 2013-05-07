fe11d929e398ffe282612fa749a02bc158aabee5
Jeffrey Ventrella <jeffreyventrella@gmail.com>
2013-05-07 08:43:14 -0700

    cleaned up code spacing

--- interface/src/AvatarTouch.cpp
+++ interface/src/AvatarTouch.cpp
@@ -16,10 +16,10 @@ const float THREAD_RADIUS = 0.012;
 
 AvatarTouch::AvatarTouch() {
 
-    _myHandPosition   = glm::vec3( 0.0f, 0.0f, 0.0f );
-    _yourHandPosition = glm::vec3( 0.0f, 0.0f, 0.0f );
-    _myBodyPosition   = glm::vec3( 0.0f, 0.0f, 0.0f );
-    _yourBodyPosition = glm::vec3( 0.0f, 0.0f, 0.0f );
+    _myHandPosition   = glm::vec3(0.0f, 0.0f, 0.0f);
+    _yourHandPosition = glm::vec3(0.0f, 0.0f, 0.0f);
+    _myBodyPosition   = glm::vec3(0.0f, 0.0f, 0.0f);
+    _yourBodyPosition = glm::vec3(0.0f, 0.0f, 0.0f);
     _myHandState      = 0;
     _yourHandState    = 0;  
     _reachableRadius  = 0.0f;  
@@ -28,7 +28,7 @@ AvatarTouch::AvatarTouch() {
     _handsCloseEnoughToGrasp = false;
 
     for (int p=0; p<NUM_POINTS; p++) {
-        _point[p] = glm::vec3( 0.0, 0.0, 0.0 );
+        _point[p] = glm::vec3(0.0, 0.0, 0.0);
     }
 }
 
@@ -65,7 +65,7 @@ void AvatarTouch::render(glm::vec3 cameraPosition) {
 
     if (_canReachToOtherAvatar) {
     
-        glColor4f( 0.3, 0.4, 0.5, 0.5 ); 
+        glColor4f(0.3, 0.4, 0.5, 0.5); 
         glm::vec3 p(_yourBodyPosition);
         p.y = 0.0005f;
         renderCircle(p, _reachableRadius, glm::vec3(0.0f, 1.0f, 0.0f), 30);
@@ -74,9 +74,9 @@ void AvatarTouch::render(glm::vec3 cameraPosition) {
         if (_yourHandState == 1) {
             glPushMatrix();
             glTranslatef(_yourHandPosition.x, _yourHandPosition.y, _yourHandPosition.z);
-            glColor4f( 1.0, 1.0, 0.8, 0.3 ); glutSolidSphere( 0.020f, 10.0f, 10.0f );
-            glColor4f( 1.0, 1.0, 0.4, 0.2 ); glutSolidSphere( 0.025f, 10.0f, 10.0f );
-            glColor4f( 1.0, 1.0, 0.2, 0.1 ); glutSolidSphere( 0.030f, 10.0f, 10.0f );
+            glColor4f(1.0, 1.0, 0.8, 0.3); glutSolidSphere(0.020f, 10.0f, 10.0f);
+            glColor4f(1.0, 1.0, 0.4, 0.2); glutSolidSphere(0.025f, 10.0f, 10.0f);
+            glColor4f(1.0, 1.0, 0.2, 0.1); glutSolidSphere(0.030f, 10.0f, 10.0f);
             glPopMatrix();
         }
         
@@ -86,13 +86,13 @@ void AvatarTouch::render(glm::vec3 cameraPosition) {
 
         if (_handsCloseEnoughToGrasp) {
             glLineWidth(2.0);
-            glColor4f( 0.7f, 0.4f, 0.1f, 0.3 );
+            glColor4f(0.7f, 0.4f, 0.1f, 0.3);
             glBegin(GL_LINE_STRIP);
-            glVertex3f( v1.x, v1.y, v1.z );
-            glVertex3f( v2.x, v2.y, v2.z );
+            glVertex3f(v1.x, v1.y, v1.z);
+            glVertex3f(v2.x, v2.y, v2.z);
             glEnd();
 
-            glColor4f( 1.0f, 1.0f, 0.0f, 0.8 );
+            glColor4f(1.0f, 1.0f, 0.0f, 0.8);
 
             for (int p=0; p<NUM_POINTS; p++) {
                 glBegin(GL_POINTS);
@@ -106,9 +106,9 @@ void AvatarTouch::render(glm::vec3 cameraPosition) {
     if (_myHandState == 1) {
         glPushMatrix();
         glTranslatef(_myHandPosition.x, _myHandPosition.y, _myHandPosition.z);
-        glColor4f( 1.0, 1.0, 0.8, 0.3 ); glutSolidSphere( 0.020f, 10.0f, 10.0f );
-        glColor4f( 1.0, 1.0, 0.4, 0.2 ); glutSolidSphere( 0.025f, 10.0f, 10.0f );
-        glColor4f( 1.0, 1.0, 0.2, 0.1 ); glutSolidSphere( 0.030f, 10.0f, 10.0f );
+        glColor4f(1.0, 1.0, 0.8, 0.3); glutSolidSphere(0.020f, 10.0f, 10.0f);
+        glColor4f(1.0, 1.0, 0.4, 0.2); glutSolidSphere(0.025f, 10.0f, 10.0f);
+        glColor4f(1.0, 1.0, 0.2, 0.1); glutSolidSphere(0.030f, 10.0f, 10.0f);
         glPopMatrix();
     }
 }
@@ -120,7 +120,7 @@ void AvatarTouch::simulate (float deltaTime) {
 
     float distance = glm::length(v);
 
-    if (distance < _reachableRadius ) {
+    if (distance < _reachableRadius) {
         _canReachToOtherAvatar = true;
     } else {
         _canReachToOtherAvatar = false;
@@ -129,10 +129,10 @@ void AvatarTouch::simulate (float deltaTime) {
 /*
 
     for (int p=0; p<NUM_POINTS; p++) {
-        _point[p] = _myHandPosition + v * ( (float)p / (float)NUM_POINTS );
-        _point[p].x += randFloatInRange( -THREAD_RADIUS, THREAD_RADIUS );
-        _point[p].y += randFloatInRange( -THREAD_RADIUS, THREAD_RADIUS );
-        _point[p].z += randFloatInRange( -THREAD_RADIUS, THREAD_RADIUS );
+        _point[p] = _myHandPosition + v * ((float)p / (float)NUM_POINTS);
+        _point[p].x += randFloatInRange(-THREAD_RADIUS, THREAD_RADIUS);
+        _point[p].y += randFloatInRange(-THREAD_RADIUS, THREAD_RADIUS);
+        _point[p].z += randFloatInRange(-THREAD_RADIUS, THREAD_RADIUS);
     }
     */
     
