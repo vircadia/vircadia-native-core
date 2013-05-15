63a2b203d913fc4cbc086d30172ffa695e339b41
Stephen Birarda <commit@birarda.com>
2013-05-15 12:36:19 -0700

    constantize VOLUME_BASELINE in addProceduralSounds

--- interface/src/Audio.cpp
+++ interface/src/Audio.cpp
@@ -315,9 +315,10 @@ Audio::~Audio() {
 void Audio::addProceduralSounds(int16_t* inputBuffer, int numSamples) {
     const float MAX_AUDIBLE_VELOCITY = 6.0;
     const float MIN_AUDIBLE_VELOCITY = 0.1;
+    const int VOLUME_BASELINE = 400;
     
     float speed = glm::length(_lastVelocity);
-    float volume = 400 * (1.f - speed / MAX_AUDIBLE_VELOCITY);
+    float volume = VOLUME_BASELINE * (1.f - speed / MAX_AUDIBLE_VELOCITY);
     
     //  Add a noise-modulated sinewave with volume that tapers off with speed increasing
     if ((speed > MIN_AUDIBLE_VELOCITY) && (speed < MAX_AUDIBLE_VELOCITY)) {
