13c10c95e891a12a53e36e67508f7c99059f9ed7
Stephen Birarda <commit@birarda.com>
2013-05-15 16:17:38 -0700

    type squishes in UDPSocket header

--- libraries/shared/src/UDPSocket.h
+++ libraries/shared/src/UDPSocket.h
@@ -25,20 +25,20 @@ public:
     bool init();
     void setBlocking(bool blocking);
     bool isBlocking() { return blocking; }
-    int send(sockaddr *destAddress, const void *data, size_t byteLength) const;
-    int send(char *destAddress, int destPort, const void *data, size_t byteLength) const;
-    bool receive(void *receivedData, ssize_t *receivedBytes) const;
-    bool receive(sockaddr *recvAddress, void *receivedData, ssize_t *receivedBytes) const;
+    int send(sockaddr* destAddress, const void* data, size_t byteLength) const;
+    int send(char* destAddress, int destPort, const void* data, size_t byteLength) const;
+    bool receive(void* receivedData, ssize_t* receivedBytes) const;
+    bool receive(sockaddr* recvAddress, void* receivedData, ssize_t* receivedBytes) const;
 private:
     int handle;
     bool blocking;
 };
 
-bool socketMatch(sockaddr *first, sockaddr *second);
-int packSocket(unsigned char *packStore, in_addr_t inAddress, in_port_t networkOrderPort);
-int packSocket(unsigned char *packStore, sockaddr *socketToPack);
-int unpackSocket(unsigned char *packedData, sockaddr *unpackDestSocket);
+bool socketMatch(sockaddr* first, sockaddr* second);
+int packSocket(unsigned char* packStore, in_addr_t inAddress, in_port_t networkOrderPort);
+int packSocket(unsigned char* packStore, sockaddr* socketToPack);
+int unpackSocket(unsigned char* packedData, sockaddr* unpackDestSocket);
 int getLocalAddress();
-unsigned short loadBufferWithSocketInfo(char *addressBuffer, sockaddr *socket);
+unsigned short loadBufferWithSocketInfo(char* addressBuffer, sockaddr* socket);
 
 #endif /* defined(__interface__UDPSocket__) */
