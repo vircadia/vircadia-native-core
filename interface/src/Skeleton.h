5715860f6b580d3058200590830839f8ab9be829
Jeffrey Ventrella <jeffreyventrella@gmail.com>
2013-05-27 18:55:23 -0700

    added Skeleton class (to be completed soon)

---
+++ interface/src/Skeleton.h
@@ -0,0 +1,54 @@
+//
+//  Skeleton.h
+//  interface
+//
+//  Copyright (c) 2013 High Fidelity, Inc. All rights reserved.
+//
+
+#ifndef hifi_Skeleton_h
+#define hifi_Skeleton_h
+
+enum AvatarJointID
+{
+	AVATAR_JOINT_NULL = -1,
+	AVATAR_JOINT_PELVIS,	
+	AVATAR_JOINT_TORSO,	
+	AVATAR_JOINT_CHEST,	
+	AVATAR_JOINT_NECK_BASE,	
+	AVATAR_JOINT_HEAD_BASE,	
+	AVATAR_JOINT_HEAD_TOP,	
+	AVATAR_JOINT_LEFT_COLLAR,
+	AVATAR_JOINT_LEFT_SHOULDER,
+	AVATAR_JOINT_LEFT_ELBOW,
+	AVATAR_JOINT_LEFT_WRIST,
+	AVATAR_JOINT_LEFT_FINGERTIPS,
+	AVATAR_JOINT_RIGHT_COLLAR,
+	AVATAR_JOINT_RIGHT_SHOULDER,
+	AVATAR_JOINT_RIGHT_ELBOW,
+	AVATAR_JOINT_RIGHT_WRIST,
+	AVATAR_JOINT_RIGHT_FINGERTIPS,
+	AVATAR_JOINT_LEFT_HIP,
+	AVATAR_JOINT_LEFT_KNEE,
+	AVATAR_JOINT_LEFT_HEEL,		
+	AVATAR_JOINT_LEFT_TOES,		
+	AVATAR_JOINT_RIGHT_HIP,	
+	AVATAR_JOINT_RIGHT_KNEE,	
+	AVATAR_JOINT_RIGHT_HEEL,	
+	AVATAR_JOINT_RIGHT_TOES,	
+    
+	NUM_AVATAR_JOINTS
+};
+
+
+class Skeleton {
+public:
+    Skeleton();
+
+    void initialize();
+    void simulate(float deltaTime);
+    void render();
+    
+private:
+};
+
+#endif
