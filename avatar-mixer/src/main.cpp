bb3feb13eb03512604556f81b3768e96adbdf1e1
Stephen Birarda <commit@birarda.com>
2013-05-29 16:57:13 -0700

    correct the currentBufferPosition for injector packets as well

--- avatar-mixer/src/main.cpp
+++ avatar-mixer/src/main.cpp
@@ -84,9 +84,9 @@ int main(int argc, const char* argv[]) {
                     
                     // parse positional data from an agent
                     agentList->updateAgentWithData(avatarAgent, packetData, receivedBytes);
-                
-                    currentBufferPosition = broadcastPacket + 1;
                 case PACKET_HEADER_INJECT_AUDIO:
+                    currentBufferPosition = broadcastPacket + 1;
+                    
                     // send back a packet with other active agent data to this agent
                     for (AgentList::iterator agent = agentList->begin(); agent != agentList->end(); agent++) {
                         if (agent->getLinkedData() && !socketMatch(agentAddress, agent->getActiveSocket())) {
