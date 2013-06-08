a175ad8fdade0d135bba979e75d9d7fbca28b8a5
ZappoMan <bradh@konamoxt.com>
2013-06-07 17:25:05 -0700

    fix spacing

--- libraries/voxels/src/AABox.h
+++ libraries/voxels/src/AABox.h
@@ -24,29 +24,29 @@ enum BoxFace {
 
 const int FACE_COUNT = 6;
 
-class AABox 
+    class AABox 
 {
 
 public:
 
     AABox(const glm::vec3& corner, float size) : _corner(corner), _size(size, size, size) { };
-	AABox(const glm::vec3& corner, float x, float y, float z) : _corner(corner), _size(x, y, z) { };
-	AABox(const glm::vec3& corner, const glm::vec3& size) : _corner(corner), _size(size) { };
+    AABox(const glm::vec3& corner, float x, float y, float z) : _corner(corner), _size(x, y, z) { };
+    AABox(const glm::vec3& corner, const glm::vec3& size) : _corner(corner), _size(size) { };
     AABox() : _corner(0,0,0), _size(0,0,0) { }
     ~AABox() { }
 
-	void setBox(const glm::vec3& corner, float x, float y, float z) { setBox(corner,glm::vec3(x,y,z)); };
-	void setBox(const glm::vec3& corner, const glm::vec3& size);
+    void setBox(const glm::vec3& corner, float x, float y, float z) { setBox(corner,glm::vec3(x,y,z)); };
+    void setBox(const glm::vec3& corner, const glm::vec3& size);
 
-	// for use in frustum computations
-	glm::vec3 getVertexP(const glm::vec3& normal) const;
-	glm::vec3 getVertexN(const glm::vec3& normal) const;
-	
-	void scale(float scale);
-	
-	const glm::vec3& getCorner() const { return _corner; };
-	const glm::vec3& getSize() const { return _size; };
-	const glm::vec3& getCenter() const { return _center; };
+    // for use in frustum computations
+    glm::vec3 getVertexP(const glm::vec3& normal) const;
+    glm::vec3 getVertexN(const glm::vec3& normal) const;
+
+    void scale(float scale);
+
+    const glm::vec3& getCorner() const { return _corner; };
+    const glm::vec3& getSize() const { return _size; };
+    const glm::vec3& getCenter() const { return _center; };
 
     bool contains(const glm::vec3& point) const;
     bool expandedContains(const glm::vec3& point, float expansion) const;
@@ -56,16 +56,16 @@ public:
     bool findCapsulePenetration(const glm::vec3& start, const glm::vec3& end, float radius, glm::vec3& penetration) const;
 
 private:
-    
+
     glm::vec3 getClosestPointOnFace(const glm::vec3& point, BoxFace face) const;
     glm::vec3 getClosestPointOnFace(const glm::vec4& origin, const glm::vec4& direction, BoxFace face) const;
     glm::vec4 getPlane(BoxFace face) const;
-    
+
     static BoxFace getOppositeFace(BoxFace face);
-    
-	glm::vec3 _corner;
-	glm::vec3 _center;
-	glm::vec3 _size;
+
+    glm::vec3 _corner;
+    glm::vec3 _center;
+    glm::vec3 _size;
 };
 
 
