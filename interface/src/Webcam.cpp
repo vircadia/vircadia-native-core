0fa710e5183db8c7f8526a1afb5527d79cb45372
Andrzej Kapolka <drzej.k@gmail.com>
2013-06-18 13:39:54 -0700

    Let's try using glPixelStore to specify the widthStep.

--- interface/src/Webcam.cpp
+++ interface/src/Webcam.cpp
@@ -63,33 +63,19 @@ Webcam::~Webcam() {
 
 void Webcam::setFrame(void* image) {
     IplImage* img = static_cast<IplImage*>(image);
-    GLint internalFormat;
-    GLenum format;
-    int bytesPerPixel = img->widthStep / img->width;
-    if (bytesPerPixel == 3) {
-        internalFormat = GL_RGB;
-        format = GL_BGR;
-        
-    } else {
-        internalFormat = GL_RGBA;
-        format = GL_BGRA;
-    
-        // set the alpha values
-        for (quint32* pixel = (quint32*)img->imageData, *end = pixel + img->imageSize / 4; pixel != end; pixel++) {
-            *pixel |= 0xFF000000;
-        }
-    }
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, img->widthStep / 3);
     if (_frameTextureID == 0) {
         glGenTextures(1, &_frameTextureID);
         glBindTexture(GL_TEXTURE_2D, _frameTextureID);
-        glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, _frameWidth = img->width, _frameHeight = img->height, 0, format,
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, _frameWidth = img->width, _frameHeight = img->height, 0, GL_BGR,
             GL_UNSIGNED_BYTE, img->imageData);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     
     } else {
         glBindTexture(GL_TEXTURE_2D, _frameTextureID);
-        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, _frameWidth, _frameHeight, format, GL_UNSIGNED_BYTE, img->imageData);    
+        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, _frameWidth, _frameHeight, GL_BGR, GL_UNSIGNED_BYTE, img->imageData);    
     }
+    glPixelStorei(GL_UNPACK_ROW_LENGTH, 0);
     glBindTexture(GL_TEXTURE_2D, 0);
     
     // let the grabber know we're ready for the next frame
@@ -116,9 +102,8 @@ void FrameGrabber::grabFrame() {
         return;
     }
     // make sure it's in the format we expect
-    int bytesPerPixel = image->widthStep / image->width;
-    if ((bytesPerPixel != 3 && bytesPerPixel != 4) || image->depth != IPL_DEPTH_8U ||
-            image->dataOrder != IPL_DATA_ORDER_PIXEL || image->origin != 0) {
+    if (image->nChannels != 3 || image->depth != IPL_DEPTH_8U || image->dataOrder != IPL_DATA_ORDER_PIXEL ||
+            image->origin != 0) {
         printLog("Invalid webcam image format.\n");
         return;
     }
