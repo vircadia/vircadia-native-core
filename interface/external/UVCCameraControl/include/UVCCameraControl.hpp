344b8a31c8afce27a8c386a3265a65a7983f6a57
Andrzej Kapolka <drzej.k@gmail.com>
2013-06-19 10:20:01 -0700

    Updated header.

--- interface/external/UVCCameraControl/include/UVCCameraControl.hpp
+++ interface/external/UVCCameraControl/include/UVCCameraControl.hpp
@@ -2,8 +2,8 @@
 //  UVCCameraControl.hpp
 //  UVCCameraControl
 //
-//  Created by Philip Rosedale on 6/18/13.
-//  Copyright (c) 2013 Phobos Lab. All rights reserved.
+//  Created by Andrzej Kapolka on 6/18/13.
+//  Copyright (c) 2013 High Fidelity, Inc. All rights reserved.
 //
 
 #ifndef UVCCameraControl_UVCCameraControl_hpp
