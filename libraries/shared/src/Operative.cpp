1c142ecc303e0f8fc771948afdb6446248d1e78d
Stephen Birarda <commit@birarda.com>
2013-07-01 15:57:35 -0700

    add a stubbed Operative class

---
+++ libraries/shared/src/Operative.cpp
@@ -0,0 +1,15 @@
+//
+//  Operative.cpp
+//  hifi
+//
+//  Created by Stephen Birarda on 7/1/13.
+//  Copyright (c) 2013 HighFidelity, Inc. All rights reserved.
+//
+
+#include "AgentList.h"
+
+#include "Operative.h"
+
+void Operative::run() {
+    
+}
---
+++ libraries/shared/src/Operative.h
@@ -0,0 +1,19 @@
+//
+//  Operative.h
+//  hifi
+//
+//  Created by Stephen Birarda on 7/1/13.
+//  Copyright (c) 2013 HighFidelity, Inc. All rights reserved.
+//
+
+#ifndef __hifi__Operative__
+#define __hifi__Operative__
+
+#include <iostream>
+
+class Operative {
+public:
+    void run();
+};
+
+#endif /* defined(__hifi__Operative__) */
