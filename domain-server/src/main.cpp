2fde29e04a1a6e7c033ba0b73065589265e68466
Stephen Birarda <commit@birarda.com>
2013-08-01 11:53:18 -0700

    use a 0 public address to designate server local to DS

--- domain-server/src/main.cpp
+++ domain-server/src/main.cpp
@@ -104,9 +104,10 @@ int main(int argc, const char * argv[])
             // so hardcode the EC2 public address for now
             if (nodePublicAddress.sin_addr.s_addr == serverLocalAddress) {
             	// If we're not running "local" then we do replace the IP
-            	// with the EC2 IP. Otherwise, we use our normal public IP
+            	// with 0. This designates to clients that the server is reachable
+                // at the same IP address 
             	if (!isLocalMode) {
-	                nodePublicAddress.sin_addr.s_addr = 895283510; // local IP in this format...
+	                nodePublicAddress.sin_addr.s_addr = 0;
                     destinationSocket = (sockaddr*) &nodeLocalAddress;
 	            }
             }
