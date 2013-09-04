54116cbc85a712d506723d78509acbbc6b5baa1a
Andrzej Kapolka <drzej.k@gmail.com>
2013-09-04 16:04:36 -0700

    Testing audio shutdown before menu destruction.

--- interface/src/Menu.cpp
+++ interface/src/Menu.cpp
@@ -416,6 +416,8 @@ Menu::Menu() :
 Menu::~Menu() {
     bandwidthDetailsClosed();
     voxelStatsDetailsClosed();
+    
+    qDebug() << "menu deleted\n";
 }
 
 void Menu::loadSettings(QSettings* settings) {
