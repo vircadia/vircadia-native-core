64e3c19a8c789db748a0519e67ae8440d73d29c2
Stephen Birarda <commit@birarda.com>
2013-09-09 10:07:31 -0700

    fix an incorrectly typed const

--- domain-server/src/main.cpp
+++ domain-server/src/main.cpp
@@ -85,8 +85,8 @@ int main(int argc, const char* argv[]) {
     
     timeval lastStatSendTime = {};
     
-    const char ASSIGNMENT_POOL_OPTION = "-p";
-    const char ASSIGNMENT_SERVER_OPTION = "-a";
+    const char ASSIGNMENT_POOL_OPTION[] = "-p";
+    const char ASSIGNMENT_SERVER_OPTION[]g = "-a";
     
     // set our assignment pool from argv, if it exists
     const char* assignmentPool = getCmdOption(argc, argv, ASSIGNMENT_POOL_OPTION);
