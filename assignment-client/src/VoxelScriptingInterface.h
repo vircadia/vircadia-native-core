75c59c486393901c10cd199a5862e7c5c2a58122
Stephen Birarda <commit@birarda.com>
2013-09-17 10:54:44 -0700

    add doxygen comments to VoxelScriptingInterface

--- assignment-client/src/VoxelScriptingInterface.h
+++ assignment-client/src/VoxelScriptingInterface.h
@@ -13,13 +13,23 @@
 
 #include <VoxelEditPacketSender.h>
 
+/// handles scripting of voxel commands from JS passed to assigned clients
 class VoxelScriptingInterface : public QObject {
     Q_OBJECT
 public:
     VoxelEditPacketSender* getVoxelPacketSender() { return &_voxelPacketSender; }
 public slots:
+    /// queues the creation of a voxel which will be sent by calling process on the PacketSender
+    /// \param x the x-coordinate of the voxel (in VS space)
+    /// \param y the y-coordinate of the voxel (in VS space)
+    /// \param z the z-coordinate of the voxel (in VS space)
+    /// \param scale the scale of the voxel (in VS space)
+    /// \param red the R value for RGB color of voxel
+    /// \param green the G value for RGB color of voxel
+    /// \param blue the B value for RGB color of voxel
     void queueVoxelAdd(float x, float y, float z, float scale, uchar red, uchar green, uchar blue);
 private:
+    /// attached VoxelEditPacketSender that handles queuing and sending of packets to VS
     VoxelEditPacketSender _voxelPacketSender;
 };
 
