d85ed014ba9ff723ee3eeb710b30c185e6c42289
Stephen Birarda <commit@birarda.com>
2013-10-23 14:43:59 -0700

    switch to bootstrap 3, complete initial index page

--- domain-server/resources/web/js/tables.js
+++ domain-server/resources/web/js/tables.js
@@ -1,29 +1,31 @@
 $(document).ready(function(){
   // setup a function to grab the assignments
-  function getAssignments() {
-    $.getJSON("assignments.json", function (json) {
-      assignedTableBody = "";
+  function getNodesAndAssignments() {
+    $.getJSON("nodes.json", function(json){
+      nodesTableBody = "";
       
-      $.each(json.fulfilled, function (type, data) {
-        assignedTableBody += "<tr>";
-        assignedTableBody += "<td>" + type + "</td>";
-        assignedTableBody += "<td>" + data.UUID + "</td>";
-        assignedTableBody += "<td>" + (data.pool ? data.pool : "N/A") + "</td>";
-        assignedTableBody += "<td>" + "</td>";
-        assignedTableBody += "<td>" + "</td>";
-        assignedTableBody += "<td>" + "</td>";
-        assignedTableBody += "</tr>";
+      $.each(json.nodes, function (type, data) {
+        nodesTableBody += "<tr>";
+        nodesTableBody += "<td>" + type + "</td>";
+        nodesTableBody += "<td>" + data.UUID + "</td>";
+        nodesTableBody += "<td>" + (data.pool ? data.pool : "") + "</td>";
+        nodesTableBody += "<td>" + data.public.ip + ":" + "<span class='port'>" + data.public.port + "</span></td>";
+        nodesTableBody += "<td>" + data.local.ip + ":" + "<span class='port'>" + data.local.port + "</span></td>";
+        nodesTableBody += "<td><span class='glyphicon glyphicon-remove' data-uuid=" + data.UUID + "></span></td>";
+        nodesTableBody += "</tr>";
       });
       
-      $('#nodes-table tbody').html(assignedTableBody);
-      
+      $('#nodes-table tbody').html(nodesTableBody);
+    });
+    
+    $.getJSON("assignments.json", function(json){      
       queuedTableBody = "";
         
       $.each(json.queued, function (type, data) {
         queuedTableBody += "<tr>";
         queuedTableBody += "<td>" + type + "</td>";
         queuedTableBody += "<td>" + data.UUID + "</td>";
-        queuedTableBody += "<td>" + (data.pool ? data.pool : "N/A") + "</td>";
+        queuedTableBody += "<td>" + (data.pool ? data.pool : "") + "</td>";
         queuedTableBody += "</tr>";
       });
       
@@ -32,7 +34,19 @@ $(document).ready(function(){
   }
   
   // do the first GET on page load
-  getAssignments();
+  getNodesAndAssignments();
   // grab the new assignments JSON every two seconds
-  var getAssignmentsInterval = setInterval(getAssignments, 2000);
+  var getNodesAndAssignmentsInterval = setInterval(getNodesAndAssignments, 2000);
+  
+  // hook the node delete to the X button
+  $(document.body).on('click', '.glyphicon-remove', function(){
+    // fire off a delete for this node
+    $.ajax({
+        url: "/node/" + $(this).data('uuid'),
+        type: 'DELETE',
+        success: function(result) {
+          console.log("Succesful request to delete node.");
+        }
+    });
+  });
 });
