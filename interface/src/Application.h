e44b0af9b6f2f74c937bd79083dd943084306735
Don Park <donp@donp.org>
2013-10-25 11:36:50 -0700

    make updateWindowTitle public, call it after Menu loads data into the Profile

--- interface/src/Application.h
+++ interface/src/Application.h
@@ -109,6 +109,8 @@ public:
     void touchEndEvent(QTouchEvent* event);
     void touchUpdateEvent(QTouchEvent* event);
     
+    void updateWindowTitle();
+
     void wheelEvent(QWheelEvent* event);
     
     const glm::vec3 getMouseVoxelWorldCoordinates(const VoxelDetail _mouseVoxel);
@@ -206,7 +208,6 @@ private:
     
     void initDisplay();
     void init();
-    void updateWindowTitle();
     
     void update(float deltaTime);
     
