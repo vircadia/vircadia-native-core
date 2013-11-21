898135fbf257b17b4fe4fc22005a0fdf2325a190
Andrzej Kapolka <drzej.k@gmail.com>
2013-11-21 13:36:16 -0800

    Linear filtering for more smoothness.

--- interface/src/renderer/TextureCache.cpp
+++ interface/src/renderer/TextureCache.cpp
@@ -190,8 +190,8 @@ QOpenGLFramebufferObject* TextureCache::getShadowFramebufferObject() {
         glBindTexture(GL_TEXTURE_2D, _shadowDepthTextureID);
         glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_MAP_SIZE, SHADOW_MAP_SIZE,
             0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, 0);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
         const float DISTANT_BORDER[] = { 1.0f, 1.0f, 1.0f, 1.0f };
