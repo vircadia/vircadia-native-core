9d8f0b82df0a99856278bdfd77783ca92ddae330
ZappoMan <bradh@konamoxt.com>
2013-11-22 17:59:47 -0800

    make append() private

--- libraries/voxels/src/VoxelPacket.h
+++ libraries/voxels/src/VoxelPacket.h
@@ -29,12 +29,9 @@ public:
     void discardLevel();
     void endLevel();
     
-    bool append(const unsigned char* data, int length); /// appends raw bytes
-    bool append(unsigned char byte); /// append a single byte
-    bool setByte(int offset, unsigned char byte); /// sets a single raw byte from previously appended portion of the stream
-
-    //int getBytesInUse() const { return _bytesInUse; }
-    //int getBytesAvailable() const { return _bytesAvailable; }
+    /// sets a single raw byte from previously appended portion of the uncompressed stream, might fail if the new byte would
+    /// cause packet to be less compressed, or if offset was out of range.
+    bool setByte(int offset, unsigned char byte); 
 
     unsigned char* getCompressedData() { return &_buffer[0]; } /// get pointer to start of compressed stream
     int getCompressedSize() const { return _bytesInUse; } /// the size of the packet in compressed form
@@ -54,8 +51,13 @@ public:
     unsigned char* getStartOfBuffer() { return &_buffer[0]; } /// get pointer to the start of stream buffer
     unsigned char* getEndOfBuffer() { return &_buffer[_bytesInUse]; } /// get pointer to current end of stream buffer
     
-    
 private:
+    /// appends raw bytes, might fail if byte would cause packet to be too large
+    bool append(const unsigned char* data, int length);
+    
+    /// append a single byte, might fail if byte would cause packet to be too large
+    bool append(unsigned char byte);
+
     unsigned char _buffer[MAX_VOXEL_PACKET_SIZE];
     int _bytesInUse;
     int _bytesAvailable;
