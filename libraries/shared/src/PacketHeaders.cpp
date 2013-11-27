a3b69c1e758fbfc5fdd3f756fc209797adad94d3
ZappoMan <bradh@konamoxt.com>
2013-11-27 15:55:49 -0800

    bumb version number of PACKET_TYPE_VOXEL_QUERY

--- libraries/shared/src/PacketHeaders.cpp
+++ libraries/shared/src/PacketHeaders.cpp
@@ -37,7 +37,7 @@ PACKET_VERSION versionForPacketType(PACKET_TYPE type) {
             return 1;
         
         case PACKET_TYPE_VOXEL_QUERY:
-            return 1;
+            return 2;
 
         case PACKET_TYPE_SET_VOXEL:
         case PACKET_TYPE_SET_VOXEL_DESTRUCTIVE:
