b3e9d369e474e936b7c133894d7b3bcecf4af5db
ZappoMan <bradh@konamoxt.com>
2013-12-04 19:20:11 -0800

    removed all dead VoxelServer code, now mostly handled by OctreeServer

--- libraries/voxel-server-library/src/VoxelServer.h
+++ libraries/voxel-server-library/src/VoxelServer.h
@@ -40,11 +40,17 @@ public:
     virtual unsigned char getMyNodeType();
     virtual const char* getMyLoggingServerTargetName();
     virtual const char* getMyDefaultPersistFilename();
+    virtual bool hasSpecialPacketToSend();
+    virtual int sendSpecialPacket(Node* node);
+
+    // subclass may implement these method
+    virtual void beforeRun();
 
 private:
     bool _sendEnvironments;
     bool _sendMinimalEnvironment;
     EnvironmentData _environmentData[3];
+    unsigned char _tempOutputBuffer[MAX_PACKET_SIZE];
 };
 
 #endif // __voxel_server__VoxelServer__
