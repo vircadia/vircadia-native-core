db8661f1524548cefb1c9e867af2783c2c11f305
Stephen Birarda <commit@birarda.com>
2013-12-17 14:13:23 -0800

    add a stubbed AudioInjectionManager

---
+++ libraries/audio/src/AudioInjectionManager.cpp
@@ -0,0 +1,9 @@
+//
+//  AudioInjectionManager.cpp
+//  hifi
+//
+//  Created by Stephen Birarda on 12/17/2013.
+//  Copyright (c) 2013 HighFidelity, Inc. All rights reserved.
+//
+
+#include "AudioInjectionManager.h"
---
+++ libraries/audio/src/AudioInjectionManager.h
@@ -0,0 +1,18 @@
+//
+//  AudioInjectionManager.h
+//  hifi
+//
+//  Created by Stephen Birarda on 12/17/2013.
+//  Copyright (c) 2013 HighFidelity, Inc. All rights reserved.
+//
+
+#ifndef __hifi__AudioInjectionManager__
+#define __hifi__AudioInjectionManager__
+
+#include <QtCore/QObject>
+
+class AudioInjectionManager : public QObject {
+    
+};
+
+#endif /* defined(__hifi__AudioInjectionManager__) */
