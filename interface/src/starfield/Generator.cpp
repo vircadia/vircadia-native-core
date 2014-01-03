696afd78752c37d5d42d09c18dcd9d061dee7434
ZappoMan <bradh@konamoxt.com>
2014-01-02 16:18:49 -0800

    more warnings fixes

--- interface/src/starfield/Generator.cpp
+++ interface/src/starfield/Generator.cpp
@@ -62,7 +62,7 @@ void Generator::computeStarPositions(InputVertices& destination, unsigned limit,
         vertices->push_back(InputVertex(azimuth, altitude, computeStarColor(STAR_COLORIZATION)));
     }
     
-    qDebug() << "Total time to generate stars: " << (usecTimestampNow() - usecTimestamp(&startTime)) / 1000) << " msec\n";
+    qDebug() << "Total time to generate stars: " << ((usecTimestampNow() - usecTimestamp(&startTime)) / 1000) << " msec\n";
 }
 
 // computeStarColor
