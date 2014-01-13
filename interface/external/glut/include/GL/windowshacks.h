57f400f2d296b9c05e01cbd5e898d503fedac0de
Brad Hefta-Gaub <brad@highfidelity.io>
2014-01-12 21:45:41 -0800

    add windowshacks.h

---
+++ interface/external/glut/include/GL/windowshacks.h
@@ -0,0 +1,16 @@
+//
+//  windowshacks.h
+//  hifi
+//
+//  Created by Brad Hefta-Gaub on 1/12/14.
+//  Copyright (c) 2014 HighFidelity, Inc. All rights reserved.
+//
+//  hacks to get windows to compile
+//
+
+#ifndef __hifi__windowshacks__
+#define __hifi__windowshacks__
+
+#define GL_DOUBLE 0x140A
+
+#endif // __hifi__windowshacks__
