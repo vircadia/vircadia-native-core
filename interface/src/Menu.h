156fa6dc5aaa57465c585b6b6cda7dfc0a98c9a3
ZappoMan <bradh@konamoxt.com>
2014-02-13 11:03:35 -0800

    more tweaks, removed old clouds

--- interface/src/Menu.h
+++ interface/src/Menu.h
@@ -223,7 +223,6 @@ namespace MenuOption {
     const QString KillLocalVoxels = "Kill Local Voxels";
     const QString GoHome = "Go Home";
     const QString Gravity = "Use Gravity";
-    const QString ParticleCloud = "Particle Cloud";
     const QString LodTools = "LOD Tools";
     const QString Log = "Log";
     const QString Login = "Login";
