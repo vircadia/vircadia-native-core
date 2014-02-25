27be157d70c3cf447c349c0ec8ec2e766c262328
Stephen Birarda <commit@birarda.com>
2014-02-25 14:38:14 -0800

    fix oauth early token expiry

--- libraries/shared/src/OAuthAccessToken.cpp
+++ libraries/shared/src/OAuthAccessToken.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <QtCore/QDataStream>
+#include <QtCore/QDebug>
 
 #include "OAuthAccessToken.h"
 
@@ -22,7 +23,7 @@ OAuthAccessToken::OAuthAccessToken() :
 OAuthAccessToken::OAuthAccessToken(const QJsonObject& jsonObject) :
     token(jsonObject["access_token"].toString()),
     refreshToken(jsonObject["refresh_token"].toString()),
-    expiryTimestamp(QDateTime::currentMSecsSinceEpoch() + jsonObject["expires_in"].toDouble()),
+    expiryTimestamp(QDateTime::currentMSecsSinceEpoch() + (jsonObject["expires_in"].toDouble() * 1000)),
     tokenType(jsonObject["token_type"].toString())
 {
     
