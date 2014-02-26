4da6af96bb808491f6e8fa5765597d7721a7d941
Philip Rosedale <philip@highfidelity.io>
2014-02-25 22:08:56 -0800

    Bots will move

--- examples/bot.js
+++ examples/bot.js
@@ -17,12 +17,19 @@ function getRandomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// choose a random x and y in the range of 0 to 50
-positionX = getRandomFloat(0, 18);
-positionZ = getRandomFloat(0, 18);
+var CHANCE_OF_MOVING = 0.05; 
+var isMoving = false;
+
+var STARTING_RANGE = 18.0;
+var MOVE_RANGE = 1.0;
+var STOP_TOLERANCE = 0.05;
+var MOVE_RATE = 0.10;
+var firstPosition = { x: getRandomFloat(0, STARTING_RANGE), y: 0, z: getRandomFloat(0, STARTING_RANGE) };
+var targetPosition =  { x: 0, y: 0, z: 0 };
+
 
 // change the avatar's position to the random one
-Avatar.position = {x: positionX, y: 0, z: positionZ};
+Avatar.position = firstPosition;
 
 // pick an integer between 1 and 20 for the face model for this bot
 botNumber = getRandomInt(1, 100);
@@ -53,3 +60,20 @@ Avatar.skeletonModelURL = "https://s3-us-west-1.amazonaws.com/highfidelity-publi
 Avatar.billboardURL = "https://dl.dropboxusercontent.com/u/1864924/bot-billboard.png";
 
 Agent.isAvatar = true;
+
+function updateBehavior() {
+  if (!isMoving && (Math.random() < CHANCE_OF_MOVING)) {
+    // Set new target location
+    targetPosition = {  x: Avatar.position.x + getRandomFloat(-MOVE_RANGE, MOVE_RANGE), 
+                        y: Avatar.position.y, 
+                        z: Avatar.position.z + getRandomFloat(-MOVE_RANGE, MOVE_RANGE) };
+    isMoving = true;
+  } else { 
+
+    Avatar.position = Vec3.sum(Avatar.position, Vec3.multiply(Vec3.subtract(targetPosition, Avatar.position), MOVE_RATE));
+    if (Vec3.length(Vec3.subtract(Avatar.position, targetPosition)) < STOP_TOLERANCE) {
+       isMoving = false;  
+    }
+  }
+}
+Script.willSendVisualDataCallback.connect(updateBehavior);
