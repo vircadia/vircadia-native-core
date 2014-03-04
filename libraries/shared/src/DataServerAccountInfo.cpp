07d3eb886a9fc515c5ebe8d3584193b3f4bb0339
Dimitar Dobrev <dpldobrev@yahoo.com>
2014-03-04 21:27:06 +0200

    Added reading and storing of the XMPP password of the current user.

--- libraries/shared/src/DataServerAccountInfo.cpp
+++ libraries/shared/src/DataServerAccountInfo.cpp
@@ -12,21 +12,26 @@
 
 DataServerAccountInfo::DataServerAccountInfo() :
     _accessToken(),
-    _username()
+    _username(),
+    _xmppPassword()
 {
     
 }
 
 DataServerAccountInfo::DataServerAccountInfo(const QJsonObject& jsonObject) :
     _accessToken(jsonObject),
-    _username()
+    _username(),
+    _xmppPassword()
 {
-    setUsername(jsonObject["user"].toObject()["username"].toString());
+    QJsonObject userJSONObject = jsonObject["user"].toObject();
+    setUsername(userJSONObject["username"].toString());
+    setXMPPPassword(userJSONObject["xmpp_password"].toString());
 }
 
 DataServerAccountInfo::DataServerAccountInfo(const DataServerAccountInfo& otherInfo) {
     _accessToken = otherInfo._accessToken;
     _username = otherInfo._username;
+    _xmppPassword = otherInfo._xmppPassword;
 }
 
 DataServerAccountInfo& DataServerAccountInfo::operator=(const DataServerAccountInfo& otherInfo) {
@@ -40,6 +45,7 @@ void DataServerAccountInfo::swap(DataServerAccountInfo& otherInfo) {
     
     swap(_accessToken, otherInfo._accessToken);
     swap(_username, otherInfo._username);
+    swap(_xmppPassword, otherInfo._xmppPassword);
 }
 
 void DataServerAccountInfo::setUsername(const QString& username) {
@@ -50,12 +56,21 @@ void DataServerAccountInfo::setUsername(const QString& username) {
     }
 }
 
+void DataServerAccountInfo::setXMPPPassword(const QString& xmppPassword)
+{
+     if (_xmppPassword != xmppPassword) {
+         _xmppPassword = xmppPassword;
+
+         qDebug() << "XMPP password changed to " << xmppPassword;
+     }
+}
+
 QDataStream& operator<<(QDataStream &out, const DataServerAccountInfo& info) {
-    out << info._accessToken << info._username;
+    out << info._accessToken << info._username << info._xmppPassword;
     return out;
 }
 
 QDataStream& operator>>(QDataStream &in, DataServerAccountInfo& info) {
-    in >> info._accessToken >> info._username;
+    in >> info._accessToken >> info._username >> info._xmppPassword;
     return in;
 }
