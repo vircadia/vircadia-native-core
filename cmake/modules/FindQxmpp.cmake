1c2e752f3ce87055d52289c5713aad0a06447c2f
Dimitar Dobrev <dpldobrev@yahoo.com>
2014-03-05 19:57:59 +0200

    Integrated QXmpp in the build system.

---
+++ cmake/modules/FindQxmpp.cmake
@@ -0,0 +1,38 @@
+#  Try to find the qxmpp library
+#
+#  You can provide a QXMPP_ROOT_DIR which contains lib and include directories
+#
+#  Once done this will define
+#
+#  QXMPP_FOUND - system found qxmpp
+#  QXMPP_INCLUDE_DIRS - the qxmpp include directory
+#  QXMPP_LIBRARIES - Link this to use qxmpp
+#
+#  Created on 3/10/2014 by Stephen Birarda
+#  Copyright (c) 2014 High Fidelity
+#
+
+if (QXMPP_LIBRARIES AND QXMPP_INCLUDE_DIRS)
+  # in cache already
+  set(QXMPP_FOUND TRUE)
+else ()
+  
+  set(QXMPP_SEARCH_DIRS "${QXMPP_ROOT_DIR}" "$ENV{HIFI_LIB_DIR}/qxmpp")
+  
+  find_path(QXMPP_INCLUDE_DIR QXmppClient.h PATH_SUFFIXES include/qxmpp HINTS ${QXMPP_SEARCH_DIRS})
+
+  find_library(QXMPP_LIBRARY NAMES qxmpp qxmpp0 PATH_SUFFIXES lib HINTS ${QXMPP_SEARCH_DIRS})
+  
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(QXMPP DEFAULT_MSG QXMPP_INCLUDE_DIR QXMPP_LIBRARY)
+ 
+  if (QXMPP_FOUND)
+    if (NOT QXMPP_FIND_QUIETLY)
+      message(STATUS "Found qxmpp: ${QXMPP_LIBRARY}")
+    endif (NOT QXMPP_FIND_QUIETLY)
+  else ()
+    if (QXMPP_FIND_REQUIRED)
+      message(FATAL_ERROR "Could not find qxmpp")
+    endif (SIXENSE_FIND_REQUIRED)
+  endif ()
+endif ()
