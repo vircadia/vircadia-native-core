03a9d6c02dff412e315438d79eac774f50e41525
Stephen Birarda <commit@birarda.com>
2014-03-07 14:14:15 -0800

    glut dll search is for path, not library

--- cmake/modules/FindGLUT.cmake
+++ cmake/modules/FindGLUT.cmake
@@ -23,7 +23,7 @@ else ()
     endif()
     
     find_library(GLUT_glut_LIBRARY freeglut PATH_SUFFIXES lib/${WIN_ARCH_DIR} HINTS ${WIN_GLUT_SEARCH_DIRS})
-    find_library(GLUT_DLL_PATH freeglut.dll PATH_SUFFIXES bin/${WIN_ARCH_DIR} HINTS ${WIN_GLUT_SEARCH_DIRS})
+    find_path(GLUT_DLL_PATH freeglut.dll PATH_SUFFIXES bin/${WIN_ARCH_DIR} HINTS ${WIN_GLUT_SEARCH_DIRS})
   else ()
       find_path(GLUT_INCLUDE_DIR GL/glut.h
           "${GLUT_LOCATION}/include"
