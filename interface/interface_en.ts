38d03febbc7ccb1790119878c65550949782e2e9
Andrzej Kapolka <drzej.k@gmail.com>
2014-03-13 16:58:15 -0700

    Add this file back.  Hope it didn't have anything important!

---
+++ interface/interface_en.ts
@@ -0,0 +1,154 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>Application</name>
+    <message>
+        <location filename="src/Application.cpp" line="1362"/>
+        <source>Export Voxels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/Application.cpp" line="1363"/>
+        <source>Sparse Voxel Octree Files (*.svo)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/Application.cpp" line="3573"/>
+        <source>Open Script</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/Application.cpp" line="3574"/>
+        <source>JavaScript Files (*.js)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ChatWindow</name>
+    <message>
+        <location filename="ui/chatWindow.ui" line="20"/>
+        <location filename="../build/interface/ui_chatWindow.h" line="143"/>
+        <source>Chat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/chatWindow.ui" line="50"/>
+        <location filename="../build/interface/ui_chatWindow.h" line="144"/>
+        <source>Connecting to XMPP...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/chatWindow.ui" line="71"/>
+        <location filename="../build/interface/ui_chatWindow.h" line="145"/>
+        <source> online now:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="src/ui/ChatWindow.cpp" line="124"/>
+        <source>day</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="src/ui/ChatWindow.cpp" line="124"/>
+        <source>hour</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <location filename="src/ui/ChatWindow.cpp" line="124"/>
+        <source>minute</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="src/ui/ChatWindow.cpp" line="179"/>
+        <source>%1 online now:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="ui/updateDialog.ui" line="20"/>
+        <location filename="ui/updateDialog.ui" line="73"/>
+        <location filename="../build/interface/ui_updateDialog.h" line="137"/>
+        <location filename="../build/interface/ui_updateDialog.h" line="138"/>
+        <source>Update Required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/updateDialog.ui" line="129"/>
+        <location filename="../build/interface/ui_updateDialog.h" line="140"/>
+        <source>Download</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/updateDialog.ui" line="151"/>
+        <location filename="../build/interface/ui_updateDialog.h" line="141"/>
+        <source>Skip Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/updateDialog.ui" line="173"/>
+        <location filename="../build/interface/ui_updateDialog.h" line="142"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Menu</name>
+    <message>
+        <location filename="src/Menu.cpp" line="429"/>
+        <source>Open .ini config file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/Menu.cpp" line="431"/>
+        <location filename="src/Menu.cpp" line="443"/>
+        <source>Text files (*.ini)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/Menu.cpp" line="441"/>
+        <source>Save .ini config file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="src/ImportDialog.cpp" line="22"/>
+        <location filename="src/ImportDialog.cpp" line="23"/>
+        <source>Import Voxels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ImportDialog.cpp" line="24"/>
+        <source>Loading ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ImportDialog.cpp" line="25"/>
+        <source>Place voxels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ImportDialog.cpp" line="26"/>
+        <source>&lt;b&gt;Import&lt;/b&gt; %1 as voxels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="src/ImportDialog.cpp" line="27"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
