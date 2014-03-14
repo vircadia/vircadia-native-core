d02f0ba6870b7008fbd08d1648480a8afa5e59cd
Geenz <geenz@geenzo.com>
2014-03-14 19:38:17 -0400

    Fix for the “ringing” issue.  Don’t let Qt handle our buffer swaps.  Handle the swaps ourselves.

--- interface/src/GLCanvas.cpp
+++ interface/src/GLCanvas.cpp
@@ -13,9 +13,9 @@
 #include <QUrl>
 #include <QMainWindow>
 
-GLCanvas::GLCanvas() : QGLWidget(QGLFormat(QGL::NoDepthBuffer, QGL::NoStencilBuffer)),
+GLCanvas::GLCanvas() : QGLWidget(QGLFormat(QGL::NoDepthBuffer)),
     _throttleRendering(false),
-    _idleRenderInterval(100)
+    _idleRenderInterval(64)
 {
 }
 
@@ -25,11 +25,15 @@ void GLCanvas::initializeGL() {
     setAcceptDrops(true);
     connect(Application::getInstance(), SIGNAL(applicationStateChanged(Qt::ApplicationState)), this, SLOT(activeChanged(Qt::ApplicationState)));
     connect(&_frameTimer, SIGNAL(timeout()), this, SLOT(throttleRender()));
+    
+    // Note, we *DO NOT* want Qt to automatically swap buffers for us.  This results in the "ringing" bug mentioned in WL#19514 when we're throttling the framerate.
+    setAutoBufferSwap(false);
 }
 
 void GLCanvas::paintGL() {
     if (!_throttleRendering && !Application::getInstance()->getWindow()->isMinimized()) {
         Application::getInstance()->paintGL();
+        swapBuffers();
     }
 }
 
@@ -86,6 +90,7 @@ void GLCanvas::throttleRender() {
     _frameTimer.start(_idleRenderInterval);
     if (!Application::getInstance()->getWindow()->isMinimized()) {
         Application::getInstance()->paintGL();
+        swapBuffers();
     }
 }
 
