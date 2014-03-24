c623d8e9ac28878cd1d2c43d82287d97f131bb6e
ZappoMan <bradh@konamoxt.com>
2014-03-24 13:07:42 -0700

    tweak example script

--- examples/audioDeviceExample.js
+++ examples/audioDeviceExample.js
@@ -9,8 +9,26 @@
 //
 
 
+var outputDevices = AudioDevice.getOutputDevices();
+var defaultOutputDevice = AudioDevice.getDefaultOutputDevice();
+var selectOutputDevice = outputDevices[0];
+print("Output Devices:");
+for(var i = 0; i < outputDevices.length; i++) {
+    if (outputDevices[i] == defaultOutputDevice) {
+        print("    " + outputDevices[i] + " << default");
+    } else {
+        print("    " + outputDevices[i]);
+    }
+}
+
+print("Default Output Device:" + defaultOutputDevice);
+print("Selected Output Device:" + selectOutputDevice);
+print("Current Audio Output Device: " + AudioDevice.getOutputDevice());
+AudioDevice.setOutputDevice(selectOutputDevice);
+print("Audio Output Device: " + AudioDevice.getOutputDevice());
 
 var inputDevices = AudioDevice.getInputDevices();
+var selectInputDevice = inputDevices[0];
 var defaultInputDevice = AudioDevice.getDefaultInputDevice();
 print("Input Devices:");
 for(var i = 0; i < inputDevices.length; i++) {
@@ -21,23 +39,14 @@ for(var i = 0; i < inputDevices.length; i++) {
     }
 }
 
-var outputDevices = AudioDevice.getInputDevices();
-var defaultOutputDevice = AudioDevice.getDefaultInputDevice();
-print("Output Devices:");
-for(var i = 0; i < outputDevices.length; i++) {
-    if (outputDevices[i] == defaultOutputDevice) {
-        print("    " + outputDevices[i] + " << default");
-    } else {
-        print("    " + outputDevices[i]);
-    }
-}
-
-print("Audio Input Device: " + AudioDevice.getInputDevice());
-AudioDevice.setInputDevice("Shure Digital");
+print("Default Input Device:" + defaultInputDevice);
+print("Selected Input Device:" + selectInputDevice);
+print("Current Audio Input Device: " + AudioDevice.getInputDevice());
+AudioDevice.setInputDevice(selectInputDevice);
 print("Audio Input Device: " + AudioDevice.getInputDevice());
 
-print("Audio Output Device: " + AudioDevice.getOutputDevice());
-AudioDevice.setOutputDevice("Shure Digital");
-print("Audio Output Device: " + AudioDevice.getOutputDevice());
+print("Audio Input Device Level: " + AudioDevice.getInputVolume());
+AudioDevice.setInputVolume(AudioDevice.getInputVolume() * 2); // twice as loud!
+print("Audio Input Device Level: " + AudioDevice.getInputVolume());
 
 Script.stop();
