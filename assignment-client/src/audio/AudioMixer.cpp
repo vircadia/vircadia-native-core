d63acfc46f2ece206233fa6a5cf9199068e8de19
ZappoMan <bradh@konamoxt.com>
2014-03-23 18:12:02 -0700

    revert

--- assignment-client/src/audio/AudioMixer.cpp
+++ assignment-client/src/audio/AudioMixer.cpp
@@ -383,7 +383,7 @@ void AudioMixer::run() {
         
         const float STRUGGLE_TRIGGER_SLEEP_PERCENTAGE_THRESHOLD = 0.10f;
         const float BACK_OFF_TRIGGER_SLEEP_PERCENTAGE_THRESHOLD = 0.20f;
-        const float CUTOFF_EPSILON = 0.02f;
+        const float CUTOFF_DELTA = 0.02f;
         
         const float CURRENT_FRAME_RATIO = 1.0f / TRAILING_AVERAGE_FRAMES;
         const float PREVIOUS_FRAMES_RATIO = 1.0f - CURRENT_FRAME_RATIO;
