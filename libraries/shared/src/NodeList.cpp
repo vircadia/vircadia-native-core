ce169dd87d25e126a3f6805290d20fce36b81664
Stephen Birarda <commit@birarda.com>
2014-03-25 13:06:44 -0700

    add code return debug to DS check in packet

--- libraries/shared/src/NodeList.cpp
+++ libraries/shared/src/NodeList.cpp
@@ -584,7 +584,7 @@ void NodeList::sendDomainServerCheckIn() {
             }
             
             qDebug() << "sending DS check in size" << domainServerPacket.size() << "to" << _domainInfo.getSockAddr();
-            writeDatagram(domainServerPacket, _domainInfo.getSockAddr(), _domainInfo.getConnectionSecret());
+            qDebug() << "Code returned is" << writeDatagram(domainServerPacket, _domainInfo.getSockAddr(), _domainInfo.getConnectionSecret());
             const int NUM_DOMAIN_SERVER_CHECKINS_PER_STUN_REQUEST = 5;
             static unsigned int numDomainCheckins = 0;
             
