01c42c741c57ecd01c51238ab3996228a45d2e0c
Andrew Meadows <andrew@highfidelity.io>
2014-04-04 14:46:54 -0700

    fix #2567 thrust + oculus doesn't change body rot

--- interface/src/Application.cpp
+++ interface/src/Application.cpp
@@ -510,7 +510,7 @@ void Application::paintGL() {
         _myCamera.setDistance(0.0f);
         _myCamera.setTightness(0.0f);     //  Camera is directly connected to head without smoothing
         _myCamera.setTargetPosition(_myAvatar->getHead()->calculateAverageEyePosition());
-        _myCamera.setTargetRotation(_myAvatar->getHead()->getOrientation());
+        _myCamera.setTargetRotation(_myAvatar->getHead()->getCameraOrientation());
 
     } else if (_myCamera.getMode() == CAMERA_MODE_FIRST_PERSON) {
         _myCamera.setTightness(0.0f);  //  In first person, camera follows (untweaked) head exactly without delay
