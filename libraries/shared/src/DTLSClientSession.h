5a79079c090d7a004126e4ee5fc8beddff3c2dc6
Stephen Birarda <commit@birarda.com>
2014-04-09 15:03:56 -0700

    fix boilerplates in new DTLS files

--- libraries/shared/src/DTLSClientSession.h
+++ libraries/shared/src/DTLSClientSession.h
@@ -1,9 +1,12 @@
 //
 //  DTLSClientSession.h
-//  hifi
+//  libraries/shared/src
 //
 //  Created by Stephen Birarda on 2014-04-01.
-//  Copyright (c) 2014 High Fidelity, Inc. All rights reserved.
+//  Copyright 2014 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
 #ifndef __hifi__DTLSClientSession__
--- libraries/shared/src/DummyDTLSSession.cpp
+++ libraries/shared/src/DummyDTLSSession.cpp
@@ -1,9 +1,12 @@
 //
 //  DummyDTLSSession.cpp
-//  hifi
+//  libraries/shared/src
 //
 //  Created by Stephen Birarda on 2014-04-04.
-//  Copyright (c) 2014 High Fidelity, Inc. All rights reserved.
+//  Copyright 2014 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
 #include "DummyDTLSSession.h"
