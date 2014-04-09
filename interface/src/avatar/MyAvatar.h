2eef95f67e922865d4d55a2f4b952e55acdfe419
Andrew Meadows <andrew@highfidelity.io>
2014-04-08 18:44:34 -0700

    remove getAbsoluteHeadYaw() from the header

--- interface/src/avatar/MyAvatar.h
+++ interface/src/avatar/MyAvatar.h
@@ -54,7 +54,6 @@ public:
     float getLeanScale() const { return _leanScale; }
     float getElapsedTimeStopped() const { return _elapsedTimeStopped; }
     float getElapsedTimeMoving() const { return _elapsedTimeMoving; }
-    float getAbsoluteHeadYaw() const;   // degrees
     const glm::vec3& getMouseRayOrigin() const { return _mouseRayOrigin; }
     const glm::vec3& getMouseRayDirection() const { return _mouseRayDirection; }
     glm::vec3 getGravity() const { return _gravity; }
