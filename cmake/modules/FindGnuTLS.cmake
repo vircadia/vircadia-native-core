31382b7e121a167d401f7d8a9034e14ab5f7346f
Stephen Birarda <commit@birarda.com>
2014-04-09 17:41:18 -0700

    add a message for Win32 users to generate the MSVC GnuTLS import library

--- cmake/modules/FindGnuTLS.cmake
+++ cmake/modules/FindGnuTLS.cmake
@@ -21,8 +21,7 @@
 if (GNUTLS_LIBRARY AND GNUTLS_INCLUDE_DIRS)
   # in cache already
   set(GNUTLS_FOUND TRUE)
-else ()
-  
+else ()  
   set(GNUTLS_SEARCH_DIRS "${GNUTLS_ROOT_DIR}" "$ENV{HIFI_LIB_DIR}/gnutls")
   
   find_path(GNUTLS_INCLUDE_DIR gnutls/gnutls.h PATH_SUFFIXES include HINTS ${GNUTLS_SEARCH_DIRS})
@@ -31,4 +30,12 @@ else ()
   
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(GNUTLS DEFAULT_MSG GNUTLS_INCLUDE_DIR GNUTLS_LIBRARY)
+  
+  if (WIN32 AND NOT GNUTLS_FOUND)
+    message(STATUS "If you're generating a MSVC environment, you'll need to run the command")
+    message(STATUS "$GnuTLS-DIR\\bin>lib /def:libgnutls-28.def")
+    message(STATUS "From the MSVC command prompt to generate the .lib file and copy it into")
+    message(STATUS "your lib folder. Replace $GnuTLS-DIR in the command with the directory")
+    message(STATUS "containing GnuTLS.")
+  endif ()
 endif ()
