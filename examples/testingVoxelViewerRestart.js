ae443a3136d40a2fb2a4643a2f784e05861f7137
ZappoMan <bradh@konamoxt.com>
2014-04-22 11:27:50 -0700

    fix headers

--- examples/testingVoxelViewerRestart.js
+++ examples/testingVoxelViewerRestart.js
@@ -5,7 +5,8 @@
 //  Created by Brad Hefta-Gaub on 2/26/14
 //  Copyright (c) 2014 HighFidelity, Inc. All rights reserved.
 //
-//  This is an example script
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
 var count = 0;
