d56a4e169e726ea67f55b890fa57dfba9c1928d9
Ryan Huffman <ryanhuffman@gmail.com>
2014-04-23 15:24:21 -0700

    Add ChatInputArea

---
+++ interface/src/ui/ChatInputArea.h
@@ -0,0 +1,28 @@
+//
+//  ChatInputArea.h
+//  interface/src/ui
+//
+//  Created by Ryan Huffman on 4/11/14.
+//  Copyright 2014 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_ChatInputArea_h
+#define hifi_ChatInputArea_h
+
+#include <QTextBrowser>
+
+class ChatInputArea : public QTextEdit {
+    Q_OBJECT
+public:
+    ChatInputArea(QWidget* parent) : QTextEdit(parent) { };
+
+protected:
+    void insertFromMimeData(const QMimeData* source) {
+        insertPlainText(source->text());
+    };
+};
+
+#endif // hifi_ChatInputArea_h
