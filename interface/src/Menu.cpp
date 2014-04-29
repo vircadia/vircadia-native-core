ac8adad0e07d3d088813bb31047a6820884f6953
Konstantin <SydoxX@users.noreply.github.com>
2014-04-29 20:49:20 +0200

    Remove 0,0,0 entry.

--- interface/src/Menu.cpp
+++ interface/src/Menu.cpp
@@ -935,9 +935,6 @@ void Menu::goTo() {
                 // location coordinates or place name
                 QString domain = urlParts[0];
                 goToDomain(domain);
-                // as there are no coordinates, go to 0,0,0
-                QString destination = "0,0,0";
-                goTo(destination);
             }
             else if (urlParts.count() > 1) {
                 // if url has 2 or more parts, the first one is domain name
