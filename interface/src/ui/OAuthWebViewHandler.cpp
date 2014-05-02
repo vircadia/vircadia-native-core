1fda012364d1a93b118e010e141df63b16df3745
Stephen Birarda <commit@birarda.com>
2014-05-02 16:22:12 -0700

    fix another casing issue in OAuthWebViewHandler

--- interface/src/ui/OAuthWebViewHandler.cpp
+++ interface/src/ui/OAuthWebViewHandler.cpp
@@ -9,7 +9,7 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
-#include <QtWebkitWidgets/QWebView>
+#include <QtWebKitWidgets/QWebView>
 
 #include "Application.h"
 
