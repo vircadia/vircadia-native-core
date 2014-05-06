72ebeffa5c471e595072a8cb055a7258102e83af
Andrzej Kapolka <drzej.k@gmail.com>
2014-05-06 10:59:28 -0700

    Scratch that; I just didn't understand how magic invisible disappearing scroll bars worked on OS X.

--- interface/src/ui/AttachmentsDialog.cpp
+++ interface/src/ui/AttachmentsDialog.cpp
@@ -33,7 +33,6 @@ AttachmentsDialog::AttachmentsDialog() :
     QScrollArea* area = new QScrollArea();
     layout->addWidget(area);
     area->setWidgetResizable(true);
-    area->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
     QWidget* container = new QWidget();
     container->setLayout(_attachments = new QVBoxLayout());
     container->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Preferred);
