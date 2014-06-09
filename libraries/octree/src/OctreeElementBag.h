139a64a0d245fd6f529b084f5d485d982c504c90
ZappoMan <bradh@konamoxt.com>
2014-06-09 14:10:40 -0700

    fix comment

--- libraries/octree/src/OctreeElementBag.h
+++ libraries/octree/src/OctreeElementBag.h
@@ -5,10 +5,9 @@
 //  Created by Brad Hefta-Gaub on 4/25/2013.
 //  Copyright 2013 High Fidelity, Inc.
 //
-//  This class is used by the VoxelTree:encodeTreeBitstream() functions to store extra nodes that need to be sent
-//  it's a generic bag style storage mechanism. But It has the property that you can't put the same node into the bag
-//  more than once (in other words, it de-dupes automatically), also, it supports collapsing it's several peer nodes
-//  into a parent node in cases where you add enough peers that it makes more sense to just add the parent.
+//  This class is used by the Octree:encodeTreeBitstream() functions to store elements and element data that need to be sent.
+//  It's a generic bag style storage mechanism. But It has the property that you can't put the same element into the bag
+//  more than once (in other words, it de-dupes automatically).
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
