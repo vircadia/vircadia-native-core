46f2ab73bccf6fa8669856534f0c20f96ee8c416
matsukaze <matsukaze314@gmail.com>
2014-06-10 22:29:58 -0400

    Job #19766 BUG: Stop or reload all scripts crashes interface fixed, part 3
    Move the conversion of scriptName to QUrl to the beginning of
    Application::loadScript. Use the scriptURLString to query the
    _scriptEngineHash map.

--- assignment-client/src/AssignmentClient.h
+++ assignment-client/src/AssignmentClient.h
@@ -16,11 +16,57 @@
 
 #include "ThreadedAssignment.h"
 
+class Parser {
+public:
+    Parser(const QString& option, bool required) : _option(option), _required(required), _exists(false), _isValid(false) {}
+    virtual bool parse(const QStringList& argumentList) { return false; }
+    bool exists() { return _exists; }
+    bool isValid() { return _isValid; }
+
+protected:
+    int findToken(const QStringList& argumentList) {
+        int argumentIndex = argumentList.indexOf(_option);
+        validateOption(argumentIndex);
+        return argumentIndex;
+    }
+
+    void validateOption(int index) {
+        _exists = true;
+        if (index == -1) {
+            _exists = false;
+            if (_required) {
+                qDebug() << "Command line option " << _option << " is missing";
+            }
+        }
+    }
+
+    void valueNotSpecified() {
+        _isValid = false;
+        qDebug() << "Command line option " << _option << " value is missing";
+    }
+
+    void validateValue(bool ok) {
+        _isValid = ok;
+        if (!ok) {
+            qDebug() << "Command line option " << _option << " value is invalid";
+        }
+    }
+
+private:
+    QString _option;
+    bool _required;
+    bool _exists;
+    bool _isValid;
+};
+
 class AssignmentClient : public QCoreApplication {
     Q_OBJECT
 public:
     AssignmentClient(int &argc, char **argv);
+    ~AssignmentClient();
+
     static const SharedAssignmentPointer& getCurrentAssignment() { return _currentAssignment; }
+
 private slots:
     void sendAssignmentRequest();
     void readPendingDatagrams();
@@ -31,6 +77,8 @@ private:
     Assignment _requestAssignment;
     static SharedAssignmentPointer _currentAssignment;
     QString _assignmentServerHostname;
+
+    QMap<QString, Parser*> _parsers;
 };
 
 #endif // hifi_AssignmentClient_h
