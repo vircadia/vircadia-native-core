0e6abe33597dd0fc39a63d6f044d1374fdb0795d
Ryan Huffman <ryanhuffman@gmail.com>
2014-06-20 16:31:22 -0700

    Update inactive chat window opacity to 0.8

--- interface/src/ui/ChatWindow.cpp
+++ interface/src/ui/ChatWindow.cpp
@@ -31,7 +31,7 @@
 const int NUM_MESSAGES_TO_TIME_STAMP = 20;
 
 const float OPACITY_ACTIVE = 1.0;
-const float OPACITY_INACTIVE = 0.6;
+const float OPACITY_INACTIVE = 0.8;
 
 const QRegularExpression regexLinks("((?:(?:ftp)|(?:https?)|(?:hifi))://\\S+)");
 const QRegularExpression regexHifiLinks("([#@]\\S+)");
