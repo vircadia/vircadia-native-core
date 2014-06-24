cf0e1ab1a93bc26f9fe8e51d5481a86c5a86be20
Stephen Birarda <commit@birarda.com>
2014-06-24 14:24:56 -0700

    finish up handlebars template for settings

--- domain-server/resources/web/js/settings.js
+++ domain-server/resources/web/js/settings.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
   $.getJSON('describe.json', function(data){
+    
+    Handlebars.registerHelper('setKey', function(value){
+        this.key = value;
+    });
+    
     var source = $('#template').html();
     var template = Handlebars.compile(source);
     
