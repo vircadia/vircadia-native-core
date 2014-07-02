29976d7861f041322f11ccdf29b92bab26470c30
Atlante45 <clement.brisset@gmail.com>
2014-07-01 17:17:34 -0700

    Created base for wrapper singleton class NetworkAccessManager

---
+++ libraries/networking/src/NetworkAccessManager.cpp
@@ -0,0 +1,20 @@
+//
+//  NetworkAccessManager.cpp
+//
+//
+//  Created by Clement on 7/1/14.
+//  Copyright 2014 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#include "NetworkAccessManager.h"
+
+NetworkAccessManager& NetworkAccessManager::getInstance() {
+    static NetworkAccessManager sharedInstance;
+    return sharedInstance;
+}
+
+NetworkAccessManager::NetworkAccessManager() {
+}
