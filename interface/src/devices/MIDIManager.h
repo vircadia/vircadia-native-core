73312ecc5f8780ae73cc51a2319c829d89aedbb7
Stephen Birarda <commit@birarda.com>
2014-07-02 15:07:49 -0700

    add the PitchWheel constant to the MIDIManager class

--- interface/src/devices/MIDIManager.h
+++ interface/src/devices/MIDIManager.h
@@ -25,6 +25,8 @@ class MIDIManager : public QObject {
     Q_PROPERTY(unsigned int NoteOn READ NoteOn)
     Q_PROPERTY(unsigned int NoteOff READ NoteOff)
     Q_PROPERTY(unsigned int ModWheel READ ModWheel)
+    Q_PROPERTY(unsigned int PitchWheel READ PitchWheel)
+    
 public:
     static MIDIManager& getInstance();
     static void midiCallback(double deltaTime, std::vector<unsigned char>* message, void* userData);
@@ -37,6 +39,7 @@ public slots:
     unsigned int NoteOn() const { return 144; }
     unsigned int NoteOff() const { return 128; }
     unsigned int ModWheel() const  { return 176; }
+    unsigned int PitchWheel() const { return 224; }
 signals:
     void midiEvent(const MIDIEvent& event);
     
