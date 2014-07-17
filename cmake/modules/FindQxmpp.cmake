3fad0e6ac89573badeaad2550a7c040266404e86
Stephen Birarda <commit@birarda.com>
2014-07-17 16:49:17 -0700

    cleanup qxmpp find module, use select_library_confgurations

--- cmake/modules/FindQxmpp.cmake
+++ cmake/modules/FindQxmpp.cmake
@@ -18,17 +18,15 @@
 #  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 # 
 
-if (QXMPP_LIBRARIES AND QXMPP_INCLUDE_DIRS)
-  # in cache already
-  set(QXMPP_FOUND TRUE)
-else ()
-  
-  set(QXMPP_SEARCH_DIRS "${QXMPP_ROOT_DIR}" "$ENV{HIFI_LIB_DIR}/qxmpp")
-  
-  find_path(QXMPP_INCLUDE_DIR QXmppClient.h PATH_SUFFIXES include/qxmpp HINTS ${QXMPP_SEARCH_DIRS})
+set(QXMPP_SEARCH_DIRS "${QXMPP_ROOT_DIR}" "$ENV{HIFI_LIB_DIR}/qxmpp")
 
-  find_library(QXMPP_LIBRARY NAMES qxmpp qxmpp0 qxmpp_d PATH_SUFFIXES lib HINTS ${QXMPP_SEARCH_DIRS})
-  
-  include(FindPackageHandleStandardArgs)
-  find_package_handle_standard_args(QXMPP DEFAULT_MSG QXMPP_INCLUDE_DIR QXMPP_LIBRARY)
-endif ()
+find_path(QXMPP_INCLUDE_DIR QXmppClient.h PATH_SUFFIXES include/qxmpp HINTS ${QXMPP_SEARCH_DIRS})
+
+find_library(QXMPP_LIBRARY_RELEASE NAMES qxmpp PATH_SUFFIXES lib HINTS ${QXMPP_SEARCH_DIRS})
+find_library(QXMPP_LIBRARY_DEBUG NAMES qxmpp_d PATH_SUFFIXES lib HINTS ${QXMPP_SEARCH_DIRS})
+
+include(SelectLibraryConfigurations)
+select_library_configurations(QXMPP)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(QXMPP DEFAULT_MSG QXMPP_INCLUDE_DIR QXMPP_LIBRARY)
