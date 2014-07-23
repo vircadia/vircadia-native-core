0d039b1cdb16c06af447d9bc54afec8fe699545d
Atlante45 <clement.brisset@gmail.com>
2014-07-23 14:38:37 -0700

    Fixed invokeMethod bug + removed debug

--- examples/editModels.js
+++ examples/editModels.js
@@ -1168,7 +1168,6 @@ function handeMenuEvent(menuItem){
             angles.z = array[7].value;
             selectedModelProperties.modelRotation = Quat.fromVec3Degrees(angles);
             selectedModelProperties.radius = array[8].value / 2;
-            print(selectedModelProperties.radius);
 
             Models.editModel(selectedModelID, selectedModelProperties);
         }
