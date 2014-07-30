92b75d29cff1d340366c6aec6c0428b9cbc3ecd7
Atlante45 <clement.brisset@gmail.com>
2014-07-30 14:18:55 -0700

    Added referentials base class

---
+++ libraries/avatars/src/Referential.cpp
@@ -0,0 +1,18 @@
+//
+//  Referential.cpp
+//
+//
+//  Created by Clement on 7/30/14.
+//  Copyright 2014 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#include "Referential.h"
+
+Referential::Referential(AvatarData* avatar) :
+    _isValid(true),
+    _avatar(avatar)
+{
+}
---
+++ libraries/avatars/src/Referential.h
@@ -0,0 +1,38 @@
+//
+//  Referential.h
+//
+//
+//  Created by Clement on 7/30/14.
+//  Copyright 2014 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_Referential_h
+#define hifi_Referential_h
+
+#include "AvatarData.h"
+
+class Referential {
+public:
+    virtual bool isValid() { return _isValid; }
+    virtual void update() = 0;
+    
+protected:
+    Referential(AvatarData* avatar);
+    
+    bool _isValid;
+    AvatarData* _avatar;
+    
+    glm::vec3 _refPosition;
+    glm::quat _refRotation;
+    float _refScale;
+    
+    glm::vec3 _translation;
+    glm::quat _rotation;
+    float _scale;
+};
+
+
+#endif // hifi_Referential_h
