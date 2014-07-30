069f1ed247ec1991bd462004cb2ed6af06e50780
Atlante45 <clement.brisset@gmail.com>
2014-07-30 14:19:25 -0700

    Added ModelReferential

---
+++ libraries/models/src/ModelReferential.cpp
@@ -0,0 +1,60 @@
+//
+//  ModelReferential.cpp
+//
+//
+//  Created by Clement on 7/30/14.
+//  Copyright 2014 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#include "ModelTree.h"
+
+#include "ModelReferential.h"
+
+ModelReferential::ModelReferential(uint32_t modelID, ModelTree* tree, AvatarData* avatar) :
+Referential(avatar),
+_modelID(modelID),
+_tree(tree)
+{
+    const ModelItem* item = _tree->findModelByID(_modelID);
+    if (!_isValid || item == NULL || _avatar == NULL) {
+        _isValid = false;
+        return;
+    }
+    
+    _refScale = item->getRadius();
+    _refRotation = item->getModelRotation();
+    _refPosition = item->getPosition();
+    
+    glm::quat refInvRot = glm::inverse(_refRotation);
+    _scale = _avatar->getTargetScale() / _refScale;
+    _rotation = refInvRot * _avatar->getOrientation();
+    _translation = refInvRot * (avatar->getPosition() - _refPosition) / _refScale;
+}
+
+void ModelReferential::update() {
+    const ModelItem* item = _tree->findModelByID(_modelID);
+    if (!_isValid || item == NULL || _avatar == NULL) {
+        _isValid = false;
+        return;
+    }
+    
+    bool somethingChanged = false;
+    if (item->getRadius() != _refScale) {
+        _refScale = item->getRadius();
+        _avatar->setTargetScale(_refScale * _scale);
+        somethingChanged = true;
+    }
+    if (item->getModelRotation() != _refRotation) {
+        _refRotation = item->getModelRotation();
+        _avatar->setOrientation(_refRotation * _rotation);
+        somethingChanged = true;
+    }
+    if (item->getPosition() != _refPosition || somethingChanged) {
+        _refPosition = item->getPosition();
+        _avatar->setPosition(_refPosition + _refRotation * (_translation * _refScale));
+        somethingChanged = true;
+    }
+}
---
+++ libraries/models/src/ModelReferential.h
@@ -0,0 +1,29 @@
+//
+//  ModelReferential.h
+//
+//
+//  Created by Clement on 7/30/14.
+//  Copyright 2014 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_ModelReferential_h
+#define hifi_ModelReferential_h
+
+#include <Referential.h>
+
+class ModelTree;
+
+class ModelReferential : public Referential {
+public:
+    ModelReferential(uint32_t modelID, ModelTree* tree, AvatarData* avatar);
+    virtual void update();
+    
+protected:
+    uint32_t _modelID;
+    ModelTree* _tree;
+};
+
+#endif // hifi_ModelReferential_h
