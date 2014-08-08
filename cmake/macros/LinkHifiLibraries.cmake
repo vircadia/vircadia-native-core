93b6f167f5d5b5ee5fd5aeae96b9b85334aefc0c
Stephen Birarda <commit@birarda.com>
2014-08-08 09:51:20 -0700

    change more CMakeLists to use simplified hifi library linker

--- cmake/macros/LinkHifiLibraries.cmake
+++ cmake/macros/LinkHifiLibraries.cmake
@@ -7,12 +7,13 @@
 #  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 # 
 
-macro(LINK_HIFI_LIBRARIES TARGET LIBRARIES)
+macro(LINK_HIFI_LIBRARIES TARGET)
   
   file(RELATIVE_PATH RELATIVE_LIBRARY_DIR_PATH ${CMAKE_CURRENT_SOURCE_DIR} "${HIFI_LIBRARY_DIR}")
   
-  foreach(HIFI_LIBRARY ${LIBRARIES})
-    
+  set(LIBRARIES_TO_LINK ${ARGN})
+  
+  foreach(HIFI_LIBRARY ${LIBRARIES_TO_LINK})    
     if (NOT TARGET ${HIFI_LIBRARY})
       add_subdirectory("${RELATIVE_LIBRARY_DIR_PATH}/${HIFI_LIBRARY}" "${RELATIVE_LIBRARY_DIR_PATH}/${HIFI_LIBRARY}")
     endif ()
