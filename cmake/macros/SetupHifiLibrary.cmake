c9f8433a2dba2c78951cb5a35ef0fe17ceb0f3a1
Stephen Birarda <commit@birarda.com>
2014-08-08 10:44:58 -0700

    tweak setup_hifi_library to bubble up full path of Qt modules

--- cmake/macros/SetupHifiLibrary.cmake
+++ cmake/macros/SetupHifiLibrary.cmake
@@ -13,14 +13,28 @@ macro(SETUP_HIFI_LIBRARY TARGET)
 
   # grab the implemenation and header files
   file(GLOB LIB_SRCS src/*.h src/*.cpp)
-  set(LIB_SRCS ${LIB_SRCS} ${WRAPPED_SRCS})
+  set(LIB_SRCS ${LIB_SRCS})
 
-  # create a library and set the property so it can be referenced later
-  add_library(${TARGET} ${LIB_SRCS} ${ARGN})
+  # create a library and set the property so it can be referenced later  
+  add_library(${TARGET} ${LIB_SRCS} ${AUTOMTC_SRC})
   
-  find_package(Qt5Core REQUIRED)
-  qt5_use_modules(${TARGET} Core)
-
-  target_link_libraries(${TARGET} ${QT_LIBRARIES})
+  set(QT_MODULES_TO_LINK ${ARGN})
+  list(APPEND QT_MODULES_TO_LINK Core)
+  
+  find_package(Qt5 COMPONENTS ${QT_MODULES_TO_LINK})
+  
+  foreach(QT_MODULE ${QT_MODULES_TO_LINK})
+    # link this Qt module to ourselves
+    target_link_libraries(${TARGET} Qt5::${QT_MODULE})
+    
+    get_target_property(QT_LIBRARY_LOCATION Qt5::${QT_MODULE} LOCATION)
+    
+    # add the actual path to the Qt module to a QT_LIBRARIES variable
+    list(APPEND QT_LIBRARIES ${QT_LIBRARY_LOCATION})
+  endforeach()
+  
+  if (QT_LIBRARIES)
+    set_target_properties(${TARGET} PROPERTIES DEPENDENCY_LIBRARIES "${QT_LIBRARIES}")
+  endif ()
 
 endmacro(SETUP_HIFI_LIBRARY _target)
