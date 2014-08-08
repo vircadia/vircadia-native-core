5b4a3bd5f5eaed8bbfcb3a31a2c18ac8848799c5
Atlante45 <clement.brisset@gmail.com>
2014-08-08 14:26:16 -0700

    Recording ground work

--- interface/src/Recorder.cpp
+++ interface/src/Recorder.cpp
@@ -10,3 +10,52 @@
 //
 
 #include "Recorder.h"
+
+void Recording::addFrame(int timestamp, RecordingFrame &frame) {
+    _timestamps << timestamp;
+    _frames << frame;
+}
+
+void Recording::clear() {
+    _timestamps.clear();
+    _frames.clear();
+}
+
+void writeRecordingToFile(Recording& recording, QString file) {
+    qDebug() << "Writing recording to " << file;
+}
+
+Recording& readRecordingFromFile(QString file) {
+    qDebug() << "Reading recording from " << file;
+    return *(new Recording());
+}
+
+
+bool Recorder::isRecording() const {
+    return _timer.isValid();
+}
+
+qint64 Recorder::elapsed() const {
+    if (isRecording()) {
+        return _timer.elapsed();
+    } else {
+        return 0;
+    }
+}
+
+void Recorder::startRecording() {
+    _recording.clear();
+    _timer.start();
+}
+
+void Recorder::stopRecording() {
+    _timer.invalidate();
+}
+
+void Recorder::saveToFile(QString file) {
+    if (_recording.isEmpty()) {
+        qDebug() << "Cannot save recording to file, recording is empty.";
+    }
+    
+    writeRecordingToFile(_recording, file);
+}
