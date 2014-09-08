5a6f8015f66df4d2013ad85f412466e062140584
Craig Hansen-Sturm <chansensturm@comcast.net>
2014-09-08 14:05:47 -0700

    unroll audio gain rendering loop

--- libraries/audio/src/AudioGain.h
+++ libraries/audio/src/AudioGain.h
@@ -56,7 +56,7 @@ public:
         
         float32_t** samples = frameBuffer.getFrameData();
         for (uint16_t j = 0; j < frameBuffer.getChannelCount(); ++j) {
-            for (uint16_t i = 0; i < frameBuffer.getFrameCount(); i += 8) {
+            for (uint16_t i = 0; i < frameBuffer.getFrameCount(); i += 32) {
                 samples[j][i + 0] *= _gain;
                 samples[j][i + 1] *= _gain;
                 samples[j][i + 2] *= _gain;
@@ -65,6 +65,30 @@ public:
                 samples[j][i + 5] *= _gain;
                 samples[j][i + 6] *= _gain;
                 samples[j][i + 7] *= _gain;
+                samples[j][i + 8] *= _gain;
+                samples[j][i + 9] *= _gain;
+                samples[j][i + 10] *= _gain;
+                samples[j][i + 11] *= _gain;
+                samples[j][i + 12] *= _gain;
+                samples[j][i + 13] *= _gain;
+                samples[j][i + 14] *= _gain;
+                samples[j][i + 15] *= _gain;
+                samples[j][i + 16] *= _gain;
+                samples[j][i + 17] *= _gain;
+                samples[j][i + 18] *= _gain;
+                samples[j][i + 19] *= _gain;
+                samples[j][i + 20] *= _gain;
+                samples[j][i + 21] *= _gain;
+                samples[j][i + 22] *= _gain;
+                samples[j][i + 23] *= _gain;
+                samples[j][i + 24] *= _gain;
+                samples[j][i + 25] *= _gain;
+                samples[j][i + 26] *= _gain;
+                samples[j][i + 27] *= _gain;
+                samples[j][i + 28] *= _gain;
+                samples[j][i + 29] *= _gain;
+                samples[j][i + 30] *= _gain;
+                samples[j][i + 31] *= _gain;
             }
         }
     }
