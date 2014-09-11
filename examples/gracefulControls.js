ffa47b1923441440e7d39bcbdaaec8d2d9c5d780
Ryan Huffman <ryanhuffman@gmail.com>
2014-09-11 15:22:04 -0700

    Add header comment to gracefulControls.js

--- examples/gracefulControls.js
+++ examples/gracefulControls.js
@@ -1,7 +1,17 @@
-// Coefficient to use for linear drag.  Higher numbers will cause motion to
-// slow down more quickly.
+//
+//  gracefulControls.js
+//  examples
+//
+//  Created by Ryan Huffman on 9/11/14
+//  Copyright 2014 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
 
 var DEFAULT_PARAMETERS = {
+    // Coefficient to use for linear drag.  Higher numbers will cause motion to
+    // slow down more quickly.
     DRAG_COEFFICIENT: 0.9,
     MAX_SPEED: 40.0,
     ACCELERATION: 1.0,
