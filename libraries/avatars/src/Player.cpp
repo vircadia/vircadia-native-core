29ff8c09d9c2f81d8ad60d2c936144f1ff6acf90
Atlante45 <clement.brisset@gmail.com>
2014-09-18 15:23:58 -0700

    Fix LookAtPosition

--- libraries/avatars/src/Player.cpp
+++ libraries/avatars/src/Player.cpp
@@ -203,7 +203,7 @@ void Player::play() {
         head->setFinalPitch(eulers.x);
         head->setFinalYaw(eulers.y);
         head->setFinalRoll(eulers.z);
-        head->setLookAtPosition(currentFrame.getLookAtPosition());
+        head->setLookAtPosition(context->position + context->orientation * currentFrame.getLookAtPosition());
     } else {
         qDebug() << "WARNING: Player couldn't find head data.";
     }
