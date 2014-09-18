a0675162b6d1f52105f895311a4e8a94f37d08b6
Stephen Birarda <commit@birarda.com>
2014-09-18 09:35:33 -0700

    hook My Locations to data-web dialog

--- interface/src/Menu.cpp
+++ interface/src/Menu.cpp
@@ -1210,13 +1210,17 @@ void Menu::displayNameLocationResponse(const QString& errorString) {
 
 void Menu::toggleLocationList() {
     if (!_userLocationsDialog) {
-        _userLocationsDialog = new UserLocationsDialog(Application::getInstance()->getWindow());
+        _userLocationsDialog = DataWebDialog::dialogForPath("/locations");
     }
-    if (_userLocationsDialog->isVisible()) {
-        _userLocationsDialog->hide();
-    } else {
+    
+    if (!_userLocationsDialog->isVisible()) {
         _userLocationsDialog->show();
     }
+    
+    _userLocationsDialog->raise();
+    _userLocationsDialog->activateWindow();
+    _userLocationsDialog->showNormal();
+    
 }
 
 void Menu::nameLocation() {
