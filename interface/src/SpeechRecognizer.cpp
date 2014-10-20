f872902ab9c0724ac7c9236b132fe62064588d25
David Rowe <david@ctrlaltstudio.com>
2014-10-20 13:40:11 -0700

    Add stubs for Windows SpeechRecognizer interface

--- interface/src/SpeechRecognizer.cpp
+++ interface/src/SpeechRecognizer.cpp
@@ -9,3 +9,39 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
+#include <QtGlobal>
+
+#ifdef Q_OS_WIN
+
+#include "SpeechRecognizer.h"
+
+SpeechRecognizer::SpeechRecognizer() :
+    QObject() {
+
+}
+
+SpeechRecognizer::~SpeechRecognizer() {
+
+}
+
+void SpeechRecognizer::handleCommandRecognized(const char* command) {
+
+}
+
+void SpeechRecognizer::setEnabled(bool enabled) {
+
+}
+
+void SpeechRecognizer::addCommand(const QString& command) {
+
+}
+
+void SpeechRecognizer::removeCommand(const QString& command) {
+
+}
+
+void SpeechRecognizer::reloadCommands() {
+
+}
+
+#endif // Q_OS_WIN
