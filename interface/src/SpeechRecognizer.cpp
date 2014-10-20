7566b7e546aaac788fe94e5344c33f05efdbf379
David Rowe <david@ctrlaltstudio.com>
2014-10-20 13:26:51 -0700

    Add SpeechRecognizer.h and .cpp to Windows builds

---
+++ interface/src/SpeechRecognizer.cpp
@@ -0,0 +1,11 @@
+//
+//  SpeechRecognizer.cpp
+//  interface/src
+//
+//  Created by David Rowe on 10/20/2014.
+//  Copyright 2014 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
