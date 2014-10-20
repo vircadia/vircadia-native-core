5a11104bdf92ee0ac6eaa45a68a8ad56deeaa09c
Atlante45 <clement.brisset@gmail.com>
2014-10-20 13:45:16 -0700

    Updated script

--- examples/audioReverbOn.js
+++ examples/audioReverbOn.js
@@ -8,25 +8,32 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 
 // http://wiki.audacityteam.org/wiki/GVerb#Instant_reverb_settings
-var audioOptions = new AudioEffectOptions();
-
-// Square Meters
-audioOptions.maxRoomSize = 50;
-audioOptions.roomSize = 50;
-
-// Seconds
-audioOptions.reverbTime = 4;
-
-// Between 0 - 1
-audioOptions.damping = 0.50;
-audioOptions.inputBandwidth = 0.75;
-
-// dB
-audioOptions.earlyLevel = -22;
-audioOptions.tailLevel = -28;
-audioOptions.dryLevel = 0;
-audioOptions.wetLevel = 6;
+var audioOptions = new AudioEffectOptions({
+                                          // Square Meters
+                                          maxRoomSize: 50,
+                                          roomSize: 50,
+                                          
+                                          // Seconds
+                                          reverbTime: 4,
+                                          
+                                          // Between 0 - 1
+                                          damping: 0.50,
+                                          inputBandwidth: 0.75,
+                                          
+                                          // dB
+                                          earlyLevel: -22,
+                                          tailLevel: -28,
+                                          dryLevel: 0,
+                                          wetLevel: 6
+});
 
 AudioDevice.setReverbOptions(audioOptions);
 AudioDevice.setReverb(true);
 print("Reverb is now on with the updated options.");
+
+function scriptEnding() {
+    AudioDevice.setReverb(false);
+    print("Reberb is now off.");
+}
+
+Script.scriptEnding.connect(scriptEnding);
