54197b2c2304948add3bc17c259473f34284432c
Philip Rosedale <philip@highfidelity.io>
2014-10-24 17:10:46 -0700

    fix for spacing

--- examples/guidedTour.js
+++ examples/guidedTour.js
@@ -14,34 +14,34 @@ var LANDING_RANDOM = 0.2;
 
 function update(deltaTime) {
 
-    if (Math.random() < deltaTime) {
-        guide = AvatarList.avatarWithDisplayName(leaderName);
-        if (guide && !isGuide) {
-        	print("found a guide!");
-        	isGuide = true;
-        } else if (!isGuide) {
-        	print("Lost My Guide");
-        	isguide = false;
-        }
+  if (Math.random() < deltaTime) {
+    guide = AvatarList.avatarWithDisplayName(leaderName);
+    if (guide && !isGuide) {
+      print("found a guide!");
+      isGuide = true;
+    } else if (!isGuide) {
+      print("Lost My Guide");
+      isguide = false;
     }
+  }
 
-    if (guide) {
-    	//  Check whether guide has moved, update if so
-    	if (Vec3.length(lastGuidePosition) == 0.0) {
-    		lastGuidePosition = guide.position;
-    	} else {
-    		if (Vec3.length(Vec3.subtract(lastGuidePosition, guide.position)) > MIN_CHANGE) {
-    			var meToGuide = Vec3.multiply(Vec3.normalize(Vec3.subtract(guide.position, MyAvatar.position)), LANDING_DISTANCE);
-    			var newPosition = Vec3.subtract(guide.position, meToGuide);
-    			newPosition = Vec3.sum(newPosition, { 	x: Math.random() * LANDING_RANDOM - LANDING_RANDOM / 2.0, 
-    													y: 0, 
-    													z: Math.random() * LANDING_RANDOM - LANDING_RANDOM / 2.0 });
-    			MyAvatar.position = newPosition;
+  if (guide) {
+    //  Check whether guide has moved, update if so
+    if (Vec3.length(lastGuidePosition) == 0.0) {
+      lastGuidePosition = guide.position;
+    } else {
+      if (Vec3.length(Vec3.subtract(lastGuidePosition, guide.position)) > MIN_CHANGE) {
+        var meToGuide = Vec3.multiply(Vec3.normalize(Vec3.subtract(guide.position, MyAvatar.position)), LANDING_DISTANCE);
+        var newPosition = Vec3.subtract(guide.position, meToGuide);
+        newPosition = Vec3.sum(newPosition, {   x: Math.random() * LANDING_RANDOM - LANDING_RANDOM / 2.0, 
+                                                y: 0, 
+                                                z: Math.random() * LANDING_RANDOM - LANDING_RANDOM / 2.0 });
+        MyAvatar.position = newPosition;
 
-    			lastGuidePosition = guide.position;
-    			MyAvatar.orientation = guide.orientation;
-    		}
-    	}
+        lastGuidePosition = guide.position;
+        MyAvatar.orientation = guide.orientation;
+      }
     }
+  }
 }
 Script.update.connect(update);
