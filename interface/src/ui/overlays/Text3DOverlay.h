7e6d616f207fa34acb725f8915f1e98b1e8148e5
David Rowe <david@ctrlaltstudio.com>
2014-11-07 08:58:31 -0800

    Add text overlay textWidth() method
    Is accessible from JavaScript.

--- interface/src/ui/overlays/Text3DOverlay.h
+++ interface/src/ui/overlays/Text3DOverlay.h
@@ -48,6 +48,8 @@ public:
 
     virtual void setProperties(const QScriptValue& properties);
 
+    float textWidth(const QString& text);  // Meters
+
 private:
     void enableClipPlane(GLenum plane, float x, float y, float z, float w);
 
