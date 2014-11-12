04335862938ce81cd270ff2a344394307746fa8d
Stephen Birarda <commit@birarda.com>
2014-11-11 16:59:15 -0800

    switch lobby noise back to drone

--- examples/lobby.js
+++ examples/lobby.js
@@ -39,7 +39,7 @@ var ORB_SHIFT = { x: 0, y: -1.4, z: -0.8};
 
 var HELMET_ATTACHMENT_URL =  HIFI_PUBLIC_BUCKET + "models/attachments/IronManMaskOnly.fbx"
 
-var droneSound = new Sound(HIFI_PUBLIC_BUCKET + "sounds/Lobby/latin.raw")
+var droneSound = new Sound(HIFI_PUBLIC_BUCKET + "sounds/Lobby/drone.raw")
 var currentDrone;
 
 function reticlePosition() {
