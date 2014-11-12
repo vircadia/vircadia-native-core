d97d1119c780b88023d8627967ee3d9a8953fc6c
ZappoMan <bradh@konamoxt.com>
2014-11-12 12:25:02 -0800

    keep tree locked until after endScene

--- interface/src/entities/EntityTreeRenderer.cpp
+++ interface/src/entities/EntityTreeRenderer.cpp
@@ -258,37 +258,39 @@ void EntityTreeRenderer::render(RenderArgs::RenderMode renderMode) {
     if (dontRenderAsScene) {
         OctreeRenderer::render(renderMode);
     } else {
-        Model::startScene();
-        RenderArgs args = { this, _viewFrustum, getSizeScale(), getBoundaryLevelAdjust(), renderMode, 
-                                            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
         if (_tree) {
+            Model::startScene();
+            RenderArgs args = { this, _viewFrustum, getSizeScale(), getBoundaryLevelAdjust(), renderMode, 
+                                                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
             _tree->lockForRead();
             _tree->recurseTreeWithOperation(renderOperation, &args);
+
+            Model::RenderMode modelRenderMode = renderMode == RenderArgs::SHADOW_RENDER_MODE
+                                                ? Model::SHADOW_RENDER_MODE : Model::DEFAULT_RENDER_MODE;
+
+            // we must call endScene while we still have the tree locked so that no one deletes a model
+            // on us while rendering the scene    
+            Model::endScene(modelRenderMode, &args);
             _tree->unlock();
-        }
-
-        Model::RenderMode modelRenderMode = renderMode == RenderArgs::SHADOW_RENDER_MODE
-                                            ? Model::SHADOW_RENDER_MODE : Model::DEFAULT_RENDER_MODE;
-    
-        Model::endScene(modelRenderMode, &args);
         
-        // stats...
-        _meshesConsidered = args._meshesConsidered;
-        _meshesRendered = args._meshesRendered;
-        _meshesOutOfView = args._meshesOutOfView;
-        _meshesTooSmall = args._meshesTooSmall;
+            // stats...
+            _meshesConsidered = args._meshesConsidered;
+            _meshesRendered = args._meshesRendered;
+            _meshesOutOfView = args._meshesOutOfView;
+            _meshesTooSmall = args._meshesTooSmall;
 
-        _elementsTouched = args._elementsTouched;
-        _itemsRendered = args._itemsRendered;
-        _itemsOutOfView = args._itemsOutOfView;
-        _itemsTooSmall = args._itemsTooSmall;
+            _elementsTouched = args._elementsTouched;
+            _itemsRendered = args._itemsRendered;
+            _itemsOutOfView = args._itemsOutOfView;
+            _itemsTooSmall = args._itemsTooSmall;
 
-        _materialSwitches = args._materialSwitches;
-        _trianglesRendered = args._trianglesRendered;
-        _quadsRendered = args._quadsRendered;
+            _materialSwitches = args._materialSwitches;
+            _trianglesRendered = args._trianglesRendered;
+            _quadsRendered = args._quadsRendered;
 
-        _translucentMeshPartsRendered = args._translucentMeshPartsRendered;
-        _opaqueMeshPartsRendered = args._opaqueMeshPartsRendered;
+            _translucentMeshPartsRendered = args._translucentMeshPartsRendered;
+            _opaqueMeshPartsRendered = args._opaqueMeshPartsRendered;
+        }
     }
     deleteReleasedModels(); // seems like as good as any other place to do some memory cleanup
 }
