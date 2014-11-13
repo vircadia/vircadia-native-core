bada1d8e38d29d9099666557b30287627b2148e2
Atlante45 <clement.brisset@gmail.com>
2014-11-12 17:13:29 -0800

    Cleaner code + metadata

--- examples/playChess.js
+++ examples/playChess.js
@@ -8,89 +8,86 @@
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 
+var gamePosition = { x: 1.0, y: 1.0, z: 1.0 };
+var gameSize = 1.0;
+
 // Script namespace
 var extraDebug = extraDebug || true;
 var ChessGame = ChessGame || {
-  BOARD_SIZE: 8,
-  MAX_PLAYERS: 2,
-  ROWS: ['1', '2', '3', '4', '5', '6', '7', '8'],
-  COLUMNS: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+  BOARD: {
+    modelURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/Board.fbx",
+    dimensions: { x: 773.191, y: 20.010, z: 773.191 },
+    rotation: Quat.fromPitchYawRollDegrees(0, 90, 0),
+    numTiles: 10.0
+  },
+  KING: 0, QUEEN: 1, BISHOP: 2, KNIGHT: 3, ROOK: 4, PAWN: 5,
   
-  whitesTileColor: { red: 250, green: 195, blue: 135 },
-  blacksTileColor: { red: 190, green: 115, blue: 50 },
-
   board: null,
-  boardURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/Board.fbx",
-  boardDimensions: { x: 773.191, y: 20.010, z: 773.191 },
-  sizeFactor: 1.0 / 773.191,
-  pieces: {
-    all: new Array(),
-    king: {
-      whiteURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/King_White.fbx",
-      blackURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/King_Black.fbx",
-      dimensions: { x: 110.496, y: 306.713, z: 110.496 },
-      rotation: Quat.fromPitchYawRollDegrees(0, 90, 0)
-    },
-    queen: {
-      whiteURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/Queen_White.fbx",
-      blackURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/Queen_Black.fbx",
-      dimensions: { x: 110.496, y: 257.459, z: 110.496 },
-      rotation: Quat.fromPitchYawRollDegrees(0, 0, 0)
-    },
-    bishop: {
-      whiteURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/Bishop_White.fbx",
-      blackURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/Bishop_Black.fbx",
-      dimensions: { x: 102.002, y: 213.941, z: 102.002 },
-      rotation: Quat.fromPitchYawRollDegrees(0, 90, 0),
-      white: [],
-      black: []
-    },
-    knight: {
-      whiteURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/Knight_White.fbx",
-      blackURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/Knight_Black.fbx",
-      dimensions: { x: 95.602, y: 186.939, z: 95.602 },
-      rotation: Quat.fromPitchYawRollDegrees(0, 180, 0),
-      white: [],
-      black: []
-    },
-    rook: {
-      whiteURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/Rook_White.fbx",
-      blackURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/Rook_Black.fbx",
-      dimensions: { x: 101.024, y: 167.523, z: 101.024 },
-      rotation: Quat.fromPitchYawRollDegrees(0, 0, 0),
-      white: [],
-      black: []
-    },
-    pawn: {
-      whiteURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/Pawn_White.fbx",
-      blackURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/Pawn_Black.fbx",
-      dimensions: { x: 93.317, y: 138.747, z: 93.317 },
-      rotation: Quat.fromPitchYawRollDegrees(0, 0, 0),
-      white: [],
-      black: []
-    }
-  }
+  pieces: new Array()
 };
+
+ChessGame.getPieceInfo = function(type, isWhite) {
+  var info = {
+    modelURL: "https://s3.amazonaws.com/hifi-public/models/props/chess/",
+    dimensions: { x: 1.0, y: 1.0, z: 1.0 },
+    rotation: Quat.fromPitchYawRollDegrees(0, 0, 0)
+  }
+  
+  switch(type) {
+  case ChessGame.KING:
+    info.modelURL += "King";
+    info.dimensions = { x: 110.496, y: 306.713, z: 110.496 };
+    info.rotation = Quat.fromPitchYawRollDegrees(0, 90, 0);
+    break;
+  case ChessGame.QUEEN:
+    info.modelURL += "Queen";
+    info.dimensions = { x: 110.496, y: 257.459, z: 110.496 };
+    break;
+  case ChessGame.BISHOP:
+    info.modelURL += "Bishop";
+    info.dimensions = { x: 102.002, y: 213.941, z: 102.002 };
+    info.rotation = Quat.fromPitchYawRollDegrees(0, 90, 0);
+    break;
+  case ChessGame.KNIGHT:
+    info.modelURL += "Knight";
+    info.dimensions = { x: 95.602, y: 186.939, z: 95.602 };
+    info.rotation = Quat.fromPitchYawRollDegrees(0, 180, 0);
+    break;
+  case ChessGame.ROOK:
+    info.modelURL += "Rook";
+    info.dimensions = { x: 101.024, y: 167.523, z: 101.024 };
+    break;
+  case ChessGame.PAWN:
+    info.modelURL += "Pawn";
+    info.dimensions = { x: 93.317, y: 138.747, z: 93.317 };
+    break;
+  }
+  info.modelURL += ((isWhite) ? "_White" : "_Black") + ".fbx"
+  
+  return info;
+}
 
 
 // Board class
 ChessGame.Board = (function(position, size) {
-  this.tileSize = size / 10.0;
-  this.dimensions = Vec3.multiply(ChessGame.sizeFactor * size,
-                                 ChessGame.boardDimensions);
+  this.dimensions = Vec3.multiply(1.0 / ChessGame.BOARD.dimensions.x, ChessGame.BOARD.dimensions); // 1 by 1
+  this.dimensions = Vec3.multiply(size, this.dimensions); // size by size
   this.position = Vec3.sum(position, Vec3.multiply(0.5, this.dimensions));
+  this.tileSize = size / ChessGame.BOARD.numTiles;
 
   this.entity = null;
 });
-// Returns tile position
+// Returns the top center point of tile i,j
 ChessGame.Board.prototype.tilePosition = function(i, j) {
-  return { x: this.position.x + (i - 5.0 + 0.5) * this.tileSize,
-           y: this.position.y + this.dimensions.y / 2.0,
-           z: this.position.z + (j - 5.0 + 0.5) * this.tileSize };
+  return {
+    x: this.position.x + (i - ChessGame.BOARD.numTiles / 2.0 + 0.5) * this.tileSize,
+    y: this.position.y + this.dimensions.y / 2.0,
+    z: this.position.z + (j - ChessGame.BOARD.numTiles / 2.0 + 0.5) * this.tileSize
+  };
 }
 // Checks the color of the tile
-ChessGame.Board.prototype.isWhite = function(x, y) {
-  return (x + y) % 2 != 0;
+ChessGame.Board.prototype.isWhite = function(i, j) {
+  return (i + j) % 2 != 0;
 }
 // Spawns the board using entities
 ChessGame.Board.prototype.spawn = function() {
@@ -100,7 +97,7 @@ ChessGame.Board.prototype.spawn = function() {
   
   this.entity = Entities.addEntity({
     type: "Model",
-    modelURL: ChessGame.boardURL,
+    modelURL: ChessGame.BOARD.modelURL,
     position: this.position,
     dimensions: this.dimensions
   });
@@ -117,15 +114,15 @@ ChessGame.Board.prototype.cleanup = function() {
 }
 
 // Piece class
-ChessGame.Piece = (function(position, size, url, rotation) {
-  this.size = size;
+ChessGame.Piece = (function(position, dimensions, url, rotation) {
+  this.dimensions = dimensions;
   this.position = position;
-  this.position.y += this.size.y / 2.0;
+  this.position.y += this.dimensions.y / 2.0;
   this.entityProperties = {
     type: "Model",
     position: this.position,
     rotation: rotation,
-    dimensions: this.size,
+    dimensions: this.dimensions,
     modelURL: url,
     //script: "https://s3-us-west-1.amazonaws.com/highfidelity-dev/scripts/chessPiece.js"
     script: "file:/Users/clement/hifi/examples/entityScripts/chessPiece.js"
@@ -137,110 +134,93 @@ ChessGame.Piece.prototype.spawn = function() {
   this.entity = Entities.addEntity(this.entityProperties);
 }
 // Updates the metadata stored by the piece
-ChessGame.Piece.prototype.updateMetaData = function(metaDataString) {
-  Entities.editEntity(this.entity, { animationURL: metaDataString });
+ChessGame.Piece.prototype.updateMetadata = function(metadataString) {
+  Entities.editEntity(this.entity, { animationURL: metadataString });
 }
 // Cleans up the piece
 ChessGame.Piece.prototype.cleanup = function() {
   Entities.deleteEntity(this.entity);
 }
-ChessGame.makePiece = function(properties, i, j, isWhite) {
-  var url = (isWhite) ? properties.whiteURL : properties.blackURL;
-  var size = Vec3.multiply(0.5 * ChessGame.sizeFactor, properties.dimensions);
-  var rotation = (isWhite) ? properties.rotation
+ChessGame.makePiece = function(piece, i, j, isWhite) {
+  var info = ChessGame.getPieceInfo(piece, isWhite);
+  var url = info.modelURL; 
+  var size = Vec3.multiply(0.5 * (1.0 / ChessGame.BOARD.dimensions.x), info.dimensions);
+  var rotation = (isWhite) ? info.rotation
                            : Quat.multiply(Quat.fromPitchYawRollDegrees(0, 180, 0),
-                                           properties.rotation);
+                                           info.rotation);
   var position = ChessGame.board.tilePosition(i, j);
                 
   var piece = new ChessGame.Piece(position, size, url, rotation);
   piece.spawn();
-  ChessGame.pieces.all.push(piece);
+  ChessGame.pieces.push(piece);
   return piece;
 }
 
-
-// Player class
-ChessGame.Player = (function() {
-  
-});
-
-ChessGame.update = function() {
-  
-}
 ChessGame.buildMetaDataString = function() {
   var metadataObject = {
-    board: {
-      position: ChessGame.board.position,
-      size: ChessGame.board.size
-    }
+    gamePosition: gamePosition,
+    gameSize: gameSize
   };
-  
   return JSON.stringify(metadataObject);
 }
 
 ChessGame.buildBoard = function() {
   // Setup board
-  ChessGame.board = new ChessGame.Board({ x: 1, y: 1, z: 1 }, 1);
+  ChessGame.board = new ChessGame.Board(gamePosition, gameSize);
   ChessGame.board.spawn();
   
   // Setup white pieces
+  var isWhite = true;
+  var row = 1;
   // King
-  var piece = ChessGame.makePiece(ChessGame.pieces.king, 1, 4, true);
-  ChessGame.pieces.king.white = piece;
+  var piece =ChessGame.makePiece(ChessGame.KING, row, 5, isWhite);
   // Queen
-  piece = ChessGame.makePiece(ChessGame.pieces.queen, 1, 5, true);
-  ChessGame.pieces.queen.white = piece;
+  piece = ChessGame.makePiece(ChessGame.QUEEN, row, 4, isWhite);
   // Bishop
-  piece = ChessGame.makePiece(ChessGame.pieces.bishop, 1, 3, true);
-  ChessGame.pieces.bishop.white.push(piece);
-  piece = ChessGame.makePiece(ChessGame.pieces.bishop, 1, 6, true);
-  ChessGame.pieces.bishop.white.push(piece);
+  piece = ChessGame.makePiece(ChessGame.BISHOP, row, 3, isWhite);
+  piece = ChessGame.makePiece(ChessGame.BISHOP, row, 6, isWhite);
   // Knight
-  piece = ChessGame.makePiece(ChessGame.pieces.knight, 1, 2, true);
-  ChessGame.pieces.knight.white.push(piece);
-  piece = ChessGame.makePiece(ChessGame.pieces.knight, 1, 7, true);
-  ChessGame.pieces.knight.white.push(piece);
+  piece = ChessGame.makePiece(ChessGame.KNIGHT, row, 2, isWhite);
+  piece = ChessGame.makePiece(ChessGame.KNIGHT, row, 7, isWhite);
   // Rook
-  piece = ChessGame.makePiece(ChessGame.pieces.rook, 1, 1, true);
-  ChessGame.pieces.rook.white.push(piece);
-  piece = ChessGame.makePiece(ChessGame.pieces.rook, 1, 8, true);
-  ChessGame.pieces.rook.white.push(piece);
-  for(var j = 1; j <= ChessGame.BOARD_SIZE; j++) {
-    piece = ChessGame.makePiece(ChessGame.pieces.pawn, 2, j, true);
-    ChessGame.pieces.pawn.white.push(piece);
+  piece = ChessGame.makePiece(ChessGame.ROOK, row, 1, isWhite);
+  piece = ChessGame.makePiece(ChessGame.ROOK, row, 8, isWhite);
+  for(var j = 1; j <= 8; j++) {
+    piece = ChessGame.makePiece(ChessGame.PAWN, row + 1, j, isWhite);
   }
   
   // Setup black pieces
+  isWhite = false;
+  row = 8;
   // King
-  var piece = ChessGame.makePiece(ChessGame.pieces.king, 8, 4, false);
-  ChessGame.pieces.king.white = piece;
+  piece = ChessGame.makePiece(ChessGame.KING, row, 5, isWhite);
   // Queen
-  piece = ChessGame.makePiece(ChessGame.pieces.queen, 8, 5, false);
-  ChessGame.pieces.queen.white = piece;
+  piece = ChessGame.makePiece(ChessGame.QUEEN, row, 4, isWhite);
   // Bishop
-  piece = ChessGame.makePiece(ChessGame.pieces.bishop, 8, 3, false);
-  ChessGame.pieces.bishop.white.push(piece);
-  piece = ChessGame.makePiece(ChessGame.pieces.bishop, 8, 6, false);
-  ChessGame.pieces.bishop.white.push(piece);
+  piece = ChessGame.makePiece(ChessGame.BISHOP, row, 3, isWhite);
+  piece = ChessGame.makePiece(ChessGame.BISHOP, row, 6, isWhite);
   // Knight
-  piece = ChessGame.makePiece(ChessGame.pieces.knight, 8, 2, false);
-  ChessGame.pieces.knight.white.push(piece);
-  piece = ChessGame.makePiece(ChessGame.pieces.knight, 8, 7, false);
-  ChessGame.pieces.knight.white.push(piece);
+  piece = ChessGame.makePiece(ChessGame.KNIGHT, row, 2, isWhite);
+  piece = ChessGame.makePiece(ChessGame.KNIGHT, row, 7, isWhite);
   // Rook
-  piece = ChessGame.makePiece(ChessGame.pieces.rook, 8, 1, false);
-  ChessGame.pieces.rook.white.push(piece);
-  piece = ChessGame.makePiece(ChessGame.pieces.rook, 8, 8, false);
-  ChessGame.pieces.rook.white.push(piece);
-  for(var j = 1; j <= ChessGame.BOARD_SIZE; j++) {
-    piece = ChessGame.makePiece(ChessGame.pieces.pawn, 7, j, false);
-    ChessGame.pieces.pawn.black.push(piece);
+  piece = ChessGame.makePiece(ChessGame.ROOK, row, 1, isWhite);
+  piece = ChessGame.makePiece(ChessGame.ROOK, row, 8, isWhite);
+  for(var j = 1; j <= 8; j++) {
+    piece = ChessGame.makePiece(ChessGame.PAWN, row - 1, j, isWhite);
+  }
+  
+  var metadataString = ChessGame.buildMetadataString();
+  for (i in ChessGame.pieces) {
+    ChessGame.pieces[i].updateMetadata(metadataString);
   }
 }
 
 
 ChessGame.scriptStarting = function() {
   print("playChess.js started");
+  gamePosition = Vec3.sum(MyAvatar.position,
+                          Vec3.multiplyQbyV(MyAvatar.orientation,
+                                            { x: 0, y: 0, z: -1 }));
   ChessGame.buildBoard();
 }
 
@@ -250,15 +230,13 @@ ChessGame.scriptEnding = function() {
   delete ChessGame.board;
   
   // Cleaning up pieces
-  for(var i in ChessGame.pieces.all) {
-    ChessGame.pieces.all[i].cleanup();
+  for(var i in ChessGame.pieces) {
+    ChessGame.pieces[i].cleanup();
   }
   delete ChessGame.pieces;
   
   print("playChess.js finished");
 }
 
-
-Script.update.connect(ChessGame.update)
 Script.scriptEnding.connect(ChessGame.scriptEnding);
 ChessGame.scriptStarting();
