069f1be89d870c0011ae05a31db3457e019bd76b
Philip Rosedale <philip@highfidelity.io>
2014-11-20 22:48:03 -0600

    jump with number keys to preset places

---
+++ examples/hotPlaces.js
@@ -0,0 +1,29 @@
+//
+//  hotPlaces.js 
+//
+//  Press the number keys to jump to different places in the metaverse!
+//  
+//  Created by Philip Rosedale on November 20, 2014
+//  Copyright 2014 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+
+
+function keyPressEvent(event) {
+    // if our tools are off, then don't do anything
+    if (event.text == "1") {
+        Window.location = "hifi://starchamber";
+    } else if (event.text == "2") {
+    	Window.location = "hifi://apartment";
+    } else if (event.text == "3") {
+    	Window.location = "hifi://rivenglen";
+    } else if (event.text == "4") {
+    	Window.location = "hifi://sanfrancisco";
+    } else if (event.text == "5") {
+    	Window.location = "hifi://porto";
+    } else if (event.text == "6") {
+    	Window.location = "hifi://hoo";
+    } 
+}
+Controller.keyPressEvent.connect(keyPressEvent);
