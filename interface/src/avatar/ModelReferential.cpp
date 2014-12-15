bc78beb05fcea9ce536a0fabc5c88078cbb12439
ZappoMan <bradh@konamoxt.com>
2014-12-15 14:41:15 -0800

    windows build hackery

--- interface/src/avatar/ModelReferential.cpp
+++ interface/src/avatar/ModelReferential.cpp
@@ -9,6 +9,8 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
+#include <gpu/GPUConfig.h> // hack to get windows to build
+
 #include <AvatarData.h>
 
 #include <EntityTree.h>
