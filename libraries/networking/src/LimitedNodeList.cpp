587df1178dfc19dc31e261466bd54ffa138fe646
Stephen Birarda <commit@birarda.com>
2014-12-18 13:57:19 -0800

    use constant for MSEC to USEC conversion

--- libraries/networking/src/LimitedNodeList.cpp
+++ libraries/networking/src/LimitedNodeList.cpp
@@ -504,7 +504,7 @@ void LimitedNodeList::removeSilentNodes() {
         SharedNodePointer node = it->second;
         node->getMutex().lock();
         
-        if ((usecTimestampNow() - node->getLastHeardMicrostamp()) > (NODE_SILENCE_THRESHOLD_MSECS * 1000)) {
+        if ((usecTimestampNow() - node->getLastHeardMicrostamp()) > (NODE_SILENCE_THRESHOLD_MSECS * USECS_PER_MSEC)) {
             // call the NodeHash erase to get rid of this node
             it = _nodeHash.unsafe_erase(it);
             
