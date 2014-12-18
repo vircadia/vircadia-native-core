2884de1fe4f6493a888ce7601e7f97994fbe8b0d
Thijs Wenker <me@thoys.nl>
2014-12-18 01:18:05 +0100

    include fix

--- interface/src/devices/RealSense.cpp
+++ interface/src/devices/RealSense.cpp
@@ -9,6 +9,7 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 #include "RealSense.h"
+#include "MainWindow.h"
 #include "Menu.h"
 #include "SharedUtil.h"
 
