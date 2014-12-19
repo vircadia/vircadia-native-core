e6123ec4a7721c427f43728d951eb492c1c5d7c4
Atlante45 <clement.brisset@gmail.com>
2014-12-18 16:31:19 -0800

    Typo

--- domain-server/resources/web/assignment/placeholder.js
+++ domain-server/resources/web/assignment/placeholder.js
@@ -1,3 +1,3 @@
-// Here you can put a script that will be run by an Assignement Client (AC)
+// Here you can put a script that will be run by an assignment-client (AC)
 // For examples, please go to http://public.highfidelity.io/scripts
-// The directory named acScripts contains Assignement Clients specific scripts you can try.
+// The directory named acScripts contains assignment-client specific scripts you can try.
