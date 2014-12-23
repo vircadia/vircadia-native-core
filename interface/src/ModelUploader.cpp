2a54ef01ef2b50d4745206cd4f24be2ea41da3f7
Atlante45 <clement.brisset@gmail.com>
2014-12-23 15:06:22 -0800

    Set texture limit back to 1024

--- interface/src/ModelUploader.cpp
+++ interface/src/ModelUploader.cpp
@@ -57,7 +57,7 @@ static const QString SETTING_NAME = "LastModelUploadLocation";
 
 static const int BYTES_PER_MEGABYTES = 1024 * 1024;
 static const unsigned long MAX_SIZE = 50 * 1024 * BYTES_PER_MEGABYTES; // 50 GB (Virtually remove limit)
-static const int MAX_TEXTURE_SIZE = 4096;
+static const int MAX_TEXTURE_SIZE = 1024;
 static const int TIMEOUT = 1000;
 static const int MAX_CHECK = 30;
 
