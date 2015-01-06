51c175d160d4d9b0fce6ca9194131c059e6cf90c
Stephen Birarda <commit@birarda.com>
2015-01-06 15:30:38 -0800

    put input default back after row add

--- domain-server/resources/web/js/settings.js
+++ domain-server/resources/web/js/settings.js
@@ -364,7 +364,7 @@ function makeTableInputs(setting) {
   _.each(setting.columns, function(col) {
     html += "<td class='" + Settings.DATA_COL_CLASS + "'name='" + col.name + "'>\
              <input type='text' class='form-control' placeholder='" + (col.placeholder ? col.placeholder : "") + "'\
-             value='" + (col.default ? col.default : "") + "'>\
+             value='" + (col.default ? col.default : "") + "' data-default='" + (col.default ? col.default : "") + "'>\
              </td>"
   })
     
@@ -504,7 +504,7 @@ function addTableRow(add_glyphicon) {
   })
   
   input_clone.find('input').each(function(){
-    $(this).val('')
+    $(this).val($(this).attr('data-default'));
   });
   
   if (isArray) {
