6771252eab69c000631d82c48ff02a603eb0bb76
Thijs Wenker <me@thoys.nl>
2015-01-07 21:34:34 +0100

    seems like you need to offset the include from the script that uses it

--- examples/libraries/virtualKeyboard.js
+++ examples/libraries/virtualKeyboard.js
@@ -13,7 +13,7 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
-Script.include("globals.js");
+Script.include("../../libraries/globals.js");
 
 // experimental 3dmode
 THREE_D_MODE = false;
