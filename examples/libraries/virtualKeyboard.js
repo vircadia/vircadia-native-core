4fdcdf095c9af35f2f77e22340eb771dbe440860
Thijs Wenker <me@thoys.nl>
2015-01-07 21:27:14 +0100

    trying to fix url

--- examples/libraries/virtualKeyboard.js
+++ examples/libraries/virtualKeyboard.js
@@ -13,7 +13,7 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
-Script.include("libraries/globals.js");
+Script.include("globals.js");
 
 // experimental 3dmode
 THREE_D_MODE = false;
