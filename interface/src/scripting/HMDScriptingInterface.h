0f6ad4eba02c55447b7e70332e854d16a24687d1
Thijs Wenker <me@thoys.nl>
2015-01-14 00:07:15 +0100

    HMD.VRMode -> HMD.active , and hooked it to a function which isn't dependent on HMD type, since there will be more HMD's than Oculus supported in the near future.

--- interface/src/scripting/HMDScriptingInterface.h
+++ interface/src/scripting/HMDScriptingInterface.h
@@ -13,12 +13,11 @@
 #define hifi_HMDScriptingInterface_h
 
 #include "Application.h"
-#include "devices/OculusManager.h"
 
 class HMDScriptingInterface : public QObject {
     Q_OBJECT
     Q_PROPERTY(bool magnifier READ getMagnifier)
-    Q_PROPERTY(bool VRMode READ getVRMode)
+    Q_PROPERTY(bool active READ isHMDMode)
 public:
     static HMDScriptingInterface& getInstance();
 
@@ -28,7 +27,7 @@ public slots:
 private:
     HMDScriptingInterface() {};
     bool getMagnifier() const { return Application::getInstance()->getApplicationOverlay().hasMagnifier(); };
-    bool getVRMode() const { return OculusManager::isConnected(); }
+    bool isHMDMode() const { return Application::getInstance()->isHMDMode(); }
 
 };
 
