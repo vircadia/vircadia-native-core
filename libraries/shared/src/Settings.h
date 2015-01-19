33d0e1bad52169c8b01a1205c4abdf853549d936
Atlante45 <clement.brisset@gmail.com>
2015-01-18 18:08:20 -0800

    Added Settings class
    	Just a simple Wrapper for now

---
+++ libraries/shared/src/Settings.h
@@ -0,0 +1,25 @@
+//
+//  Settings.h
+//
+//
+//  Created by Clement on 1/18/15.
+//  Copyright 2015 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_Settings_h
+#define hifi_Settings_h
+
+#include <QSettings>
+
+// Just a simple wrapper for now
+class Settings : public QSettings {
+};
+
+namespace SettingsKey {
+
+}
+
+#endif // hifi_Settings_h
