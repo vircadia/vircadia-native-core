a645d784d8a66bb1e4298449916689a8f1ece228
Seth Alves <seth.alves@gmail.com>
2015-02-04 11:26:05 -0800

    uint32_t rather than uint, which mac doesn't like

--- interface/src/Audio.h
+++ interface/src/Audio.h
@@ -212,7 +212,7 @@ private:
     bool _outputStarveDetectionEnabled;
     quint64 _outputStarveDetectionStartTimeMsec;
     int _outputStarveDetectionCount;
-    uint _outputStarveDetectionPeriodMsec;
+    uint32_t _outputStarveDetectionPeriodMsec;
     int _outputStarveDetectionThreshold; // Maximum number of starves per _outputStarveDetectionPeriod before increasing buffer size
 
     
