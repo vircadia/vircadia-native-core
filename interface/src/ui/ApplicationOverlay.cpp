e2f942c02637ecf9cab12b1514e94d7c8717c72f
Triplelexx <mojolexx@gmail.com>
2015-02-11 06:11:38 +0000

    Coding standard fixes
    Corrected else format, removed additional comment to fit other
    declarations and break block.

--- interface/src/ui/ApplicationOverlay.cpp
+++ interface/src/ui/ApplicationOverlay.cpp
@@ -881,13 +881,12 @@ void ApplicationOverlay::renderAudioMeter() {
 
         audioLevel = AUDIO_GREEN_START;
     }
-    //   Draw Blue Quad
+
     if (audioLevel >= 0) {
         glm::vec4 quadColor;
         if (!isClipping) {
             quadColor = AUDIO_METER_BLUE;
-        }
-        else {
+        } else {
             quadColor = glm::vec4(1, 1, 1, 1);
         }
         // Draw Blue (low level) quad
