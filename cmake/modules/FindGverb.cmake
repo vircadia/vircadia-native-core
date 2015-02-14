9683ed92e321cdec5b7607a03f0bf08dd1753fd5
Stephen Birarda <commit@birarda.com>
2015-02-13 16:40:48 -0800

    use proper lib path on WIN32 for gverb

--- cmake/modules/FindGverb.cmake
+++ cmake/modules/FindGverb.cmake
@@ -19,7 +19,7 @@ include("${MACRO_DIR}/HifiLibrarySearchHints.cmake")
 hifi_library_search_hints("gverb")
 
 find_path(GVERB_INCLUDE_DIRS gverb.h PATH_SUFFIXES include HINTS ${GVERB_SEARCH_DIRS})
-find_path(GVERB_LIBRARIES gverb PATH_SUFFIXES lib HINTS ${GVERB_SEARCH_DIRS})
+find_library(GVERB_LIBRARIES gverb PATH_SUFFIXES lib HINTS ${GVERB_SEARCH_DIRS})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(GVERB DEFAULT_MSG GVERB_INCLUDE_DIRS GVERB_LIBRARIES)
