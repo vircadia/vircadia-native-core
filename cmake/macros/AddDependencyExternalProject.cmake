feaad377514864c1b5347b532566923a66d32173
Stephen Birarda <commit@birarda.com>
2015-02-16 09:38:53 -0800

    add a GET_PROJ_NAME var to skip external projects

--- cmake/macros/AddDependencyExternalProject.cmake
+++ cmake/macros/AddDependencyExternalProject.cmake
@@ -10,16 +10,23 @@
 # 
 
 macro(ADD_DEPENDENCY_EXTERNAL_PROJECT _PROJ_NAME)
-  if (NOT TARGET ${_PROJ_NAME})
+  
+  string(TOUPPER ${_PROJ_NAME} _PROJ_NAME_UPPER)
+  
+  if (NOT DEFINED GET_${_PROJ_NAME_UPPER} OR GET_${_PROJ_NAME_UPPER})
+    if (NOT TARGET ${_PROJ_NAME})
     
-    if (ANDROID)
-      set(_PROJ_BINARY_DIR ${EXTERNAL_PROJECT_DIR}/${_PROJ_NAME}/build/android)
-    else ()
-      set(_PROJ_BINARY_DIR ${EXTERNAL_PROJECT_DIR}/${_PROJ_NAME}/build)
+      if (ANDROID)
+        set(_PROJ_BINARY_DIR ${EXTERNAL_PROJECT_DIR}/${_PROJ_NAME}/build/android)
+      else ()
+        set(_PROJ_BINARY_DIR ${EXTERNAL_PROJECT_DIR}/${_PROJ_NAME}/build)
+      endif ()
+    
+      add_subdirectory(${EXTERNAL_PROJECT_DIR}/${_PROJ_NAME} ${_PROJ_BINARY_DIR})
     endif ()
+  
+    add_dependencies(${TARGET_NAME} ${_PROJ_NAME})
     
-    add_subdirectory(${EXTERNAL_PROJECT_DIR}/${_PROJ_NAME} ${_PROJ_BINARY_DIR})
   endif ()
   
-  add_dependencies(${TARGET_NAME} ${_PROJ_NAME})
 endmacro()
