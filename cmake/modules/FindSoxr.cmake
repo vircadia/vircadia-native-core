d0d4eaec868d1691cc897596764269accec1991a
Stephen Birarda <commit@birarda.com>
2015-02-17 15:44:03 -0800

    fix AddPathsToLibPaths foreach, soxr requirements

--- cmake/modules/FindSoxr.cmake
+++ cmake/modules/FindSoxr.cmake
@@ -25,11 +25,19 @@ find_path(SOXR_INCLUDE_DIRS soxr.h PATH_SUFFIXES include HINTS ${SOXR_SEARCH_DIR
 find_library(SOXR_LIBRARIES NAMES soxr PATH_SUFFIXES lib HINTS ${SOXR_SEARCH_DIRS})
 
 if (WIN32)
-  find_path(SOXR_DLL_PATH soxr.dll PATH_SUFFIXES bin HINTS ${SOXR_SEARCH_DIRS})
-  add_paths_to_lib_paths(${SOXR_DLL_PATH})
+  find_path(SOXR_DLL_PATH soxr.dll PATH_SUFFIXES bin HINTS ${SOXR_SEARCH_DIRS}) 
 endif()
 
+set(SOXR_REQUIREMENTS SOXR_INCLUDE_DIRS SOXR_LIBRARIES)
+if (WIN32)
+  list(APPEND SOXR_REQUIREMENTS SOXR_DLL_PATH)
+endif ()
+
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SOXR DEFAULT_MSG SOXR_INCLUDE_DIRS SOXR_LIBRARIES)
+find_package_handle_standard_args(SOXR DEFAULT_MSG ${SOXR_REQUIREMENTS})
+
+if (WIN32)
+  add_paths_to_lib_paths(${SOXR_DLL_PATH})
+endif ()
 
 mark_as_advanced(SOXR_INCLUDE_DIRS SOXR_LIBRARIES SOXR_SEARCH_DIRS)
