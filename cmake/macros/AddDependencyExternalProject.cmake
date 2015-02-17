d299f015d1f7e2e549cb14b869e34b5a4fe6a146
Stephen Birarda <commit@birarda.com>
2015-02-17 09:46:39 -0800

    use binary dir for externals as well

--- cmake/macros/AddDependencyExternalProject.cmake
+++ cmake/macros/AddDependencyExternalProject.cmake
@@ -14,15 +14,8 @@ macro(ADD_DEPENDENCY_EXTERNAL_PROJECT _PROJ_NAME)
   string(TOUPPER ${_PROJ_NAME} _PROJ_NAME_UPPER)
   
   if (NOT DEFINED GET_${_PROJ_NAME_UPPER} OR GET_${_PROJ_NAME_UPPER})
-    if (NOT TARGET ${_PROJ_NAME})
-    
-      if (ANDROID)
-        set(_PROJ_BINARY_DIR ${EXTERNAL_PROJECT_DIR}/${_PROJ_NAME}/build/android)
-      else ()
-        set(_PROJ_BINARY_DIR ${EXTERNAL_PROJECT_DIR}/${_PROJ_NAME}/build)
-      endif ()
-    
-      add_subdirectory(${EXTERNAL_PROJECT_DIR}/${_PROJ_NAME} ${_PROJ_BINARY_DIR})
+    if (NOT TARGET ${_PROJ_NAME})    
+      add_subdirectory(${EXTERNAL_PROJECT_DIR}/${_PROJ_NAME} ${CMAKE_BINARY_DIR}/externals/${_PROJ_NAME})
     endif ()
   
     add_dependencies(${TARGET_NAME} ${_PROJ_NAME})
