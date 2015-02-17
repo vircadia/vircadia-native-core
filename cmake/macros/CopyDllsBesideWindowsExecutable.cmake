18f293b59b727147acecd7fb1b6841b7c12bc9a5
Stephen Birarda <commit@birarda.com>
2015-02-17 14:53:22 -0800

    disable bundle fixup on Release build

--- cmake/macros/CopyDllsBesideWindowsExecutable.cmake
+++ cmake/macros/CopyDllsBesideWindowsExecutable.cmake
@@ -10,7 +10,7 @@
 # 
 
 macro(COPY_DLLS_BESIDE_WINDOWS_EXECUTABLE)
-  if (WIN32)
+  if (WIN32 AND NOT CMAKE_BUILD_TYPE STREQUAL "Release")
     # add a post-build command to copy DLLs beside the interface executable
     add_custom_command(
       TARGET ${TARGET_NAME}
