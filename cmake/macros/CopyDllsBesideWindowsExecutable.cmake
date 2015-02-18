1a0a4d0e208b8705dd1167063807dac68c4587e8
Stephen Birarda <commit@birarda.com>
2015-02-17 16:05:32 -0800

    use seperate_arguments to allow cmake path escape

--- cmake/macros/CopyDllsBesideWindowsExecutable.cmake
+++ cmake/macros/CopyDllsBesideWindowsExecutable.cmake
@@ -11,13 +11,14 @@
 
 macro(COPY_DLLS_BESIDE_WINDOWS_EXECUTABLE)
   if (WIN32 AND NOT CMAKE_BUILD_TYPE STREQUAL "Release")
+    seperate_arguments(LIB_PATHS_ARG WINDOWS_COMMAND ${LIB_PATHS})
     # add a post-build command to copy DLLs beside the interface executable
     add_custom_command(
       TARGET ${TARGET_NAME}
       POST_BUILD
       COMMAND ${CMAKE_COMMAND} 
         -DBUNDLE_EXECUTABLE=$<TARGET_FILE:${TARGET_NAME}> 
-        -DLIB_PATHS="${LIB_PATHS}"
+        -DLIB_PATHS=${LIB_PATHS_ARG}
         -P ${HIFI_CMAKE_DIR}/scripts/FixupBundlePostBuild.cmake
     )
   endif ()
