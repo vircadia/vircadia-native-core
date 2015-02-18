7920f7afa324e73bf40f7debb25aaf13711c6fe3
Stephen Birarda <commit@birarda.com>
2015-02-17 16:13:53 -0800

    spell things correctly so cmake knows what you want

--- cmake/macros/CopyDllsBesideWindowsExecutable.cmake
+++ cmake/macros/CopyDllsBesideWindowsExecutable.cmake
@@ -11,7 +11,7 @@
 
 macro(COPY_DLLS_BESIDE_WINDOWS_EXECUTABLE)
   if (WIN32 AND NOT CMAKE_BUILD_TYPE STREQUAL "Release")
-    seperate_arguments(LIB_PATHS_ARG WINDOWS_COMMAND ${LIB_PATHS})
+    separate_arguments(LIB_PATHS_ARG WINDOWS_COMMAND ${LIB_PATHS})
     # add a post-build command to copy DLLs beside the interface executable
     add_custom_command(
       TARGET ${TARGET_NAME}
