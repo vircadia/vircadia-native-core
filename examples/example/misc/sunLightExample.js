bc8d688cdea8dab4871523a66c6a123198b6ecc2
Sam Gateau <sam@highfidelity.io>
2015-02-26 12:04:33 -0800

    fixing tab maybe

--- examples/example/misc/sunLightExample.js
+++ examples/example/misc/sunLightExample.js
@@ -11,21 +11,21 @@
 var intensity = 1.0;
 var day = 0.0;
 var hour = 12.0;
-var longitude = 0.0;
-var latitude = -45.0;
+var longitude = -115.0;
+var latitude = -31.0;
 
 Scene.setDayTime(hour);
-Scene.setOriginLocation(longitude, latitude, 0.0);
+//Scene.setOriginLocation(longitude, latitude, 0.0);
 
 function ticktack() {
 	hour += 0.1;
-	//Scene.setSunIntensity(Math.cos(time));
-	if (hour > 24.0) {
-		hour = 0.0;
-		day++;
-		Scene.setYearTime(day);
-	}
-	Scene.setDayTime(hour);
+    //Scene.setSunIntensity(Math.cos(time));
+    if (hour > 24.0) {
+        hour = 0.0;
+        day++;
+        Scene.setYearTime(day);
+    }
+    Scene.setDayTime(hour);
 }
 
 Script.setInterval(ticktack, 41);
