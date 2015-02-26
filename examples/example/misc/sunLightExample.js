b4cb5ca9d1e455ec7425a950259bdbc7bb65c9f7
Sam Gateau <sam@highfidelity.io>
2015-02-25 19:09:18 -0800

    add an example script to control the sun light

---
+++ examples/example/misc/sunLightExample.js
@@ -0,0 +1,32 @@
+//
+//  SunLightExample.js
+//  examples
+//  Sam Gateau
+//  Copyright 2015 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+var intensity = 1.0;
+var day = 0.0;
+var hour = 12.0;
+var longitude = 0.0;
+var latitude = -45.0;
+
+Scene.setDayTime(hour);
+Scene.setOriginLocation(longitude, latitude, 0.0);
+
+function ticktack() {
+	hour += 0.1;
+	//Scene.setSunIntensity(Math.cos(time));
+	if (hour > 24.0) {
+		hour = 0.0;
+		day++;
+		Scene.setYearTime(day);
+	}
+	Scene.setDayTime(hour);
+
+}
+
+Script.setInterval(ticktack, 41);
