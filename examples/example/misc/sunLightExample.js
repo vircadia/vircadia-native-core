74f0d98fd6ff65ae26dfda6374edc03c2a1d5ec9
Sam Gateau <sam@highfidelity.io>
2015-02-26 17:44:04 -0800

    REfining the naming and introducing an orientation offset

--- examples/example/misc/sunLightExample.js
+++ examples/example/misc/sunLightExample.js
@@ -11,8 +11,8 @@
 var intensity = 1.0;
 var day = 0.0;
 var hour = 12.0;
-var longitude = -115.0;
-var latitude = -31.0;
+var longitude = 115.0;
+var latitude = 31.0;
 var stageOrientation = Quat.fromPitchYawRollDegrees(0.0, 180.0, 0.0);
 
 Scene.setStageDayTime(hour);
