60867cc9310e3615c042bfc384eda88f63b53ff7
Atlante45 <clement.brisset@gmail.com>
2015-03-10 11:48:31 +0100

    Added ModelSelector class

---
+++ interface/src/ModelSelector.h
@@ -0,0 +1,15 @@
+//
+//  ModelSelector.h
+//
+//
+//  Created by Clement on 3/10/15.
+//  Copyright 2015 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_ModelSelector_h
+#define hifi_ModelSelector_h
+
+#endif // hifi_ModelSelector_h
