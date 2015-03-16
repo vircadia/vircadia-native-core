2d4a95c63487ae7552f64192b15aed5a49804b61
Seth Alves <seth.alves@gmail.com>
2015-03-16 11:54:50 -0700

    move to-map recurse operator class to its own file

--- libraries/entities/src/EntityTree.cpp
+++ libraries/entities/src/EntityTree.cpp
@@ -21,6 +21,7 @@
 #include "MovingEntitiesOperator.h"
 #include "UpdateEntityOperator.h"
 #include "QVariantGLM.h"
+#include "RecurseOctreeToMapOperator.h"
 
 EntityTree::EntityTree(bool shouldReaverage) : 
     Octree(shouldReaverage), 
@@ -1042,73 +1043,10 @@ bool EntityTree::sendEntitiesOperation(OctreeElement* element, void* extraData) 
     return true;
 }
 
-
-class ToMapOperator : public RecurseOctreeOperator {
-public:
-    ToMapOperator(QVariantMap& map, OctreeElement *top, QScriptEngine *engine) :
-        RecurseOctreeOperator(),
-        _map(map),
-        _top(top),
-        _engine(engine)
-    {
-        // if some element "top" was given, only save information for that element and it's children.
-        if (_top) {
-            _withinTop = false;
-        } else {
-            // top was NULL, export entire tree.
-            _withinTop = true;
-        }
-    };
-    bool preRecursion(OctreeElement* element) {
-        if (element == _top) {
-            _withinTop = true;
-        }
-        return true;
-    }
-    bool postRecursion(OctreeElement* element) {
-
-        EntityTreeElement* entityTreeElement = static_cast<EntityTreeElement*>(element);
-        const QList<EntityItem*>& entities = entityTreeElement->getEntities();
-
-        QVariantList entitiesQList = qvariant_cast<QVariantList>(_map["Entities"]);
-
-        foreach (EntityItem* entityItem, entities) {
-            EntityItemProperties properties = entityItem->getProperties();
-
-            // XXX this is copied out of EntityScriptingInterface::getEntityProperties
-            // is it needed here?
-
-            // if (entityItem->getType() == EntityTypes::Model) {
-            //     const FBXGeometry* geometry = getGeometryForEntity(entityItem);
-            //     if (geometry) {
-            //         properties.setSittingPoints(geometry->sittingPoints);
-            //         Extents meshExtents = geometry->getUnscaledMeshExtents();
-            //         properties.setNaturalDimensions(meshExtents.maximum - meshExtents.minimum);
-            //     }
-            // }
-
-            QScriptValue qScriptValues =
-                EntityItemPropertiesToScriptValue(_engine, properties);
-
-            entitiesQList << qScriptValues.toVariant();
-        }
-        _map["Entities"] = entitiesQList;
-        if (element == _top) {
-            _withinTop = false;
-        }
-        return true;
-    }
- private:
-    QVariantMap& _map;
-    OctreeElement *_top;
-    QScriptEngine *_engine;
-    bool _withinTop;
-};
-
 bool EntityTree::writeToMap(QVariantMap& entityDescription, OctreeElement* element) {
     entityDescription["Entities"] = QVariantList();
     QScriptEngine scriptEngine;
-    ToMapOperator theOperator(entityDescription, element, &scriptEngine);
+    RecurseOctreeToMapOperator theOperator(entityDescription, element, &scriptEngine);
     recurseTreeWithOperator(&theOperator);
     return true;
 }
