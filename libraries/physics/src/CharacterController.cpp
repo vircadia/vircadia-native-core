674b2c97a28ec5d5000a6e597325d9cef152ea36
Andrew Meadows <andrew@highfidelity.io>
2015-03-25 08:53:33 -0700

    added comments to zlib license

--- libraries/physics/src/CharacterController.cpp
+++ libraries/physics/src/CharacterController.cpp
@@ -1,6 +1,7 @@
 /*
 Bullet Continuous Collision Detection and Physics Library
 Copyright (c) 2003-2008 Erwin Coumans  http://bulletphysics.com
+2015.03.25 -- modified by Andrew Meadows andrew@highfidelity.io
 
 This software is provided 'as-is', without any express or implied warranty.
 In no event will the authors be held liable for any damages arising from the use of this software.
