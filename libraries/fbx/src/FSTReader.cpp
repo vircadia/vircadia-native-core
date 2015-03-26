c26b499671091e638ed7f01788c6269af94a9862
Atlante45 <clement.brisset@gmail.com>
2015-03-26 15:58:18 +0100

    Add forgotten include

--- libraries/fbx/src/FSTReader.cpp
+++ libraries/fbx/src/FSTReader.cpp
@@ -9,6 +9,8 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
+#include <QBuffer>
+
 #include "FSTReader.h"
 
 QVariantHash parseMapping(QIODevice* device) {
