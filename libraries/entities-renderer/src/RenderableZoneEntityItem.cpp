287d625c5221e205a068bdf4b02924946ff75451
Atlante45 <clement.brisset@gmail.com>
2015-04-22 20:19:45 +0200

    Added RenderableZoneEntities class

---
+++ libraries/entities-renderer/src/RenderableZoneEntityItem.cpp
@@ -0,0 +1,50 @@
+//
+//  RenderableZoneEntityItem.cpp
+//
+//
+//  Created by Clement on 4/22/15.
+//  Copyright 2015 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#include "RenderableZoneEntityItem.h"
+
+#include <DependencyManager.h>
+#include <GeometryCache.h>
+
+EntityItem* RenderableZoneEntityItem::factory(const EntityItemID& entityID, const EntityItemProperties& properties) {
+    return new RenderableZoneEntityItem(entityID, properties);
+}
+
+bool RenderableZoneEntityItem::setProperties(const EntityItemProperties& properties) {
+    QString oldShapeURL = getCompoundShapeURL();
+    bool somethingChanged = ZoneEntityItem::setProperties(properties);
+    if (somethingChanged && oldShapeURL != getCompoundShapeURL()) {
+        _compoundShapeModel = DependencyManager::get<GeometryCache>()->getGeometry(getCompoundShapeURL(), QUrl(), true);
+    }
+    return somethingChanged;
+}
+
+int RenderableZoneEntityItem::readEntitySubclassDataFromBuffer(const unsigned char* data, int bytesLeftToRead,
+                                                                ReadBitstreamToTreeParams& args,
+                                                                EntityPropertyFlags& propertyFlags, bool overwriteLocalData) {
+    QString oldShapeURL = getCompoundShapeURL();
+    int bytesRead = ZoneEntityItem::readEntitySubclassDataFromBuffer(data, bytesLeftToRead,
+                                                                      args, propertyFlags, overwriteLocalData);
+    if (oldShapeURL != getCompoundShapeURL()) {
+        _compoundShapeModel = DependencyManager::get<GeometryCache>()->getGeometry(getCompoundShapeURL(), QUrl(), true);
+    }
+    return bytesRead;
+}
+
+bool RenderableZoneEntityItem::contains(const glm::vec3& point) const {
+    if (EntityItem::contains(point) && _compoundShapeModel) {
+        const FBXGeometry& geometry = _compoundShapeModel->getFBXGeometry();
+        
+        return geometry.convexHullContains(point);
+    }
+    
+    return false;
+}
