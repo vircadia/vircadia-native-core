0fbccf1d336e6bc0f9828385d43a1313ce648a3e
Sam Gateau <sam@highfidelity.io>
2015-05-06 12:22:19 -0700

    get ready for PR, SKY_DOME by default

--- libraries/model/src/model/Stage.h
+++ libraries/model/src/model/Stage.h
@@ -223,7 +223,7 @@ public:
     const SkyboxPointer& getSkybox() const { valid(); return _skybox; }
 
 protected:
-    BackgroundMode _backgroundMode = SKY_BOX;
+    BackgroundMode _backgroundMode = SKY_DOME;
 
     LightPointer _sunLight;
     AtmospherePointer _atmosphere;
