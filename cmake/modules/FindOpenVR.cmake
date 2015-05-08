5f12af75bdacdd9a77228053ec0ede87cb849276
Bradley Austin Davis <bdavis@saintandreas.org>
2015-05-08 11:11:46 -0700

    Tweaking to match master

--- cmake/modules/FindOpenVR.cmake
+++ cmake/modules/FindOpenVR.cmake
@@ -13,12 +13,9 @@
 #  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 # 
 
-
 if (NOT ANDROID)
-
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(OPENVR DEFAULT_MSG OPENVR_INCLUDE_DIRS OPENVR_LIBRARIES)
-
-endif (NOT ANDROID)
+endif()
 
 mark_as_advanced(OPENVR_INCLUDE_DIRS OPENVR_LIBRARIES OPENVR_SEARCH_DIRS)
