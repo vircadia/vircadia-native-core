9bfca7f54d656d5b00d5fa00669a6a907904e97e
Eric Levin <ericrius1@gmail.com>
2015-05-15 15:43:23 -0700

    'moved icons up so they don't interfere with dice icons and changed spanw icon from dice to puck icon' '

--- examples/example/games/airHockey.js
+++ examples/example/games/airHockey.js
@@ -75,7 +75,7 @@ var names = ['floor', 'table', 'paddle', 'edge', 'puck', 'hockeyLight'];
 
 var deleteButton = Overlays.addOverlay("image", {
   x: screenSize.x / 2 - BUTTON_SIZE,
-  y: screenSize.y - (BUTTON_SIZE + PADDING),
+  y: screenSize.y - (BUTTON_SIZE * 2 + PADDING),
   width: BUTTON_SIZE,
   height: BUTTON_SIZE,
   imageURL: HIFI_PUBLIC_BUCKET + "images/delete.png",
@@ -89,10 +89,10 @@ var deleteButton = Overlays.addOverlay("image", {
 
 var spawnButton = Overlays.addOverlay("image", {
   x: screenSize.x / 2 + PADDING,
-  y: screenSize.y - (BUTTON_SIZE + PADDING),
+  y: screenSize.y - (BUTTON_SIZE * 2 + PADDING),
   width: BUTTON_SIZE,
   height: BUTTON_SIZE,
-  imageURL: HIFI_PUBLIC_BUCKET + "images/die.png",
+  imageURL: HIFI_PUBLIC_BUCKET + "images/puck.png",
   color: {
     red: 255,
     green: 255,
