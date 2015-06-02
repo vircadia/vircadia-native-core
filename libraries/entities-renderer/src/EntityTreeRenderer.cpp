622a1e30688982863eb73ef1f5edc229c2f5db6f
Howard Stearns <howard@highfidelity.io>
2015-06-01 21:19:42 -0700

    Pitch-shift collision sounds, and make entities-renderer depend on soxr.

--- libraries/entities-renderer/src/EntityTreeRenderer.cpp
+++ libraries/entities-renderer/src/EntityTreeRenderer.cpp
@@ -27,6 +27,7 @@
 #include <ScriptEngine.h>
 #include <TextureCache.h>
 #include <SoundCache.h>
+#include <soxr.h>
 
 #include "EntityTreeRenderer.h"
 
@@ -1158,7 +1159,30 @@ void EntityTreeRenderer::playEntityCollisionSound(const QUuid& myNodeID, EntityT
     options.stereo = sound->isStereo();
     options.position = position;
     options.volume = volume;
-    AudioInjector* injector = new AudioInjector(sound.data(), options);
+
+    // Shift the pitch down by ln(1 + (size / COLLISION_SIZE_FOR_STANDARD_PITCH)) / ln(2)
+    const float COLLISION_SIZE_FOR_STANDARD_PITCH = 0.2f;
+    QByteArray samples = sound->getByteArray();
+    soxr_io_spec_t spec = soxr_io_spec(SOXR_INT16_I, SOXR_INT16_I);
+    soxr_quality_spec_t qualitySpec = soxr_quality_spec(SOXR_MQ, 0);
+    const int channelCount = sound->isStereo() ? 2 : 1;
+    const float factor = log(1.0f + (entity->getMaximumAACube().getLargestDimension() / COLLISION_SIZE_FOR_STANDARD_PITCH)) / log(2);
+    const int HIFI_SAMPLE_RATE = 24000; //AudioConstants::SAMPLE_RATE
+    const int resampledRate = HIFI_SAMPLE_RATE * factor;
+    const int nInputSamples = samples.size() / sizeof(int16_t);
+    const int nOutputSamples = nInputSamples * factor;
+    QByteArray resampled(nOutputSamples * sizeof(int16_t), '\0');
+    const int16_t* receivedSamples = reinterpret_cast<const int16_t*>(samples.data());
+    soxr_error_t soxError = soxr_oneshot(HIFI_SAMPLE_RATE, resampledRate, channelCount,
+                                         receivedSamples, nInputSamples, NULL,
+                                         reinterpret_cast<int16_t*>(resampled.data()), nOutputSamples, NULL,
+                                         &spec, &qualitySpec, 0);
+    if (soxError) {
+        qCDebug(entitiesrenderer) << "Unable to resample" << collisionSoundURL << "from" << nInputSamples << "@" << HIFI_SAMPLE_RATE << "to" << nOutputSamples << "@" << resampledRate;
+        resampled = samples;
+    }
+
+    AudioInjector* injector = new AudioInjector(resampled, options);
     injector->setLocalAudioInterface(_localAudioInterface);
     injector->triggerDeleteAfterFinish();
     QThread* injectorThread = new QThread();
