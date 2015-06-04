5e15c14539947639f1c2e96b73b4c56cfc1b324f
Eric Levin <ericrius1@gmail.com>
2015-06-04 16:45:32 -0700

    added sanitization for invalid line points data. Fixed bug where occasionally wrong view frustum would be sent to script"

--- interface/src/Application.cpp
+++ interface/src/Application.cpp
@@ -3093,8 +3093,11 @@ PickRay Application::computePickRay(float x, float y) const {
     if (isHMDMode()) {
         getApplicationOverlay().computeHmdPickRay(glm::vec2(x, y), result.origin, result.direction);
     } else {
-        auto frustum = activeRenderingThread ? getDisplayViewFrustum() : getViewFrustum();
-        frustum->computePickRay(x, y, result.origin, result.direction);
+        if (QThread::currentThread() == activeRenderingThread) {
+            getDisplayViewFrustum()->computePickRay(x, y, result.origin, result.direction);
+        } else {
+            getViewFrustum()->computePickRay(x, y, result.origin, result.direction);
+        }
     }
     return result;
 }
