362d9a24817577ddae17d51147eb64717eb449c5
ericrius1 <ericrius1@gmail.com>
2015-06-22 10:35:00 -0700

    syncing brush sphere with line color

--- examples/paint.js
+++ examples/paint.js
@@ -83,7 +83,7 @@ function MousePaint() {
     linePosition = position;
     line = Entities.addEntity({
       position: position,
-      type: "Line",
+      type: "Quad",
       color: currentColor,
       dimensions: {
         x: LINE_DIMENSIONS,
---
+++ libraries/entities/src/QuadEntityItem.h
@@ -0,0 +1,14 @@
+//
+//  QuadEntityItem.h
+//  hifi
+//
+//  Created by eric levin on 6/22/15.
+//
+//
+
+#ifndef __hifi__QuadEntityItem__
+#define __hifi__QuadEntityItem__
+
+#include <stdio.h>
+
+#endif /* defined(__hifi__QuadEntityItem__) */
