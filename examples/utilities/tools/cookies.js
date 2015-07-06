8b4398cd5a626067346a96e8aded574b67489653
bwent <bwent@bowdoin.edu>
2015-07-06 15:03:21 -0700

    Update cookies.js

--- examples/utilities/tools/cookies.js
+++ examples/utilities/tools/cookies.js
@@ -8,7 +8,7 @@
 //  
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
-
+//
 // The Slider class
 Slider = function(x,y,width,thumbSize) {
     this.background = Overlays.addOverlay("text", {
