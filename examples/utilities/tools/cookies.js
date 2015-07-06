5d91f1be834ce46555a1831847623df0e5df7fa0
bwent <bwent@bowdoin.edu>
2015-07-06 13:26:16 -0700

    Removed header comments

--- examples/utilities/tools/cookies.js
+++ examples/utilities/tools/cookies.js
@@ -7,8 +7,6 @@
 //  Created by Sam Gateau, 4/1/2015
 //  A simple ui panel that present a list of porperties and the proper widget to edit it
 //  
-//  Modified by Bridget Went, June 2015
-//  Fixed checkBox class functionality, enabled mouseDoublePressEvent to reset panel items
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
