1dce5f80cc68501ea8c72f99ce4450e2a856dfc2
Niraj Venkat <venkatn93@gmail.com>
2015-07-09 16:17:58 -0700

    Entities are now added/removed from rendering based on Scene.shouldRenderEntities

--- interface/src/Application.cpp
+++ interface/src/Application.cpp
@@ -1014,7 +1014,6 @@ void Application::paintGL() {
 
 void Application::runTests() {
     runTimingTests();
-    runUnitTests();
 }
 
 void Application::audioMuteToggled() {
@@ -1183,6 +1182,7 @@ bool Application::event(QEvent* event) {
 }
 
 bool Application::eventFilter(QObject* object, QEvent* event) {
+
     if (event->type() == QEvent::ShortcutOverride) {
         if (DependencyManager::get<OffscreenUi>()->shouldSwallowShortcut(event)) {
             event->accept();
@@ -1787,7 +1787,6 @@ void Application::checkFPS() {
 }
 
 void Application::idle() {
-    PROFILE_RANGE(__FUNCTION__);
     static SimpleAverage<float> interIdleDurations;
     static uint64_t lastIdleEnd{ 0 };
 
@@ -3387,6 +3386,14 @@ void Application::displaySide(RenderArgs* renderArgs, Camera& theCamera, bool se
     // load the view frustum
     loadViewFrustum(theCamera, _displayViewFrustum);
 
+    // flip x if in mirror mode (also requires reversing winding order for backface culling)
+    if (theCamera.getMode() == CAMERA_MODE_MIRROR) {
+        //glScalef(-1.0f, 1.0f, 1.0f);
+        //glFrontFace(GL_CW);
+    } else {
+        glFrontFace(GL_CCW);
+    }
+
     // transform view according to theCamera
     // could be myCamera (if in normal mode)
     // or could be viewFrustumOffsetCamera if in offset mode
@@ -3404,6 +3411,9 @@ void Application::displaySide(RenderArgs* renderArgs, Camera& theCamera, bool se
     Transform viewTransform;
     viewTransform.setTranslation(theCamera.getPosition());
     viewTransform.setRotation(rotation);
+    if (theCamera.getMode() == CAMERA_MODE_MIRROR) {
+//         viewTransform.setScale(Transform::Vec3(-1.0f, 1.0f, 1.0f));
+    }
     if (renderArgs->_renderSide != RenderArgs::MONO) {
         glm::mat4 invView = glm::inverse(_untranslatedViewMatrix);
 
@@ -3458,9 +3468,9 @@ void Application::displaySide(RenderArgs* renderArgs, Camera& theCamera, bool se
    // Assuming nothing get's rendered through that
 
     if (!selfAvatarOnly) {
-
+        auto var = DependencyManager::get<SceneScriptingInterface>()->shouldRenderEntities();
         // render models...
-        if (DependencyManager::get<SceneScriptingInterface>()->shouldRenderEntities()) {
+        //if (DependencyManager::get<SceneScriptingInterface>()->shouldRenderEntities()) {
             PerformanceTimer perfTimer("entities");
             PerformanceWarning warn(Menu::getInstance()->isOptionChecked(MenuOption::PipelineWarnings),
                 "Application::displaySide() ... entities...");
@@ -3476,7 +3486,7 @@ void Application::displaySide(RenderArgs* renderArgs, Camera& theCamera, bool se
             }
             renderArgs->_debugFlags = renderDebugFlags;
             _entities.render(renderArgs);
-        }
+        //}
 
         // render the ambient occlusion effect if enabled
         if (Menu::getInstance()->isOptionChecked(MenuOption::AmbientOcclusion)) {
