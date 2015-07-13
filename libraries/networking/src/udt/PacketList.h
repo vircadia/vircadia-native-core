4a22f18add3fc8d2cb33c15c124dab1ce453c00a
Atlante45 <clement.brisset@gmail.com>
2015-07-13 15:25:25 -0700

    Setup udt namespace/move relevant files

---
+++ libraries/networking/src/udt/PacketList.h
@@ -0,0 +1,15 @@
+//
+//  PacketList.h
+//
+//
+//  Created by Clement on 7/13/15.
+//  Copyright 2015 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_PacketList_h
+#define hifi_PacketList_h
+
+#endif // hifi_PacketList_h
