5312993e56ea34ebfc555f8ebccd9f6d1537acf4
Howard Stearns <howard@highfidelity.io>
2015-07-15 15:30:34 -0700

    Hmm, last was missing some emptiness.

--- libraries/animation/src/Rig.h
+++ libraries/animation/src/Rig.h
@@ -1,14 +1,23 @@
 //
 //  Rig.h
-//  hifi
+//  libraries/script-engine/src/
 //
-//  Created by Howard Stearns on 7/15/15.
+//  Produces animation data and hip placement for the current timestamp.
 //
+//  Created by Howard Stearns, Seth Alves, Anthony Thibault, Andrew Meadows on 7/15/15.
+//  Copyright (c) 2015 High Fidelity, Inc. All rights reserved.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
 #ifndef __hifi__Rig__
 #define __hifi__Rig__
 
-#include <stdio.h>
+#include <QObject>
+
+class AnimationObject : public QObject {
+
+};
 
 #endif /* defined(__hifi__Rig__) */
