849b584596c095e25c56c6d49fea90302a8cf939
ZappoMan <bradh@konamoxt.com>
2015-07-15 13:16:38 -0700

    more  GlowEctomy

--- interface/src/Menu.cpp
+++ interface/src/Menu.cpp
@@ -9,6 +9,9 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
+// include this before QGLWidget, which includes an earlier version of OpenGL
+#include "InterfaceConfig.h"
+
 #include <QFileDialog>
 #include <QMenuBar>
 #include <QShortcut>
