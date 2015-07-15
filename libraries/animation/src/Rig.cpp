bbb5f832a2abcd79e9989d3bb39e5fd3b0890c47
Howard Stearns <howard@highfidelity.io>
2015-07-15 15:11:16 -0700

    Empty Rig.

---
+++ libraries/animation/src/Rig.cpp
@@ -0,0 +1,9 @@
+//
+//  Rig.cpp
+//  hifi
+//
+//  Created by Howard Stearns on 7/15/15.
+//
+//
+
+#include "Rig.h"
---
+++ libraries/animation/src/Rig.h
@@ -0,0 +1,14 @@
+//
+//  Rig.h
+//  hifi
+//
+//  Created by Howard Stearns on 7/15/15.
+//
+//
+
+#ifndef __hifi__Rig__
+#define __hifi__Rig__
+
+#include <stdio.h>
+
+#endif /* defined(__hifi__Rig__) */
