09430b6596107f9fbdf7dfd76ed6851dcc2411a7
ZappoMan <bradh@konamoxt.com>
2015-07-16 16:27:41 -0700

    hacking on more naked GL

--- interface/src/ui/ApplicationOverlay.cpp
+++ interface/src/ui/ApplicationOverlay.cpp
@@ -90,8 +90,8 @@ void ApplicationOverlay::renderOverlay(RenderArgs* renderArgs) {
     glm::vec4 color { 0.0f, 0.0f, 0.0f, 0.0f };
     float depth = 1.0f;
     int stencil = 0;
-    //batch.clearFramebuffer(gpu::Framebuffer::BUFFER_COLORS | gpu::Framebuffer::BUFFER_DEPTH, color, depth, stencil);
-    batch.clearColorFramebuffer(_overlayFramebuffer->getBufferMask(), glm::vec4(0.0f, 0.0f, 0.0f, 0.0f));
+    batch.clearFramebuffer(gpu::Framebuffer::BUFFER_COLORS | gpu::Framebuffer::BUFFER_DEPTH, color, depth, stencil);
+    //batch.clearColorFramebuffer(_overlayFramebuffer->getBufferMask(), glm::vec4(0.0f, 0.0f, 0.0f, 0.0f));
 
     int width = _overlayFramebuffer ? _overlayFramebuffer->getWidth() : 0;
     int height = _overlayFramebuffer ? _overlayFramebuffer->getHeight() : 0;
