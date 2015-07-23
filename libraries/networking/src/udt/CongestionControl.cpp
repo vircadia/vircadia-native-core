9a51aef712e556fa9ccda96ec2c6272a84ae2476
Atlante45 <clement.brisset@gmail.com>
2015-07-23 15:02:34 -0700

    Added congestion control base classes

---
+++ libraries/networking/src/udt/CongestionControl.cpp
@@ -0,0 +1,148 @@
+//
+//  CongestionControl.cpp
+//
+//
+//  Created by Clement on 7/23/15.
+//  Copyright 2015 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#include "CongestionControl.h"
+
+#include <SharedUtil.h>
+
+namespace udt {
+
+void UdtCC::init() {
+    _rcInterval = _synInterval;
+    _lastRCTime = usecTimestampNow();
+    setAckTimer(_rcInterval);
+    
+    _lastAck = _sendCurrSeqNum;
+    _lastDecSeq = SeqNum::MAX;
+    
+    _congestionWindowSize = 16.0;
+    _packetSendPeriod = 1.0;
+}
+
+void UdtCC::onACK(SeqNum ackNum) {
+    int64_t B = 0;
+    double inc = 0;
+    // Note: 1/24/2012
+    // The minimum increase parameter is increased from "1.0 / _mss" to 0.01
+    // because the original was too small and caused sending rate to stay at low level
+    // for long time.
+    const double min_inc = 0.01;
+    
+    uint64_t currtime = usecTimestampNow();
+    if (currtime - _lastRCTime < (uint64_t)_rcInterval) {
+        return;
+    }
+    
+    _lastRCTime = currtime;
+    
+    if (_slowStart) {
+        _congestionWindowSize += seqlen(_lastAck, ackNum);
+        _lastAck = ackNum;
+        
+        if (_congestionWindowSize > _maxCongestionWindowSize) {
+            _slowStart = false;
+            if (_recvieveRate > 0) {
+                _packetSendPeriod = 1000000.0 / _recvieveRate;
+            } else {
+                _packetSendPeriod = (_rtt + _rcInterval) / _congestionWindowSize;
+            }
+        }
+    } else {
+        _congestionWindowSize = _recvieveRate / 1000000.0 * (_rtt + _rcInterval) + 16;
+    }
+    
+    // During Slow Start, no rate increase
+    if (_slowStart) {
+        return;
+    }
+    
+    if (_loss) {
+        _loss = false;
+        return;
+    }
+    
+    B = (int64_t)(_bandwidth - 1000000.0 / _packetSendPeriod);
+    if ((_packetSendPeriod > _lastDecPeriod) && ((_bandwidth / 9) < B)) {
+        B = _bandwidth / 9;
+    }
+    if (B <= 0) {
+        inc = min_inc;
+    } else {
+        // inc = max(10 ^ ceil(log10( B * MSS * 8 ) * Beta / MSS, 1/MSS)
+        // Beta = 1.5 * 10^(-6)
+        
+        inc = pow(10.0, ceil(log10(B * _mss * 8.0))) * 0.0000015 / _mss;
+        
+        if (inc < min_inc) {
+            inc = min_inc;
+        }
+    }
+    
+    _packetSendPeriod = (_packetSendPeriod * _rcInterval) / (_packetSendPeriod * inc + _rcInterval);
+}
+
+void UdtCC::onLoss(const std::vector<SeqNum>& losslist) {
+    //Slow Start stopped, if it hasn't yet
+    if (_slowStart) {
+        _slowStart = false;
+        if (_recvieveRate > 0) {
+            // Set the sending rate to the receiving rate.
+            _packetSendPeriod = 1000000.0 / _recvieveRate;
+            return;
+        }
+        // If no receiving rate is observed, we have to compute the sending
+        // rate according to the current window size, and decrease it
+        // using the method below.
+        _packetSendPeriod = _congestionWindowSize / (_rtt + _rcInterval);
+    }
+    
+    _loss = true;
+    
+    if (seqcmp(losslist[0], _lastDecSeq) > 0) {
+        _lastDecPeriod = _packetSendPeriod;
+        _packetSendPeriod = ceil(_packetSendPeriod * 1.125);
+        
+        _avgNAKNum = (int)ceil(_avgNAKNum * 0.875 + _nakCount * 0.125);
+        _nakCount = 1;
+        _decCount = 1;
+        
+        _lastDecSeq = _sendCurrSeqNum;
+        
+        // remove global synchronization using randomization
+        srand((uint32_t)_lastDecSeq);
+        _decRandom = (int)ceil(_avgNAKNum * (double(rand()) / RAND_MAX));
+        if (_decRandom < 1)
+            _decRandom = 1;
+    } else if ((_decCount ++ < 5) && (0 == (++ _nakCount % _decRandom))) {
+        // 0.875^5 = 0.51, rate should not be decreased by more than half within a congestion period
+        _packetSendPeriod = ceil(_packetSendPeriod * 1.125);
+        _lastDecSeq = _sendCurrSeqNum;
+    }
+}
+
+void UdtCC::onTimeout() {
+    if (_slowStart) {
+        _slowStart = false;
+        if (_recvieveRate > 0) {
+            _packetSendPeriod = 1000000.0 / _recvieveRate;
+        } else {
+            _packetSendPeriod = _congestionWindowSize / (_rtt + _rcInterval);
+        }
+    } else {
+        /*
+         _lastDecPeriod = _packetSendPeriod;
+         _packetSendPeriod = ceil(_packetSendPeriod * 2);
+         _lastDecSeq = _lastAck;
+         */
+    }
+}
+
+}
---
+++ libraries/networking/src/udt/CongestionControl.h
@@ -0,0 +1,115 @@
+//
+//  CongestionControl.h
+//
+//
+//  Created by Clement on 7/23/15.
+//  Copyright 2015 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_CongestionControl_h
+#define hifi_CongestionControl_h
+
+#include <vector>
+
+#include "SeqNum.h"
+
+namespace udt {
+
+class Packet;
+
+class CongestionControl {
+public:
+    static const int32_t DEFAULT_SYN_INTERVAL = 10000; // 10 ms
+
+    CongestionControl() {}
+    virtual ~CongestionControl() {}
+
+    virtual void init() {}
+    virtual void close() {}
+    virtual void onAck(SeqNum ackNum) {}
+    virtual void onLoss(const std::vector<SeqNum>& lossList) {}
+    virtual void onPacketSent(const Packet& packet) {}
+    virtual void onPacketReceived(const Packet& packet) {}
+    
+protected:
+    void setAckTimer(int syn) { _ackPeriod = (syn > _synInterval) ? _synInterval : syn; }
+    void setAckInterval(int interval) { _ackInterval = interval; }
+    void setRto(int rto) { _userDefinedRto = true; _rto = rto; }
+    
+    int32_t _synInterval = DEFAULT_SYN_INTERVAL; // UDT constant parameter, SYN
+    
+    double _packetSendPeriod = 1.0; // Packet sending period, in microseconds
+    double _congestionWindowSize = 16.0; // Congestion window size, in packets
+    
+    int _bandwidth = 0; // estimated bandwidth, packets per second
+    double _maxCongestionWindowSize = 0.0; // maximum cwnd size, in packets
+    
+    int _mss = 0; // Maximum Packet Size, including all packet headers
+    SeqNum _sendCurrSeqNum; // current maximum seq num sent out
+    int _recvieveRate = 0; // packet arrive rate at receiver side, packets per second
+    int _rtt = 0; // current estimated RTT, microsecond
+    
+private:
+    CongestionControl(const CongestionControl& other) = delete;
+    CongestionControl& operator=(const CongestionControl& other) = delete;
+    
+    void setMss(int mss) { _mss = mss; }
+    void setMaxCongestionWindowSize(int window) { _maxCongestionWindowSize = window; }
+    void setBandwidth(int bandwidth) { _bandwidth = bandwidth; }
+    void setSndCurrSeqNum(SeqNum seqNum) { _sendCurrSeqNum = seqNum; }
+    void setRcvRate(int rate) { _recvieveRate = rate; }
+    void setRtt(int rtt) { _rtt = rtt; }
+    
+    int _ackPeriod = 0; // Periodical timer to send an ACK, in milliseconds
+    int _ackInterval = 0; // How many packets to send one ACK, in packets
+    
+    bool _userDefinedRto = false; // if the RTO value is defined by users
+    int _rto = -1; // RTO value, microseconds
+};
+    
+    
+class CongestionControlVirtualFactory {
+public:
+    virtual ~CongestionControlVirtualFactory() {}
+    
+    virtual std::unique_ptr<CongestionControl> create() = 0;
+};
+
+template <class T> class CongestionControlFactory: public CongestionControlVirtualFactory
+{
+public:
+    virtual ~CongestionControlFactory() {}
+    
+    virtual std::unique_ptr<CongestionControl> create() { return std::unique_ptr<T>(new T()); }
+};
+
+class UdtCC: public CongestionControl {
+public:
+    UdtCC() {}
+    
+public:
+    virtual void init();
+    virtual void onACK(SeqNum ackNum);
+    virtual void onLoss(const std::vector<SeqNum>& lossList);
+    virtual void onTimeout();
+    
+private:
+    int _rcInterval = 0;	// UDT Rate control interval
+    uint64_t _lastRCTime = 0; // last rate increase time
+    bool _slowStart = true;	// if in slow start phase
+    SeqNum _lastAck; // last ACKed seq num
+    bool _loss = false;	// if loss happened since last rate increase
+    SeqNum _lastDecSeq; // max pkt seq num sent out when last decrease happened
+    double _lastDecPeriod = 1; // value of pktsndperiod when last decrease happened
+    int _nakCount = 0; // NAK counter
+    int _decRandom = 1; // random threshold on decrease by number of loss events
+    int _avgNAKNum = 0; // average number of NAKs per congestion
+    int _decCount = 0; // number of decreases in a congestion epoch
+};
+    
+}
+
+#endif // hifi_CongestionControl_h
