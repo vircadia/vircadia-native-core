198b6cfa887b8c5127ab049d6ac91f320c54c7f5
Ryan Huffman <ryanhuffman@gmail.com>
2015-07-23 06:17:08 -0700

    Fix ray picking working intermittently
    Ray picking relies on the Application's viewFrustum which was being set
    to both the main camera and mirror camera for their respective pass
    while rendering. This makes sure the viewFrustum only ever holds the
        main camera. Application::_displayViewFrustum still gets updated to
        the current camera while rendering.

--- interface/src/Application.cpp
+++ interface/src/Application.cpp
@@ -3395,9 +3395,6 @@ void Application::renderRearViewMirror(RenderArgs* renderArgs, const QRect& regi
         renderArgs->_context->render(batch);
     }
 
-    bool updateViewFrustum = false;
-    loadViewFrustum(_mirrorCamera, _viewFrustum);
-
     // render rear mirror view
     displaySide(renderArgs, _mirrorCamera, true, billboard);
 
