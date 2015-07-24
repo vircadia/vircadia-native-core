08a1c6c5ab83d612562c0019d0c7c54b68f8b242
Bradley Austin Davis <bdavis@saintandreas.org>
2015-07-24 17:33:41 -0400

    Removing dead code

--- libraries/display-plugins/src/display-plugins/oculus/Oculus_0_5_DisplayPlugin.h
+++ libraries/display-plugins/src/display-plugins/oculus/Oculus_0_5_DisplayPlugin.h
@@ -40,24 +40,6 @@ protected:
 
 private:
     ovrTexture _eyeTextures[2];
-#if RIFT_SDK_DISTORTION
-#else
-    ovrVector2f     _scaleAndOffset[2][2];
-    ProgramPtr      _distortProgram;
-    BufferPtr       _eyeIndexBuffers[2];
-    BufferPtr       _eyeVertexBuffers[2];
-    GLuint          _indexCount[2];
-
-    GLint          _uniformScale{ -1 };
-    GLint          _uniformOffset{ -1 };
-    GLint          _uniformEyeRotStart{ -1 };
-    GLint          _uniformEyeRotEnd{ -1 };
-    GLint          _attrPosition{ -1 };
-    GLint          _attrTexCoord0{ -1 };
-    GLint          _attrTexCoord1{ -1 };
-    GLint          _attrTexCoord2{ -1 };
-
-#endif
     static const QString NAME;
     GlWindow* _hmdWindow;
 };
