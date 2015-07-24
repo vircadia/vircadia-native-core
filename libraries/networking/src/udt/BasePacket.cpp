ff3cca1c20541575fa76ffeb04c6e051b5b24b73
Stephen Birarda <commit@birarda.com>
2015-07-24 16:17:49 -0700

    fix indentation in BasePacket

--- libraries/networking/src/udt/BasePacket.cpp
+++ libraries/networking/src/udt/BasePacket.cpp
@@ -24,8 +24,7 @@ std::unique_ptr<BasePacket> BasePacket::create(qint64 size) {
 }
 
 std::unique_ptr<BasePacket> BasePacket::fromReceivedPacket(std::unique_ptr<char> data,
-                                                                   qint64 size,
-                                                                   const HifiSockAddr& senderSockAddr) {
+                                                           qint64 size, const HifiSockAddr& senderSockAddr) {
     // Fail with invalid size
     Q_ASSERT(size >= 0);
     
