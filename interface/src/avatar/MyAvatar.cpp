24fff719c57d2a6daf565b9a5410b29fb8c431f1
Seth Alves <seth.alves@gmail.com>
2015-07-25 21:11:23 -0700

    quiet compiler

--- interface/src/avatar/MyAvatar.cpp
+++ interface/src/avatar/MyAvatar.cpp
@@ -1290,7 +1290,6 @@ bool MyAvatar::shouldRenderHead(const RenderArgs* renderArgs) const {
 
 void MyAvatar::updateOrientation(float deltaTime) {
     //  Smoothly rotate body with arrow keys
-    float driveLeft = _driveKeys[ROT_LEFT] - _driveKeys[ROT_RIGHT];
     float targetSpeed = (_driveKeys[ROT_LEFT] - _driveKeys[ROT_RIGHT]) * YAW_SPEED;
     if (targetSpeed != 0.0f) {
         const float ROTATION_RAMP_TIMESCALE = 0.1f;
