1193b89918dba29ab25a0d5d14ee0d2c2b9b525f
Niraj Venkat <venkatn93@gmail.com>
2015-07-27 12:14:29 -0700

    Coding standard and tab fixes

--- libraries/render-utils/src/AmbientOcclusionEffect.cpp
+++ libraries/render-utils/src/AmbientOcclusionEffect.cpp
@@ -1,9 +1,9 @@
 //
 //  AmbientOcclusionEffect.cpp
-//  interface/src/renderer
+//  libraries/render-utils/src/
 //
-//  Created by Niraj Venkat on 7/14/13.
-//  Copyright 2013 High Fidelity, Inc.
+//  Created by Niraj Venkat on 7/15/15.
+//  Copyright 2015 High Fidelity, Inc.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
