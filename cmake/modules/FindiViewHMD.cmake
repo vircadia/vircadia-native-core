12010737093608f178d931c3388241941ed8958e
David Rowe <david@ctrlaltstudio.com>
2015-08-04 13:15:26 -0700

    Add DLLs needed for Interface to run without iViewHMD installed

--- cmake/modules/FindiViewHMD.cmake
+++ cmake/modules/FindiViewHMD.cmake
@@ -21,23 +21,46 @@ if (WIN32)
     hifi_library_search_hints("iViewHMD")
 
     find_path(IVIEWHMD_INCLUDE_DIRS iViewHMDAPI.h PATH_SUFFIXES include HINTS ${IVIEWHMD_SEARCH_DIRS})
-
     find_library(IVIEWHMD_LIBRARIES NAMES iViewHMDAPI PATH_SUFFIXES libs HINTS ${IVIEWHMD_SEARCH_DIRS})
-    find_path(IVIEWHMD_DLL_PATH iViewHMDAPI.dll PATH_SUFFIXES libs HINTS ${IVIEWHMD_SEARCH_DIRS})
+    find_path(IVIEWHMD_API_DLL_PATH iViewHMDAPI.dll PATH_SUFFIXES libs HINTS ${IVIEWHMD_SEARCH_DIRS})
+    list(APPEND IVIEWHMD_REQUIREMENTS IVIEWHMD_INCLUDE_DIRS IVIEWHMD_LIBRARIES IVIEWHMD_API_DLL_PATH)
 
-    find_path(LIBIVIEWNG_LIBCORE_DLL_PATH libiViewNG-LibCore.dll PATH_SUFFIXES 3rdParty HINTS ${IVIEWHMD_SEARCH_DIRS})
-    find_path(OPENCV_HIGHGUI220_DLL_PATH opencv_highgui220.dll PATH_SUFFIXES 3rdParty HINTS ${IVIEWHMD_SEARCH_DIRS})
-    find_path(OPENCV_CORE200_DLL_PATH opencv_core220.dll PATH_SUFFIXES 3rdParty HINTS ${IVIEWHMD_SEARCH_DIRS})
-    find_path(OPENCV_IMGPROC220_DLL_PATH opencv_imgproc220.dll PATH_SUFFIXES 3rdParty HINTS ${IVIEWHMD_SEARCH_DIRS})
-    
-    list(APPEND IVIEWHMD_REQUIREMENTS IVIEWHMD_INCLUDE_DIRS IVIEWHMD_LIBRARIES IVIEWHMD_DLL_PATH)
-    list(APPEND IVIEWHMD_REQUIREMENTS LIBIVIEWNG_LIBCORE_DLL_PATH OPENCV_HIGHGUI220_DLL_PATH OPENCV_CORE200_DLL_PATH OPENCV_IMGPROC220_DLL_PATH)
+    set(IVIEWHMD_DLLS
+        avcodec-53.dll
+        avformat-53.dll
+        avutil-51.dll
+        libboost_filesystem-mgw45-mt-1_49.dll
+        libboost_system-mgw45-mt-1_49.dll
+        libboost_thread-mgw45-mt-1_49.dll
+        libgcc_s_dw2-1.dll
+        libiViewNG-LibCore.dll
+        libopencv_calib3d244.dll
+        libopencv_core244.dll
+        libopencv_features2d244.dll
+        libopencv_flann244.dll
+        libopencv_highgui244.dll
+        libopencv_imgproc244.dll
+        libopencv_legacy244.dll
+        libopencv_ml244.dll
+        libopencv_video244.dll
+        libstdc++-6.dll
+        opencv_core220.dll
+        opencv_highgui220.dll
+        opencv_imgproc220.dll
+        swscale-2.dll
+    )
+
+    foreach(IVIEWHMD_DLL ${IVIEWHMD_DLLS})
+        find_path(IVIEWHMD_DLL_PATH ${IVIEWHMD_DLL} PATH_SUFFIXES 3rdParty HINTS ${IVIEWHMD_SEARCH_DIRS})
+        list(APPEND IVIEWHMD_REQUIREMENTS IVIEWHMD_DLL_PATH)
+        list(APPEND IVIEWHMD_DLL_PATHS ${IVIEWHMD_DLL_PATH})
+    endforeach()
 
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(IVIEWHMD DEFAULT_MSG ${IVIEWHMD_REQUIREMENTS})
 
-    add_paths_to_fixup_libs(${IVIEWHMD_DLL_PATH} ${LIBIVIEWNG_LIBCORE_DLL_PATH} ${OPENCV_HIGHGUI220_DLL_PATH} ${OPENCV_CORE200_DLL_PATH} ${OPENCV_IMGPROC220_DLL_PATH})
+    add_paths_to_fixup_libs(${IVIEWHMD_API_DLL_PATH} ${IVIEWHMD_DLL_PATHS})
 
     mark_as_advanced(IVIEWHMD_INCLUDE_DIRS IVIEWHMD_LIBRARIES IVIEWHMD_SEARCH_DIRS)
 
-endif ()
+endif()
