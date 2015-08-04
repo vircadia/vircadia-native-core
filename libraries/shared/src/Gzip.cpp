fb9fa760189ef449b56b0b81dac1028723b21ce9
Seth Alves <seth.alves@gmail.com>
2015-08-04 08:26:28 -0700

    adjust comments

--- libraries/shared/src/Gzip.cpp
+++ libraries/shared/src/Gzip.cpp
@@ -3,7 +3,7 @@
 //  libraries/shared/src
 //
 //  Created by Seth Alves on 2015-08-03.
-//  Copyright 2014 High Fidelity, Inc.
+//  Copyright 2015 High Fidelity, Inc.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
