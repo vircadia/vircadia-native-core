5124fc1ea0ae86443560c20813352f13b3840ceb
Stephen Birarda <commit@birarda.com>
2015-08-04 16:31:40 -0700

    add LogHandler to UDTTest

--- tools/udt-test/src/UDTTest.cpp
+++ tools/udt-test/src/UDTTest.cpp
@@ -59,6 +59,8 @@ const QStringList SERVER_STATS_TABLE_HEADERS {
 UDTTest::UDTTest(int& argc, char** argv) :
     QCoreApplication(argc, argv)
 {
+    qInstallMessageHandler(LogHandler::verboseMessageHandler);
+    
     parseArguments();
     
     // randomize the seed for packet size randomization
