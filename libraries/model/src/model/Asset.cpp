799a3cae551fcd5f45a124075c7fd36a1bbf23e9
Sam Gateau <sam@highfidelity.io>
2015-08-21 20:14:06 -0700

    adding the top model Asset

---
+++ libraries/model/src/model/Asset.cpp
@@ -0,0 +1,20 @@
+//
+//  Asset.cpp
+//  libraries/model/src/model
+//
+//  Created by Sam Gateau on 08/21/2015.
+//  Copyright 2015 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+#include "Asset.h"
+
+using namespace model;
+
+Asset::Asset() {
+}
+
+Asset::~Asset() {
+}
+
---
+++ libraries/model/src/model/Asset.h
@@ -0,0 +1,27 @@
+//
+//  Asset.h
+//  libraries/model/src/model
+//
+//  Created by Sam Gateau on 08/21/2015.
+//  Copyright 2015 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+#ifndef hifi_model_Asset_h
+#define hifi_model_Asset_h
+
+#include <bitset>
+#include <map>
+
+
+namespace model {
+
+class Asset {
+public:    
+};
+
+typedef std::shared_ptr< Asset > AssetPointer;
+
+};
+#endif
