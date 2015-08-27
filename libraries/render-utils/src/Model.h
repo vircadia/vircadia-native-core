8ee27bca2b3e0c6b77dc97b156fb0f0f20457a00
Stephen Birarda <commit@birarda.com>
2015-08-27 16:00:21 -0700

    remove urlAsString from Model

--- libraries/render-utils/src/Model.h
+++ libraries/render-utils/src/Model.h
@@ -70,7 +70,6 @@ public:
     /// Sets the URL of the model to render.
     Q_INVOKABLE void setURL(const QUrl& url);
     const QUrl& getURL() const { return _url; }
-    const QString& getURLAsString() const { return _urlAsString; }
 
     // new Scene/Engine rendering support
     void setVisibleInScene(bool newValue, std::shared_ptr<render::Scene> scene);
@@ -314,7 +313,6 @@ private:
     QVector<float> _blendshapeCoefficients;
 
     QUrl _url;
-    QString _urlAsString;
     QUrl _collisionUrl;
     bool _isVisible;
 
