fbf6d0efe8b21947307d1014595e142da52161c3
Seiji Emery <seijiemery@gmail.com>
2015-08-27 11:57:53 -0700

    Added failsafe so dependencies don't break

--- examples/example/entities/platform.js
+++ examples/example/entities/platform.js
@@ -12,7 +12,12 @@
 // See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
+
+// UI and debug console implemented using uiwidgets / 2d overlays
 Script.include("../../libraries/uiwidgets.js");
+if (typeof('UI') === 'undefined') {	// backup link in case the user downloaded this somewhere
+	Script.include('http://public.highfidelity.io/scripts/libraries/uiwidgets.js');
+}
 
 // Platform script
 (function () {
