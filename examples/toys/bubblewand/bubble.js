455ed27c2bd40d861a2f71183e26398f90fd2382
James B. Pollack <jamesbradenpollack@gmail.com>
2015-09-16 16:26:09 -0700

    pre-test with eric

--- examples/toys/bubblewand/bubble.js
+++ examples/toys/bubblewand/bubble.js
@@ -1,54 +1,110 @@
 //  bubble.js
 //  part of bubblewand
 //
+//  Script Type: Entity
 //  Created by James B. Pollack @imgntn -- 09/03/2015
 //  Copyright 2015 High Fidelity, Inc.
 //
 //  example of a nested entity. it doesn't do much now besides delete itself if it collides with something (bubbles are fragile!  it would be cool if it sometimes merged with other bubbbles it hit)
-//  todo: play bubble sounds from the bubble itself instead of the wand. 
+//  todo: play bubble sounds & particle bursts from the bubble itself instead of the wand. 
 //  blocker: needs some sound fixes and a way to find its own position before unload for spatialization
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 
-
 (function() {
-    // Script.include("https://raw.githubusercontent.com/highfidelity/hifi/master/examples/utilities.js");
-    // Script.include("https://raw.githubusercontent.com/highfidelity/hifi/master/examples/libraries/utils.js");
+    Script.include("../../utilities.js");
+    Script.include("../../libraries/utils.js");
 
-    //var popSound;
+    var BUBBLE_PARTICLE_TEXTURE = "https://raw.githubusercontent.com/ericrius1/SantasLair/santa/assets/smokeparticle.png"
+
+
+    BUBBLE_PARTICLE_COLOR = {
+        red: 0,
+        green: 40,
+        blue: 255,
+    };
+
+    var _this = this;
+
+    var properties;
+
     this.preload = function(entityID) {
         //  print('bubble preload')
-        this.entityID = entityID;
-      //  popSound = SoundCache.getSound("http://hifi-public.s3.amazonaws.com/james/bubblewand/sounds/pop.wav");
+        _this.entityID = entityID;
+        Script.update.connect(_t.internalUpdate);
+    };
 
-    }
-
-    this.collisionWithEntity = function(myID, otherID, collision) {
-        //if(Entites.getEntityProperties(otherID).userData.objectType==='') { merge bubbles?}
-      // Entities.deleteEntity(myID);
-      //  this.burstBubbleSound(collision.contactPoint)
-
+    this.internalUpdate = function() {
+        // we want the position at unload but for some reason it keeps getting set to 0,0,0 -- so i just exclude that location.  sorry origin bubbles.
+        var tmpProperties = Entities.getEntityProperties(_this.entityID);
+        if (tmpProperties.position.x !== 0 && tmpProperties.position.y !== 0 && tmpProperties.position.z !== 0) {
+            properties = tmpProperties;
+        }
     };
 
     this.unload = function(entityID) {
-        // this.properties = Entities.getEntityProperties(entityID);
-        //var location = this.properties.position;
-        //this.burstBubbleSound();
+        Script.update.disconnect(this.internalUpdate);
+        var position = properties.position;
+        _this.endOfBubble(position);
+
     };
 
+    this.endOfBubble = function(position) {
+        this.createBurstParticles(position);
+    };
 
+    this.createBurstParticles = function(position) {
+        //get the current position of the bubble
+        var position = properties.position;
+        //var orientation = properties.orientation;
 
-    this.burstBubbleSound = function(location) {
+        var animationSettings = JSON.stringify({
+            fps: 30,
+            frameIndex: 0,
+            running: true,
+            firstFrame: 0,
+            lastFrame: 30,
+            loop: false
+        });
 
-        // var audioOptions = {
-        //     volume: 0.5,
-        //     position: location
-        // }
+        var particleBurst = Entities.addEntity({
+            type: "ParticleEffect",
+            animationSettings: animationSettings,
+            animationIsPlaying: true,
+            position: position,
+            lifetime: 0.2,
+            dimensions: {
+                x: 1,
+                y: 1,
+                z: 1
+            },
+            emitVelocity: {
+                x: 0,
+                y: 0,
+                z: 0
+            },
+            velocitySpread: {
+                x: 0.45,
+                y: 0.45,
+                z: 0.45
+            },
+            emitAcceleration: {
+                x: 0,
+                y: -0.1,
+                z: 0
+            },
+            alphaStart: 1.0,
+            alpha: 1,
+            alphaFinish: 0.0,
+            textures: BUBBLE_PARTICLE_TEXTURE,
+            color: BUBBLE_PARTICLE_COLOR,
+            lifespan: 0.2,
+            visible: true,
+            locked: false
+        });
 
-        //Audio.playSound(popSound, audioOptions);
-
-    }
+    };
 
 
 })
--- examples/toys/bubblewand/createWand.js
+++ examples/toys/bubblewand/createWand.js
@@ -1,41 +1,42 @@
 //  createWand.js
 //  part of bubblewand
 //
+//  Script Type: Entity Spawner
 //  Created by James B. Pollack @imgntn -- 09/03/2015
 //  Copyright 2015 High Fidelity, Inc.
 //
-// 	Loads a wand model and attaches the bubble wand behavior.
+//  Loads a wand model and attaches the bubble wand behavior.
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 
 
 
-Script.include("https://raw.githubusercontent.com/highfidelity/hifi/master/examples/utilities.js");
-Script.include("https://raw.githubusercontent.com/highfidelity/hifi/master/examples/libraries/utils.js");
+Script.include("../../utilities.js");
+Script.include("../../libraries/utils.js");
 
-var wandModel = "http://hifi-public.s3.amazonaws.com/james/bubblewand/models/wand/wand.fbx?" + randInt(0, 10000);
-var scriptURL = "http://hifi-public.s3.amazonaws.com/james/bubblewand/scripts/wand.js?" + randInt(1, 100500)
-
-
+var WAND_MODEL = 'http://hifi-public.s3.amazonaws.com/james/bubblewand/models/wand/wand.fbx';
+var WAND_COLLISION_SHAPE = 'http://hifi-public.s3.amazonaws.com/james/bubblewand/models/wand/collisionHull.obj';
+var WAND_SCRIPT_URL = Script.resolvePath("wand.js?"+randInt(0,4000));
 //create the wand in front of the avatar
-var center = Vec3.sum(MyAvatar.position, Vec3.multiply(3, Quat.getFront(Camera.getOrientation())));
+var center = Vec3.sum(MyAvatar.position, Vec3.multiply(1, Quat.getFront(Camera.getOrientation())));
+
 var wand = Entities.addEntity({
-	type: "Model",
-	modelURL: wandModel,
-	position: center,
-	dimensions: {
-		x: 0.1,
-		y: 1,
-		z: 0.1
-	},
-	//must be enabled to be grabbable in the physics engine
-	collisionsWillMove: true,
-	shapeType: 'box',
-	script: scriptURL
+    type: "Model",
+    modelURL: WAND_MODEL,
+    position: center,
+    dimensions: {
+        x: 0.05,
+        y: 0.5,
+        z: 0.05
+    },
+    //must be enabled to be grabbable in the physics engine
+    collisionsWillMove: true,
+    compoundShapeURL: WAND_COLLISION_SHAPE,
+    script: WAND_SCRIPT_URL
 });
 
 function cleanup() {
-	Entities.deleteEntity(wand);
+    Entities.deleteEntity(wand);
 }
 
 
