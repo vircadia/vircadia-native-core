75c1e4956785188ce3aa0f6a2198a5adb34c0018
ericrius1 <ericrius1>
2015-09-17 15:32:40 -0700

    more tweaks
    yeah

--- examples/toybox/masterResetEntity.js
+++ examples/toybox/masterResetEntity.js
@@ -1,14 +1,20 @@
 var fireScriptURL = Script.resolvePath("managers/fireManager.js");
 Script.include(fireScriptURL);
 
-var fireManager = new FireManager();
+var blockScriptURL = Script.resolvePath("managers/blockManager.js");
+Script.include(blockScriptURL);
 
+var fireManager = new FireManager();
 fireManager.reset();
+
+var blockManager = new BlockManager();
+blockManager.reset();
 
 
 
 function cleanup() {
     fireManager.reset();
+    blockManager.reset();
 }
 
 Script.scriptEnding.connect(cleanup);
