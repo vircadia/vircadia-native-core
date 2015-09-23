9436d2922fb750fb18681e10044504a319c17929
Brad Hefta-Gaub <brad@highfidelity.io>
2015-09-22 21:18:04 -0700

    more warnings fixes

--- libraries/shared/src/MatrixStack.cpp
+++ libraries/shared/src/MatrixStack.cpp
@@ -6,4 +6,7 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
-#include "MatrixStack.h"
+// FIXME - I don't think we need to actually include this in a CPP, all callers will simply
+//         use the header file and the result will be the code will be expanded inline.
+//         however, including it in a CPP file produces a linker warning on windows.
+//#include "MatrixStack.h"
