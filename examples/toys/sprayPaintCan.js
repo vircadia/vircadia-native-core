2c20a12f1300045eb17037d50626dd80cef7ad66
ericrius1 <ericrius1>
2015-09-23 13:34:21 -0700

    refactoring spraypaint to use new events

--- examples/toys/sprayPaintCan.js
+++ examples/toys/sprayPaintCan.js
@@ -21,59 +21,8 @@
     var MIN_POINT_DISTANCE = 0.01;
     var STROKE_WIDTH = 0.02;
 
-    var self = this;
-
-    var timeSinceLastMoved = 0;
-    var RESET_TIME_THRESHOLD = 5;
-    var DISTANCE_FROM_HOME_THRESHOLD = 0.5;
-    var HOME_POSITION = {
-        x: 549.12,
-        y: 495.555,
-        z: 503.77
-    };
-    this.getUserData = function() {
-
-
-        if (this.properties.userData) {
-            this.userData = JSON.parse(this.properties.userData);
-        }
-    }
-
-    this.updateUserData = function() {
-        Entities.editEntity(this.entityId, {
-            userData: JSON.stringify(this.userData)
-        });
-    }
-
-    this.update = function(deltaTime) {
-        self.getUserData();
-        self.properties = Entities.getEntityProperties(self.entityId);
-
-        if (Vec3.length(self.properties.velocity) < 0.1 && Vec3.distance(HOME_POSITION, self.properties.position) > DISTANCE_FROM_HOME_THRESHOLD) {
-            timeSinceLastMoved += deltaTime;
-            if (timeSinceLastMoved > RESET_TIME_THRESHOLD) {
-                self.reset();
-                timeSinceLastMoved = 0;
-            }
-        } else {
-            timeSinceLastMoved = 0;
-        }
-
-        //Only activate for the user who grabbed the object
-        if (self.userData.grabKey && self.userData.grabKey.activated === true && self.userData.grabKey.avatarId == MyAvatar.sessionUUID) {
-            if (self.activated !== true) {
-                //We were just grabbed, so create a particle system 
-                self.grab();
-            }
-            //Move emitter to where entity is always when its activated
-            self.sprayStream();
-        } else if (self.userData.grabKey && self.userData.grabKey.activated === false && self.activated) {
-            self.letGo();
-        }
-    }
-
-    this.grab = function() {
-        this.activated = true;
+    this.startNearGrab = function() {
+        var position = Entities.getEntityProperties(this.entityId, "position").position;
         var animationSettings = JSON.stringify({
             fps: 30,
             loop: true,
@@ -85,7 +34,7 @@
         this.paintStream = Entities.addEntity({
             type: "ParticleEffect",
             animationSettings: animationSettings,
-            position: this.properties.position,
+            position: position,
             textures: "https://raw.githubusercontent.com/ericrius1/SantasLair/santa/assets/smokeparticle.png",
             emitVelocity: ZERO_VEC,
             emitAcceleration: ZERO_VEC,
@@ -110,30 +59,24 @@
         });
     }
 
-    this.letGo = function() {
-        this.activated = false;
+
+    this.releaseGrab = function() {
         Entities.deleteEntity(this.paintStream);
         this.paintStream = null;
+        this.painting = false;
         this.sprayInjector.stop();
     }
 
-    this.reset = function() {
-        Entities.editEntity(self.entityId, {
-            position: HOME_POSITION,
-            rotation: Quat.fromPitchYawRollDegrees(0, 0, 0),
-            angularVelocity: ZERO_VEC,
-            velocity: ZERO_VEC
-        });
-    }
 
-    this.sprayStream = function() {
-        var forwardVec = Quat.getFront(Quat.multiply(self.properties.rotation , Quat.fromPitchYawRollDegrees(0, 90, 0)));
+    this.continueNearGrab = function() {
+        var props = Entities.getEntityProperties(this.entityId, ["position, rotation"]);
+        var forwardVec = Quat.getFront(Quat.multiply(props.rotation, Quat.fromPitchYawRollDegrees(0, 90, 0)));
         forwardVec = Vec3.normalize(forwardVec);
 
-        var upVec = Quat.getUp(self.properties.rotation);
-        var position = Vec3.sum(self.properties.position, Vec3.multiply(forwardVec, TIP_OFFSET_Z));
+        var upVec = Quat.getUp(props.rotation);
+        var position = Vec3.sum(props.position, Vec3.multiply(forwardVec, TIP_OFFSET_Z));
         position = Vec3.sum(position, Vec3.multiply(upVec, TIP_OFFSET_Y))
-        Entities.editEntity(self.paintStream, {
+        Entities.editEntity(this.paintStream, {
             position: position,
             emitVelocity: Vec3.multiply(5, forwardVec)
         });
@@ -151,8 +94,6 @@
             var normal = Vec3.multiply(-1, Quat.getFront(intersection.properties.rotation));
             this.paint(intersection.intersection, normal);
         }
-
-
     }
 
     this.paint = function(position, normal) {
@@ -214,40 +155,18 @@
 
     this.preload = function(entityId) {
         this.strokes = [];
-        this.activated = false;
         this.entityId = entityId;
-        this.properties = Entities.getEntityProperties(self.entityId);
-        this.getUserData();
-
-        //Only activate for the avatar who is grabbing the can!
-        if (this.userData.grabKey && this.userData.grabKey.activated) {
-            this.activated = true;
-        }
-        if (!this.userData.grabFrame) {
-            var data = {
-                relativePosition: {
-                    x: 0,
-                    y: 0,
-                    z: 0
-                },
-                relativeRotation: Quat.fromPitchYawRollDegrees(0, 0, 0)
-            }
-            setEntityCustomData(GRAB_FRAME_USER_DATA_KEY, this.entityId, data);
-        }
     }
 
 
     this.unload = function() {
-        Script.update.disconnect(this.update);
-        if(this.paintStream) {
+        if (this.paintStream) {
             Entities.deleteEntity(this.paintStream);
         }
         this.strokes.forEach(function(stroke) {
             Entities.deleteEntity(stroke);
         });
     }
-    
-    Script.update.connect(this.update);
 });
 
 
