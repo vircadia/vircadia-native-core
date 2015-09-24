83a8636a3668870ae6aecced4f0d7f6459fd8736
James Pollack <james@highfidelity.io>
2015-09-24 14:49:28 -0700

    Remove whitespace, batch entity edits

--- examples/toys/doll/createDoll.js
+++ examples/toys/doll/createDoll.js
@@ -4,7 +4,6 @@
 //  Created by James B. Pollack @imgntn 9/23/2015
 //  Copyright 2015 High Fidelity, Inc.
 //
-//  ATTENTION: requires that you run handController.js
 //  Creates a doll entity in front of you.
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
@@ -13,19 +12,15 @@
 
 function createDoll() {
     var modelURL = "http://hifi-public.s3.amazonaws.com/models/Bboys/bboy2/bboy2.fbx";
-    var scriptURL = Script.resolvePath("doll.js");
-    var center = Vec3.sum(Vec3.sum(MyAvatar.position, {
-        x: 0,
-        y: 0.5,
-        z: 0
-    }), Vec3.multiply(0.5, Quat.getFront(Camera.getOrientation())));
 
-    var naturalDimensions = {
-        x: 1.63,
-        y: 1.67,
-        z: 0.26
-    };
+    var scriptURL = Script.resolvePath("doll.js");
+
+    var center = Vec3.sum(Vec3.sum(MyAvatar.position, { x: 0, y: 0.5, z: 0 }), Vec3.multiply(0.5, Quat.getFront(Camera.getOrientation())));
+
+    var naturalDimensions = { x: 1.63, y: 1.67, z: 0.26};
+
     var desiredDimensions = Vec3.multiply(naturalDimensions, 0.15);
+    
     var doll = Entities.addEntity({
         type: "Model",
         name: "doll",
