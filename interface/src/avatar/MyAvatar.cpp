745a59af1c8fd6644e282e122943ee12d0593e2f
Howard Stearns <howard@highfidelity.io>
2015-09-29 12:48:57 -0700

    Default animation is anim graph. Remove state change logging from animation graph state machine.

--- interface/src/avatar/MyAvatar.cpp
+++ interface/src/avatar/MyAvatar.cpp
@@ -153,7 +153,8 @@ void MyAvatar::reset() {
 
     // This should be simpler when we have only graph animations always on.
     bool isRig = _rig->getEnableRig();
-    bool isGraph = _rig->getEnableAnimGraph();
+    // seting rig animation to true, below, will clear the graph animation menu item, so grab it now.
+    bool isGraph = _rig->getEnableAnimGraph() || Menu::getInstance()->isOptionChecked(MenuOption::EnableAnimGraph);
     qApp->setRawAvatarUpdateThreading(false);
     _rig->disableHands = true;
     setEnableRigAnimations(true);
