25e25726d59d09c719234ea7694f376336fcd856
ericrius1 <ericrius1>
2015-10-01 15:43:25 -0700

    only lights

--- examples/toys/masterResetEntity.js
+++ examples/toys/masterResetEntity.js
@@ -20,75 +20,89 @@ var HIFI_PUBLIC_BUCKET = "http://s3.amazonaws.com/hifi-public/";
 var shouldDeleteOnEndScript = false;
 
 //Before creating anything, first search a radius and delete all the things that should be deleted
-deleteAllToys();
+// deleteAllToys();
+deleteLights();
 createAllToys();
 
 function createAllToys() {
-    createBlocks({
-        x: 548.3,
-        y: 495.55,
-        z: 504.4
-    });
+    // createBlocks({
+    //     x: 548.3,
+    //     y: 495.55,
+    //     z: 504.4
+    // });
 
-    createSprayCan({
-        x: 549.7,
-        y: 495.6,
-        z: 503.91
-    });
+    // createSprayCan({
+    //     x: 549.7,
+    //     y: 495.6,
+    //     z: 503.91
+    // });
 
-    createBasketBall({
-        x: 547.73,
-        y: 495.5,
-        z: 505.47
-    });
+    // createBasketBall({
+    //     x: 547.73,
+    //     y: 495.5,
+    //     z: 505.47
+    // });
 
-    createDoll({
-        x: 546.67,
-        y: 495.41,
-        z: 505.09
-    });
+    // createDoll({
+    //     x: 546.67,
+    //     y: 495.41,
+    //     z: 505.09
+    // });
 
-    createWand({
-        x: 546.71,
-        y: 495.55,
-        z: 506.15
-    });
+    // createWand({
+    //     x: 546.71,
+    //     y: 495.55,
+    //     z: 506.15
+    // });
 
-    createDice();
+    // createDice();
 
-    createFlashlight({
-        x: 545.72,
-        y: 495.41,
-        z: 505.78
-    });
+    // createFlashlight({
+    //     x: 545.72,
+    //     y: 495.41,
+    //     z: 505.78
+    // });
 
-    createCat({
-        x: 551.09,
-        y: 494.98,
-        z: 503.49
-    });
+    // createCat({
+    //     x: 551.09,
+    //     y: 494.98,
+    //     z: 503.49
+    // });
 
+    // createCombinedArmChair({
+    //     x: 549.29,
+    //     y: 495.05,
+    //     z: 508.22
+    // })
+
+    // createPottedPlant({
+    //     x: 554.26,
+    //     y: 495.23,
+    //     z: 504.53
+    // });
+
+
+    // createGates();
+
+    // createFire();
     // //Handles toggling of all sconce lights 
     createLightSwitches();
 
 
 
-    createCombinedArmChair({
-        x: 549.29,
-        y: 495.05,
-        z: 508.22
-    })
+}
 
-    createPottedPlant({
-        x: 554.26,
-        y: 495.23,
-        z: 504.53
+function deleteLights() {
+    var entities = Entities.findEntities(MyAvatar.position, 100);
+    
+    entities.forEach(function(entity) {
+        //params: customKey, id, defaultValue
+        var shouldReset = getEntityCustomData(resetKey, entity, {}).resetMe;
+        var lightType =  getEntityCustomData(resetKey, entity, {}).lightType;
+        if (shouldReset === true && (lightType === "Sconce Light Garage" || lightType === "Sconce Light Hall")) {
+            Entities.deleteEntity(entity);
+        }
     });
-
-
-    createGates();
-
-    createFire();
 }
 
 function deleteAllToys() {
