9098585e2fa975abef2a5694713bb9eaee3c740d
James B. Pollack <jamesbradenpollack@gmail.com>
2015-10-01 18:14:08 -0700

    Update cat.js
    remove a lot of unused code and add flag for whether cat is already meowing so that it doesn't start another one until its done.  unfortunately i dont know how to read the audio isplaying state so we have to use a lame timeout

--- examples/toys/cat.js
+++ examples/toys/cat.js
@@ -1,12 +1,11 @@
 //
-//  doll.js
+//  cat.js
 //  examples/toybox/entityScripts
 //
 //  Created by Eric Levin on 9/21/15.
 //  Copyright 2015 High Fidelity, Inc.
 //
-//  This entity script breathes movement and sound- one might even say life- into a doll.
-//
+
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
@@ -15,20 +14,23 @@
 
     var _this;
 
-    // this is the "constructor" for the entity as a JS object we don't do much here, but we do want to remember
-    // our this object, so we can access it in cases where we're called without a this (like in the case of various global signals)
     Cat = function() {
         _this = this;
         this.meowSound = SoundCache.getSound("https://s3.amazonaws.com/hifi-public/sounds/Animals/cat_meow.wav");
-        this.distanceThreshold = 1;
-        this.canMeow = true;
-        this.meowBreakTime = 3000;
     };
 
     Cat.prototype = {
-
+        isMeowing:false,
         startTouch: function() {
-            this.meow();
+            var _ t=this;
+            if(this.isMeowing!==true){
+                this.meow();
+                this.isMeowing=true;
+                    Script.setTimeout(function(){
+                    _t.isMeowing=false;
+                },2000)
+            }
+
         },
 
         meow: function() {
@@ -38,14 +40,10 @@
                 volume: .1
             });
         },
-        // preload() will be called when the entity has become visible (or known) to the interface
-        // it gives us a chance to set our local JavaScript object up. In this case it means:
-        //   * remembering our entityID, so we can access it in cases where we're called without an entityID
-        //   * connecting to the update signal so we can check our grabbed state
+      
         preload: function(entityID) {
             this.entityID = entityID;
             this.position = Entities.getEntityProperties(this.entityID, "position").position;
-            Script.update.connect(this.update);
         },
 
         unload: function() {
