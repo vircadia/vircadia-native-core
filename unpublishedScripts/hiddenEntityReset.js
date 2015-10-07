3465e3cc133e2799c5f40dbfe32a03951d1c4b29
ericrius1 <ericrius1>
2015-10-07 15:21:24 -0700

    Adding hidden reset switch to room which, when grabbed or clicked on, will reset room

---
+++ unpublishedScripts/hiddenEntityReset.js
@@ -0,0 +1,50 @@
+//
+//  ResetSwitch.js
+//
+//  Created by Eric Levin on 10/2/15.
+//  Copyright 2015 High Fidelity, Inc.
+//
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+
+(function () {
+
+    var _this;
+
+    var masterResetScript = Script.resolvePath("masterReset.js");
+    Script.include(masterResetScript);
+
+    ResetSwitch = function () {
+        _this = this;
+    };
+
+    ResetSwitch.prototype = {
+
+        clickReleaseOnEntity: function (entityId, mouseEvent) {
+            if (!mouseEvent.isLeftButton) {
+                return;
+            }
+            this.triggerReset();
+
+        },
+
+        startNearGrabNonColliding: function () {
+            this.triggerReset();
+        },
+
+        triggerReset: function () {
+            MasterReset();
+        },
+
+        preload: function (entityID) {
+            this.entityID = entityID;
+        }
+
+    };
+
+    // entity scripts always need to return a newly constructed object of our type
+    return new ResetSwitch();
+})
---
+++ unpublishedScripts/immediateClientReset.js
@@ -0,0 +1,8 @@
+/*global print, MyAvatar, Entities, AnimationCache, SoundCache, Scene, Camera, Overlays, Audio, HMD, AvatarList, AvatarManager, Controller, UndoStack, Window, Account, GlobalServices, Script, ScriptDiscoveryService, LODManager, Menu, Vec3, Quat, AudioDevice, Paths, Clipboard, Settings, XMLHttpRequest, pointInExtents, vec3equal, setEntityCustomData, getEntityCustomData */
+
+/*global MasterReset */
+
+var masterResetScript = Script.resolvePath("masterReset.js");
+Script.include(masterResetScript);
+
+MasterReset();
