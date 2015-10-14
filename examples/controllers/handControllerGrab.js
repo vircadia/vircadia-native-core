0b92bc11f6a47068cbc8814cd651138f824b473e
Eric Levin <Ericrius1@gmail.com>
2015-10-14 13:26:10 -0700

    fixed spacing

--- examples/controllers/handControllerGrab.js
+++ examples/controllers/handControllerGrab.js
@@ -568,7 +568,7 @@ function MyController(hand, triggerAction) {
             this.setState(STATE_RELEASE);
             return;
         }
-    
+        
         Entities.callEntityMethod(this.grabbedEntity, "continueNearGrabbingNonColliding");
     };
 
