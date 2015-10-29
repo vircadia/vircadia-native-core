143b9c663f2e6cb530133aadf19e6f27d49ebe2d
Ken Cooke <ken@highfidelity.io>
2015-10-29 12:21:28 -0700

    Quick fix for audio having channels swapped under Qt 5.5.1.

--- libraries/audio-client/src/AudioClient.cpp
+++ libraries/audio-client/src/AudioClient.cpp
@@ -313,7 +313,9 @@ QAudioDeviceInfo defaultAudioDeviceForMode(QAudio::Mode mode) {
 bool adjustedFormatForAudioDevice(const QAudioDeviceInfo& audioDevice,
                                   const QAudioFormat& desiredAudioFormat,
                                   QAudioFormat& adjustedAudioFormat) {
-    if (!audioDevice.isFormatSupported(desiredAudioFormat)) {
+    // FIXME: direcly using 24khz has a bug somewhere that causes channels to be swapped.
+    // Continue using our internal resampler, for now.
+    if (false && !audioDevice.isFormatSupported(desiredAudioFormat)) {
         qCDebug(audioclient) << "The desired format for audio I/O is" << desiredAudioFormat;
         qCDebug(audioclient, "The desired audio format is not supported by this device");
 
