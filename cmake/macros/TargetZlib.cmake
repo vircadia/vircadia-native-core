99d8554cc8050fa29ba73205155e7d455f0d2458
Brad Davis <bdavis@saintandreas.org>
2015-11-01 01:36:54 -0700

    Make zlib a true external on windows (linux and OSX should rely on system libraries)

---
+++ cmake/macros/TargetZlib.cmake
@@ -0,0 +1,17 @@
+# 
+#  Copyright 2015 High Fidelity, Inc.
+#  Created by Bradley Austin Davis on 2015/10/10
+#
+#  Distributed under the Apache License, Version 2.0.
+#  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+# 
+macro(TARGET_ZLIB)
+    if (WIN32)
+        add_dependency_external_projects(zlib)
+        add_paths_to_fixup_libs(${ZLIB_DLL_PATH})
+    else()
+        find_package(ZLIB REQUIRED)
+    endif()
+    target_include_directories(${TARGET_NAME} SYSTEM PRIVATE ${ZLIB_INCLUDE_DIRS})
+    target_link_libraries(${TARGET_NAME} ${ZLIB_LIBRARIES})
+endmacro()
