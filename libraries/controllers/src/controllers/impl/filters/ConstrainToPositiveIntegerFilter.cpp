43df5b55fe5adf6c3fefdc61c431225fe7587668
Brad Hefta-Gaub <brad@highfidelity.io>
2015-11-01 11:55:55 -0800

    fix linker warnings

--- libraries/controllers/src/controllers/impl/filters/ConstrainToPositiveIntegerFilter.cpp
+++ libraries/controllers/src/controllers/impl/filters/ConstrainToPositiveIntegerFilter.cpp
@@ -6,4 +6,10 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
-#include "ConstrainToPositiveIntegerFilter.h"
+// NOTE: we don't need to include this header unless/until we add additional symbols.
+//       By removing this header we prevent these warnings on windows:
+//
+//           warning LNK4221: This object file does not define any previously undefined public symbols, 
+//           so it will not be used by any link operation that consumes this library
+//
+//#include "ConstrainToPositiveIntegerFilter.h"
