bb0ce67f6b103b671e1598f6cbaf1291c10e21ac
Ryan Huffman <ryanhuffman@gmail.com>
2015-11-02 15:34:42 -0800

    Update bat.js to lazy load the pitching machine

--- examples/baseball/bat.js
+++ examples/baseball/bat.js
@@ -1,8 +1,11 @@
 (function() {
     Script.include("pitching.js");
-    var pitchingMachine = getPitchingMachine();
-    Script.update.connect(function(dt) { pitchingMachine.update(dt); });
+    var pitchingMachine = null;
     this.startNearGrab = function() {
+        if (!pitchingMachine) {
+            getPitchingMachine();
+            Script.update.connect(function(dt) { pitchingMachine.update(dt); });
+        }
         print("Started near grab!");
         pitchingMachine.start();
         MyAvatar.shouldRenderLocally = false;
