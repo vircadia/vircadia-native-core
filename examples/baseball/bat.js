8607a1ef5064b27eacfb70b40cbcb056f6124403
Stephen Birarda <commit@birarda.com>
2015-11-04 10:22:53 -0800

    Revert "remove continueNearGrab from bat script"
    This reverts commit ea2b9e906b9dcbf4c606339bcf863404bf615da2.

--- examples/baseball/bat.js
+++ examples/baseball/bat.js
@@ -2,14 +2,17 @@
     Script.include("pitching.js");
     var pitchingMachine = null;
     this.startNearGrab = function() {
-        print("Started near grab!");
         if (!pitchingMachine) {
-            pitchingMachine = getPitchingMachine();
+            getPitchingMachine();
             Script.update.connect(function(dt) { pitchingMachine.update(dt); });
         }
+        print("Started near grab!");
         pitchingMachine.start();
         MyAvatar.shouldRenderLocally = false;
     };
+    this.continueNearGrab = function() {
+        MyAvatar.shouldRenderLocally = false;
+    }
     this.releaseGrab = function() {
         print("Stopped near grab!");
         if (pitchingMachine) {
