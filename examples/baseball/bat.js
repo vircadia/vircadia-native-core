44ab236673af5cc4dc3f49872f3ccc3eee369b69
Stephen Birarda <commit@birarda.com>
2015-11-09 15:33:23 -0800

    Fix include in bat.js

--- examples/baseball/bat.js
+++ examples/baseball/bat.js
@@ -9,9 +9,9 @@
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
-Script.include("pitching.js");
-
 (function() {
+    Script.include("pitching.js");
+
     var pitchingMachine = null;
 
     this.pitchAndHideAvatar = function() {
