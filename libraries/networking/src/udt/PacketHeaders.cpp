7f6a20af99b3a118475c2c4dc87a9c507e55b533
Thijs Wenker <me@thoys.nl>
2015-11-10 19:22:57 +0100

    bump entities protocol version

--- libraries/networking/src/udt/PacketHeaders.cpp
+++ libraries/networking/src/udt/PacketHeaders.cpp
@@ -38,7 +38,7 @@ PacketVersion versionForPacketType(PacketType packetType) {
         case PacketType::EntityAdd:
         case PacketType::EntityEdit:
         case PacketType::EntityData:
-            return VERSION_ENTITIES_PARTICLES_ADDITIVE_BLENDING;
+            return VERSION_ENTITIES_ANCHOR;
         case PacketType::AvatarData:
         case PacketType::BulkAvatarData:
         default:
