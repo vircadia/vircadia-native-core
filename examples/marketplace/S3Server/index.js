806d303dcd8b245de1d2093846c6d445d97655e5
ericrius1 <ericrius1>
2015-11-11 15:55:32 -0800

    Added header file

--- examples/marketplace/S3Server/index.js
+++ examples/marketplace/S3Server/index.js
@@ -1,3 +1,18 @@
+//
+//  index.js
+//  examples
+//
+//  Created by Eric Levin on 11/10/2015.
+//  Copyright 2013 High Fidelity, Inc.
+//
+//  This is a simple REST API that allows an interface client script to get a list of files paths from an S3 bucket.
+//  To change your bucket, modify line 34 to your desired bucket.
+//  Please refer to  http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html
+//  for instructions on how to configure the SDK to work with your bucket.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+
 var express = require('express');
 var app = express();
 var AWS = require('aws-sdk');
@@ -40,5 +55,3 @@ app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 })
 
-
-//ozan/3d_marketplace
