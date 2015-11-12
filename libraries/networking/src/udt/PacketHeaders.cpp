0a64242160e8d1b90c0573a4eed226951abaa423
Stephen Birarda <commit@birarda.com>
2015-11-12 14:43:25 -0800

    cleanup string grabbing for PacketType enum

--- libraries/networking/src/udt/PacketHeaders.cpp
+++ libraries/networking/src/udt/PacketHeaders.cpp
@@ -14,6 +14,7 @@
 #include <math.h>
 
 #include <QtCore/QDebug>
+#include <QtCore/QMetaEnum>
 
 const QSet<PacketType> NON_VERIFIED_PACKETS = QSet<PacketType>()
     << PacketType::NodeJsonStats << PacketType::EntityQuery
@@ -46,60 +47,6 @@ PacketVersion versionForPacketType(PacketType packetType) {
     }
 }
 
-#define PACKET_TYPE_NAME_LOOKUP(x) case x:  return QString(#x);
-
-QString nameForPacketType(PacketType packetType) {
-    switch (packetType) {
-        PACKET_TYPE_NAME_LOOKUP(PacketType::Unknown);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::StunResponse);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::DomainList);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::Ping);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::PingReply);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::KillAvatar);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::AvatarData);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::InjectAudio);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::MixedAudio);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::MicrophoneAudioNoEcho);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::MicrophoneAudioWithEcho);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::BulkAvatarData);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::SilentAudioFrame);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::DomainListRequest);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::RequestAssignment);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::CreateAssignment);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::DomainConnectionDenied);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::MuteEnvironment);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::AudioStreamStats);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::OctreeStats);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::Jurisdiction);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::JurisdictionRequest);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::AvatarIdentity);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::AvatarBillboard);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::DomainConnectRequest);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::DomainServerRequireDTLS);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::NodeJsonStats);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::EntityQuery);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::EntityData);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::EntityErase);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::OctreeDataNack);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::StopNode);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::AudioEnvironment);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::EntityEditNack);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::ICEServerHeartbeat);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::DomainServerAddedNode);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::ICEServerQuery);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::ICEServerPeerInformation);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::ICEPing);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::ICEPingReply);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::EntityAdd);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::EntityEdit);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::DomainServerConnectionToken);
-        PACKET_TYPE_NAME_LOOKUP(PacketType::NodeDisconnect)
-        default:
-            return QString("Type: ") + QString::number((int)packetType);
-    }
-    return QString("unexpected");
-}
-
 uint qHash(const PacketType& key, uint seed) {
     // seems odd that Qt couldn't figure out this cast itself, but this fixes a compile error after switch to
     // strongly typed enum for PacketType
@@ -107,6 +54,10 @@ uint qHash(const PacketType& key, uint seed) {
 }
 
 QDebug operator<<(QDebug debug, const PacketType& type) {
-    debug.nospace() << (uint8_t) type << " (" << qPrintable(nameForPacketType(type)) << ")";
+    QMetaObject metaObject = PacketTypeEnum::staticMetaObject;
+    QMetaEnum metaEnum = metaObject.enumerator(metaObject.enumeratorOffset());
+    QString typeName = metaEnum.valueToKey((int) type);
+    
+    debug.nospace().noquote() << (uint8_t) type << " (" << typeName << ")";
     return debug.space();
 }
