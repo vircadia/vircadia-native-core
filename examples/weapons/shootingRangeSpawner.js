c5898896bcf4d50cc0ad56014cbbfdf5cabb89c3
ericrius1 <ericrius1>
2015-11-18 19:12:23 -0800

    monster model

--- examples/weapons/shootingRangeSpawner.js
+++ examples/weapons/shootingRangeSpawner.js
@@ -1,5 +1,5 @@
-var shootingRangeURL = "file:///C:/Users/Eric/Desktop/shootingRange.fbx?v1"
-var floorURL = "file:///C:/Users/Eric/Desktop/shootingRangeFloor.fbx?v1"
+var shootingRangeURL = "file:///C:/Users/Eric/Desktop/shootingRange/shootingRange.fbx?v1"
+var floorURL = "file:///C:/Users/Eric/Desktop/shootingRange/shootingRangeFloor.fbx?v1"
 var shootingRange = Entities.addEntity({
     type: 'Model',
     modelURL: shootingRangeURL,
@@ -15,10 +15,18 @@ var shootingRangeFloor = Entities.addEntity({
     position: floorPosition
 })
 
+var monsterPosition = Vec3.sum(MyAvatar.position, Vec3.multiply(3, Quat.getFront(Camera.getOrientation())));
+var monsterURL = "file:///C:/Users/Eric/Desktop/shootingRange/monster1.fbx?v3"
+var monster = Entities.addEntity({
+    type: "Model",
+    modelURL: monsterURL,
+    position: monsterPosition
+});
 
 function cleanup() {
     Entities.deleteEntity(shootingRange);
     Entities.deleteEntity(shootingRangeFloor);
+    Entities.deleteEntity(monster);
 }
 
 Script.scriptEnding.connect(cleanup);
