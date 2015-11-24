b7cdca75b7ed403c212b2f05b3b937718c2738f9
Leonardo Murillo <leo@highfidelity.io>
2015-11-24 16:12:04 -0600

    Checkpoint towards integrated delivery

---
+++ cmake/macros/ConsolidateStackComponents.cmake
@@ -0,0 +1,9 @@
+macro(CONSOLIDATE_STACK_COMPONENTS)
+
+  # Copy all the output for this target into the common deployment location
+  add_custom_command(
+  	TARGET ${TARGET_NAME} POST_BUILD
+    COMMAND "${CMAKE_COMMAND}" -E copy_directory $<TARGET_FILE_DIR:${TARGET_NAME}> ${CMAKE_BINARY_DIR}/full-stack-deployment
+  )
+
+endmacro()
