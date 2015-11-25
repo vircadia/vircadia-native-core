3a9dcf56dffba84b307442e9a9f26fdda3a890cb
ericrius1 <ericrius1>
2015-11-24 16:11:07 -0800

    tweak

--- unpublishedScripts/masterReset.js
+++ unpublishedScripts/masterReset.js
@@ -161,7 +161,7 @@ MasterReset = function() {
                         relativePosition: {
                             x: 0,
                             y: 0,
-                            z: -.2
+                            z: -0.2
                         },
                         relativeRotation: Quat.fromPitchYawRollDegrees(45, 90, 0)
                     },
