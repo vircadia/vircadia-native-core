65bc6aa37114fc3fcd9a6a9437ee3df2a702283e
Atlante45 <clement.brisset@gmail.com>
2015-12-08 10:48:11 -0800

    Fix left hand grab

--- interface/src/avatar/AvatarActionHold.cpp
+++ interface/src/avatar/AvatarActionHold.cpp
@@ -85,8 +85,8 @@ void AvatarActionHold::updateActionWorker(float deltaTimeStep) {
     QList<EntityActionPointer> holdActions = ownerEntity->getActionsOfType(ACTION_TYPE_HOLD);
     foreach (EntityActionPointer action, holdActions) {
         std::shared_ptr<AvatarActionHold> holdAction = std::static_pointer_cast<AvatarActionHold>(action);
-        glm::quat rotationForAction;
-        glm::vec3 positionForAction;
+        glm::quat rotationForAction { Quaternions::IDENTITY };
+        glm::vec3 positionForAction { Vectors::ZERO };
         std::shared_ptr<Avatar> holdingAvatar = holdAction->getTarget(rotationForAction, positionForAction);
         if (holdingAvatar) {
             holdCount ++;
