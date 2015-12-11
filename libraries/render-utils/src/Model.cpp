94b6ca4450179b5b360b7765c96fc112799f0616
samcake <samuel.gateau@gmail.com>
2015-12-11 10:48:58 -0800

    FIxing the bad lambda capture for local variables

--- libraries/render-utils/src/Model.cpp
+++ libraries/render-utils/src/Model.cpp
@@ -120,7 +120,7 @@ void Model::enqueueLocationChange() {
 
     render::PendingChanges pendingChanges;
     foreach (auto itemID, _renderItems.keys()) {
-        pendingChanges.updateItem<MeshPartPayload>(itemID, [=](MeshPartPayload& data) {
+        pendingChanges.updateItem<MeshPartPayload>(itemID, [transform, offset](MeshPartPayload& data) {
             data.updateTransform(transform, offset);
             data.notifyLocationChanged();
         });
@@ -505,7 +505,7 @@ bool Model::addToScene(std::shared_ptr<render::Scene> scene, render::PendingChan
         auto item = scene->allocateID();
         auto renderPayload = std::make_shared<MeshPartPayload::Payload>(renderItem);
         pendingChanges.resetItem(item, renderPayload);
-        pendingChanges.updateItem<MeshPartPayload>(item, [&](MeshPartPayload& data) {
+        pendingChanges.updateItem<MeshPartPayload>(item, [](MeshPartPayload& data) {
             data.notifyLocationChanged();
         });
         _renderItems.insert(item, renderPayload);
@@ -533,7 +533,7 @@ bool Model::addToScene(std::shared_ptr<render::Scene> scene,
         auto renderPayload = std::make_shared<MeshPartPayload::Payload>(renderItem);
         renderPayload->addStatusGetters(statusGetters);
         pendingChanges.resetItem(item, renderPayload);
-        pendingChanges.updateItem<MeshPartPayload>(item, [&](MeshPartPayload& data) {
+        pendingChanges.updateItem<MeshPartPayload>(item, [](MeshPartPayload& data) {
             data.notifyLocationChanged();
         });
         _renderItems.insert(item, renderPayload);
@@ -1210,7 +1210,7 @@ bool Model::initWhenReady(render::ScenePointer scene) {
             auto renderPayload = std::make_shared<MeshPartPayload::Payload>(renderItem);
             _renderItems.insert(item, renderPayload);
             pendingChanges.resetItem(item, renderPayload);
-            pendingChanges.updateItem<MeshPartPayload>(item, [&](MeshPartPayload& data) {
+            pendingChanges.updateItem<MeshPartPayload>(item, [transform,offset](MeshPartPayload& data) {
                 data.updateTransform(transform, offset);
                 data.notifyLocationChanged();
             });
