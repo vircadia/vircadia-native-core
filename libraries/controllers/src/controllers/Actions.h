6420a43858fa57095a498771d93ba47c0804c880
Brad Hefta-Gaub <brad@highfidelity.io>
2015-12-15 15:35:17 -0800

    fix type

--- libraries/controllers/src/controllers/Actions.h
+++ libraries/controllers/src/controllers/Actions.h
@@ -60,33 +60,33 @@ enum class Action {
     RETICLE_X,
     RETICLE_Y,
 
-    // Biseced aliases for RETICLE_X/RETICLE_Y
+    // Bisected aliases for RETICLE_X/RETICLE_Y
     RETICLE_LEFT,
     RETICLE_RIGHT,
     RETICLE_UP,
     RETICLE_DOWN,
 
-    // Biseced aliases for TRANSLATE_Z
+    // Bisected aliases for TRANSLATE_Z
     LONGITUDINAL_BACKWARD,
     LONGITUDINAL_FORWARD,
 
-    // Biseced aliases for TRANSLATE_X
+    // Bisected aliases for TRANSLATE_X
     LATERAL_LEFT,
     LATERAL_RIGHT,
 
-    // Biseced aliases for TRANSLATE_Y
+    // Bisected aliases for TRANSLATE_Y
     VERTICAL_DOWN,
     VERTICAL_UP,
 
-    // Biseced aliases for ROTATE_Y
+    // Bisected aliases for ROTATE_Y
     YAW_LEFT,
     YAW_RIGHT,
 
-    // Biseced aliases for ROTATE_X
+    // Bisected aliases for ROTATE_X
     PITCH_DOWN,
     PITCH_UP,
 
-    // Biseced aliases for TRANSLATE_CAMERA_Z
+    // Bisected aliases for TRANSLATE_CAMERA_Z
     BOOM_IN,
     BOOM_OUT,
 
