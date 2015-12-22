119f3e9895e46ff412bb83a9c03b06f245c6c84e
Brad Hefta-Gaub <brad@highfidelity.io>
2015-12-22 13:35:16 -0800

    make both hands and mouse work

--- examples/controllers/reticleHandRotationTest.js
+++ examples/controllers/reticleHandRotationTest.js
@@ -32,14 +32,14 @@ function moveReticleAbsolute(x, y) {
     // some debugging to see if position is jumping around on us...
     var distanceSinceLastMove = length(lastPos, globalPos);
     if (distanceSinceLastMove > EXPECTED_CHANGE) {
-        print("------------------ distanceSinceLastMove:" + distanceSinceLastMove + "----------------------------");
+        debugPrint("------------------ distanceSinceLastMove:" + distanceSinceLastMove + "----------------------------");
     }
 
     if (Math.abs(dX) > EXPECTED_CHANGE) {
-        print("surpressing unexpectedly large change dX:" + dX + "----------------------------");
+        debugPrint("surpressing unexpectedly large change dX:" + dX + "----------------------------");
     }
     if (Math.abs(dY) > EXPECTED_CHANGE) {
-        print("surpressing unexpectedly large change dY:" + dY + "----------------------------");
+        debugPrint("surpressing unexpectedly large change dY:" + dY + "----------------------------");
     }
 
     globalPos.x = x;
@@ -59,6 +59,31 @@ mapping.enable();
 var lastRotatedLeft = Vec3.UNIT_NEG_Y;
 var lastRotatedRight = Vec3.UNIT_NEG_Y;
 
+function debugPrint(message) {
+    if (DEBUGGING) {
+        print(message);
+    }
+}
+
+var MAX_WAKE_UP_DISTANCE = 0.005;
+var MIN_WAKE_UP_DISTANCE = 0.001;
+var INITIAL_WAKE_UP_DISTANCE = MIN_WAKE_UP_DISTANCE;
+var INCREMENTAL_WAKE_UP_DISTANCE = 0.001;
+
+var MAX_SLEEP_DISTANCE = 0.0004;
+var MIN_SLEEP_DISTANCE = 0.00002;
+var INITIAL_SLEEP_DISTANCE = MIN_SLEEP_DISTANCE;
+var INCREMENTAL_SLEEP_DISTANCE = 0.00002;
+
+var leftAsleep = true;
+var rightAsleep = true;
+
+var leftWakeUpDistance = INITIAL_WAKE_UP_DISTANCE;
+var rightWakeUpDistance = INITIAL_WAKE_UP_DISTANCE;
+
+var leftSleepDistance = INITIAL_SLEEP_DISTANCE;
+var rightSleepDistance = INITIAL_SLEEP_DISTANCE;
+
 Script.update.connect(function(deltaTime) {
 
     var poseRight = Controller.getPoseValue(Controller.Standard.RightHand);
@@ -71,18 +96,109 @@ Script.update.connect(function(deltaTime) {
     var rotatedRight = Vec3.multiplyQbyV(poseRight.rotation, Vec3.UNIT_NEG_Y);
     var rotatedLeft = Vec3.multiplyQbyV(poseLeft.rotation, Vec3.UNIT_NEG_Y);
 
-    // check to see if hand is on base station
-    if (Vec3.equal(rotatedLeft, Vec3.UNIT_NEG_Y)) {
-        rotatedLeft = rotatedRight;
+    var suppressRight = false;
+    var suppressLeft = false;
+
+    // What I really want to do is to slowly increase the epsilon you have to move it
+    // to wake up, the longer you go without moving it
+    var leftDistance = Vec3.distance(rotatedLeft, lastRotatedLeft);
+    var rightDistance = Vec3.distance(rotatedRight, lastRotatedRight);
+
+    // check to see if hand should wakeup or sleep
+    if (leftAsleep) {
+        if (leftDistance > leftWakeUpDistance) {
+            leftAsleep = false;
+            leftSleepDistance = INITIAL_SLEEP_DISTANCE;
+            leftWakeUpDistance = INITIAL_WAKE_UP_DISTANCE;
+        } else {
+            // grow the wake up distance to make it harder to wake up
+            leftWakeUpDistance = Math.min(leftWakeUpDistance + INCREMENTAL_WAKE_UP_DISTANCE, MAX_WAKE_UP_DISTANCE);
+        }
+    } else {
+        // we are awake, determine if we should fall asleep, if we haven't moved
+        // at least as much as our sleep distance then we sleep
+        if (leftDistance < leftSleepDistance) {
+            leftAsleep = true;
+            leftSleepDistance = INITIAL_SLEEP_DISTANCE;
+            leftWakeUpDistance = INITIAL_WAKE_UP_DISTANCE;
+        } else {
+            // if we moved more than the sleep amount, but we moved less than the max sleep
+            // amount, then increase our liklihood of sleep.
+            if (leftDistance < MAX_SLEEP_DISTANCE) {
+                print("growing sleep....");
+                leftSleepDistance = Math.max(leftSleepDistance + INCREMENTAL_SLEEP_DISTANCE, MAX_SLEEP_DISTANCE);
+            } else {
+                // otherwise reset it to initial 
+                leftSleepDistance = INITIAL_SLEEP_DISTANCE;
+            }
+        }
     }
-    if (Vec3.equal(rotatedRight, Vec3.UNIT_NEG_Y)) {
-        rotatedRight = rotatedLeft;
+    if (leftAsleep) {
+        suppressLeft = true;
+        debugPrint("suppressing left not moving enough");
     }
 
-    // Keep track of last rotations, to potentially detect resting (but not on
-    // base station hands) in the future
+    // check to see if hand should wakeup or sleep
+    if (rightAsleep) {
+        if (rightDistance > rightWakeUpDistance) {
+            rightAsleep = false;
+            rightSleepDistance = INITIAL_SLEEP_DISTANCE;
+            rightWakeUpDistance = INITIAL_WAKE_UP_DISTANCE;
+        } else {
+            // grow the wake up distance to make it harder to wake up
+            rightWakeUpDistance = Math.min(rightWakeUpDistance + INCREMENTAL_WAKE_UP_DISTANCE, MAX_WAKE_UP_DISTANCE);
+        }
+    } else {
+        // we are awake, determine if we should fall asleep, if we haven't moved
+        // at least as much as our sleep distance then we sleep
+        if (rightDistance < rightSleepDistance) {
+            rightAsleep = true;
+            rightSleepDistance = INITIAL_SLEEP_DISTANCE;
+            rightWakeUpDistance = INITIAL_WAKE_UP_DISTANCE;
+        } else {
+            // if we moved more than the sleep amount, but we moved less than the max sleep
+            // amount, then increase our liklihood of sleep.
+            if (rightDistance < MAX_SLEEP_DISTANCE) {
+                print("growing sleep....");
+                rightSleepDistance = Math.max(rightSleepDistance + INCREMENTAL_SLEEP_DISTANCE, MAX_SLEEP_DISTANCE);
+            } else {
+                // otherwise reset it to initial 
+                rightSleepDistance = INITIAL_SLEEP_DISTANCE;
+            }
+        }
+    }
+    if (rightAsleep) {
+        suppressRight = true;
+        debugPrint("suppressing right not moving enough");
+    }
+
+    // check to see if hand is on base station
+    if (Vec3.equal(rotatedLeft, Vec3.UNIT_NEG_Y)) {
+        suppressLeft = true;
+        debugPrint("suppressing left on base station");
+    }
+    if (Vec3.equal(rotatedRight, Vec3.UNIT_NEG_Y)) {
+        suppressRight = true;
+        debugPrint("suppressing right on base station");
+    }
+
+    // Keep track of last rotations, to detect resting (but not on base station hands) in the future
     lastRotatedLeft = rotatedLeft;
     lastRotatedRight = rotatedRight;
+
+    if (suppressLeft && suppressRight) {
+        debugPrint("both hands suppressed bail out early");
+        return;
+    }
+
+    if (suppressLeft) {
+        debugPrint("right only");
+        rotatedLeft = rotatedRight;
+    }
+    if (suppressRight) {
+        debugPrint("left only");
+        rotatedRight = rotatedLeft;
+    }
 
     // Average the two hand positions, if either hand is on base station, the
     // other hand becomes the only used hand and the average is the hand in use
