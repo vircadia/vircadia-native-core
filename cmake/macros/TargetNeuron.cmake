c35995b8e3363dda1ec0f129db5e6423cfd4c408
Anthony J. Thibault <tony@highfidelity.io>
2015-12-29 08:51:09 -0800

    Build fix for linux?

--- cmake/macros/TargetNeuron.cmake
+++ cmake/macros/TargetNeuron.cmake
@@ -6,9 +6,12 @@
 #  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 #
 macro(TARGET_NEURON)
-    add_dependency_external_projects(neuron)
-    find_package(Neuron REQUIRED)
-    target_include_directories(${TARGET_NAME} PRIVATE ${NEURON_INCLUDE_DIRS})
-    target_link_libraries(${TARGET_NAME} ${NEURON_LIBRARIES})
-    add_definitions(-DHAVE_NEURON)
+    # Neuron data reader is only available on these platforms
+    if (WIN32 OR APPLE)
+        add_dependency_external_projects(neuron)
+        find_package(Neuron REQUIRED)
+        target_include_directories(${TARGET_NAME} PRIVATE ${NEURON_INCLUDE_DIRS})
+        target_link_libraries(${TARGET_NAME} ${NEURON_LIBRARIES})
+        add_definitions(-DHAVE_NEURON)
+    endif(WIN32 OR APPLE)
 endmacro()
