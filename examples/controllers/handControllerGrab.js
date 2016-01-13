699431a9ab91a2648d113f91f6666259eb5adf8b
Seth Alves <seth.alves@gmail.com>
2016-01-12 17:27:36 -0800

    testing something with chris

--- examples/controllers/handControllerGrab.js
+++ examples/controllers/handControllerGrab.js
@@ -1039,13 +1039,13 @@ function MyController(hand) {
             this.handleParticleBeam(distantPickRay.origin, this.getHandRotation(), NO_INTERSECT_COLOR);
         }
 
-        if (USE_OVERLAY_LINES_FOR_SEARCHING === true) {
-            this.overlayLineOn(searchVisualizationPickRay.origin,
-                               Vec3.sum(searchVisualizationPickRay.origin,
-                                        Vec3.multiply(searchVisualizationPickRay.direction,
-                                                      LINE_LENGTH)),
-                               NO_INTERSECT_COLOR);
-        }
+        // if (USE_OVERLAY_LINES_FOR_SEARCHING === true) {
+        //     this.overlayLineOn(searchVisualizationPickRay.origin,
+        //                        Vec3.sum(searchVisualizationPickRay.origin,
+        //                                 Vec3.multiply(searchVisualizationPickRay.direction,
+        //                                               LINE_LENGTH)),
+        //                        NO_INTERSECT_COLOR);
+        // }
 
         if (this.intersectionDistance > 0) {
             var SPHERE_INTERSECTION_SIZE = 0.011;
