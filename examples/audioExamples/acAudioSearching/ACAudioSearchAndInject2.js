c8704234b19dd866fe69ed3469fdd991a4a66f96
ericrius1 <ericrius1>
2016-02-04 16:19:23 -0800

    basic setup for new design

--- examples/audioExamples/acAudioSearching/ACAudioSearchAndInject2.js
+++ examples/audioExamples/acAudioSearching/ACAudioSearchAndInject2.js
@@ -1,5 +1,5 @@
 //
-//  ACAudioSearchAndInject.js
+//  ACAudioSearchAndInject2.js
 //  audio
 //
 //  Created by Eric Levin 2/1/2016
@@ -15,25 +15,13 @@
 Script.include("https://rawgit.com/highfidelity/hifi/master/examples/libraries/utils.js");
 
 var SOUND_DATA_KEY = "soundKey";
-var MESSAGE_CHANNEL = "Hifi-Sound-Entity";
 
-// Map of all sound entities in domain- key is entity id, value is sound data
-var soundEntityMap = {};
-// Map of sound urls so a sound that's already been downloaded from one entity is not re-downloaded if 
-// another entity with same sound url is discovered
-var soundUrls = {};
+var QUERY_RADIUS = 5;
+
+EntityViewer.setKeyholeRadius(QUERY_RADIUS);
+Entities.setPacketsPerSecond(6000);
 
 Agent.isAvatar = true;
-
-EntityViewer.setPosition({
-    x: 0,
-    y: 0,
-    z: 0
-});
-
-EntityViewer.setKeyholeRadius(60000);
-Entities.setPacketsPerSecond(6000);
-EntityViewer.queryOctree();
 
 var DEFAULT_SOUND_DATA = {
     volume: 0.5,
@@ -43,13 +31,79 @@ var DEFAULT_SOUND_DATA = {
 };
 var MIN_INTERVAL = 0.2;
 
+print("EBL STARTING SCRIPT");
 
-// First, constantly query for avatars and get their position
-Script.setTimeout(function() {
-   var avatars = AvatarList.getAvatarIdentifiers();
-   var avatarID = avatars[0];
-   var avatar = AvatarList.getAvatar(avatarID);
-   print(" EBL avatar Position " + JSON.stringify(avatar.position));
-    
-}, 2000);
+function slowUpdate() {
+    var avatars = AvatarList.getAvatarIdentifiers();
+    print("AVATARS LENGTH " + avatars.length);
+    for (var i = 0; i < avatars.length; i++) {
+        print("In loop");
+        var avatar = AvatarList.getAvatar(avatars[i]);
+        EntityViewer.setPosition(avatar.position);
+        EntityViewer.queryOctree();
+        Script.setTimeout(function() {
+            var entities = Entities.findEntities(avatar.position, QUERY_RADIUS);
+            print("EBL ENTITIES LENGTH " + entities.length);
+        }, 1000);
+    }
+}
+Script.setInterval(slowUpdate, 1000);
 
+// Script.setInterval(function() {
+//     var avatars = AvatarList.getAvatarIdentifiers();
+//     print("Checking avatar identifiers" + JSON.stringify(avatars));
+// }, 2000)//
+//  ACAudioSearchAndInject2.js
+//  audio
+//
+//  Created by Eric Levin 2/1/2016
+//  Copyright 2016 High Fidelity, Inc.
+
+//  This AC script constantly searches for entities with a special userData field that specifies audio settings, and then
+//  injects the sound with the specified URL with other specified settings (playback volume) or playback at interval, or random interval
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+Script.include("https://rawgit.com/highfidelity/hifi/master/examples/libraries/utils.js");
+
+var SOUND_DATA_KEY = "soundKey";
+
+var QUERY_RADIUS = 5;
+
+EntityViewer.setKeyholeRadius(QUERY_RADIUS);
+Entities.setPacketsPerSecond(6000);
+
+Agent.isAvatar = true;
+
+var DEFAULT_SOUND_DATA = {
+    volume: 0.5,
+    loop: false,
+    interval: -1, // An interval of -1 means this sound only plays once (if it's non-looping) (In seconds)
+    intervalSpread: 0 // amount of randomness to add to the interval
+};
+var MIN_INTERVAL = 0.2;
+
+print("EBL STARTING SCRIPT");
+
+function slowUpdate() {
+    var avatars = AvatarList.getAvatarIdentifiers();
+    print("AVATARS LENGTH " + avatars.length);
+    for (var i = 0; i < avatars.length; i++) {
+        print("In loop");
+        var avatar = AvatarList.getAvatar(avatars[i]);
+        EntityViewer.setPosition(avatar.position);
+        EntityViewer.queryOctree();
+        Script.setTimeout(function() {
+            var entities = Entities.findEntities(avatar.position, QUERY_RADIUS);
+            print("EBL ENTITIES LENGTH " + entities.length);
+        }, 1000);
+    }
+}
+Script.setInterval(slowUpdate, 1000);
+
+// Script.setInterval(function() {
+//     var avatars = AvatarList.getAvatarIdentifiers();
+//     print("Checking avatar identifiers" + JSON.stringify(avatars));
+// }, 2000)
