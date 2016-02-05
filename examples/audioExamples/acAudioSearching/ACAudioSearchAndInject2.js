16af9b9e3165869e6717c879cb45cf6ae0c26d3a
ericrius1 <ericrius1>
2016-02-04 17:12:25 -0800

    deleting sound entity if it hasn't been around for a while

--- examples/audioExamples/acAudioSearching/ACAudioSearchAndInject2.js
+++ examples/audioExamples/acAudioSearching/ACAudioSearchAndInject2.js
@@ -1,16 +1,4 @@
 //
-//  ACAudioSearchAndInject2.js
-//  audio
-//
-//  Created by Eric Levin 2/1/2016
-//  Copyright 2016 High Fidelity, Inc.
-
-//  This AC script constantly searches for entities with a special userData field that specifies audio settings, and then
-//  injects the sound with the specified URL with other specified settings (playback volume) or playback at interval, or random interval
-//
-//  Distributed under the Apache License, Version 2.0.
-//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
-//
 
 Script.include("https://rawgit.com/highfidelity/hifi/master/examples/libraries/utils.js");
 
@@ -31,79 +19,72 @@ var DEFAULT_SOUND_DATA = {
 };
 var MIN_INTERVAL = 0.2;
 
-print("EBL STARTING SCRIPT");
+var UPDATE_TIME = 3000;
+var EXPIRATION_TIME = 5000; 
 
-function slowUpdate() {
-    var avatars = AvatarList.getAvatarIdentifiers();
-    print("AVATARS LENGTH " + avatars.length);
-    for (var i = 0; i < avatars.length; i++) {
-        print("In loop");
-        var avatar = AvatarList.getAvatar(avatars[i]);
-        EntityViewer.setPosition(avatar.position);
-        EntityViewer.queryOctree();
-        Script.setTimeout(function() {
-            var entities = Entities.findEntities(avatar.position, QUERY_RADIUS);
-            print("EBL ENTITIES LENGTH " + entities.length);
-        }, 1000);
-    }
-}
-Script.setInterval(slowUpdate, 1000);
-
-// Script.setInterval(function() {
-//     var avatars = AvatarList.getAvatarIdentifiers();
-//     print("Checking avatar identifiers" + JSON.stringify(avatars));
-// }, 2000)//
-//  ACAudioSearchAndInject2.js
-//  audio
-//
-//  Created by Eric Levin 2/1/2016
-//  Copyright 2016 High Fidelity, Inc.
-
-//  This AC script constantly searches for entities with a special userData field that specifies audio settings, and then
-//  injects the sound with the specified URL with other specified settings (playback volume) or playback at interval, or random interval
-//
-//  Distributed under the Apache License, Version 2.0.
-//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
-//
-
-Script.include("https://rawgit.com/highfidelity/hifi/master/examples/libraries/utils.js");
-
-var SOUND_DATA_KEY = "soundKey";
-
-var QUERY_RADIUS = 5;
-
-EntityViewer.setKeyholeRadius(QUERY_RADIUS);
-Entities.setPacketsPerSecond(6000);
-
-Agent.isAvatar = true;
-
-var DEFAULT_SOUND_DATA = {
-    volume: 0.5,
-    loop: false,
-    interval: -1, // An interval of -1 means this sound only plays once (if it's non-looping) (In seconds)
-    intervalSpread: 0 // amount of randomness to add to the interval
-};
-var MIN_INTERVAL = 0.2;
+var soundEntityMap = {};
 
 print("EBL STARTING SCRIPT");
 
 function slowUpdate() {
     var avatars = AvatarList.getAvatarIdentifiers();
-    print("AVATARS LENGTH " + avatars.length);
     for (var i = 0; i < avatars.length; i++) {
-        print("In loop");
         var avatar = AvatarList.getAvatar(avatars[i]);
         EntityViewer.setPosition(avatar.position);
         EntityViewer.queryOctree();
         Script.setTimeout(function() {
             var entities = Entities.findEntities(avatar.position, QUERY_RADIUS);
-            print("EBL ENTITIES LENGTH " + entities.length);
+            handleFoundSoundEntities(entities);
         }, 1000);
     }
+    handleActiveSoundEntities();
 }
-Script.setInterval(slowUpdate, 1000);
 
-// Script.setInterval(function() {
-//     var avatars = AvatarList.getAvatarIdentifiers();
-//     print("Checking avatar identifiers" + JSON.stringify(avatars));
-// }, 2000)
+function handleActiveSoundEntities() {
+    // Go through all our sound entities, if they have passed expiration time, remove them from map
+    for (var potentialSoundEntity in soundEntityMap) {
+        if (!soundEntityMap.hasOwnProperty(potentialSoundEntity)) {
+            // The current property is not a direct property of soundEntityMap so ignore it
+            continue;
+        }
+        var soundEntity = potentialSoundEntity;
+        var soundProperties = soundEntityMap[soundEntity];
+        soundProperties.timeWithoutAvatarInRange += UPDATE_TIME;
+        if (soundProperties.timeWithoutAvatarInRange > EXPIRATION_TIME) {
+            // An avatar hasn't been within range of this sound entity recently, so remove it from map
+            print ("NO AVATARS HAVE BEEN AROUND FOR A WHILE SO REMOVE THIS SOUND FROM SOUNDMAP!");
+            delete soundEntityMap[soundEntity];
+            print("NEW MAP " + JSON.stringify(soundEntityMap));
+        } 
+    }
+}
+
+function handleFoundSoundEntities(entities) {
+    entities.forEach(function(entity) {
+        var soundData = getEntityCustomData(SOUND_DATA_KEY, entity);
+        if (soundData && soundData.url) {
+            //check sound entities list- if it's not in, add it
+            if (!soundEntityMap[entity]) {
+                print("FOUND A NEW SOUND ENTITY!")
+                var soundProperties = {
+                    url: soundData.url,
+                    volume: soundData.volume || DEFAULT_SOUND_DATA.volume,
+                    loop: soundData.loop || DEFAULT_SOUND_DATA.loop,
+                    interval: soundData.interval || DEFAULT_SOUND_DATA.interval,
+                    intervalSpread: soundData.intervalSpread || DEFAULT_SOUND_DATA.intervalSpread,
+                    readyToPlay: true,
+                    position: Entities.getEntityProperties(entity, "position").position,
+                    timeSinceLastPlay: 0,
+                    timeWithoutAvatarInRange: 0
+                };
+
+                // If it's not in list, add it
+                soundEntityMap[entity] = soundProperties;
+            } else {
+                //If this sound is in our map already, we want to reset timeWithoutAvatarInRange 
+                soundEntityMap[entity].timeWithoutAvatarInRange = 0;
+            }
+        }
+    });
+}
+Script.setInterval(slowUpdate, UPDATE_TIME);
