82fcfc1d640ed242abe6fc52b7edc8d6577bbba6
Seth Alves <seth.alves@gmail.com>
2016-02-08 15:31:32 -0800

    don't apply clock-skew to a lastEdited of 0

--- libraries/entities/src/EntityTreeElement.cpp
+++ libraries/entities/src/EntityTreeElement.cpp
@@ -459,6 +459,7 @@ bool EntityTreeElement::bestFitEntityBounds(EntityItemPointer entity) const {
     bool success;
     auto queryCube = entity->getQueryAACube(success);
     if (!success) {
+        qDebug() << "EntityTreeElement::bestFitEntityBounds couldn't get queryCube for" << entity->getName() << entity->getID();
         return false;
     }
     return bestFitBounds(queryCube);
@@ -904,6 +905,8 @@ int EntityTreeElement::readElementDataFromBuffer(const unsigned char* data, int 
                         _myTree->entityChanged(entityItem);
                     }
                     bool bestFitAfter = bestFitEntityBounds(entityItem);
+                    qDebug() << "got update for" << entityItem->getName() << bestFitBefore << bestFitAfter <<
+                        entityItem->getQueryAACube();
 
                     if (bestFitBefore != bestFitAfter) {
                         // This is the case where the entity existed, and is in some element in our tree...
