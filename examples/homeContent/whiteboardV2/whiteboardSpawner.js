9e8d123b1664a6a189859eaeef9c31fe957f2451
ericrius1 <ericrius1>
2016-02-17 13:58:04 -0800

    spawning marker and whiteboard correctly

--- examples/homeContent/whiteboardV2/whiteboardSpawner.js
+++ examples/homeContent/whiteboardV2/whiteboardSpawner.js
@@ -14,24 +14,41 @@
 var orientation = Camera.getOrientation();
 orientation = Quat.safeEulerAngles(orientation);
 orientation.x = 0;
+var whiteboardRotation = Quat.fromVec3Degrees({x: orientation.x, y: orientation.y - 90, z: orientation.z});
 orientation = Quat.fromVec3Degrees(orientation);
-var center = Vec3.sum(MyAvatar.position, Vec3.multiply(2, Quat.getFront(orientation)));
 
 
+var whiteboardPosition = Vec3.sum(MyAvatar.position, Vec3.multiply(2, Quat.getFront(orientation)));
 var WHITEBOARD_MODEL_URL = "http://hifi-content.s3.amazonaws.com/alan/dev/Whiteboard-3.fbx";
 var WHITEBOARD_COLLISION_HULL_URL = "http://hifi-content.s3.amazonaws.com/alan/dev/Whiteboard.obj";
 var whiteboard = Entities.addEntity({
     type: "Model",
     modelURL: WHITEBOARD_MODEL_URL,
-    position: center,
+    position: whiteboardPosition,
+    rotation: whiteboardRotation,
     shapeType: 'compound',
     compoundShapeURL: WHITEBOARD_COLLISION_HULL_URL,
     dimensions: { x: 0.4636, y: 2.7034, z: 1.8653}
 });
 
+var markerPosition = Vec3.sum(MyAvatar.position, Vec3.multiply(1.9, Quat.getFront(orientation)));
+var MARKER_MODEL_URL = "http://hifi-content.s3.amazonaws.com/alan/dev/marker-blue.fbx";
+var marker = Entities.addEntity({
+    type: "Model",
+    modelURL: MARKER_MODEL_URL,
+    shapeType: "box",
+    dynamic: true,
+    gravity: {x: 0, y: -1, z: 0},
+    rotation: whiteboardRotation,
+    velocity: {x: 0, y: -0.1, z: 0},
+    position: markerPosition,
+    dimensions: {x: 0.0270, y: 0.0272, z: 0.1641}
+});
+
 
 function cleanup() {
     Entities.deleteEntity(whiteboard);
+    Entities.deleteEntity(marker);
 }
 
 Script.scriptEnding.connect(cleanup);
