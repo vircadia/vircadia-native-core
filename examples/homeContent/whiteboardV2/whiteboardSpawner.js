bfc619497e735861ccd9ae4f956a51df2cb6becd
ericrius1 <ericrius1>
2016-02-17 13:33:19 -0800

    spawning model

---
+++ examples/homeContent/whiteboardV2/whiteboardSpawner.js
@@ -0,0 +1,32 @@
+//
+//  whiteboardSpawner.js
+//  examples/homeContent/whiteboardV2
+//
+//  Created by Eric Levina on 2/17/16
+//  Copyright 2016 High Fidelity, Inc.
+//
+//  Run this script to spawn a whiteboard and associated acoutrements that one can paint on usignmarkers
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+
+
+var orientation = Camera.getOrientation();
+orientation = Quat.safeEulerAngles(orientation);
+orientation.x = 0;
+orientation = Quat.fromVec3Degrees(orientation);
+var center = Vec3.sum(MyAvatar.position, Vec3.multiply(2, Quat.getFront(orientation)));
+
+var WHITEBOARD_MODEL_URL = "http://hifi-content.s3.amazonaws.com/alan/dev/Whiteboard-3.fbx";
+var whiteboard = Entities.addEntity({
+    type: "Model",
+    modelURL: WHITEBOARD_MODEL_URL,
+    position: center
+});
+
+
+function cleanup() {
+    Entities.deleteEntity(whiteboard);
+}
+
+Script.scriptEnding.connect(cleanup);
