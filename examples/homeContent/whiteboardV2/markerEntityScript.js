94bf15f3284e8a5c13fb466556474c2b3cf7c09a
ericrius1 <ericrius1>
2016-02-19 11:21:17 -0800

    need to fix zfighting

--- examples/homeContent/whiteboardV2/markerEntityScript.js
+++ examples/homeContent/whiteboardV2/markerEntityScript.js
@@ -18,7 +18,9 @@
 
     MarkerTip = function() {
         _this = this;
-        _this.MARKER_TEXTURE_URL = "https://s3-us-west-1.amazonaws.com/hifi-content/eric/textures/paintStroke.png";
+        _this.MARKER_TEXTURE_URL = "https://s3-us-west-1.amazonaws.com/hifi-content/eric/textures/markerStroke.png";
+        this.strokeForwardOffset = 0.0005;
+        this.STROKE_FORWARD_OFFSET_INCRERMENT = 0.00001;
     };
 
     MarkerTip.prototype = {
@@ -76,8 +78,11 @@
             }
 
             var localPoint = Vec3.subtract(position, this.strokeBasePosition);
+            // localPoint = Vec3.sum(localPoint, Vec3.multiply(_this.whiteboardNormal, _this.strokeForwardOffset));
+            _this.strokeForwardOffset += _this.STROKE_FORWARD_OFFSET_INCRERMENT;
+
             _this.linePoints.push(localPoint);
-            _this.normals.push(_this._whiteboardNormal);
+            _this.normals.push(_this.whiteboardNormal);
             this.strokeWidths.push(0.02);
 
             Entities.editEntity(_this.currentStroke, {
@@ -99,7 +104,7 @@
         setWhiteboard: function(myId, data) {
             _this.whiteboard = JSON.parse(data[0]);
             var props = Entities.getEntityProperties(_this.whiteboard, ["rotation"]);
-            this._whiteboardNormal = Vec3.multiply(Quat.getRight(props.rotation), -1);
+            _this.whiteboardNormal = Vec3.multiply(Quat.getRight(props.rotation), -1);
         }
     };
 
