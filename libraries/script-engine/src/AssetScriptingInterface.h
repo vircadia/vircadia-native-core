5a197838f4785020776964499800c5babf8a6637
Stephen Birarda <commit@birarda.com>
2016-03-09 13:14:39 -0800

    add a deleteMapping call for single path to scripting interface

--- libraries/script-engine/src/AssetScriptingInterface.h
+++ libraries/script-engine/src/AssetScriptingInterface.h
@@ -29,6 +29,7 @@ public:
     Q_INVOKABLE void setMapping(QString path, QString hash, QScriptValue callback);
     Q_INVOKABLE void getMapping(QString path, QScriptValue callback);
     Q_INVOKABLE void deleteMappings(QStringList paths, QScriptValue callback);
+    Q_INVOKABLE void deleteMapping(QString path, QScriptValue callback) { deleteMappings(QStringList(path), callback); }
     Q_INVOKABLE void getAllMappings(QScriptValue callback);
     Q_INVOKABLE void renameMapping(QString oldPath, QString newPath, QScriptValue callback);
 protected:
