8903a61c6e73a92620c433d1abfe973bdd164778
ericrius1 <ericrius1>
2016-03-09 10:24:46 -0800

    basic shader

---
+++ examples/tests/rapidUniformChange/rapidUniformChangeTest.js
@@ -0,0 +1,44 @@
+
+//  rapidUniformChangeTest.js
+//  examples
+//
+//  Created by Eric Levin on  3/9/2016.
+//  Copyright 2016 High Fidelity, Inc.
+//  This test creates a primitive with a fragment shader and rapidly updates its uniform values
+//  For the test to pass, the uniform should update at rate of update loop without the client crashing
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+var orientation = Camera.getOrientation();
+orientation = Quat.safeEulerAngles(orientation);
+orientation.x = 0;
+orientation = Quat.fromVec3Degrees(orientation);
+var center = Vec3.sum(MyAvatar.position, Vec3.multiply(3, Quat.getFront(orientation)));
+
+var SHADER_URL = "file:///C:/Users/Eric/hifi/examples/tests/rapidUniformChange/rapidUniformChangeTest.fs";
+
+var testEntity = Entities.addEntity({
+    type: "Box",
+    dimensions: {
+        x: 0.5,
+        y: 0.5,
+        z: 0.5
+    },
+    position: center,
+    userData: JSON.stringify({
+        ProceduralEntity: {
+            shaderUrl: SHADER_URL,
+            uniforms: {
+                red: 0.5
+            }
+        }
+    })
+});
+
+Script.scriptEnding.connect(cleanup);
+
+function cleanup() {
+    Entities.deleteEntity(testEntity);
+}
