7efe26e8543e4dcdff05f4ff650f1323d737b0f6
Atlante45 <clement.brisset@gmail.com>
2016-03-11 11:53:14 -0800

    Disable asset browser if user can't rez

--- interface/src/Menu.cpp
+++ interface/src/Menu.cpp
@@ -130,8 +130,12 @@ Menu::Menu() {
     editMenu->addSeparator();
 
     // Edit > My Asset Server
-    addActionToQMenuAndActionHash(editMenu, MenuOption::AssetServer, Qt::CTRL | Qt::SHIFT | Qt::Key_A,
-                                  qApp, SLOT(toggleAssetServerWidget()));
+    auto assetServerAction = addActionToQMenuAndActionHash(editMenu, MenuOption::AssetServer,
+                                                           Qt::CTRL | Qt::SHIFT | Qt::Key_A,
+                                                           qApp, SLOT(toggleAssetServerWidget()));
+    auto nodeList = DependencyManager::get<NodeList>();
+    QObject::connect(nodeList.data(), &NodeList::canRezChanged, assetServerAction, &QAction::setEnabled);
+    assetServerAction->setEnabled(nodeList->getThisNodeCanRez());
 
     // Edit > Reload All Content [advanced]
     addActionToQMenuAndActionHash(editMenu, MenuOption::ReloadContent, 0, qApp, SLOT(reloadResourceCaches()),
