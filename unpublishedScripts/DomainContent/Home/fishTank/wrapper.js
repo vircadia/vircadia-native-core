87bdffa2738450980fe48e2f239b7f8b1deb0f3a
James B. Pollack <jamesbradenpollack@gmail.com>
2016-03-18 17:28:48 -0700

    visualize them

--- unpublishedScripts/DomainContent/Home/fishTank/wrapper.js
+++ unpublishedScripts/DomainContent/Home/fishTank/wrapper.js
@@ -265,7 +265,7 @@ FishTank = function(spawnPosition, spawnRotation) {
             },
             collisionless: true,
             position: getOffsetFromTankCenter(LOWER_CORNER_VERTICAL_OFFSET, LOWER_CORNER_FORWARD_OFFSET, LOWER_CORNER_LATERAL_OFFSET),
-            visible: false
+            visible: true
         }
 
         var upperProps = {
@@ -284,7 +284,7 @@ FishTank = function(spawnPosition, spawnRotation) {
             },
             collisionless: true,
             position: getOffsetFromTankCenter(UPPER_CORNER_VERTICAL_OFFSET, UPPER_CORNER_FORWARD_OFFSET, UPPER_CORNER_LATERAL_OFFSET),
-            visible: false
+            visible: true
         }
 
         lowerCorner = Entities.addEntity(lowerProps);
