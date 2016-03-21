c71d8914d5527a6e3b31ffe67dbfd9b209370338
James B. Pollack <jamesbradenpollack@gmail.com>
2016-03-21 10:24:50 -0700

    clean

--- unpublishedScripts/DomainContent/Home/reset.js
+++ unpublishedScripts/DomainContent/Home/reset.js
@@ -118,15 +118,15 @@
 
         createDynamicEntities: function() {
 
-            // var fishTank = new FishTank({
-            //     x: 1098.9254,
-            //     y: 460.5814,
-            //     z: -79.1103
-            // }, {
-            //     x: 0,
-            //     y: 0,
-            //     z: 0
-            // });
+            var fishTank = new FishTank({
+                x: 1098.9254,
+                y: 460.5814,
+                z: -79.1103
+            }, {
+                x: 0,
+                y: 0,
+                z: 0
+            });
 
 
             var tiltMaze = new TiltMaze({
