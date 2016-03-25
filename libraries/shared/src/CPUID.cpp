c4a87925e2355f5e49e22bd8a3f73aef8599421a
Ryan Huffman <ryanhuffman@gmail.com>
2016-03-25 15:34:53 -0700

    Add system information to log file

---
+++ libraries/shared/src/CPUID.cpp
@@ -0,0 +1,13 @@
+//
+//  CPUID.cpp
+//
+//  Created by Ryan Huffman on 3/25/16.
+//  Copyright 2016 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#include "CPUID.h"
+
+const CPUID::CPUID_Internal CPUID::CPU_Rep;
