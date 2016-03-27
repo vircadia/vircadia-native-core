b3b58b561b8d74c565be56ebaacb8580d9266984
James B. Pollack <jamesbradenpollack@gmail.com>
2016-03-27 13:15:48 -0700

    remove drawers from reset script

--- unpublishedScripts/DomainContent/Home/reset.js
+++ unpublishedScripts/DomainContent/Home/reset.js
@@ -216,17 +216,17 @@
                 z: -80.2837
             });
 
-            var rightDeskDrawer = new RightDeskDrawer({
-                x: 1105.1735,
-                y: 460.0446,
-                z: -81.3612
-            });
+            // var rightDeskDrawer = new RightDeskDrawer({
+            //     x: 1105.1735,
+            //     y: 460.0446,
+            //     z: -81.3612
+            // });
 
-            var leftDeskDrawer = new LeftDeskDrawer({
-                x: 1104.6478,
-                y: 460.0463,
-                z: -82.1095
-            });
+            // var leftDeskDrawer = new LeftDeskDrawer({
+            //     x: 1104.6478,
+            //     y: 460.0463,
+            //     z: -82.1095
+            // });
 
             var chair = new Chair({
                 x: 1105.2716,
