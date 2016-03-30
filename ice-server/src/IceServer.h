1771875186873097789cbabdbd376b6925ae974d
Stephen Birarda <commit@birarda.com>
2016-03-30 11:31:46 -0700

    use one QNetworkAccessManager for the ice-server

--- ice-server/src/IceServer.h
+++ ice-server/src/IceServer.h
@@ -54,6 +54,8 @@ private:
 
     HTTPManager _httpManager;
 
+    QNetworkAccessManager _networkAccessManager;
+
     using RSAUniquePtr = std::unique_ptr<RSA, std::function<void(RSA*)>>;
     using DomainPublicKeyHash = std::unordered_map<QUuid, RSAUniquePtr>;
     DomainPublicKeyHash _domainPublicKeys;
