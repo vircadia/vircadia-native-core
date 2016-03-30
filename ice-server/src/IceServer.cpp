4210bc178e1302275a75fd2a7c8f6814e01facb3
Stephen Birarda <commit@birarda.com>
2016-03-30 11:44:55 -0700

    default the inactive timestamp for healthy startup

--- ice-server/src/IceServer.cpp
+++ ice-server/src/IceServer.cpp
@@ -34,7 +34,8 @@ IceServer::IceServer(int argc, char* argv[]) :
     _id(QUuid::createUuid()),
     _serverSocket(),
     _activePeers(),
-    _httpManager(QHostAddress::AnyIPv4, ICE_SERVER_MONITORING_PORT, QString("%1/web/").arg(QCoreApplication::applicationDirPath()), this)
+    _httpManager(QHostAddress::AnyIPv4, ICE_SERVER_MONITORING_PORT, QString("%1/web/").arg(QCoreApplication::applicationDirPath()), this),
+	_lastInactiveCheckTimestamp(QDateTime::currentMSecsSinceEpoch())
 {
     // start the ice-server socket
     qDebug() << "ice-server socket is listening on" << ICE_SERVER_DEFAULT_PORT;
