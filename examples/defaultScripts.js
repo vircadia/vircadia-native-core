eaa0ba9e32050e606be26d6822b294477dc8649f
Stephen Birarda <commit@birarda.com>
2016-04-01 14:08:30 -0700

    rename marketplace.js to examples.js

--- examples/defaultScripts.js
+++ examples/defaultScripts.js
@@ -11,7 +11,7 @@
 Script.load("away.js");
 Script.load("progress.js");
 Script.load("edit.js");
-Script.load("marketplace.js");
+Script.load("examples.js");
 Script.load("selectAudioDevice.js");
 Script.load("notifications.js");
 Script.load("users.js");
