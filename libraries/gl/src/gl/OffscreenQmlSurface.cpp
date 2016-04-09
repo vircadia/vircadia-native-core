7827c0f6f6039aa28615d3e760b7b746f2ba2149
Brad Davis <bdavis@saintandreas.org>
2016-04-08 17:09:11 -0700

    Suppress key release events for presses that were absorbed by QML

--- libraries/gl/src/gl/OffscreenQmlSurface.cpp
+++ libraries/gl/src/gl/OffscreenQmlSurface.cpp
@@ -569,7 +569,7 @@ QPointF OffscreenQmlSurface::mapToVirtualScreen(const QPointF& originalPoint, QO
 // Event handling customization
 //
 
-bool OffscreenQmlSurface::eventFilter(QObject* originalDestination, QEvent* event) {
+bool OffscreenQmlSurface::filterEnabled(QObject* originalDestination, QEvent* event) const {
     if (_renderer->_quickWindow == originalDestination) {
         return false;
     }
@@ -577,7 +577,13 @@ bool OffscreenQmlSurface::eventFilter(QObject* originalDestination, QEvent* even
     if (_paused) {
         return false;
     }
+    return true;
+}
 
+bool OffscreenQmlSurface::eventFilter(QObject* originalDestination, QEvent* event) {
+    if (!filterEnabled(originalDestination, event)) {
+        return false;
+    }
 #ifdef DEBUG
     // Don't intercept our own events, or we enter an infinite recursion
     QObject* recurseTest = originalDestination;
