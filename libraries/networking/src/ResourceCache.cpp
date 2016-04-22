5ea8b83745aee65fafb4d54435dd2454c95aade8
Stephen Birarda <commit@birarda.com>
2016-04-21 17:16:21 -0700

    use noquote so URLs are not quoted

--- libraries/networking/src/ResourceCache.cpp
+++ libraries/networking/src/ResourceCache.cpp
@@ -520,7 +520,7 @@ void Resource::attemptRequest() {
     _startedLoading = true;
 
     if (_attempts > 0) {
-        qCDebug(networking) << "Server unavailable for" << _url
+        qCDebug(networking).noquote() << "Server unavailable for" << _url
             << "- retrying asset load - attempt" << _attempts << " of " << MAX_ATTEMPTS;
     }
 
@@ -614,7 +614,7 @@ void Resource::handleReplyFinished() {
                 if (_attempts++ < MAX_ATTEMPTS) {
                     auto waitTime = BASE_DELAY_MS * (int)pow(2.0, _attempts);
 
-                    qCDebug(networking) << "Server unavailable for" << _url << "- may retry in" << waitTime << "ms"
+                    qCDebug(networking).noquote() << "Server unavailable for" << _url << "- may retry in" << waitTime << "ms"
                         << "if resource is still needed";
 
                     QTimer::singleShot(waitTime, this, &Resource::attemptRequest);
