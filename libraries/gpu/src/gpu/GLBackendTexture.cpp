407e0c0dd74fa260a08de6e05e2c48bdd7ca2c46
Bradley Austin Davis <bdavis@saintandreas.org>
2016-04-22 15:48:18 -0700

    Fixing texture storage allocation on OSX/GL 4.1

--- libraries/gpu/src/gpu/GLBackendTexture.cpp
+++ libraries/gpu/src/gpu/GLBackendTexture.cpp
@@ -243,7 +243,7 @@ void GLBackend::GLTexture::allocateStorage() {
         glTexStorage2D(_target, usedMipLevels(), texelFormat.internalFormat, dimensions.x, dimensions.y);
         (void)CHECK_GL_ERROR();
     } else {
-        for (uint16_t l = _minMip; l < _maxMip; l++) {
+        for (uint16_t l = _minMip; l <= _maxMip; l++) {
             // Get the mip level dimensions, accounting for the downgrade level
             Vec3u dimensions = _gpuTexture.evalMipDimensions(l);
             for (GLenum target : getFaceTargets()) {
