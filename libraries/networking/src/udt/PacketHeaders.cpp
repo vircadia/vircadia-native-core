0ec7eae58ca7191531d7464ceebca40ddf5895d4
Brad Hefta-Gaub <brad@highfidelity.io>
2016-05-27 16:59:42 -0700

    fix shapes to property polymorph and persist

--- libraries/networking/src/udt/PacketHeaders.cpp
+++ libraries/networking/src/udt/PacketHeaders.cpp
@@ -49,7 +49,7 @@ PacketVersion versionForPacketType(PacketType packetType) {
         case PacketType::EntityAdd:
         case PacketType::EntityEdit:
         case PacketType::EntityData:
-            return VERSION_ENTITIES_MORE_SHAPES;
+            return VERSION_ENTITIES_PROPERLY_ENCODE_SHAPE_EDITS;
         case PacketType::AvatarIdentity:
         case PacketType::AvatarData:
         case PacketType::BulkAvatarData:
