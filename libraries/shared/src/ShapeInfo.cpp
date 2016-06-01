9dc0fa77962dfaa299adbb0209936b2d95244d3f
Andrew Meadows <andrew@highfidelity.io>
2016-06-01 12:20:57 -0700

    don't automatically clear ShapeInfo points

--- libraries/shared/src/ShapeInfo.cpp
+++ libraries/shared/src/ShapeInfo.cpp
@@ -23,7 +23,6 @@ void ShapeInfo::clear() {
 
 void ShapeInfo::setParams(ShapeType type, const glm::vec3& halfExtents, QString url) {
     _type = type;
-    _points.clear();
     switch(type) {
         case SHAPE_TYPE_NONE:
             _halfExtents = glm::vec3(0.0f);
@@ -52,7 +51,6 @@ void ShapeInfo::setBox(const glm::vec3& halfExtents) {
     _url = "";
     _type = SHAPE_TYPE_BOX;
     _halfExtents = halfExtents;
-    _points.clear();
     _doubleHashKey.clear();
 }
 
@@ -60,7 +58,6 @@ void ShapeInfo::setSphere(float radius) {
     _url = "";
     _type = SHAPE_TYPE_SPHERE;
     _halfExtents = glm::vec3(radius, radius, radius);
-    _points.clear();
     _doubleHashKey.clear();
 }
 
@@ -74,7 +71,6 @@ void ShapeInfo::setCapsuleY(float radius, float halfHeight) {
     _url = "";
     _type = SHAPE_TYPE_CAPSULE_Y;
     _halfExtents = glm::vec3(radius, halfHeight, radius);
-    _points.clear();
     _doubleHashKey.clear();
 }
 
