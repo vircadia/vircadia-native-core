a1766539f402a249758f87640163e6ea2edded26
Seth Alves <seth.alves@gmail.com>
2016-06-08 12:17:07 -0700

    don't do simple simulation on children of avatars

--- libraries/entities/src/EntitySimulation.cpp
+++ libraries/entities/src/EntitySimulation.cpp
@@ -261,7 +261,11 @@ void EntitySimulation::moveSimpleKinematics(const quint64& now) {
     SetOfEntities::iterator itemItr = _simpleKinematicEntities.begin();
     while (itemItr != _simpleKinematicEntities.end()) {
         EntityItemPointer entity = *itemItr;
-        if (entity->isMovingRelativeToParent() && !entity->getPhysicsInfo()) {
+        if (entity->isMovingRelativeToParent() &&
+            !entity->getPhysicsInfo() &&
+            // The entity-server doesn't know where avatars are, so don't attempt to do simple extrapolation for
+            // children of avatars.
+            !entity->hasAncestorOfType(NestableType::Avatar)) {
             entity->simulate(now);
             _entitiesToSort.insert(entity);
             ++itemItr;
