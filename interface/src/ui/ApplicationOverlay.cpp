f0b78eae478e69cbcefe78b1efc9ef75595f924f
SamGondelman <samuel_gondelman@brown.edu>
2016-06-21 16:48:32 -0700

    fix mini mirror

--- interface/src/ui/ApplicationOverlay.cpp
+++ interface/src/ui/ApplicationOverlay.cpp
@@ -169,8 +169,10 @@ void ApplicationOverlay::renderRearView(RenderArgs* renderArgs) {
         float renderRatio = ((float)qApp->getRenderResolutionScale());
         
         auto viewport = qApp->getMirrorViewRect();
-        glm::vec2 bottomLeft(viewport.left(), viewport.top() + screenRatio * viewport.height());
-        glm::vec2 topRight(viewport.left() + screenRatio * viewport.width(), viewport.top());
+        glm::vec2 bottomLeft(viewport.left(), viewport.top() + viewport.height());
+        glm::vec2 topRight(viewport.left() + viewport.width(), viewport.top());
+        bottomLeft *= screenRatio;
+        topRight *= screenRatio;
         glm::vec2 texCoordMinCorner(0.0f, 0.0f);
         glm::vec2 texCoordMaxCorner(viewport.width() * renderRatio / float(selfieTexture->getWidth()), viewport.height() * renderRatio / float(selfieTexture->getHeight()));
 
