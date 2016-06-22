890de1bfea6569b8b569d012638a70641bc9a470
Ryan Huffman <ryanhuffman@gmail.com>
2016-06-22 08:54:58 -0700

    Add went-to user activity back in

--- libraries/networking/src/UserActivityLogger.cpp
+++ libraries/networking/src/UserActivityLogger.cpp
@@ -18,6 +18,7 @@
 
 #include "UserActivityLogger.h"
 #include <DependencyManager.h>
+#include "AddressManager.h"
 
 static const QString USER_ACTIVITY_URL = "/api/v1/user_activities";
 
@@ -161,12 +162,34 @@ void UserActivityLogger::loadedScript(QString scriptName) {
 
 }
 
-void UserActivityLogger::wentTo(QString destinationType, QString destinationName) {
+void UserActivityLogger::wentTo(AddressManager::LookupTrigger lookupTrigger, QString destinationType, QString destinationName) {
+    // Only accept these types of triggers. Other triggers are usually used internally in AddressManager.
+    QString trigger;
+    switch (lookupTrigger) {
+        case AddressManager::UserInput:
+            trigger = "UserInput";
+            break;
+        case AddressManager::Back:
+            trigger = "Back";
+            break;
+        case AddressManager::Forward:
+            trigger = "Forward";
+            break;
+        case AddressManager::StartupFromSettings:
+            trigger = "StartupFromSettings";
+            break;
+        default:
+            return;
+    }
+
+
     const QString ACTION_NAME = "went_to";
     QJsonObject actionDetails;
+    const QString TRIGGER_TYPE_KEY = "trigger";
     const QString DESTINATION_TYPE_KEY = "destination_type";
     const QString DESTINATION_NAME_KEY = "detination_name";
     
+    actionDetails.insert(TRIGGER_TYPE_KEY, trigger);
     actionDetails.insert(DESTINATION_TYPE_KEY, destinationType);
     actionDetails.insert(DESTINATION_NAME_KEY, destinationName);
     
