057fc8adcecfc059ebfc9898da3cc9b174e4c1aa
SamGondelman <samuel_gondelman@brown.edu>
2016-06-30 10:25:49 -0700

    endable blending

--- libraries/display-plugins/src/display-plugins/hmd/HmdDisplayPlugin.cpp
+++ libraries/display-plugins/src/display-plugins/hmd/HmdDisplayPlugin.cpp
@@ -439,6 +439,8 @@ void HmdDisplayPlugin::internalPresent() {
             _firstPreview = false;
         }
         useProgram(_previewProgram);
+        glEnable (GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
         glClearColor(0, 0, 0, 1);
         glClear(GL_COLOR_BUFFER_BIT);
         glViewport(targetViewportPosition.x, targetViewportPosition.y, targetViewportSize.x, targetViewportSize.y);
