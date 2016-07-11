dc9c06a159810f0f3fdc039fd9896c1f172042ac
James B. Pollack <jamesbradenpollack@gmail.com>
2016-07-11 15:14:19 -0700

    clean util

--- scripts/developer/utilities/tools/overlayFinder.js
+++ scripts/developer/utilities/tools/overlayFinder.js
@@ -1,17 +1,17 @@
 function mousePressEvent(event) {
-    var overlay = Overlays.getOverlayAtPoint({x:event.x,y:event.y});
-    print('overlay is: ' + overlay)
-   //      var pickRay = Camera.computePickRay(event.x, event.y);
-
-   // var intersection =  Overlays.findRayIntersection(pickRay);
-   // print('intersection is: ' + intersection)
+    var overlay = Overlays.getOverlayAtPoint({
+        x: event.x,
+        y: event.y
+    });
+    // var pickRay = Camera.computePickRay(event.x, event.y);
+    // var intersection =  Overlays.findRayIntersection(pickRay);
+    // print('intersection is: ' + intersection)
 };
 
-Controller.mouseMoveEvent.connect(function(event){
-    print('mouse press')
+Controller.mousePressEvent.connect(function(event) {
     mousePressEvent(event)
 });
 
-Script.scriptEnding.connect(function(){
+Script.scriptEnding.connect(function() {
     Controller.mousePressEvent.disconnect(mousePressEvent);
 })
