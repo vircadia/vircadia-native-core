d84c7524bf76efd09a213d9557573d22232aa70b
Ken Cooke <ken@highfidelity.io>
2016-07-14 14:56:58 -0700

    Remove global HRTF headroom. The initial HRTF reduced overall gain by -6dB to avoid clipping at spectral peaks. With the addition of a peak limiter, this is no longer necessary. Changing to 0dB improves the loudness match between spatialized and unspatialized sounds.

--- libraries/audio/src/AudioHRTF.h
+++ libraries/audio/src/AudioHRTF.h
@@ -21,7 +21,7 @@ static const int HRTF_TABLES = 25;      // number of HRTF subjects
 static const int HRTF_DELAY = 24;       // max ITD in samples (1.0ms at 24KHz)
 static const int HRTF_BLOCK = 256;      // block processing size
 
-static const float HRTF_GAIN = 0.5f;    // HRTF global gain adjustment
+static const float HRTF_GAIN = 1.0f;    // HRTF global gain adjustment
 
 class AudioHRTF {
 
