c1216cbcaf98fedfda93be43576fcb24a5cf87f8
Andrew Meadows <andrew@highfidelity.io>
2016-07-14 15:05:47 -0700

    stubbery for CollisionRenderMeshCacheTests

---
+++ tests/physics/src/CollisionRenderMeshCacheTests.cpp
@@ -0,0 +1,41 @@
+//
+//  CollisionRenderMeshCacheTests.cpp
+//  tests/physics/src
+//
+//  Created by Andrew Meadows on 2014.10.30
+//  Copyright 2014 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#include <iostream>
+#include <CollisionRenderMeshCache.h>
+#include <StreamUtils.h>
+
+#include "CollisionRenderMeshCacheTests.h"
+
+QTEST_MAIN(CollisionRenderMeshCacheTests)
+
+
+void CollisionRenderMeshCacheTests::test001() {
+    CollisionRenderMeshCache cache;
+
+    // create a compound shape
+    int32_t numSubShapes = 3;
+
+
+    // get the mesh
+
+    // get the mesh again
+
+    // forget the mesh once
+
+    // collect garbage
+
+    // forget the mesh a second time
+
+    // collect garbage
+
+}
+
---
+++ tests/physics/src/CollisionRenderMeshCacheTests.h
@@ -0,0 +1,24 @@
+//
+//  CollisionRenderMeshCacheTests.h
+//  tests/physics/src
+//
+//  Created by Andrew Meadows on 2014.10.30
+//  Copyright 2014 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_CollisionRenderMeshCacheTests_h
+#define hifi_CollisionRenderMeshCacheTests_h
+
+#include <QtTest/QtTest>
+
+class CollisionRenderMeshCacheTests : public QObject {
+    Q_OBJECT
+
+private slots:
+    void test001();
+};
+
+#endif // hifi_CollisionRenderMeshCacheTests_h
