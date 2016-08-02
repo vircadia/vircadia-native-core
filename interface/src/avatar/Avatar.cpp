ed4b0b3589d700b532eb546cca007900826cd7a8
Seth Alves <seth.alves@gmail.com>
2016-08-02 09:57:17 -0700

    call _skeletonModel.setURL, but leave our copy of _skeletonModelURL alone.  This is so that we don't redo this every time we receive an identity packet from the avatar with the bad url.

--- interface/src/avatar/Avatar.cpp
+++ interface/src/avatar/Avatar.cpp
@@ -920,7 +920,9 @@ void Avatar::setSkeletonModelURL(const QUrl& skeletonModelURL) {
 void Avatar::setModelURLFinished(bool success) {
     if (!success && _skeletonModelURL != AvatarData::defaultFullAvatarModelUrl()) {
         qDebug() << "Using default after failing to load Avatar model: " << _skeletonModelURL;
-        QMetaObject::invokeMethod(this, "setSkeletonModelURL",
+        // call _skeletonModel.setURL, but leave our copy of _skeletonModelURL alone.  This is so that
+        // we don't redo this every time we receive an identity packet from the avatar with the bad url.
+        QMetaObject::invokeMethod(_skeletonModel.get(), "setURL",
                                   Qt::QueuedConnection, Q_ARG(QUrl, AvatarData::defaultFullAvatarModelUrl()));
     }
 }
