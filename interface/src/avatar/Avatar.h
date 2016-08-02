79121133dfc6e7528d8de0c91fe42ed20ccc4991
Seth Alves <seth.alves@gmail.com>
2016-08-02 12:00:45 -0700

    remove unneeded change

--- interface/src/avatar/Avatar.h
+++ interface/src/avatar/Avatar.h
@@ -115,7 +115,7 @@ public:
     virtual bool setAbsoluteJointRotationInObjectFrame(int index, const glm::quat& rotation) override { return false; }
     virtual bool setAbsoluteJointTranslationInObjectFrame(int index, const glm::vec3& translation) override { return false; }
 
-    Q_INVOKABLE virtual void setSkeletonModelURL(const QUrl& skeletonModelURL) override;
+    virtual void setSkeletonModelURL(const QUrl& skeletonModelURL) override;
     virtual void setAttachmentData(const QVector<AttachmentData>& attachmentData) override;
 
     void setShowDisplayName(bool showDisplayName);
