95c627aa711878ae60eb597dcf99e5bd33738da0
James B. Pollack <jamesbradenpollack@gmail.com>
2016-08-29 14:24:36 -0700

    dev

--- scripts/system/html/js/marketplaces.js
+++ scripts/system/html/js/marketplaces.js
@@ -1,3 +1,35 @@
-function loaded(){
-    
+function loaded() {
+    bindClaraImage();
+    bindClaraButton();
+}
+
+function hideList() {
+    $('.marketplace-tile').hide();
+}
+
+function showList() {
+    $('.marketplace-tile').show();
+}
+
+function showClaraHowTo() {
+    $('.claraHowTo').show();
+}
+
+function hideClaraHowTo() {
+    $('.claraHowTo').hide();
+}
+
+var claraVisible = false;
+
+function bindClaraImage() {
+    $('.clara-image').on('click', function() {
+        showClaraHowTo();
+       // hideList();
+    })
+}
+
+function bindClaraButton() {
+    $('#goToClara').on('click', function() {
+        window.location = "http://www.clara.io"
+    })
 }
