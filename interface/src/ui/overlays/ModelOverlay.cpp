847a5cc7a1a494708dffcab640e992ed19a7b44f
Ryan Huffman <ryanhuffman@gmail.com>
2016-09-20 12:55:15 -0700

    Remove old texture setting code in ModelOverlay

--- interface/src/ui/overlays/ModelOverlay.cpp
+++ interface/src/ui/overlays/ModelOverlay.cpp
@@ -123,18 +123,7 @@ void ModelOverlay::setProperties(const QVariantMap& properties) {
     if (texturesValue.isValid() && texturesValue.canConvert(QVariant::Map)) {
         QVariantMap textureMap = texturesValue.toMap();
         QMetaObject::invokeMethod(_model.get(), "setTextures", Qt::AutoConnection,
-                                    Q_ARG(const QVariantMap&, textureMap));
-//        foreach(const QString& key, textureMap.keys()) {
-//
-//            QUrl newTextureURL = textureMap[key].toUrl();
-//            qDebug() << "Updating texture named" << key << "to texture at URL" << newTextureURL;
-//
-//            QMetaObject::invokeMethod(_model.get(), "setTextureWithNameToURL", Qt::AutoConnection,
-//                                      Q_ARG(const QString&, key),
-//                                      Q_ARG(const QUrl&, newTextureURL));
-//
-//            _modelTextures[key] = newTextureURL;  // Keep local track of textures for getProperty()
-//        }
+                                  Q_ARG(const QVariantMap&, textureMap));
     }
 }
 
