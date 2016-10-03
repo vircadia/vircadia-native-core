b5eb943c1d906017045024c899b8d9309d9694de
Ryan Huffman <ryanhuffman@gmail.com>
2016-10-03 15:12:14 -0700

    Add hmd stats to stats event

--- interface/src/Application.cpp
+++ interface/src/Application.cpp
@@ -1134,6 +1134,7 @@ Application::Application(int& argc, char** argv, QElapsedTimer& startupTimer) :
         auto displayPlugin = qApp->getActiveDisplayPlugin();
 
         properties["fps"] = _frameCounter.rate();
+        properties["target_frame_rate"] = getTargetFrameRate();
         properties["present_rate"] = displayPlugin->presentRate();
         properties["new_frame_present_rate"] = displayPlugin->newFramePresentRate();
         properties["dropped_frame_rate"] = displayPlugin->droppedFrameRate();
@@ -1174,6 +1175,9 @@ Application::Application(int& argc, char** argv, QElapsedTimer& startupTimer) :
         properties["added_entity_cnt"] = entityActivityTracking.addedEntityCount;
         properties["deleted_entity_cnt"] = entityActivityTracking.deletedEntityCount;
         properties["edited_entity_cnt"] = entityActivityTracking.editedEntityCount;
+
+        properties["active_display_plugin"] = getActiveDisplayPlugin()->getName();
+        properties["using_hmd"] = isHMDMode();
 
         auto hmdHeadPose = getHMDSensorPose();
         properties["hmd_head_pose_changed"] = isHMDMode() && (hmdHeadPose != lastHMDHeadPose);
