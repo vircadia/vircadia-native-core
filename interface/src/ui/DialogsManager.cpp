7a3c14683ef92cc4bb244fc2ed5edfa41c509980
howard-stearns <howard.stearns@gmail.com>
2016-10-17 14:50:46 -0700

    switchable placeholder for domain-connection failure, currently a modal

--- interface/src/ui/DialogsManager.cpp
+++ interface/src/ui/DialogsManager.cpp
@@ -59,6 +59,10 @@ void DialogsManager::showFeed() {
     emit setUseFeed(true);
 }
 
+void DialogsManager::indicateDomainConnectionFailure() {
+    OffscreenUi::information("No Connection", "Unable to connect to this domain. Click the 'GO TO' button on the toolbar to visit another domain.");
+}
+
 void DialogsManager::toggleDiskCacheEditor() {
     maybeCreateDialog(_diskCacheEditor);
     _diskCacheEditor->toggle();
