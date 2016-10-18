81cb33574deff78ee1a8ac52f2a10fe42a9568ba
Stephen Birarda <commit@birarda.com>
2016-10-18 10:04:43 -0700

    check for HTTPS scheme in bearer add in XMLHttpRequest

--- libraries/script-engine/src/XMLHttpRequestClass.cpp
+++ libraries/script-engine/src/XMLHttpRequestClass.cpp
@@ -143,7 +143,7 @@ void XMLHttpRequestClass::open(const QString& method, const QString& url, bool a
         if (url.toLower().left(METAVERSE_API_URL.length()) == METAVERSE_API_URL) {
             auto accountManager = DependencyManager::get<AccountManager>();
                 
-            if (accountManager->hasValidAccessToken()) {
+            if (_url.scheme() == "https" && accountManager->hasValidAccessToken()) {
                 static const QString HTTP_AUTHORIZATION_HEADER = "Authorization";
                 QString bearerString = "Bearer " + accountManager->getAccountInfo().getAccessToken().token;
                 _request.setRawHeader(HTTP_AUTHORIZATION_HEADER.toLocal8Bit(), bearerString.toLocal8Bit());
