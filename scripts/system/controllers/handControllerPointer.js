d29b1c34b1cc564a0670cbe2d6035f8aa962111b
Dante Ruiz <dante@highfidelity.io>
2016-11-02 00:07:58 +0100

    fixed coding standard conflicts

--- scripts/system/controllers/handControllerPointer.js
+++ scripts/system/controllers/handControllerPointer.js
@@ -231,7 +231,7 @@ var gamePad = Controller.findDevice("GamePad");
 function activeHudPoint2d(activeHand) { // if controller is valid, update reticle position and answer 2d point. Otherwise falsey.
     var controllerPose = getControllerWorldLocation(activeHand, true); // note: this will return head pose if hand pose is invalid (third eye)
     if (!controllerPose.valid) {
-        if(gamePad){
+        if (gamePad) {
           return activeHudPoint2dGamePad();
         }
         return; // Controller is cradled.
@@ -458,7 +458,7 @@ clickMapping.from(Controller.Hardware.GamePad.Back).peek().to(function () {
 clickMapping.from(Controller.Hardware.Keyboard.RightMouseClicked).peek().to(function () {
     // Allow the reticle depth to be set correctly:
     // Wait a tick for the context menu to be displayed, and then simulate a (non-hand-controller) mouse move
-    // so that the system updates qml state (Reticle.pointingAtSystemOverlay) before it gives us a mouseMove.
+    // so that the system updates qml state (Reticle.pointingAtSystemverlay) before it gives us a mouseMove.
     // We don't want the system code to always do this for us, because, e.g., we do not want to get a mouseMove
     // after the Left/RightSecondaryThumb gives us a context menu. Only from the mouse.
     Script.setTimeout(function () {
