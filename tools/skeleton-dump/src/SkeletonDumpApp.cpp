1732448d3c3f6bd443ec17a660d995b36d0634b6
Anthony J. Thibault <tony@highfidelity.io>
2016-11-04 13:52:16 -0700

    Added tools/skeleton-dump tool
    Debug tool that prints out the skeleton hierarchy of fbx files including joint indices, bindPose and defaultPoses.
    The verbose option also prints the full FBX transformation set, pre/post rotations etc.

---
+++ tools/skeleton-dump/src/SkeletonDumpApp.cpp
@@ -0,0 +1,63 @@
+//
+//  SkeletonDumpApp.h
+//  tools/skeleton-dump/src
+//
+//  Created by Anthony Thibault on 11/4/16.
+//  Copyright 2016 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#include "SkeletonDumpApp.h"
+#include <QCommandLineParser>
+#include <QFile>
+#include <FBXReader.h>
+#include <AnimSkeleton.h>
+
+SkeletonDumpApp::SkeletonDumpApp(int argc, char* argv[]) : QCoreApplication(argc, argv) {
+
+    // parse command-line
+    QCommandLineParser parser;
+    parser.setApplicationDescription("High Fidelity FBX Skeleton Analyzer");
+    const QCommandLineOption helpOption = parser.addHelpOption();
+
+    const QCommandLineOption verboseOutput("v", "verbose output");
+    parser.addOption(verboseOutput);
+
+    const QCommandLineOption inputFilenameOption("i", "input file", "filename.fbx");
+    parser.addOption(inputFilenameOption);
+
+    if (!parser.parse(QCoreApplication::arguments())) {
+        qCritical() << parser.errorText() << endl;
+        parser.showHelp();
+        _returnCode = 1;
+        return;
+    }
+
+    if (parser.isSet(helpOption)) {
+        parser.showHelp();
+        return;
+    }
+
+    bool verbose = parser.isSet(verboseOutput);
+
+    QString inputFilename;
+    if (parser.isSet(inputFilenameOption)) {
+        inputFilename = parser.value(inputFilenameOption);
+    }
+
+    QFile file(inputFilename);
+    if (!file.open(QIODevice::ReadOnly)) {
+        qCritical() << "Failed to open file " << inputFilename;
+        _returnCode = 2;
+        return;
+    }
+    QByteArray blob = file.readAll();
+    std::unique_ptr<FBXGeometry> fbxGeometry(readFBX(blob, QVariantHash()));
+    std::unique_ptr<AnimSkeleton> skeleton(new AnimSkeleton(*fbxGeometry));
+    skeleton->dump(verbose);
+}
+
+SkeletonDumpApp::~SkeletonDumpApp() {
+}
---
+++ tools/skeleton-dump/src/SkeletonDumpApp.h
@@ -0,0 +1,29 @@
+//
+//  VHACDUtil.h
+//  tools/skeleton-dump/src
+//
+//  Created by Anthony Thibault on 11/4/16.
+//  Copyright 2016 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_SkeletonDumpApp_h
+#define hifi_SkeletonDumpApp_h
+
+#include <QApplication>
+
+class SkeletonDumpApp : public QCoreApplication {
+    Q_OBJECT
+public:
+    SkeletonDumpApp(int argc, char* argv[]);
+    ~SkeletonDumpApp();
+
+    int getReturnCode() const { return _returnCode; }
+
+private:
+    int _returnCode { 0 };
+};
+
+#endif //hifi_SkeletonDumpApp_h
