528b8e93f00c3a6ea5ea4b0627cbf7bdd5d4fe7d
Ryan Huffman <ryanhuffman@gmail.com>
2016-11-11 14:33:34 -0800

    Remove extraneous logging in DomainServer for whitelist

--- domain-server/src/DomainServer.cpp
+++ domain-server/src/DomainServer.cpp
@@ -1044,13 +1044,7 @@ void DomainServer::processRequestAssignmentPacket(QSharedPointer<ReceivedMessage
     };
 
     auto it = find_if(_acSubnetWhitelist.begin(), _acSubnetWhitelist.end(), isHostAddressInSubnet);
-    if (it != _acSubnetWhitelist.end()) {
-        static QString repeatedMessage = LogHandler::getInstance().addRepeatedMessageRegex(
-            "Received connection from whitelisted ip: [^ ]+ , matches subnet mask: [^ ]+");
-        auto maskString = it->first.toString() + "/" + QString::number(it->second);
-        qDebug() << "Received connection from whitelisted ip:" << senderAddr.toString()
-            << ", matches subnet mask:" << maskString;
-    } else {
+    if (it == _acSubnetWhitelist.end()) {
         static QString repeatedMessage = LogHandler::getInstance().addRepeatedMessageRegex(
             "Received an assignment connect request from a disallowed ip address: [^ ]+");
         qDebug() << "Received an assignment connect request from a disallowed ip address:"
