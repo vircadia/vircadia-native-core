9b4a8427e6cfa9d3d689b01c00926d537f49adae
Seth Alves <seth.alves@gmail.com>
2016-11-13 11:09:27 -0800

    global/static QString

--- plugins/hifiNeuron/src/NeuronPlugin.h
+++ plugins/hifiNeuron/src/NeuronPlugin.h
@@ -29,7 +29,7 @@ public:
 
     // Plugin functions
     virtual bool isSupported() const override;
-    virtual const QString& getName() const override { return NAME; }
+    virtual const char* getName() const override { return NAME; }
     const QString& getID() const override { return NEURON_ID_STRING; }
 
     virtual bool activate() override;
@@ -65,8 +65,8 @@ protected:
 
     std::shared_ptr<InputDevice> _inputDevice { std::make_shared<InputDevice>() };
 
-    static const QString NAME;
-    static const QString NEURON_ID_STRING;
+    static const char* NAME;
+    static const char* NEURON_ID_STRING;
 
     std::string _serverAddress;
     int _serverPort;
