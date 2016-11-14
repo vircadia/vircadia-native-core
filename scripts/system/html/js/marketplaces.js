e7d9f98ca436b1488152da767563d936fb78f015
David Rowe <david@ctrlaltstudio.com>
2016-11-15 09:23:51 +1300

    Get script injection working in new marketplaces window

--- scripts/system/html/js/marketplaces.js
+++ scripts/system/html/js/marketplaces.js
@@ -1,12 +1,23 @@
+//
+//  marketplaces.js
+//
+//  Copyright 2016 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
 function loaded() {
     bindExploreButtons();
 }
 
 function bindExploreButtons() {
-    $('#exploreClaraMarketplace').on('click', function() {
+    $('#exploreClaraMarketplace').on('click', function () {
+        EventBridge.emitWebEvent("INJECT_CLARA");
         window.location = "https://clara.io/library?gameCheck=true&public=true"
     })
-    $('#exploreHifiMarketplace').on('click', function() {
+    $('#exploreHifiMarketplace').on('click', function () {
+        EventBridge.emitWebEvent("INJECT_HIFI");
         window.location = "http://www.highfidelity.com/marketplace"
     })
 }
---
+++ scripts/system/html/js/marketplacesClara.js
@@ -0,0 +1,13 @@
+//
+//  marketplacesClara.js
+//
+//  Created by David Rowe on 12 Nov 2016.
+//  Copyright 2016 High Fidelity, Inc.
+//
+//  Injected into Clara.io marketplace Web page.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+console.log("Hello from marketplacesClara.js");
