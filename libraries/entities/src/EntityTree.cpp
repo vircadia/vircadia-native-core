66ff81f45066e2b890845ec3e8e0ee58fd38e5dc
Dante Ruiz <dante@highfidelity.io>
2016-11-16 22:32:15 +0000

    Another function name change

--- libraries/entities/src/EntityTree.cpp
+++ libraries/entities/src/EntityTree.cpp
@@ -438,7 +438,7 @@ void EntityTree::deleteEntity(const EntityItemID& entityID, bool force, bool ign
         return;
     }
 
-    IsAvatarParentOfEntity(entityID);
+    unhookChildAvatar(entityID);
     emit deletingEntity(entityID);
 
     // NOTE: callers must lock the tree before using this method
@@ -448,11 +448,11 @@ void EntityTree::deleteEntity(const EntityItemID& entityID, bool force, bool ign
     _isDirty = true;
 }
 
-void EntityTree::IsAvatarParentOfEntity(const EntityItemID entityID) {
+void EntityTree::unhookChildAvatar(const EntityItemID entityID) {
 
     EntityItemPointer entity = findEntityByEntityItemID(entityID);
 
-    entity->forEachChild([&](SpatiallyNestablePointer child) {
+    entity->forEachDescendant([&](SpatiallyNestablePointer child) {
         if (child->getNestableType() == NestableType::Avatar) {
             child->setParentID(nullptr);
         }
@@ -488,7 +488,7 @@ void EntityTree::deleteEntities(QSet<EntityItemID> entityIDs, bool force, bool i
         }
 
         // tell our delete operator about this entityID
-        IsAvatarParentOfEntity(entityID);
+        unhookChildAvatar(entityID);
         theOperator.addEntityIDToDeleteList(entityID);
         emit deletingEntity(entityID);
     }
--- libraries/entities/src/EntityTree.h
+++ libraries/entities/src/EntityTree.h
@@ -122,7 +122,7 @@ public:
     bool updateEntity(EntityItemPointer entity, const EntityItemProperties& properties, const SharedNodePointer& senderNode = SharedNodePointer(nullptr));
 
     // check if the avatar is a child of this entity, If so set the avatar parentID to null
-    void IsAvatarParentOfEntity(const EntityItemID entityID);
+    void unhookChildAvatar(const EntityItemID entityID);
     void deleteEntity(const EntityItemID& entityID, bool force = false, bool ignoreWarnings = true);
     void deleteEntities(QSet<EntityItemID> entityIDs, bool force = false, bool ignoreWarnings = true);
 
