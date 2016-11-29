251b070db314c5c7e13fd389f69bf78eb2adc55c
Ryan Jones <ryan.joshua.jones@gmail.com>
2016-11-29 11:50:11 -0800

    add unit test

---
+++ scripts/developer/tests/bindUnitTest.js
@@ -0,0 +1,31 @@
+Script.include('../libraries/jasmine/jasmine.js');
+Script.include('../libraries/jasmine/hifi-boot.js');
+Script.include('../../system/libraries/utils.js');
+
+describe('Bind', function() {
+    it('functions should have bind available', function() {
+        var foo = 'bar';
+
+        function callAnotherFn(anotherFn) {
+            return anotherFn();
+        }
+
+        function TestConstructor() {
+            this.foo = foo;
+        }
+
+        TestConstructor.prototype.doSomething = function() {
+            return callAnotherFn(function() {
+                return this.foo;
+            }.bind(this));
+        }
+
+        var instance = new TestConstructor();
+        
+        expect(typeof(instance.doSomething.bind) !== 'undefined');
+        expect(instance.doSomething()).toEqual(foo);
+    });
+});
+
+jasmine.getEnv().execute();
+Script.stop();
