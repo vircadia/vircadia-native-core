2216ab6deff5bc519bd8ee6ed36621a7ddaf2a8c
David Rowe <david@ctrlaltstudio.com>
2016-12-07 10:06:32 +1300

    Delete redundant code

--- libraries/script-engine/src/FileScriptingInterface.h
+++ libraries/script-engine/src/FileScriptingInterface.h
@@ -22,11 +22,7 @@ class FileScriptingInterface : public QObject {
 public:
     FileScriptingInterface(QObject* parent);
 
-
 public slots:
-    bool isZippedFbx(QUrl url);
-    bool isZipped(QUrl url);
-    bool isClaraLink(QUrl url);
     QString convertUrlToPath(QUrl url);
     void runUnzip(QString path, QUrl url, bool autoAdd);
     QString getTempDir();
