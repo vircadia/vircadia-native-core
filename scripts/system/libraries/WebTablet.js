cb4b2473e10de654e276e32438d17c8d3a55d116
Dante Ruiz <dante@highfidelity.io>
2016-12-16 18:30:15 +0000

    remmoved tabs as whitespace

--- scripts/system/libraries/WebTablet.js
+++ scripts/system/libraries/WebTablet.js
@@ -105,7 +105,7 @@ WebTablet = function (url, width, dpi, location, clientOnly) {
         dimensions: {x: 0.05, y: 0.05, z: 0.05},
         parentID: this.tabletEntityID,
         script: "https://people.ucsc.edu/~druiz4/scripts/homeButton.js"
-    }, clientOnly);
+        }, clientOnly);
 
 
 	setEntityCustomData('grabbableKey', this.homeButtonEntity, {wantsTrigger: true});
@@ -115,11 +115,11 @@ WebTablet = function (url, width, dpi, location, clientOnly) {
     this.receive = function (channel, senderID, senderUUID, localOnly) {
         if (_this.homeButtonEntity == senderID) {
             if (_this.clicked) {
-				Entities.editEntity(_this.homeButtonEntity, {color: {red: 0, green: 255, blue: 255}});
-				_this.clicked = false;
+                Entities.editEntity(_this.homeButtonEntity, {color: {red: 0, green: 255, blue: 255}});
+                _this.clicked = false;
             } else {
-				Entities.editEntity(_this.homeButtonEntity, {color: {red: 255, green: 255, blue: 0}});
-				_this.clicked = true;
+                Entities.editEntity(_this.homeButtonEntity, {color: {red: 255, green: 255, blue: 0}});
+                _this.clicked = true;
             }
         }
     }
