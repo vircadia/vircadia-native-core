96a9ef8f44dde9669ae17fa3db354062b1e25d6d
Andrew Meadows <andrew@highfidelity.io>
2016-12-28 18:11:54 -0800

    fix bitrot after UDT overhaul

--- tests/networking/src/ResourceTests.cpp
+++ tests/networking/src/ResourceTests.cpp
@@ -37,25 +37,10 @@ void ResourceTests::initTestCase() {
 static QSharedPointer<Resource> resource;
 
 
-static bool waitForSignal(QObject *sender, const char *signal, int timeout = 1000) {
-    QEventLoop loop;
-    QTimer timer;
-    timer.setInterval(timeout);
-    timer.setSingleShot(true);
-
-    loop.connect(sender, signal, SLOT(quit()));
-    loop.connect(&timer, SIGNAL(timeout()), SLOT(quit()));
-    timer.start();
-    loop.exec();
-
-    return timer.isActive();
-}
-
 void ResourceTests::downloadFirst() {
-
     // download the Mery fst file
     QUrl meryUrl = QUrl("http://hifi-public.s3.amazonaws.com/marketplace/contents/e21c0b95-e502-4d15-8c41-ea2fc40f1125/3585ddf674869a67d31d5964f7b52de1.fst");
-    resource = QSharedPointer<Resource>::create(meryUrl, false);
+    resource = QSharedPointer<Resource>::create(meryUrl);
     resource->setSelf(resource);
 
     const int timeout = 1000;
@@ -64,9 +49,9 @@ void ResourceTests::downloadFirst() {
     timer.setInterval(timeout);
     timer.setSingleShot(true);
 
-    loop.connect(resource, SIGNAL(loaded(QNetworkReply&)), SLOT(quit()));
-    loop.connect(resource, SIGNAL(failed(QNetworkReply::NetworkError)), SLOT(quit()));
-    loop.connect(&timer, SIGNAL(timeout()), SLOT(quit()));
+    connect(resource.data(), &Resource::loaded, &loop, &QEventLoop::quit);
+    connect(resource.data(), &Resource::failed, &loop, &QEventLoop::quit);
+    connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
     timer.start();
 
     resource->ensureLoading();
@@ -76,10 +61,9 @@ void ResourceTests::downloadFirst() {
 }
 
 void ResourceTests::downloadAgain() {
-
     // download the Mery fst file
     QUrl meryUrl = QUrl("http://hifi-public.s3.amazonaws.com/marketplace/contents/e21c0b95-e502-4d15-8c41-ea2fc40f1125/3585ddf674869a67d31d5964f7b52de1.fst");
-    resource = QSharedPointer<Resource>::create(meryUrl, false);
+    resource = QSharedPointer<Resource>::create(meryUrl);
     resource->setSelf(resource);
 
     const int timeout = 1000;
@@ -88,14 +72,13 @@ void ResourceTests::downloadAgain() {
     timer.setInterval(timeout);
     timer.setSingleShot(true);
 
-    loop.connect(resource, SIGNAL(loaded(QNetworkReply&)), SLOT(quit()));
-    loop.connect(resource, SIGNAL(failed(QNetworkReply::NetworkError)), SLOT(quit()));
-    loop.connect(&timer, SIGNAL(timeout()), SLOT(quit()));
+    connect(resource.data(), &Resource::loaded, &loop, &QEventLoop::quit);
+    connect(resource.data(), &Resource::failed, &loop, &QEventLoop::quit);
+    connect(&timer, &QTimer::timeout, &loop, &QEventLoop::quit);
     timer.start();
 
     resource->ensureLoading();
     loop.exec();
 
     QVERIFY(resource->isLoaded());
-
 }
