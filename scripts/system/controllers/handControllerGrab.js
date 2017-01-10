d0baa0e303c51d5455342d125973ae6381742911
Dante Ruiz <dante@highfidelity.io>
2017-01-10 22:03:54 +0000

    home button works with the stylus

--- scripts/system/controllers/handControllerGrab.js
+++ scripts/system/controllers/handControllerGrab.js
@@ -1220,9 +1220,24 @@ function MyController(hand) {
                 if (this.handleStylusOnWebOverlay(rayPickInfo)) {
                     return;
                 }
+                this.handleStylusOnHomeButton(rayPickInfo);
             }
         } else {
             this.hideStylus();
+        }
+    };
+
+   this.handleStylusOnHomeButton = function(rayPickInfo) {
+        var pointerEvent;
+
+        if (rayPickInfo.entityID) {
+            var entity = rayPickInfo.entityID;
+            var name = entityPropertiesCache.getProps(entity).name;
+            if (name == "homeButton") {
+                Messages.sendLocalMessage("home", entity);
+                //this.grabbedEntity = entity;
+                //this.setState(STATE_ENTITY_STYLUS_TOUCHING, "begin touching entity '" + name + "'");
+            }
         }
     };
 
