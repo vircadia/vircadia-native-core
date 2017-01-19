9865f134dbe771ce873f86f4dfb1bedfafb94207
Faye Li Si Fi <si.fi.faye.li@gmail.com>
2017-01-18 16:09:12 -0800

    go to users.html on tablet button click

--- scripts/system/users.js
+++ scripts/system/users.js
@@ -4,13 +4,14 @@
 //  users.js
 //
 //  Created by Faye Li on 18 Jan 2017.
-//  Copyright 2015 High Fidelity, Inc.
+//  Copyright 2017 High Fidelity, Inc.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
 
 (function() { // BEGIN LOCAL_SCOPE
+    var USERS_URL = "https://hifi-content.s3.amazonaws.com/faye/tablet-dev/users.html";
     var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
     var button = tablet.addButton({
         // TODO: work with Alan to make new icon art
@@ -18,7 +19,14 @@
         text: "Users"
     });
 
+    function onClicked() {
+        tablet.gotoWebScreen(USERS_URL);
+    }
+
+    button.clicked.connect(onClicked);
+
     function cleanup() {
+        button.clicked.disconnect(onClicked);
         tablet.removeButton(button);
     }
 
