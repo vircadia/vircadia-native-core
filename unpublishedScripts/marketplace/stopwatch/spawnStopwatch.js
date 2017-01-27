47235d24aa7d8ab9104d11cde40285b438dac24d
Ryan Huffman <ryanhuffman@gmail.com>
2017-01-27 15:29:48 -0800

    Add headers to stopwatch scripts

--- unpublishedScripts/marketplace/stopwatch/spawnStopwatch.js
+++ unpublishedScripts/marketplace/stopwatch/spawnStopwatch.js
@@ -1,3 +1,13 @@
+//
+//  spawnStopwatch.js
+//
+//  Created by Ryan Huffman on 1/20/17.
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
 var positionToSpawn = Vec3.sum(MyAvatar.position, Quat.getFront(MyAvatar.rotation));
 
 var stopwatchID = Entities.addEntity({
