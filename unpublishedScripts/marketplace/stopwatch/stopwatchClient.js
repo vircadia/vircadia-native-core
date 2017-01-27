9df7bf00df01ed3b6a1e8892cbc82200d7379757
Ryan Huffman <ryanhuffman@gmail.com>
2017-01-27 15:19:40 -0800

    Add stopwatch spawner script

--- unpublishedScripts/marketplace/stopwatch/stopwatchClient.js
+++ unpublishedScripts/marketplace/stopwatch/stopwatchClient.js
@@ -1,20 +1,12 @@
 (function() {
-    this.equipped = false;
+    var messageChannel;
     this.preload = function(entityID) {
-        this.entityID = entityID;
         this.messageChannel = "STOPWATCH-" + entityID;
-        print("In stopwatch client", this.messageChannel);
     };
-    this.startEquip = function(entityID, args) {
-        this.equipped = true;
-    };
-    this.continueEquip = function(entityID, args) {
-        var triggerValue = Controller.getValue(args[0] === 'left' ? Controller.Standard.LT : Controller.Standard.RT);
-        if (triggerValue > 0.5) {
-            Messages.sendMessage(messageChannel, 'click');
-        }
-    };
-    this.releaseEquip = function(entityID, args) {
-        this.equipped = false;
-    };
+    function click() {
+        Messages.sendMessage(this.messageChannel, 'click');
+    }
+    this.startNearTrigger = click;
+    this.startFarTrigger = click;
+    this.clickDownOnEntity = click;
 });
--- unpublishedScripts/marketplace/stopwatch/stopwatchServer.js
+++ unpublishedScripts/marketplace/stopwatch/stopwatchServer.js
@@ -2,53 +2,107 @@
     this.equipped = false;
     this.isActive = false;
 
-    this.secondsEntityID = null;
-    this.minutesEntityID = null;
+    this.secondHandID = null;
+    this.minuteHandID = null;
+
+    var tickSound = SoundCache.getSound("https://hifi-public.s3.amazonaws.com/huffman/tick.wav");
+    var tickInjector = null;
+    var tickIntervalID = null;
+
+    var chimeSound = SoundCache.getSound("https://hifi-public.s3.amazonaws.com/huffman/chime.wav");
 
     this.preload = function(entityID) {
-        print("Stopwatch Server Preload");
+        print("Preloading stopwatch: ", entityID);
         this.entityID = entityID;
         this.messageChannel = "STOPWATCH-" + entityID;
 
         var userData = Entities.getEntityProperties(this.entityID, 'userData').userData;
         var data = JSON.parse(userData);
-        this.secondsEntityID = data.secondsEntityID;
-        this.minutesEntityID = data.minutesEntityID;
+        this.secondHandID = data.secondHandID;
+        this.minuteHandID = data.minuteHandID;
 
         this.resetTimer();
 
         Messages.subscribe(this.messageChannel);
         Messages.messageReceived.connect(this, this.messageReceived);
     };
-
     this.unload = function() {
-        print("Stopwatch Server Unload");
+        print("Unloading stopwatch:", this.entityID);
+        this.resetTimer();
         Messages.unsubscribe(this.messageChannel);
         Messages.messageReceived.disconnect(this, this.messageReceived);
     };
-
+    this.messageReceived = function(channel, message, sender) {
+        print("Message received", channel, sender, message); 
+        if (channel === this.messageChannel && message === 'click') {
+            if (this.isActive) {
+                this.resetTimer();
+            } else {
+                this.startTimer();
+            }
+        }
+    };
+    this.getStopwatchPosition = function() {
+        return Entities.getEntityProperties(this.entityID, "position").position;
+    };
     this.resetTimer = function() {
-        Entities.editEntity(this.secondsEntityID, {
+        print("Stopping stopwatch");
+        if (tickInjector) {
+            tickInjector.stop();
+        }
+        if (tickIntervalID !== null) {
+            Script.clearInterval(tickIntervalID);
+            tickIntervalID = null;
+        }
+        Entities.editEntity(this.secondHandID, {
+            rotation: Quat.fromPitchYawRollDegrees(0, 0, 0),
+            angularVelocity: { x: 0, y: 0, z: 0 },
+        });
+        Entities.editEntity(this.minuteHandID, {
             rotation: Quat.fromPitchYawRollDegrees(0, 0, 0),
             angularVelocity: { x: 0, y: 0, z: 0 },
         });
         this.isActive = false;
     };
     this.startTimer = function() {
-        Entities.editEntity(this.secondsEntityID, {
-            angularVelocity: { x: 0, y: -6, z: 0 },
-        });
-        this.isActive = true;
-    };
-
-    this.messageReceived = function(channel, sender, message) {
-        print("Message received", channel, sender, message); 
-        if (channel == this.messageChannel && message === 'click') {
-            if (this.isActive) {
-                resetTimer();
-            } else {
-                startTimer();
-            }
+        print("Starting stopwatch");
+        if (!tickInjector) {
+            tickInjector = Audio.playSound(tickSound, {
+                position: this.getStopwatchPosition(),
+                volume: 0.7,
+                loop: true
+            });
+        } else {
+            tickInjector.restart();
         }
+
+        var self = this;
+        var seconds = 0;
+        tickIntervalID = Script.setInterval(function() {
+            if (tickInjector) {
+                tickInjector.setOptions({
+                    position: self.getStopwatchPosition(),
+                    volume: 0.7,
+                    loop: true
+                });
+            }
+            seconds++;
+            const degreesPerTick = -360 / 60;
+            Entities.editEntity(self.secondHandID, {
+                rotation: Quat.fromPitchYawRollDegrees(0, seconds * degreesPerTick, 0),
+            });
+            if (seconds % 60 == 0) {
+                Entities.editEntity(self.minuteHandID, {
+                    rotation: Quat.fromPitchYawRollDegrees(0, (seconds / 60) * degreesPerTick, 0),
+                });
+                Audio.playSound(chimeSound, {
+                    position: self.getStopwatchPosition(),
+                    volume: 1.0,
+                    loop: false
+                });
+            }
+        }, 1000);
+
+        this.isActive = true;
     };
 });
