d9861c2afd70eff40f2fcb769ed4f3354d7675ab
Ryan Huffman <ryanhuffman@gmail.com>
2017-02-01 10:31:21 -0800

    Remove old tower defense game content

--- unpublishedScripts/DomainContent/Toybox/towerDefense/roofEntity.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    return {
-        preload: function(entityID) {
-            Entities.editEntity(entityID, {
-                localRenderAlpha: 0.2
-            });
-        }
-    };
-});
