c4ec0cc069838643cd444a2ef92fca2cb109952d
sam <sam>
2017-02-02 22:09:52 -0800

    fixing a bad cDebug

--- libraries/model/src/model/TextureMap.cpp
+++ libraries/model/src/model/TextureMap.cpp
@@ -74,7 +74,7 @@ QImage processSourceImage(const QImage& srcImage, bool cubemap) {
 
     if (targetSize != srcImageSize) {
          PROFILE_RANGE(resource_parse, "processSourceImage Rectify");
-         qDebug(modelLog) << "Resizing texture from " << srcImageSize.x << "x" << srcImageSize.y << " to " << targetSize.x << "x" << targetSize.y;
+         qCDebug(modelLog) << "Resizing texture from " << srcImageSize.x << "x" << srcImageSize.y << " to " << targetSize.x << "x" << targetSize.y;
         return srcImage.scaled(fromGlm(targetSize));
     }
 
