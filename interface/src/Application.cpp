0cc137d26b7014b6a747f60721dce7aedafbe779
Ryan Huffman <ryanhuffman@gmail.com>
2017-02-02 16:18:04 -0800

    Remove unintentionally added fps + download profiling

--- interface/src/Application.cpp
+++ interface/src/Application.cpp
@@ -3409,7 +3409,7 @@ void Application::idle(float nsecsElapsed) {
 #ifdef Q_OS_WIN
     static std::once_flag once;
     std::call_once(once, [] {
-        initCpuUsage(); 
+        initCpuUsage();
     });
 
     vec3 kernelUserAndSystem;
