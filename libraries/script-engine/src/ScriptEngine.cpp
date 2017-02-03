huffman/hifi

Update scriptengine update rate to match displayPlugin target framerate

libraries/script-engine/src/ScriptEngine.cpp
@@ -875,11 +875,11 @@ void ScriptEngine::run() {
     while (!_isFinished) {
         auto beforeSleep = clock::now();
 
-        // Throttle to SCRIPT_FPS
-        // We'd like to try to keep the script at a solid SCRIPT_FPS update rate. And so we will 
+        // Throttle to _targetUpdateRate
+        // We'd like to try to keep the script at a solid _targetUpdateRate update rate. And so we will 
         // calculate a sleepUntil to be the time from our start time until the original target
         // sleepUntil for this frame.
-        const std::chrono::microseconds FRAME_DURATION(USECS_PER_SECOND / SCRIPT_FPS + 1);
+        const std::chrono::microseconds FRAME_DURATION(static_cast<int64_t>(USECS_PER_SECOND / _getTargetUpdateRate()) + 1);
         clock::time_point targetSleepUntil(startTime + thisFrame++ * FRAME_DURATION);
 
         // However, if our sleepUntil is not at least our average update time into the future

vircadia/vircadia-native-core

libraries/script-engine/src/ScriptEngine.cpp
@@ -898,14 +898,14 @@ void ScriptEngine::run() {
     while (!_isFinished) {
         auto beforeSleep = clock::now();
 
-        // Throttle to SCRIPT_FPS
-        // We'd like to try to keep the script at a solid SCRIPT_FPS update rate. And so we will 
+        // Throttle to _targetUpdateRate
+        // We'd like to try to keep the script at a solid _targetUpdateRate update rate. And so we will 
         // calculate a sleepUntil to be the time from our start time until the original target
         // sleepUntil for this frame. This approach will allow us to "catch up" in the event 
         // that some of our script udpates/frames take a little bit longer than the target average 
         // to execute.
         // NOTE: if we go to variable SCRIPT_FPS, then we will need to reconsider this approach
-        const std::chrono::microseconds TARGET_SCRIPT_FRAME_DURATION(USECS_PER_SECOND / SCRIPT_FPS + 1);
+        const std::chrono::microseconds TARGET_SCRIPT_FRAME_DURATION(static_cast<int64_t>(USECS_PER_SECOND / _getTargetUpdateRate()) + 1);
         clock::time_point targetSleepUntil(startTime + (thisFrame++ * TARGET_SCRIPT_FRAME_DURATION));
 
         // However, if our sleepUntil is not at least our average update and timer execution time 