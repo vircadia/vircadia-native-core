58e3351b25dc646750fe5807eac4aded9a3df08d
Ryan Huffman <ryanhuffman@gmail.com>
2017-02-08 14:27:06 -0800

    Update stopwatch to use relative paths to models + sounds

--- unpublishedScripts/marketplace/stopwatch/spawnStopwatch.js
+++ unpublishedScripts/marketplace/stopwatch/spawnStopwatch.js
@@ -18,7 +18,7 @@ var stopwatchID = Entities.addEntity({
     type: "Model",
     name: "stopwatch/base",
     position: positionToSpawn,
-    modelURL: "http://hifi-content.s3.amazonaws.com/alan/dev/Stopwatch.fbx",
+    modelURL: Script.resolvePath("models/Stopwatch.fbx"),
     dimensions: Vec3.multiply(scale, {"x":4.129462242126465,"y":1.058512806892395,"z":5.773681640625}),
     rotation: Quat.multiply(MyAvatar.orientation, Quat.fromPitchYawRollDegrees(90, 0, 0))
 });
@@ -30,7 +30,7 @@ var secondHandID = Entities.addEntity({
     localPosition: Vec3.multiply(scale, {"x":-0.004985813982784748,"y":0.39391064643859863,"z":0.8312804698944092}),
     dimensions: Vec3.multiply(scale, {"x":0.14095762372016907,"y":0.02546107769012451,"z":1.6077008247375488}),
     registrationPoint: {"x":0.5,"y":0.5,"z":1},
-    modelURL: "http://hifi-content.s3.amazonaws.com/alan/dev/Stopwatch-sec-hand.fbx",
+    modelURL: Script.resolvePath("models/Stopwatch-sec-hand.fbx"),
 });
 
 var minuteHandID = Entities.addEntity({
@@ -40,7 +40,7 @@ var minuteHandID = Entities.addEntity({
     localPosition: Vec3.multiply(scale, {"x":-0.0023056098725646734,"y":0.3308190703392029,"z":0.21810021996498108}),
     dimensions: Vec3.multiply(scale, {"x":0.045471154153347015,"y":0.015412690117955208,"z":0.22930574417114258}),
     registrationPoint: {"x":0.5,"y":0.5,"z":1},
-    modelURL: "http://hifi-content.s3.amazonaws.com/alan/dev/Stopwatch-min-hand.fbx",
+    modelURL: Script.resolvePath("models/Stopwatch-min-hand.fbx"),
 });
 
 Entities.editEntity(stopwatchID, {
