115251542d1b4c7cbf7ad95b5a1820f23e1923f9
sam <sam>
2017-02-15 00:54:16 -0800

    Adding the ktx library

---
+++ libraries/ktx/src/ktx/Writer.cpp
@@ -0,0 +1,21 @@
+//
+//  Writer.cpp
+//  ktx/src/ktx
+//
+//  Created by Zach Pomerantz on 2/08/2017.
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+#include "KTX.h"
+
+
+namespace ktx {
+  /*  size_t serialize(const gpu::Texture& texture, uint8_t* data) {
+        return 0;
+    }*/
+   /* KTX serialize(const gpu::Texture& texture) {
+        return KTX(0, nullptr);
+    }*/
+}
