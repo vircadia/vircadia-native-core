619867293e1e37b9b3580001d3eb1e661cdafe31
Ryan Huffman <ryanhuffman@gmail.com>
2017-02-22 09:12:25 -0800

    Fix syntax error in spawnBow.js

--- unpublishedScripts/marketplace/shortbow/bow/spawnBow.js
+++ unpublishedScripts/marketplace/shortbow/bow/spawnBow.js
@@ -53,7 +53,7 @@ var id = Entities.addEntity({
     "shapeType": "compound",
     "type": "Model",
     //"userData": JSON.stringify(userData),
-    "userData": "{\"grabbableKey\":{\"grabbable\":true},\"wearable\":{\"joints\":{\"RightHand\":[{\"x\":0.0813,\"y\":0.0452,\"z\":0.0095},{\"x\":-0.3946,\"y\":-0.6604,\"z\":0.4748,\"w\":-0.4275}],\"LeftHand\":[{\"x\":-0.0881,\"y\":0.0259,\"z\":0.0159},{\"x\":0.4427,\"y\":-0.6519,\"z\":0.4592,\"w\":0.4099}]}}}"
+    "userData": "{\"grabbableKey\":{\"grabbable\":true},\"wearable\":{\"joints\":{\"RightHand\":[{\"x\":0.0813,\"y\":0.0452,\"z\":0.0095},{\"x\":-0.3946,\"y\":-0.6604,\"z\":0.4748,\"w\":-0.4275}],\"LeftHand\":[{\"x\":-0.0881,\"y\":0.0259,\"z\":0.0159},{\"x\":0.4427,\"y\":-0.6519,\"z\":0.4592,\"w\":0.4099}]}}}",
     lifetime: 600
 });
 print("Created bow:", id);
