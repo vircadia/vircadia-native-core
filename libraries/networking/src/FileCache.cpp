8969d7df1d9cd61718e45043b2c172b7d4684b43
Zach Pomerantz <zmp@umich.edu>
2017-02-23 16:26:52 -0500

    FileCache

---
+++ libraries/networking/src/FileCache.cpp
@@ -0,0 +1,246 @@
+//
+//  FileCache.cpp
+//  libraries/model-networking/src
+//
+//  Created by Zach Pomerantz on 2/21/2017.
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#include "FileCache.h"
+
+#include <cstdio>
+#include <fstream>
+#include <unordered_set>
+
+#include <QDir>
+
+#include <ServerPathUtils.h>
+
+Q_LOGGING_CATEGORY(file_cache, "hifi.file_cache")
+
+static const std::string MANIFEST_NAME = "manifest";
+
+void FileCache::setUnusedFileCacheSize(size_t unusedFilesMaxSize) {
+    _unusedFilesMaxSize = std::min(unusedFilesMaxSize, MAX_UNUSED_MAX_SIZE);
+    reserve(0);
+    emit dirty();
+}
+
+void FileCache::setOfflineFileCacheSize(size_t offlineFilesMaxSize) {
+    _offlineFilesMaxSize = std::min(offlineFilesMaxSize, MAX_UNUSED_MAX_SIZE);
+}
+
+FileCache::FileCache(const std::string& dirname, const std::string& ext, QObject* parent) :
+    QObject(parent),
+    _dir(createDir(dirname)),
+    _ext(ext) {}
+
+FileCache::~FileCache() {
+    clear();
+}
+
+void fileDeleter(File* file) {
+    file->deleter();
+}
+
+FilePointer FileCache::writeFile(const Key& key, const char* data, size_t length, void* extra) {
+    std::string filepath = getFilepath(key);
+
+    Lock lock(_filesMutex);
+
+    // if file already exists, return it
+    FilePointer file = getFile(key);
+    if (file) {
+        qCWarning(file_cache) << "Attempted to overwrite" << filepath.c_str();
+        return file;
+    }
+
+    // write the new file
+    FILE* saveFile = fopen(filepath.c_str(), "wb");
+    if (saveFile != nullptr && fwrite(data, length, 1, saveFile) && fclose(saveFile) == 0) {
+        file.reset(createFile(key, filepath, length, extra), &fileDeleter);
+        fclose(saveFile);
+        file->_cache = this;
+        _files[key] = file;
+        _numTotalFiles += 1;
+        _totalFilesSize += length;
+
+        emit dirty();
+    } else {
+        qCWarning(file_cache, "Failed to write %s (%s)", filepath.c_str(), strerror(errno));
+        errno = 0;
+    }
+
+    return file;
+}
+
+FilePointer FileCache::getFile(const Key& key) {
+    FilePointer file;
+
+    Lock lock(_filesMutex);
+
+    // check if file already exists
+    const auto it = _files.find(key);
+    if (it != _files.cend()) {
+        file = it->second.lock();
+        if (file) {
+            // if it exists, it is active - remove it from the cache
+            removeUnusedFile(file);
+            emit dirty();
+        } else {
+            // if not, remove the weak_ptr
+            _files.erase(it);
+        }
+    }
+
+    return file;
+}
+
+File* FileCache::createFile(const Key& key, const std::string& filepath, size_t length, void* extra) {
+    return new File(key, filepath, length);
+}
+
+std::string FileCache::createDir(const std::string& dirname) {
+    QString dirpath = ServerPathUtils::getDataFilePath(dirname.c_str());
+    QDir dir(dirpath);
+
+    if (dir.exists()) {
+        std::unordered_set<std::string> persistedEntries;
+        if (dir.exists(MANIFEST_NAME.c_str())) {
+            std::ifstream manifest;
+            manifest.open(dir.absoluteFilePath(MANIFEST_NAME.c_str()).toStdString());
+            while (manifest.good()) {
+                std::string entry;
+                manifest >> entry;
+                persistedEntries.insert(entry);
+
+                // ZZMP: rm
+                for (const auto& entry : persistedEntries)
+                    qDebug() << "ZZMP" << entry.c_str();
+                qDebug() << "ZZMP" << "---";
+            }
+        }
+
+        foreach(QString filename, dir.entryList()) {
+            if (persistedEntries.find(filename.toStdString()) == persistedEntries.cend()) {
+                dir.remove(filename);
+            }
+        }
+    } else {
+        dir.mkpath(dirpath);
+    }
+
+    return dirpath.toStdString();
+}
+
+std::string FileCache::getFilepath(const Key& key) {
+    return _dir + key + '.' + _ext;
+}
+
+void FileCache::addUnusedFile(const FilePointer file) {
+    {
+        Lock lock(_filesMutex);
+        _files[file->getKey()] = file;
+    }
+
+    reserve(file->getLength());
+    file->_LRUKey = ++_lastLRUKey;
+
+    {
+        Lock lock(_unusedFilesMutex);
+        _unusedFiles.insert({ file->_LRUKey, file });
+        _numUnusedFiles += 1;
+        _unusedFilesSize += file->getLength();
+    }
+
+    emit dirty();
+}
+
+void FileCache::removeUnusedFile(const FilePointer file) {
+    Lock lock(_unusedFilesMutex);
+    const auto it = _unusedFiles.find(file->_LRUKey);
+    if (it != _unusedFiles.cend()) {
+        _unusedFiles.erase(it);
+        _numUnusedFiles -= 1;
+        _unusedFilesSize -= file->getLength();
+    }
+}
+
+void FileCache::reserve(size_t length) {
+    Lock unusedLock(_unusedFilesMutex);
+    while (!_unusedFiles.empty() &&
+            _unusedFilesSize + length > _unusedFilesMaxSize) {
+        auto it = _unusedFiles.begin();
+        auto file = it->second;
+        auto length = file->getLength();
+
+        unusedLock.unlock();
+        {
+            file->_cache = nullptr;
+            Lock lock(_filesMutex);
+            _files.erase(file->getKey());
+        }
+        unusedLock.lock();
+
+        _unusedFiles.erase(it);
+        _numTotalFiles -= 1;
+        _numUnusedFiles -= 1;
+        _totalFilesSize -= length;
+        _unusedFilesSize -= length;
+
+        unusedLock.unlock();
+        evictedFile(file);
+        unusedLock.lock();
+    }
+}
+
+void FileCache::clear() {
+    std::string manifestPath= _dir + MANIFEST_NAME;
+    FILE* manifest = fopen(manifestPath.c_str(), "wb");
+
+    Lock lock(_unusedFilesMutex);
+    for (const auto& val : _unusedFiles) {
+        const FilePointer& file = val.second;
+        file->_cache = nullptr;
+
+        if (_unusedFilesSize > _offlineFilesMaxSize) {
+            _unusedFilesSize -= file->getLength();
+        } else {
+            std::string key = file->getKey() + '.' + _ext + '\n';
+            if (manifest != nullptr && !fwrite(key.c_str(), key.length(), 1, manifest)) {
+                manifest = nullptr; // to prevent future writes
+            }
+            file->_shouldPersist = true;
+        }
+    }
+
+    if (manifest == nullptr || fclose(manifest) != 0) {
+        for (const auto& val : _unusedFiles) {
+            val.second->_shouldPersist = false;
+        }
+
+        qCWarning(file_cache, "Failed to write %s (%s)", manifestPath.c_str(), strerror(errno));
+        errno = 0;
+    }
+
+    _unusedFiles.clear();
+}
+
+void File::deleter() {
+    if (_cache) {
+        FilePointer self(this, &fileDeleter);
+        _cache->addUnusedFile(self);
+    } else {
+        deleteLater();
+    }
+}
+
+File::~File() {
+    QFile file(getFilepath().c_str());
+    if (file.exists() && !_shouldPersist) {
+        file.remove();
+    }
+}
---
+++ libraries/networking/src/FileCache.h
@@ -0,0 +1,149 @@
+//
+//  FileCache.h
+//  libraries/networking/src
+//
+//  Created by Zach Pomerantz on 2/21/2017.
+//  Copyright 2017 High Fidelity, Inc.  // //  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_FileCache_h
+#define hifi_FileCache_h
+
+#include <memory>
+#include <cstddef>
+#include <map>
+#include <mutex>
+#include <string>
+#include <unordered_map>
+
+#include <QObject>
+#include <QLoggingCategory>
+
+Q_DECLARE_LOGGING_CATEGORY(file_cache)
+
+class File;
+using FilePointer = std::shared_ptr<File>;
+
+class FileCache : public QObject {
+    Q_OBJECT
+    Q_PROPERTY(size_t numTotal READ getNumTotalFiles NOTIFY dirty)
+    Q_PROPERTY(size_t numCached READ getNumCachedFiles NOTIFY dirty)
+    Q_PROPERTY(size_t sizeTotal READ getSizeTotalFiles NOTIFY dirty)
+    Q_PROPERTY(size_t sizeCached READ getSizeCachedFiles NOTIFY dirty)
+
+    static const size_t BYTES_PER_MEGABYTES = 1024 * 1024;
+    static const size_t BYTES_PER_GIGABYTES = 1024 * BYTES_PER_MEGABYTES;
+    static const size_t DEFAULT_UNUSED_MAX_SIZE = 5 * BYTES_PER_GIGABYTES; // 5GB
+    static const size_t MAX_UNUSED_MAX_SIZE = 100 * BYTES_PER_GIGABYTES; // 100GB
+    static const size_t DEFAULT_OFFLINE_MAX_SIZE = 2 * BYTES_PER_GIGABYTES; // 2GB
+
+public:
+    size_t getNumTotalFiles() const { return _numTotalFiles; }
+    size_t getNumCachedFiles() const { return _numUnusedFiles; }
+    size_t getSizeTotalFiles() const { return _totalFilesSize; }
+    size_t getSizeCachedFiles() const { return _unusedFilesSize; }
+
+    void setUnusedFileCacheSize(size_t unusedFilesMaxSize);
+    size_t getUnusedFileCacheSize() const { return _unusedFilesSize; }
+
+    void setOfflineFileCacheSize(size_t offlineFilesMaxSize);
+
+    // initialize FileCache with a directory name (not a path, ex.: "temp_jpgs") and an ext (ex.: "jpg")
+    FileCache(const std::string& dirname, const std::string& ext, QObject* parent = nullptr);
+    // precondition: there should be no references to Files when FileCache is destroyed
+    virtual ~FileCache();
+
+    // derived classes are left to implement hashing of the files on their own
+    using Key = std::string;
+
+    // derived classes should implement a setter/getter, for example, for a FileCache backing a network cache:
+    //
+    // DerivedFilePointer writeFile(const DerivedData& data) {
+    //  return writeFile(data->key, data->data, data->length, &data);
+    // }
+    //
+    // DerivedFilePointer getFile(const QUrl& url) {
+    //  // assuming storage/removal of url->hash in createFile/evictedFile overrides
+    //  auto key = lookup_hash_for(url);
+    //  return getFile(key);
+    // }
+
+signals:
+    void dirty();
+
+protected:
+    FilePointer writeFile(const Key& key, const char* data, size_t length, void* extra);
+    FilePointer getFile(const Key& key);
+
+    virtual File* createFile(const Key& key, const std::string& filepath, size_t length, void* extra);
+    virtual void evictedFile(const FilePointer& file) = 0;
+
+private:
+    using Mutex = std::recursive_mutex;
+    using Lock = std::unique_lock<Mutex>;
+
+    friend class File;
+
+    std::string createDir(const std::string& dirname);
+    std::string getFilepath(const Key& key);
+
+    void addUnusedFile(const FilePointer file);
+    void removeUnusedFile(const FilePointer file);
+    void reserve(size_t length);
+    void clear();
+
+    std::atomic<size_t> _numTotalFiles { 0 };
+    std::atomic<size_t> _numUnusedFiles { 0 };
+    std::atomic<size_t> _totalFilesSize { 0 };
+    std::atomic<size_t> _unusedFilesSize { 0 };
+
+    std::string _dir;
+    std::string _ext;
+
+    std::unordered_map<Key, std::weak_ptr<File>> _files;
+    Mutex _filesMutex;
+
+    std::map<int, FilePointer> _unusedFiles;
+    Mutex _unusedFilesMutex;
+    size_t _unusedFilesMaxSize { DEFAULT_UNUSED_MAX_SIZE };
+    int _lastLRUKey { 0 };
+
+    size_t _offlineFilesMaxSize { DEFAULT_OFFLINE_MAX_SIZE };
+};
+
+class File : public QObject {
+    Q_OBJECT
+
+public:
+    using Key = FileCache::Key;
+
+    std::string getFilepath() const { return _filepath; }
+    Key getKey() const { return _key; }
+    size_t getLength() const { return _length; }
+
+    // overrides should call File::deleter to maintain caching behavior
+    virtual void deleter();
+
+protected:
+    // when constructed, the file has already been created/written
+    File(const Key& key, const std::string& filepath, size_t length) :
+        _filepath(filepath), _key(key), _length(length) {}
+    // the destructor should handle unlinking of the actual filepath
+    virtual ~File();
+
+    const std::string _filepath;
+
+private:
+    friend class FileCache;
+
+    const Key _key;
+    const size_t _length;
+
+    FileCache* _cache;
+    int _LRUKey { 0 };
+
+    bool _shouldPersist { false };
+};
+
+#endif // hifi_FileCache_h
