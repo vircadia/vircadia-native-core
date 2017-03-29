a586a31a93c61a4ba7fd5d40cc180e2aa6e3b262
Stephen Birarda <commit@birarda.com>
2017-03-29 13:48:24 -0700

    stub the oven tool and add a find module for FBX SDK

---
+++ tools/oven/src/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tools/oven/src
+//
+//  Created by Stephen Birarda on 3/28/2017.
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+
+
+int main (int argc, char** argv) {
+    return 0;
+}
