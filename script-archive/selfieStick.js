97d75e654e22cf732d104334e66357bb99b43054
Faye Li <si.fi.faye.li@gmial.com>
2017-04-05 15:04:21 -0700

    mannually bind avatar and entity

--- script-archive/selfieStick.js
+++ script-archive/selfieStick.js
@@ -7,6 +7,15 @@
 // For best result, turn off avatar collisions(Developer > Avatar > Uncheck Enable Avatar Collisions)
 // 
 
+// selfieStick.js
+//
+// Created by Faye Li on March 23, 2016
+//
+// Usage instruction: Spacebar toggles camera control - WASD first person free movement or no movement but allowing others to grab the selfie stick 
+// and control your camera.
+// For best result, turn off avatar collisions(Developer > Avatar > Uncheck Enable Avatar Collisions)
+// 
+
 (function() { // BEGIN LOCAL_SCOPE
     var MODEL_URL = "https://hifi-content.s3.amazonaws.com/faye/twitch-stream/selfie_stick.json";
     var AVATAR_URL =  "https://hifi-content.s3.amazonaws.com/jimi/avatar/camera/fst/camera.fst";
@@ -19,8 +28,8 @@
     changeAvatar();
     importModel();
     processImportedEntities();
-    parentEntityToAvatar();
     setupSpaceBarControl();
+    Script.update.connect(update);
 
     function changeAvatar() {
         originalAvatar = MyAvatar.skeletonModelURL;
@@ -46,28 +55,6 @@
         });
     }
 
-    function parentEntityToAvatar() {
-        var props = {
-            "parentID" : MyAvatar.sessionUUID
-        };
-        Entities.editEntity(selfieStickEntityID, props);
-    }
-
-    function unparentEntityFromAvatar() {
-        var props = {
-            "parentID" : 0
-        };
-        Entities.editEntity(selfieStickEntityID, props);
-    }
-
-    function parentAvatarToEntity() {
-        MyAvatar.setParentID(selfieStickEntityID);
-    }
-
-    function unparentAvatarFromEntity() {
-        MyAvatar.setParentID(0);
-    }
-
     function setupSpaceBarControl() {
         var mappingName = "Handheld-Cam-Space-Bar";
         var myMapping = Controller.newMapping(mappingName);
@@ -77,18 +64,39 @@
             }
             if (freeMovementMode) {
                 freeMovementMode = false;
-                // Camera.mode = "entity";
-                // Camera.cameraEntity = lensEntityID;
-                unparentEntityFromAvatar();
-                parentAvatarToEntity();
+                Camera.mode = "entity";
+                Camera.cameraEntity = lensEntityID;
             } else {
                 freeMovementMode = true;
-                // Camera.mode = "first person";
-                unparentAvatarFromEntity();
-                parentEntityToAvatar();
+                Camera.mode = "first person";
             }
         });
         Controller.enableMapping(mappingName);
+    }
+
+    function update(deltaTime) {
+        if (freeMovementMode) {
+            var upFactor = 0.1;
+            var upUnitVec = Vec3.normalize(Quat.getUp(MyAvatar.orientation));
+            var upOffset = Vec3.multiply(upUnitVec, upFactor);
+            var forwardFactor = -0.1;
+            var forwardUnitVec = Vec3.normalize(Quat.getFront(MyAvatar.orientation));
+            var forwardOffset = Vec3.multiply(forwardUnitVec, forwardFactor);
+            var newPos = Vec3.sum(Vec3.sum(MyAvatar.position, upOffset), forwardOffset);
+            var newRot = MyAvatar.orientation;
+            Entities.editEntity(selfieStickEntityID, {position: newPos, rotation: newRot});
+        } else {
+            var props = Entities.getEntityProperties(selfieStickEntityID);
+            var upFactor = 0.1;
+            var upUnitVec = Vec3.normalize(Quat.getUp(props.rotation));
+            var upOffset = Vec3.multiply(upUnitVec, -upFactor);
+            var forwardFactor = -0.1;
+            var forwardUnitVec = Vec3.normalize(Quat.getFront(props.rotation));
+            var forwardOffset = Vec3.multiply(forwardUnitVec, -forwardFactor);
+            var newPos = Vec3.sum(Vec3.sum(props.position, upOffset), forwardOffset);
+            MyAvatar.position = newPos;
+            MyAvatar.orientation = props.rotation;
+        }
     }
 
     // Removes all entities we imported and reset settings we've changed
