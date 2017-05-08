773e4346f965838e9f514fd366016f5cd7908c3e
Ryan Huffman <ryanhuffman@gmail.com>
2017-05-08 15:59:29 -0700

    Update copyright in sphinx scripts
    These were meant to be updated before the PR was merged, but I
    overlooked it.

--- unpublishedScripts/interaction/NPC_AC.js
+++ unpublishedScripts/interaction/NPC_AC.js
@@ -3,7 +3,7 @@
 //  scripts/interaction
 //
 //  Created by Trevor Berninger on 3/20/17.
-//  Copyright 2017 Limitless ltd.
+//  Copyright 2017 High Fidelity Inc.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
