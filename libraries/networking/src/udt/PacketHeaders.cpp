1c23f3b971bec5610cd158f77e30841d4f8bb9f7
Seth Alves <seth.alves@gmail.com>
2017-05-09 10:10:35 -0700

    bump protocol version

--- libraries/networking/src/udt/PacketHeaders.cpp
+++ libraries/networking/src/udt/PacketHeaders.cpp
@@ -49,7 +49,7 @@ PacketVersion versionForPacketType(PacketType packetType) {
         case PacketType::EntityEdit:
         case PacketType::EntityData:
         case PacketType::EntityPhysics:
-            return VERSION_ENTITIES_BULLET_DYNAMICS;
+            return VERSION_ENTITIES_DYNAMICS_MOTOR;
         case PacketType::EntityQuery:
             return static_cast<PacketVersion>(EntityQueryPacketVersion::JSONFilterWithFamilyTree);
         case PacketType::AvatarIdentity:
