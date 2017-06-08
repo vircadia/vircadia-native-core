03edb06ecb9868da944156a5fb486312e171f135
Liv <liv@highfidelity.io>
2017-06-08 13:46:33 -0700

    change design to run chat separately so it can be removed if users prefer to not use it

--- scripts/defaultScripts.js
+++ scripts/defaultScripts.js
@@ -29,11 +29,11 @@ var DEFAULT_SCRIPTS_COMBINED = [
     "system/notifications.js",
     "system/dialTone.js",
     "system/firstPersonHMD.js",
-    "system/tablet-ui/tabletUI.js",
-    "system/chat.js"
+    "system/tablet-ui/tabletUI.js"
 ];
 var DEFAULT_SCRIPTS_SEPARATE = [
-    "system/controllers/controllerScripts.js"
+    "system/controllers/controllerScripts.js",
+    "system/chat.js"
 ];
 
 // add a menu item for debugging
