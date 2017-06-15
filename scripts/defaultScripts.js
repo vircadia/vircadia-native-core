59719a13b6823eda340152015257a83a3d344a28
Delanir <danielafilipa@gmail.com>
2017-06-15 19:26:40 +0100

    Move fingerPaint folder into "scripts/system" and add fingerPaint script to DEFAULT_SCRIPTS_SEPARATE

--- scripts/defaultScripts.js
+++ scripts/defaultScripts.js
@@ -33,6 +33,7 @@ var DEFAULT_SCRIPTS_COMBINED = [
 var DEFAULT_SCRIPTS_SEPARATE = [
     "system/controllers/controllerScripts.js",
     // "system/chat.js"
+    "system/fingerPaint/fingerPaint.js"
 ];
 
 // add a menu item for debugging
