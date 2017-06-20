5b6f6ab84ff4b12db8729e47ca0cf4b9f19826e1
Sam Cake <Sam Cake>
2017-06-20 00:25:51 -0700

    Trying to fix the getCOnfig from QML...

--- libraries/render/src/task/Config.h
+++ libraries/render/src/task/Config.h
@@ -124,6 +124,11 @@ signals:
     void dirtyEnabled();
 };
 
+class TConfigProxy {
+public:
+    using Config = JobConfig;
+};
+
 class TaskConfig : public JobConfig {
     Q_OBJECT
 public:
@@ -134,12 +139,29 @@ public:
     TaskConfig() = default ;
     TaskConfig(bool enabled) : JobConfig(enabled) {}
 
+
     // getter for qml integration, prefer the templated getter
-    Q_INVOKABLE QObject* getConfig(const QString& name) { return QObject::findChild<JobConfig*>(name); }
+    Q_INVOKABLE QObject* getConfig(const QString& name) { return getConfig<TConfigProxy>(name.toStdString()); }
     // getter for cpp (strictly typed), prefer this getter
     template <class T> typename T::Config* getConfig(std::string job = "") const {
-        QString name = job.empty() ? QString() : QString(job.c_str()); // an empty string is not a null string
-        return findChild<typename T::Config*>(name);
+        const TaskConfig* root = this;
+        QString path = (job.empty() ? QString() : QString(job.c_str())); // an empty string is not a null string
+        auto tokens = path.split('.', QString::SkipEmptyParts);
+
+        if (tokens.empty()) {
+            tokens.push_back(QString());
+        } else {
+            while (tokens.size() > 1) {
+                auto name = tokens.front();
+                tokens.pop_front();
+                root = QObject::findChild<TaskConfig*>(name);
+                if (!root) {
+                    return nullptr;
+                }
+            }
+        }
+
+        return root->findChild<typename T::Config*>(tokens.front());
     }
 
     void connectChildConfig(QConfigPointer childConfig, const std::string& name);
