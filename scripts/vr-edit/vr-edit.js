84e11db772c30cc3f926619067127639187a0953
David Rowe <david@ctrlaltstudio.com>
2017-06-29 16:58:56 +1200

    VR edit app script and button

---
+++ scripts/vr-edit/vr-edit.js
@@ -0,0 +1,61 @@
+"use strict";
+
+//
+//  vr-edit.js
+//
+//  Created by David Rowe on 27 Jun 2017.
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+(function () {
+
+    var APP_NAME = "VR EDIT",  // TODO: App name.
+        APP_ICON_INACTIVE = "icons/tablet-icons/edit-i.svg",  // TODO: App icons.
+        APP_ICON_ACTIVE = "icons/tablet-icons/edit-a.svg",
+        tablet,
+        button,
+        isAppActive = false;
+
+    function onButtonClicked() {
+        isAppActive = !isAppActive;
+        button.editProperties({ isActive: isAppActive });
+    }
+
+    function setUp() {
+        tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+        if (!tablet) {
+            return;
+        }
+
+        // Tablet/toolbar button.
+        button = tablet.addButton({
+            icon: APP_ICON_INACTIVE,
+            activeIcon: APP_ICON_ACTIVE,
+            text: APP_NAME,
+            isActive: isAppActive
+        });
+        if (button) {
+            button.clicked.connect(onButtonClicked);
+        }
+    }
+
+    function tearDown() {
+        if (!tablet) {
+            return;
+        }
+
+        if (button) {
+            button.clicked.disconnect(onButtonClicked);
+            tablet.removeButton(button);
+            button = null;
+        }
+
+        tablet = null;
+    }
+
+    setUp();
+    Script.scriptEnding.connect(tearDown);
+}());
