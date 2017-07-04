e9a8c3f5e816620d168d14b8a166d056c6d3728e
Seth Alves <seth.alves@gmail.com>
2017-07-04 15:52:03 -0700

    rename atp-get to atp-client.  add ability to list atp file mappings and to upload new files

---
+++ tools/atp-client/src/ATPClientApp.cpp
@@ -0,0 +1,360 @@
+//
+//  ATPClientApp.cpp
+//  tools/atp-client/src
+//
+//  Created by Seth Alves on 2017-3-15
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#include <QDataStream>
+#include <QTextStream>
+#include <QThread>
+#include <QFile>
+#include <QLoggingCategory>
+#include <QCommandLineParser>
+
+#include <NetworkLogging.h>
+#include <SharedLogging.h>
+#include <AddressManager.h>
+#include <DependencyManager.h>
+#include <SettingHandle.h>
+#include <AssetUpload.h>
+
+#include "ATPClientApp.h"
+
+#define HIGH_FIDELITY_ATP_CLIENT_USER_AGENT "Mozilla/5.0 (HighFidelityATPClient)"
+
+ATPClientApp::ATPClientApp(int argc, char* argv[]) :
+    QCoreApplication(argc, argv)
+{
+    // parse command-line
+    QCommandLineParser parser;
+    parser.setApplicationDescription("High Fidelity ATP-Client");
+
+    const QCommandLineOption helpOption = parser.addHelpOption();
+
+    const QCommandLineOption verboseOutput("v", "verbose output");
+    parser.addOption(verboseOutput);
+
+    const QCommandLineOption uploadOption("T", "upload local file", "local-file-to-send");
+    parser.addOption(uploadOption);
+
+    const QCommandLineOption outputFilenameOption("o", "output filename", "output-file-name");
+    parser.addOption(outputFilenameOption);
+
+    const QCommandLineOption domainAddressOption("d", "domain-server address", "127.0.0.1");
+    parser.addOption(domainAddressOption);
+
+    const QCommandLineOption cacheSTUNOption("s", "cache stun-server response");
+    parser.addOption(cacheSTUNOption);
+
+    const QCommandLineOption listenPortOption("listenPort", "listen port", QString::number(INVALID_PORT));
+    parser.addOption(listenPortOption);
+
+
+    if (!parser.parse(QCoreApplication::arguments())) {
+        qCritical() << parser.errorText() << endl;
+        parser.showHelp();
+        Q_UNREACHABLE();
+    }
+
+    if (parser.isSet(helpOption)) {
+        parser.showHelp();
+        Q_UNREACHABLE();
+    }
+
+    _verbose = parser.isSet(verboseOutput);
+    if (!_verbose) {
+        QLoggingCategory::setFilterRules("qt.network.ssl.warning=false");
+
+        const_cast<QLoggingCategory*>(&networking())->setEnabled(QtDebugMsg, false);
+        const_cast<QLoggingCategory*>(&networking())->setEnabled(QtInfoMsg, false);
+        const_cast<QLoggingCategory*>(&networking())->setEnabled(QtWarningMsg, false);
+
+        const_cast<QLoggingCategory*>(&shared())->setEnabled(QtDebugMsg, false);
+        const_cast<QLoggingCategory*>(&shared())->setEnabled(QtInfoMsg, false);
+        const_cast<QLoggingCategory*>(&shared())->setEnabled(QtWarningMsg, false);
+    }
+
+
+    QStringList posArgs = parser.positionalArguments();
+    if (posArgs.size() != 1) {
+        qDebug() << "give remote url argument";
+        parser.showHelp();
+        Q_UNREACHABLE();
+    }
+
+    _url = QUrl(posArgs[0]);
+    if (_url.scheme() != "atp") {
+        qDebug() << "url should start with atp:";
+        parser.showHelp();
+        Q_UNREACHABLE();
+    }
+
+    int domainPort = 40103;
+    if (_url.port() != -1) {
+        domainPort = _url.port();
+    }
+
+    if (parser.isSet(outputFilenameOption)) {
+        _localOutputFile = parser.value(outputFilenameOption);
+    }
+
+    if (parser.isSet(uploadOption)) {
+        _localUploadFile = parser.value(uploadOption);
+    }
+
+
+    if (parser.isSet(listenPortOption)) {
+        _listenPort = parser.value(listenPortOption).toInt();
+    }
+
+    QString domainServerAddress = QString("127.0.0.1") + ":" + QString::number(domainPort);
+    if (parser.isSet(domainAddressOption)) {
+        domainServerAddress = parser.value(domainAddressOption);
+        qDebug() << "domainServerAddress is " << domainServerAddress;
+        connectToDomain(domainServerAddress);
+    } else if (!_url.host().isEmpty()) {
+        QUrl domainURL;
+        domainURL.setScheme("hifi");
+        domainURL.setHost(_url.host());
+        connectToDomain(domainURL.toString());
+    } else {
+        qDebug() << "domainServerAddress is default " << domainServerAddress;
+        connectToDomain(domainServerAddress);
+    }
+}
+
+
+void ATPClientApp::connectToDomain(QString domainServerAddress) {
+
+    if (_verbose) {
+        qDebug() << "domain-server address is" << domainServerAddress;
+    }
+
+    DependencyManager::set<AccountManager>();
+    auto accountManager = DependencyManager::get<AccountManager>();
+    QString username = accountManager->getAccountInfo().getUsername();
+    qDebug() << "username is" << username;
+
+    Setting::init();
+    DependencyManager::registerInheritance<LimitedNodeList, NodeList>();
+
+    DependencyManager::set<AccountManager>([&]{ return QString(HIGH_FIDELITY_ATP_CLIENT_USER_AGENT); });
+    DependencyManager::set<AddressManager>();
+    DependencyManager::set<NodeList>(NodeType::Agent, _listenPort);
+
+    auto nodeList = DependencyManager::get<NodeList>();
+    nodeList->startThread();
+
+    // setup a timer for domain-server check ins
+    _domainCheckInTimer = new QTimer(nodeList.data());
+    connect(_domainCheckInTimer, &QTimer::timeout, nodeList.data(), &NodeList::sendDomainServerCheckIn);
+    _domainCheckInTimer->start(DOMAIN_SERVER_CHECK_IN_MSECS);
+
+    const DomainHandler& domainHandler = nodeList->getDomainHandler();
+
+    connect(&domainHandler, SIGNAL(hostnameChanged(const QString&)), SLOT(domainChanged(const QString&)));
+    // connect(&domainHandler, SIGNAL(resetting()), SLOT(resettingDomain()));
+    // connect(&domainHandler, SIGNAL(disconnectedFromDomain()), SLOT(clearDomainOctreeDetails()));
+    connect(&domainHandler, &DomainHandler::domainConnectionRefused, this, &ATPClientApp::domainConnectionRefused);
+
+    connect(nodeList.data(), &NodeList::nodeAdded, this, &ATPClientApp::nodeAdded);
+    connect(nodeList.data(), &NodeList::nodeKilled, this, &ATPClientApp::nodeKilled);
+    connect(nodeList.data(), &NodeList::nodeActivated, this, &ATPClientApp::nodeActivated);
+    // connect(nodeList.data(), &NodeList::uuidChanged, getMyAvatar(), &MyAvatar::setSessionUUID);
+    // connect(nodeList.data(), &NodeList::uuidChanged, this, &ATPClientApp::setSessionUUID);
+    connect(nodeList.data(), &NodeList::packetVersionMismatch, this, &ATPClientApp::notifyPacketVersionMismatch);
+
+    nodeList->addSetOfNodeTypesToNodeInterestSet(NodeSet() << NodeType::AudioMixer << NodeType::AvatarMixer
+                                                 << NodeType::EntityServer << NodeType::AssetServer << NodeType::MessagesMixer);
+
+    DependencyManager::get<AddressManager>()->handleLookupString(domainServerAddress, false);
+
+    auto assetClient = DependencyManager::set<AssetClient>();
+    assetClient->init();
+
+    QTimer* _timeoutTimer = new QTimer(this);
+    _timeoutTimer->setSingleShot(true);
+    connect(_timeoutTimer, &QTimer::timeout, this, &ATPClientApp::timedOut);
+    _timeoutTimer->start(4000);
+}
+
+ATPClientApp::~ATPClientApp() {
+    delete _domainCheckInTimer;
+    delete _timeoutTimer;
+}
+
+
+void ATPClientApp::domainConnectionRefused(const QString& reasonMessage, int reasonCodeInt, const QString& extraInfo) {
+    qDebug() << "domainConnectionRefused";
+}
+
+void ATPClientApp::domainChanged(const QString& domainHostname) {
+    if (_verbose) {
+        qDebug() << "domainChanged";
+    }
+}
+
+void ATPClientApp::nodeAdded(SharedNodePointer node) {
+    if (_verbose) {
+        qDebug() << "node added: " << node->getType();
+    }
+}
+
+void ATPClientApp::nodeActivated(SharedNodePointer node) {
+    if (node->getType() == NodeType::AssetServer) {
+        auto path = _url.path();
+
+        if (_verbose) {
+            qDebug() << "path is " << path;
+        }
+
+        qDebug() << "_localUploadFile =" << _localUploadFile;
+
+        if (!_localUploadFile.isEmpty()) {
+            uploadAsset();
+        } else if (path == "/") {
+            listAssets();
+        } else {
+            lookupAsset();
+        }
+    }
+}
+
+void ATPClientApp::nodeKilled(SharedNodePointer node) {
+    qDebug() << "nodeKilled";
+}
+
+void ATPClientApp::timedOut() {
+    finish(1);
+}
+
+void ATPClientApp::notifyPacketVersionMismatch() {
+    if (_verbose) {
+        qDebug() << "packet version mismatch";
+    }
+    finish(1);
+}
+
+void ATPClientApp::uploadAsset() {
+    auto path = _url.path();
+    if (path == "/") {
+        qDebug() << "cannot upload to path of /";
+        QCoreApplication::exit(1);
+    }
+
+    auto upload = DependencyManager::get<AssetClient>()->createUpload(_localUploadFile);
+    QObject::connect(upload, &AssetUpload::finished, this, [=](AssetUpload* upload, const QString& hash) mutable {
+        if (upload->getError() != AssetUpload::NoError) {
+            qDebug() << "upload failed: " << upload->getErrorString();
+        } else {
+            setMapping(hash);
+        }
+
+        upload->deleteLater();
+    });
+
+    upload->start();
+}
+
+void ATPClientApp::setMapping(QString hash) {
+    auto path = _url.path();
+    auto assetClient = DependencyManager::get<AssetClient>();
+    auto request = assetClient->createSetMappingRequest(path, hash);
+
+    connect(request, &SetMappingRequest::finished, this, [=](SetMappingRequest* request) mutable {
+        if (request->getError() != SetMappingRequest::NoError) {
+            qDebug() << "upload succeeded, but couldn't set mapping: " << request->getErrorString();
+        }
+        request->deleteLater();
+    });
+
+    request->start();
+}
+
+void ATPClientApp::listAssets() {
+    auto request = DependencyManager::get<AssetClient>()->createGetAllMappingsRequest();
+    QObject::connect(request, &GetAllMappingsRequest::finished, this, [=](GetAllMappingsRequest* request) mutable {
+        auto result = request->getError();
+        if (result == GetAllMappingsRequest::NotFound) {
+            qDebug() << "not found: " << request->getErrorString();
+        } else if (result == GetAllMappingsRequest::NoError) {
+            auto mappings = request->getMappings();
+            for (auto& kv : mappings ) {
+                qDebug() << kv.first << kv.second;
+            }
+        } else {
+            qDebug() << "error -- " << request->getError() << " -- " << request->getErrorString();
+        }
+        request->deleteLater();
+    });
+    request->start();
+}
+
+void ATPClientApp::lookupAsset() {
+    auto path = _url.path();
+    auto request = DependencyManager::get<AssetClient>()->createGetMappingRequest(path);
+    QObject::connect(request, &GetMappingRequest::finished, this, [=](GetMappingRequest* request) mutable {
+        auto result = request->getError();
+        if (result == GetMappingRequest::NotFound) {
+            qDebug() << "not found: " << request->getErrorString();
+        } else if (result == GetMappingRequest::NoError) {
+            qDebug() << "found, hash is " << request->getHash();
+            download(request->getHash());
+        } else {
+            qDebug() << "error -- " << request->getError() << " -- " << request->getErrorString();
+        }
+        request->deleteLater();
+    });
+    request->start();
+}
+
+void ATPClientApp::download(AssetHash hash) {
+    auto assetClient = DependencyManager::get<AssetClient>();
+    auto assetRequest = new AssetRequest(hash);
+
+    connect(assetRequest, &AssetRequest::finished, this, [this](AssetRequest* request) mutable {
+        Q_ASSERT(request->getState() == AssetRequest::Finished);
+
+        if (request->getError() == AssetRequest::Error::NoError) {
+            QString data = QString::fromUtf8(request->getData());
+            if (_localOutputFile == "" || _localOutputFile == "-") {
+                QTextStream cout(stdout);
+                cout << data;
+            } else {
+                QFile outputHandle(_localOutputFile);
+                if (outputHandle.open(QIODevice::ReadWrite)) {
+                    QTextStream stream( &outputHandle );
+                    stream << data;
+                } else {
+                    qDebug() << "couldn't open output file:" << _localOutputFile;
+                }
+            }
+        }
+
+        request->deleteLater();
+        finish(0);
+    });
+
+    assetRequest->start();
+}
+
+void ATPClientApp::finish(int exitCode) {
+    auto nodeList = DependencyManager::get<NodeList>();
+
+    // send the domain a disconnect packet, force stoppage of domain-server check-ins
+    nodeList->getDomainHandler().disconnect();
+    nodeList->setIsShuttingDown(true);
+
+    // tell the packet receiver we're shutting down, so it can drop packets
+    nodeList->getPacketReceiver().setShouldDropPackets(true);
+
+    // remove the NodeList from the DependencyManager
+    DependencyManager::destroy<NodeList>();
+
+    QCoreApplication::exit(exitCode);
+}
