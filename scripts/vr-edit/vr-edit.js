bf722f084efd06dfc409d29c7290a0bc6facd911
David Rowe <david@ctrlaltstudio.com>
2017-07-05 12:49:36 +1200

    A controller object for each hand

--- scripts/vr-edit/vr-edit.js
+++ scripts/vr-edit/vr-edit.js
@@ -21,12 +21,111 @@
 
         VR_EDIT_SETTING = "io.highfidelity.isVREditing";  // Note: This constant is duplicated in utils.js.
 
+        hands = [],
+        LEFT_HAND = 0,
+        RIGHT_HAND = 1,
+
         UPDATE_LOOP_TIMEOUT = 16,
-        updateTimer = null;
+        updateTimer = null,
+
+        Hand;
+
+    Hand = function (side) {
+        var hand,
+            handController,
+            controllerTrigger,
+            controllerTriggerClicked,
+
+            TRIGGER_ON_VALUE = 0.15,  // Per handControllerGrab.js.
+            TRIGGER_OFF_VALUE = 0.1,  // Per handControllerGrab.js.
+            GRAB_POINT_SPHERE_OFFSET = { x: 0.04, y: 0.13, z: 0.039 },  // Per HmdDisplayPlugin.cpp and controllers.js.
+
+            PICK_MAX_DISTANCE = 500,  // Per handControllerGrab.js.
+            PRECISION_PICKING = true,
+            NO_INCLUDE_IDS = [],
+            NO_EXCLUDE_IDS = [],
+            VISIBLE_ONLY = true,
+
+            isLaserOn = false;
+
+        hand = side;
+        if (hand === LEFT_HAND) {
+            handController = Controller.Standard.LeftHand;
+            controllerTrigger = Controller.Standard.LT;
+            controllerTriggerClicked = Controller.Standard.LTClick;
+            GRAB_POINT_SPHERE_OFFSET.x = -GRAB_POINT_SPHERE_OFFSET.x;
+        } else {
+            handController = Controller.Standard.RightHand;
+            controllerTrigger = Controller.Standard.RT;
+            controllerTriggerClicked = Controller.Standard.RTClick;
+        }
+
+        function update() {
+            var wasLaserOn,
+                handPose,
+                handPosition,
+                handOrientation,
+                deltaOrigin,
+                pickRay,
+                intersection,
+                distance;
+
+            // Controller trigger.
+            wasLaserOn = isLaserOn;
+            isLaserOn = Controller.getValue(controllerTrigger) > (isLaserOn ? TRIGGER_OFF_VALUE : TRIGGER_ON_VALUE);
+            if (!isLaserOn) {
+                if (wasLaserOn) {
+                    // Clear laser
+                }
+                return;
+            }
+
+            // Hand position and orientation.
+            handPose = Controller.getPoseValue(handController);
+            if (!handPose.valid) {
+                isLaserOn = false;
+                if (wasLaserOn) {
+                    // Clear laser
+                }
+                return;
+            }
+            handPosition = Vec3.sum(Vec3.multiplyQbyV(MyAvatar.orientation, handPose.translation), MyAvatar.position);
+            handOrientation = Quat.multiply(MyAvatar.orientation, handPose.rotation);
+
+            // Entity intersection, if any.
+            deltaOrigin = Vec3.multiplyQbyV(handOrientation, GRAB_POINT_SPHERE_OFFSET);
+            pickRay = {
+                origin: Vec3.sum(handPosition, deltaOrigin),  // Add a bit to ...
+                direction: Quat.getUp(handOrientation),
+                length: PICK_MAX_DISTANCE
+            };
+            intersection = Entities.findRayIntersection(pickRay, PRECISION_PICKING, NO_INCLUDE_IDS, NO_EXCLUDE_IDS,
+                VISIBLE_ONLY);
+            distance = intersection.intersects ? intersection.distance : PICK_MAX_DISTANCE;
+
+            // Update laser.
+        }
+
+        function destroy() {
+        }
+
+        if (!this instanceof Hand) {
+            return new Hand();
+        }
+
+        return {
+            update: update,
+            destroy: destroy
+        };
+    };
+
 
     function update() {
         // Main update loop.
         updateTimer = null;
+
+        hands[LEFT_HAND].update();
+        hands[RIGHT_HAND].update();
 
         updateTimer = Script.setTimeout(update, UPDATE_LOOP_TIMEOUT);
     }
@@ -68,6 +167,10 @@
             button.clicked.connect(onButtonClicked);
         }
 
+        // Hands, each with a laser, selection, etc.
+        hands[LEFT_HAND] = new Hand(LEFT_HAND);
+        hands[RIGHT_HAND] = new Hand(RIGHT_HAND);
+
         if (isAppActive) {
             update();
         }
@@ -89,6 +192,15 @@
             button.clicked.disconnect(onButtonClicked);
             tablet.removeButton(button);
             button = null;
+        }
+
+        if (hands[LEFT_HAND]) {
+            hands[LEFT_HAND].destroy();
+            hands[LEFT_HAND] = null;
+        }
+        if (hands[RIGHT_HAND]) {
+            hands[RIGHT_HAND].destroy();
+            hands[RIGHT_HAND] = null;
         }
 
         tablet = null;
