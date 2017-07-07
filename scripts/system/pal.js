ea77557e387500c6342077d6344f05cbe9a3d678
Zach Fox <fox@highfidelity.io>
2017-07-07 08:08:52 -0700

    Correctly close PAL when switching domains

--- scripts/system/pal.js
+++ scripts/system/pal.js
@@ -865,6 +865,9 @@ function avatarDisconnected(nodeID) {
 
 function clearLocalQMLDataAndClosePAL() {
     sendToQml({ method: 'clearLocalQMLData' });
+    if (onPalScreen) {
+        tablet.gotoHomeScreen();
+    }
 }
 
 function avatarAdded(avatarID) {
