1bf2855e9b5a4960333e5c777816a0dfadc8bd58
Mike Moody <amvmoody@gmail.com>
2017-07-24 18:47:24 -0700

    Created script for spherical gravity.

--- unpublishedScripts/gravity.js
+++ unpublishedScripts/gravity.js
@@ -2,7 +2,7 @@
 //  gravity.js
 //
 //  Created by Alan-Michael Moody on 7/24/17
-//  Copyright 2016 High Fidelity, Inc.
+//  Copyright 2017 High Fidelity, Inc.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
