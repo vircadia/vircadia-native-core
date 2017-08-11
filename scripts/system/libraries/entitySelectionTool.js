9279290b2ea6f74c11815efa4eb19941a16e2905
LaShonda Hopper <1p-cusack@1stplayable.com>
2017-08-11 15:10:25 -0400

    [Case 6491] Remove dupe pickRay var (details below).
    Ran lint pass and found the dupe define.
    
    As noted in a previous commit, the only remaining lint
    issues are as follows:
    
    * scripts/system/libraries/entitySelectionTool.js: line 17, col 1, Read only.
        * HIFI_PUBLIC_BUCKET assignment
    * scripts/system/libraries/entitySelectionTool.js: line 19, col 1, Read only.
        * SPACE_WORLD assignment
    * scripts/system/libraries/entitySelectionTool.js: line 30, col 1, Read only.
        * SelectionManager assignment
    
    Changes Committed:
    	modified:   scripts/system/libraries/entitySelectionTool.js

--- scripts/system/libraries/entitySelectionTool.js
+++ scripts/system/libraries/entitySelectionTool.js
@@ -2770,7 +2770,6 @@ SelectionDisplay = (function() {
             }
             
             planeNormal = Vec3.multiplyQbyV(rotation, planeNormal);
-            var pickRay = generalComputePickRay(event.x, event.y);
             lastPick = rayPlaneIntersection(pickRay,
                 pickRayPosition,
                 planeNormal);
