5042b900520874bad449529a1b67e801af9054e6
Anthony J. Thibault <tony@highfidelity.io>
2017-08-24 10:47:48 -0700

    Rig: Fix for debug mode assert

--- libraries/animation/src/Rig.cpp
+++ libraries/animation/src/Rig.cpp
@@ -1129,7 +1129,6 @@ static bool findPointKDopDisplacement(const glm::vec3& point, const AnimPose& sh
     glm::vec3 localPoint = shapePose.inverse().xformPoint(point);
 
     // Only works for 14-dop shape infos.
-    assert(shapeInfo.dots.size() == DOP14_COUNT);
     if (shapeInfo.dots.size() != DOP14_COUNT) {
         return false;
     }
