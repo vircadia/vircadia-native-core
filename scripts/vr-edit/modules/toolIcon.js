850efec97bd45064fd856a9e941caf2aea4eaa71
David Rowe <david@ctrlaltstudio.com>
2017-08-25 14:07:41 +1200

    Display tool icon on dominant hand when tool is active

--- scripts/vr-edit/modules/toolIcon.js
+++ scripts/vr-edit/modules/toolIcon.js
@@ -23,37 +23,63 @@ ToolIcon = function (side) {
         PHYSICS_TOOL = 5,
         DELETE_TOOL = 6,
 
-        ICON_COLORS = [
-            { red: 0, green: 240, blue: 240 },
-            { red: 240, green: 240, blue: 0 },
-            { red: 220, green: 60, blue: 220 },
-            { red: 220, green: 220, blue: 220 },
-            { red: 0, green: 0, blue: 0 },
-            { red: 60, green: 60, blue: 240 },
-            { red: 240, green: 60, blue: 60 }
-        ],
-
         LEFT_HAND = 0,
 
-        ICON_DIMENSIONS = { x: 0.1, y: 0.01, z: 0.1 },
-        ICON_POSITION = { x: 0, y: 0.01, z: 0 },
-        ICON_ROTATION = Quat.fromVec3Degrees({ x: 0, y: 0, z: 0 }),
+        MODEL_DIMENSIONS = { x: 0.1944, y: 0.1928, z: 0.1928 },  // Raw FBX dimensions.
+        MODEL_SCALE = 0.7,  // Adjust icon dimensions so that the green bar matches that of the Tools header.
+        MODEL_POSITION_LEFT_HAND = { x: -0.03, y: 0.035, z: 0 },  // x raises in thumb direction; y moves in fingers direction.
+        MODEL_POSITION_RIGHT_HAND = { x: 0.03, y: 0.035, z: 0 },  // ""
+        MODEL_ROTATION_LEFT_HAND = Quat.fromVec3Degrees({ x: 0, y: 0, z: 100 }),
+        MODEL_ROTATION_RIGHT_HAND = Quat.fromVec3Degrees({ x: 0, y: 180, z: -100 }),
 
-        ICON_TYPE = "sphere",
-        ICON_PROPERTIES = {
-            dimensions: ICON_DIMENSIONS,
-            localPosition: ICON_POSITION,
-            localRotation: ICON_ROTATION,
+        MODEL_TYPE = "model",
+        MODEL_PROPERTIES = {
+            url: "../assets/tools/tool-icon.fbx",
+            dimensions: Vec3.multiply(MODEL_SCALE, MODEL_DIMENSIONS),
             solid: true,
             alpha: 1.0,
             parentID: Uuid.SELF,
-            ignoreRayIntersection: false,
+            ignoreRayIntersection: true,
             visible: true
         },
 
-        handJointName,
+        IMAGE_TYPE = "image3d",
+        IMAGE_PROPERTIES = {
+            localRotation: Quat.fromVec3Degrees({ x: -90, y: -90, z: 0 }),
+            alpha: 1.0,
+            emissive: true,
+            ignoreRayIntersection: true,
+            isFacingAvatar: false,
+            visible: true
+        },
 
-        iconOverlay = null;
+        ICON_PROPERTIES = {
+            url: "../assets/tools/stretch-icon.svg",
+            dimensions: { x: 0.0167, y: 0.0167 },
+            localPosition: { x: 0.020, y: 0.069, z: 0 },  // Relative to model overlay.
+            color: UIT.colors.lightGrayText               // x is in fingers direction; y is in thumb direction.
+        },
+        LABEL_PROPERTIES = {
+            url: "../assets/tools/stretch-label.svg",
+            scale: 0.0311,
+            localPosition: { x: -0.040, y: 0.067, z: 0 },
+            color: UIT.colors.white
+        },
+        SUBLABEL_PROPERTIES = {
+            url: "../assets/tools/tool-label.svg",
+            scale: 0.0152,
+            localPosition: { x: -0.055, y: 0.067, z: 0 },
+            color: UIT.colors.lightGrayText
+        },
+
+        ICON_SCALE_FACTOR = 3.0,
+        LABEL_SCALE_FACTOR = 1.8,
+
+        handJointName,
+        localPosition,
+        localRotation,
+
+        modelOverlay = null;
 
     if (!this instanceof ToolIcon) {
         return new ToolIcon();
@@ -61,7 +87,15 @@ ToolIcon = function (side) {
 
     function setHand(side) {
         // Assumes UI is not displaying.
-        handJointName = side === LEFT_HAND ? "LeftHand" : "RightHand";
+        if (side === LEFT_HAND) {
+            handJointName = "LeftHand";
+            localPosition = MODEL_POSITION_LEFT_HAND;
+            localRotation = MODEL_ROTATION_LEFT_HAND;
+        } else {
+            handJointName = "RightHand";
+            localPosition = MODEL_POSITION_RIGHT_HAND;
+            localRotation = MODEL_ROTATION_RIGHT_HAND;
+        }
     }
 
     setHand(side);
@@ -71,10 +105,18 @@ ToolIcon = function (side) {
         // TODO: Clear icon animation.
     }
 
+    function clear() {
+        // Deletes current tool model.
+        if (modelOverlay) {
+            Overlays.deleteOverlay(modelOverlay);  // Child overlays are automatically deleted.
+            modelOverlay = null;
+        }
+    }
+
     function display(icon) {
         // Displays icon on hand.
         var handJointIndex,
-            iconProperties;
+            properties;
 
         handJointIndex = MyAvatar.getJointIndex(handJointName);
         if (handJointIndex === -1) {
@@ -84,22 +126,45 @@ ToolIcon = function (side) {
             return;
         }
 
-        if (iconOverlay === null) {
-            iconProperties = Object.clone(ICON_PROPERTIES);
-            iconProperties.parentJointIndex = handJointIndex;
-            iconProperties.color = ICON_COLORS[icon];
-            iconOverlay = Overlays.addOverlay(ICON_TYPE, iconProperties);
-        } else {
-            Overlays.editOverlay(iconOverlay, { color: ICON_COLORS[icon] });
+        if (modelOverlay !== null) {
+            // Should never happen because tool needs to be cleared in order for user to return to Tools menu.
+            clear();
         }
-    }
 
-    function clear() {
-        // Deletes current icon.
-        if (iconOverlay) {
-            Overlays.deleteOverlay(iconOverlay);
-            iconOverlay = null;
-        }
+        // Model.
+        properties = Object.clone(MODEL_PROPERTIES);
+        properties.url = Script.resolvePath(properties.url);
+        properties.parentJointIndex = handJointIndex;
+        properties.localPosition = localPosition;
+        properties.localRotation = localRotation;
+        modelOverlay = Overlays.addOverlay(MODEL_TYPE, properties);
+
+        // Icon.
+        properties = Object.clone(IMAGE_PROPERTIES);
+        properties = Object.merge(properties, ICON_PROPERTIES);
+        properties.parentID = modelOverlay;
+        properties.url = Script.resolvePath(properties.url);
+        properties.dimensions = {
+            x: ICON_SCALE_FACTOR * properties.dimensions.x,
+            y: ICON_SCALE_FACTOR * properties.dimensions.y
+        };
+        Overlays.addOverlay(IMAGE_TYPE, properties);
+
+        // Label.
+        properties = Object.clone(IMAGE_PROPERTIES);
+        properties = Object.merge(properties, LABEL_PROPERTIES);
+        properties.parentID = modelOverlay;
+        properties.url = Script.resolvePath(properties.url);
+        properties.scale = LABEL_SCALE_FACTOR * properties.scale;
+        Overlays.addOverlay(IMAGE_TYPE, properties);
+
+        // Sublabel.
+        properties = Object.clone(IMAGE_PROPERTIES);
+        properties = Object.merge(properties, SUBLABEL_PROPERTIES);
+        properties.parentID = modelOverlay;
+        properties.url = Script.resolvePath(properties.url);
+        properties.scale = LABEL_SCALE_FACTOR * properties.scale;
+        Overlays.addOverlay(IMAGE_TYPE, properties);
     }
 
     function destroy() {
