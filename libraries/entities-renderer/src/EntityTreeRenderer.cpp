b8795c3ea9b23b624f8f0fe5ad05ff0dbf4c8067
Seth Alves <seth.alves@gmail.com>
2017-08-28 17:08:59 -0700

    remove unneeded bind-by-ref thing from lambdas

--- libraries/entities-renderer/src/EntityTreeRenderer.cpp
+++ libraries/entities-renderer/src/EntityTreeRenderer.cpp
@@ -55,11 +55,11 @@ EntityTreeRenderer::EntityTreeRenderer(bool wantScripts, AbstractViewStateInterf
     _displayModelBounds(false),
     _layeredZones(this)
 {
-    setMouseRayPickResultOperator([&](QUuid rayPickID) {
+    setMouseRayPickResultOperator([](QUuid rayPickID) {
         RayToEntityIntersectionResult entityResult;
         return entityResult;
     });
-    setSetPrecisionPickingOperator([&](QUuid rayPickID, bool value) {});
+    setSetPrecisionPickingOperator([](QUuid rayPickID, bool value) {});
     EntityRenderer::initEntityRenderers();
 
     REGISTER_ENTITY_TYPE_WITH_FACTORY(Model, RenderableModelEntityItem::factory)
