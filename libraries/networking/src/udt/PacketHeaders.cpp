c4419fede802689994cf2d61ad4c8217192daa76
Nissim Hadar <nissim@highfidelity.io>
2017-09-22 19:07:05 -0700

    Bumped up the version number.

--- libraries/networking/src/udt/PacketHeaders.cpp
+++ libraries/networking/src/udt/PacketHeaders.cpp
@@ -30,7 +30,7 @@ PacketVersion versionForPacketType(PacketType packetType) {
         case PacketType::EntityEdit:
         case PacketType::EntityData:
         case PacketType::EntityPhysics:
-            return VERSION_ENTITIES_ANIMATION_ALLOW_TRANSLATION_PROPERTIES;
+            return VERSION_ENTITIES_HAZE;
         case PacketType::EntityQuery:
             return static_cast<PacketVersion>(EntityQueryPacketVersion::JSONFilterWithFamilyTree);
         case PacketType::AvatarIdentity:
