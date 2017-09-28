c3225c2c761d52b5ade6a8e361189bc7f1829140
vladest <vladstelmahovsky@gmail.com>
2017-09-28 21:38:03 +0200

    Make fix precisely for Linux

--- interface/src/main.cpp
+++ interface/src/main.cpp
@@ -49,7 +49,7 @@ int main(int argc, const char* argv[]) {
     CrashReporter crashReporter { BUG_SPLAT_DATABASE, BUG_SPLAT_APPLICATION_NAME, BuildInfo::VERSION };
 #endif
 
-#ifdef Q_OS_UNIX
+#ifdef Q_OS_LINUX
     QApplication::setAttribute(Qt::AA_DontUseNativeMenuBar);
 #endif
 
