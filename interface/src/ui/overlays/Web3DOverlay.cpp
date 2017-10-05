cb21aaff721c41dcfa70fb7d01f7c0dce2c503f0
vladest <vladstelmahovsky@gmail.com>
2017-10-05 15:43:07 +0200

    Accept touch event according to last accepted values

--- interface/src/ui/overlays/Web3DOverlay.cpp
+++ interface/src/ui/overlays/Web3DOverlay.cpp
@@ -468,7 +468,7 @@ void Web3DOverlay::handlePointerEventAsTouch(const PointerEvent& event) {
 #endif
 
     touchEvent.setTimestamp((ulong)QDateTime::currentMSecsSinceEpoch());
-    touchEvent.setAccepted(false);
+    touchEvent.setAccepted(_touchBeginAccepted);
 
     if (touchType == QEvent::TouchBegin) {
         _touchBeginAccepted = QCoreApplication::sendEvent(_webSurface->getWindow(), &touchEvent);
