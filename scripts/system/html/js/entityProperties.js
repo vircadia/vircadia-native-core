3152d5808e76ab3b20034e919dea8f5ff6755c79
LaShonda Hopper <lashonda@1stplayable.com>
2017-11-06 20:09:08 -0500

    Minor: Replace tabs with spaces noticed when browsing file.
    Changes Committed:
        modified:   scripts/system/html/js/entityProperties.js

--- scripts/system/html/js/entityProperties.js
+++ scripts/system/html/js/entityProperties.js
@@ -1120,7 +1120,7 @@ function loaded() {
                             activeElement.select();
                         }
                     }
-					clearSelection();
+                    clearSelection();
                 }
             });
         }
