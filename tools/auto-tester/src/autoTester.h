3457e1de5d7f796991e5e9ef3724b23de62fd707
Nissim Hadar <nissim.hadar@gmail.com>
2017-11-11 13:19:57 -0800

    WIP.

--- tools/auto-tester/src/autoTester.h
+++ tools/auto-tester/src/autoTester.h
@@ -8,12 +8,11 @@
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
-
 #ifndef hifi_AutoTester_h
 #define hifi_AutoTester_h
 
 #include <QtWidgets/QMainWindow>
-////#include "ui_autoTester.h"
+#include "ui_autoTester.h"
 ////#include "Test.h"
 ////
 ////class AutoTester : public QMainWindow
