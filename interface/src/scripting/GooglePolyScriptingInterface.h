d6e1a9defe9e411acb7780c07b7d85ceb8c81ae8
Elisa Lupin-Jimenez <elisa@highfidelity.io>
2017-12-04 16:46:58 -0800

    Created poly scripting interface template

---
+++ interface/src/scripting/GooglePolyScriptingInterface.h
@@ -0,0 +1,28 @@
+//
+//  GooglePolyScriptingInterface.h
+//  interface/src/scripting
+//
+//  Created by Elisa Lupin-Jimenez on 12/3/2017.
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_GooglePolyScriptingInterface_h
+#define hifi_GooglePolyScriptingInterface_h
+
+#include <QObject>
+#include <DependencyManager.h>
+
+class GooglePolyScriptingInterface : public QObject, public Dependency {
+    Q_OBJECT
+public:
+    GooglePolyScriptingInterface();
+
+public slots:
+    void testPrint();
+
+};
+
+#endif // hifi_GooglePolyScriptingInterface_h
