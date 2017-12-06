cafebf6a7e89653fc6af55ca84ec68902ad4bf76
David Rowe <david@ctrlaltstudio.com>
2017-12-06 13:38:42 +1300

    Fix Unix build warnings and tidy initializers

--- interface/src/ui/overlays/ModelOverlay.cpp
+++ interface/src/ui/overlays/ModelOverlay.cpp
@@ -34,9 +34,9 @@ ModelOverlay::ModelOverlay(const ModelOverlay* modelOverlay) :
     _model(std::make_shared<Model>(nullptr, this)),
     _modelTextures(QVariantMap()),
     _url(modelOverlay->_url),
+    _updateModel(false),
     _scaleToFit(modelOverlay->_scaleToFit),
-    _loadPriority(modelOverlay->_loadPriority),
-    _updateModel(false)
+    _loadPriority(modelOverlay->_loadPriority)
 {
     _model->init();
     _model->setLoadingPriority(_loadPriority);
