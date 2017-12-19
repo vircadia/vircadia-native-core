63ac426bf27c9782c0406224275862790aed1c3f
Seth Alves <seth.alves@gmail.com>
2017-12-18 17:30:56 -0800

    fix logic that resets own-avatar collisions after a parenting grab is released

--- libraries/entities/src/EntityItem.cpp
+++ libraries/entities/src/EntityItem.cpp
@@ -1626,6 +1626,10 @@ void EntityItem::setParentID(const QUuid& value) {
             newParentNoBootstrapping |= Simulation::NO_BOOTSTRAPPING;
         }
 
+        if (!oldParentID.isNull() && (oldParentID == Physics::getSessionUUID() || oldParentID == AVATAR_SELF_ID)) {
+            oldParentNoBootstrapping |= Simulation::NO_BOOTSTRAPPING;
+        }
+
         if ((bool)(oldParentNoBootstrapping ^ newParentNoBootstrapping)) {
             if ((bool)(newParentNoBootstrapping & Simulation::NO_BOOTSTRAPPING)) {
                 markDirtyFlags(Simulation::NO_BOOTSTRAPPING);
