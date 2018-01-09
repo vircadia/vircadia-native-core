075bcba49f2fb88b542e2d5e688759a89093231c
vladest <vladstelmahovsky@gmail.com>
2018-01-09 23:48:40 +0100

    AddressBar trigger implemented. Fixed warnings on non existing context properties. Fixed warnings about non notifyable property

--- interface/src/ui/AddressBarDialog.h
+++ interface/src/ui/AddressBarDialog.h
@@ -22,7 +22,7 @@ class AddressBarDialog : public OffscreenQmlDialog {
     Q_PROPERTY(bool backEnabled READ backEnabled NOTIFY backEnabledChanged)
     Q_PROPERTY(bool forwardEnabled READ forwardEnabled NOTIFY forwardEnabledChanged)
     Q_PROPERTY(bool useFeed READ useFeed WRITE setUseFeed NOTIFY useFeedChanged)
-    Q_PROPERTY(QString metaverseServerUrl READ metaverseServerUrl)
+    Q_PROPERTY(QString metaverseServerUrl READ metaverseServerUrl CONSTANT)
 
 public:
     AddressBarDialog(QQuickItem* parent = nullptr);
