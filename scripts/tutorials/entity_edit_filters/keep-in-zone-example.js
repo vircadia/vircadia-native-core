8aa0525f4536b965a6b3bd1586291ca38df164a7
ZappoMan <brad@highfidelity.io>
2018-01-10 10:32:28 -0800

    CR style feedback

--- scripts/tutorials/entity_edit_filters/keep-in-zone-example.js
+++ scripts/tutorials/entity_edit_filters/keep-in-zone-example.js
@@ -1,8 +1,21 @@
+//
+// keep-in-zone-example.js
+// 
+//
+// Created by Brad Hefta-Gaub to use Entities on Dec. 15, 2017
+// Copyright 2017 High Fidelity, Inc.
+//
+// This sample entity edit filter script will keep any entity inside the bounding box of the zone this filter is applied to.
+//
+// Distributed under the Apache License, Version 2.0.
+// See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
 function filter(properties, type, originalProperties, zoneProperties) {    
 
     var nearZero = 0.0001 * Math.random() + 0.001;
 
-	/* Clamp position changes to bounding box of zone.*/
+    /* Clamp position changes to bounding box of zone.*/
     function clamp(val, min, max) {
         /* Random near-zero value used as "zero" to prevent two sequential updates from being
         exactly the same (which would cause them to be ignored) */
