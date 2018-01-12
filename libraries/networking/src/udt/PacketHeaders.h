f22429d0982526a1fbd0e9f2ce4ad3bbe57d0b1a
samcake <samuel.gateau@gmail.com>
2018-01-11 18:03:11 -0800

    Working on the shaders

--- libraries/networking/src/udt/PacketHeaders.h
+++ libraries/networking/src/udt/PacketHeaders.h
@@ -202,7 +202,7 @@ enum class EntityVersion : PacketVersion {
     HazeEffect,
     StaticCertJsonVersionOne,
     OwnershipChallengeFix,
-    ZoneLightInheritModes,
+    ZoneLightInheritModes = 82,
     ZoneStageRemoved
 };
 
