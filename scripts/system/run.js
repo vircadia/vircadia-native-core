508a3b37cd87cefe0fa4a8a0e7603ef8576b5403
Seth Alves <seth.alves@gmail.com>
2018-01-12 15:28:42 -0800

    fix icon.  button lights up when run in enabled

--- scripts/system/run.js
+++ scripts/system/run.js
@@ -3,18 +3,24 @@
 /* global Script, Tablet, MyAvatar */
 
 (function() { // BEGIN LOCAL_SCOPE
+    var WALK_SPEED = 2.6;
+    var RUN_SPEED = 4.5;
+    var MIDDLE_SPEED = (WALK_SPEED + RUN_SPEED) / 2.0;
+
     var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
     var button = tablet.addButton({
-        icon: Script.resolvePath("assets/images/icon-particles.svg"),
-        text: "Run",
+        icon: Script.resolvePath("assets/images/run.svg"),
+        text: "RUN",
         sortOrder: 15
     });
 
     function onClicked() {
-        if (MyAvatar.walkSpeed < 4) {
-            MyAvatar.walkSpeed = 4.5;
+        if (MyAvatar.walkSpeed < MIDDLE_SPEED) {
+            button.editProperties({isActive: true});
+            MyAvatar.walkSpeed = RUN_SPEED;
         } else {
-            MyAvatar.walkSpeed = 2.6;
+            button.editProperties({isActive: false});
+            MyAvatar.walkSpeed = WALK_SPEED;
         }
     }
 
@@ -24,4 +30,10 @@
     }
 
     button.clicked.connect(onClicked);
+    if (MyAvatar.walkSpeed < MIDDLE_SPEED) {
+        button.editProperties({isActive: false});
+    } else {
+        button.editProperties({isActive: true});
+    }
+
 }());
