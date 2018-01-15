3e7d30366125d8ec004ac57c400680c7ca65b2b9
Seth Alves <seth.alves@gmail.com>
2018-01-15 09:18:22 -0800

    export atp GetMappingRequest to js

--- libraries/script-engine/src/AssetScriptingInterface.h
+++ libraries/script-engine/src/AssetScriptingInterface.h
@@ -75,7 +75,23 @@ public:
      * @param {string} error
      */
     Q_INVOKABLE void setMapping(QString path, QString hash, QScriptValue callback);
-    
+
+    /**jsdoc
+     * Look up a path to hash mapping within the connected domain's asset server
+     * @function Assets.getMapping
+     * @static
+     * @param path {string}
+     * @param callback {Assets~getMappingCallback}
+     */
+
+    /**jsdoc
+     * Called when getMapping is complete
+     * @callback Assets~getMappingCallback
+     * @param {string} assetID
+     */
+    Q_INVOKABLE void getMapping(QString path, QScriptValue callback);
+
+
     Q_INVOKABLE void setBakingEnabled(QString path, bool enabled, QScriptValue callback);
 
 #if (PR_BUILD || DEV_BUILD)
