744da485512c691ea110145a8bb3a809402cf57e
humbletim <humbletim@gmail.com>
2018-01-17 14:00:50 -0500

    add apache 2.0 headers

--- libraries/shared/src/shared/MiniPromises.cpp
+++ libraries/shared/src/shared/MiniPromises.cpp
@@ -1,2 +1,10 @@
+//
+//  Created by Timothy Dedischew on 2017/12/21
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
 #include "MiniPromises.h"
 int MiniPromise::metaTypeID = qRegisterMetaType<MiniPromise::Promise>("MiniPromise::Promise");
--- libraries/shared/src/shared/MiniPromises.h
+++ libraries/shared/src/shared/MiniPromises.h
@@ -1,3 +1,11 @@
+//
+//  Created by Timothy Dedischew on 2017/12/21
+//  Copyright 2017 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
 #pragma once
 
 // Minimalist threadsafe Promise-like helper for managing asynchronous results
