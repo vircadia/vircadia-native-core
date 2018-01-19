c71fa110dbda9a69ba225f8f29e88ecdefcd4525
Dante Ruiz <danteruiz102@gmail.com>
2018-01-19 11:31:13 -0800

    update file dates and  remove debug state

--- interface/src/AvatarEntitiesBookmarks.h
+++ interface/src/AvatarEntitiesBookmarks.h
@@ -3,7 +3,7 @@
 //  interface/src
 //
 //  Created by Dante Ruiz on 15/01/18.
-//  Copyright 2017 High Fidelity, Inc.
+//  Copyright 2018 High Fidelity, Inc.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
