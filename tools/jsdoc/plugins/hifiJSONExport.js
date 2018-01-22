4ff3c9e0468ca69daf67bc86016100ac238e9beb
Thijs Wenker <me@thoys.nl>
2018-01-22 19:00:45 +0100

    fix jsdoc output folder

--- tools/jsdoc/plugins/hifiJSONExport.js
+++ tools/jsdoc/plugins/hifiJSONExport.js
@@ -1,11 +1,14 @@
 exports.handlers = {
     processingComplete: function(e) {
         const pathTools = require('path');
-        var rootFolder = pathTools.join(__dirname, '..');
+        var outputFolder = pathTools.join(__dirname, '../out');
         var doclets = e.doclets.map(doclet => Object.assign({}, doclet));
         const fs = require('fs');
+        if (!fs.existsSync(outputFolder)) {
+            fs.mkdirSync(outputFolder);
+        }
         doclets.map(doclet => {delete doclet.meta; delete doclet.comment});
-        fs.writeFile(pathTools.join(rootFolder, "out/hifiJSDoc.json"), JSON.stringify(doclets, null, 4), function(err) {
+        fs.writeFile(pathTools.join(outputFolder, "hifiJSDoc.json"), JSON.stringify(doclets, null, 4), function(err) {
             if (err) {
                 return console.log(err);
             }
