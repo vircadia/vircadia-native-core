483074b11e2dfd80f144e74aa0287c623c7d401d
Thijs Wenker <me@thoys.nl>
2018-01-22 17:59:01 +0100

    jsdoc cmake fixes

--- tools/jsdoc/plugins/hifiJSONExport.js
+++ tools/jsdoc/plugins/hifiJSONExport.js
@@ -1,9 +1,11 @@
 exports.handlers = {
     processingComplete: function(e) {
+        const pathTools = require('path');
+        var rootFolder = pathTools.join(__dirname, '..');
         var doclets = e.doclets.map(doclet => Object.assign({}, doclet));
         const fs = require('fs');
         doclets.map(doclet => {delete doclet.meta; delete doclet.comment});
-        fs.writeFile("out/hifiJSDoc.json", JSON.stringify(doclets, null, 4), function(err) {
+        fs.writeFile(pathTools.join(rootFolder, "out/hifiJSDoc.json"), JSON.stringify(doclets, null, 4), function(err) {
             if (err) {
                 return console.log(err);
             }
