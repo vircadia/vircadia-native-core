edfffc575a07d4c8d8b3e54419d58535e18947c3
Atlante45 <clement.brisset@gmail.com>
2018-01-30 11:48:01 -0800

    Handle multiple targets for add_crashpad

--- cmake/macros/AddCrashpad.cmake
+++ cmake/macros/AddCrashpad.cmake
@@ -10,8 +10,6 @@
 #
 
 macro(add_crashpad)
-  get_property(CRASHPAD_CHECKED GLOBAL PROPERTY CHECKED_FOR_CRASHPAD_ONCE)
-
   set (USE_CRASHPAD TRUE)
   if ("$ENV{CMAKE_BACKTRACE_URL}" STREQUAL "")
     set (USE_CRASHPAD FALSE)
@@ -25,13 +23,20 @@ macro(add_crashpad)
     set (CMAKE_BACKTRACE_TOKEN $ENV{CMAKE_BACKTRACE_TOKEN})
   endif()
 
-  if (WIN32 AND USE_CRASHPAD AND NOT CRASHPAD_CHECKED)
+  if (WIN32 AND USE_CRASHPAD)
+    get_property(CRASHPAD_CHECKED GLOBAL PROPERTY CHECKED_FOR_CRASHPAD_ONCE)
+    if (NOT CRASHPAD_CHECKED)
+        
+        add_dependency_external_projects(crashpad)
+        find_package(crashpad REQUIRED)
+
+        set_property(GLOBAL PROPERTY CHECKED_FOR_CRASHPAD_ONCE TRUE)
+    endif()
+
     add_definitions(-DHAS_CRASHPAD)
     add_definitions(-DCMAKE_BACKTRACE_URL=\"${CMAKE_BACKTRACE_URL}\")
     add_definitions(-DCMAKE_BACKTRACE_TOKEN=\"${CMAKE_BACKTRACE_TOKEN}\")
 
-    add_dependency_external_projects(crashpad)
-    find_package(crashpad REQUIRED)
     target_include_directories(${TARGET_NAME} PRIVATE ${CRASHPAD_INCLUDE_DIRS})
     target_link_libraries(${TARGET_NAME} ${CRASHPAD_LIBRARY} ${CRASHPAD_BASE_LIBRARY} ${CRASHPAD_UTIL_LIBRARY})
     
@@ -49,7 +54,5 @@ macro(add_crashpad)
       DESTINATION ${CLIENT_COMPONENT}
       COMPONENT ${INTERFACE_INSTALL_DIR}
     )
-
-    set_property(GLOBAL PROPERTY CHECKED_FOR_CRASHPAD_ONCE TRUE)
   endif ()
 endmacro()
