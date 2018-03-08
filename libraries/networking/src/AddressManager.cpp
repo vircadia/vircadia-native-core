7d7570798b68289a15ba859242136e80e52a5d85
Seth Alves <seth.alves@gmail.com>
2018-03-07 18:31:13 -0800

    oops, forgot to flip booleans along with logic

--- libraries/networking/src/AddressManager.cpp
+++ libraries/networking/src/AddressManager.cpp
@@ -212,7 +212,7 @@ bool AddressManager::handleUrl(const QUrl& lookupUrl, LookupTrigger trigger) {
 
         qCDebug(networking) << "Trying to go to URL" << lookupUrl.toString();
 
-        emit urlHandled(false);
+        emit urlHandled(true);
         DependencyManager::get<NodeList>()->flagTimeForConnectionStep(LimitedNodeList::ConnectionStep::LookupAddress);
 
         // there are 4 possible lookup strings
@@ -295,7 +295,7 @@ bool AddressManager::handleUrl(const QUrl& lookupUrl, LookupTrigger trigger) {
                lookupUrl.scheme() == URL_SCHEME_FILE) {
         _previousLookup.clear();
         QUrl domainUrl = PathUtils::expandToLocalDataAbsolutePath(lookupUrl);
-        emit urlHandled(true);
+        emit urlHandled(false);
         setDomainInfo(domainUrl, trigger);
         DependencyManager::get<NodeList>()->getDomainHandler().setIsConnected(true);
         emit lookupResultsFinished();
