a2e2d1bd6fa9aaba380ed187b67b4f0ff3445030
Alexia Mandeville <alexia@HIGHFIDELITY.IO>
2018-04-23 14:05:23 -0700

    Updating emote app with sit and love emotes. Renaming emotes and adjusting HTML and emote behaviors

--- scripts/system/emote.js
+++ scripts/system/emote.js
@@ -16,9 +16,11 @@
 (function() { // BEGIN LOCAL_SCOPE
 
 
-var EMOTE_ANIMATIONS = ['Crying', 'Surprised', 'Dancing', 'Cheering', 'Waving', 'Fall', 'Pointing', 'Clapping'];
+var EMOTE_ANIMATIONS = ['Cry', 'Surprised', 'Dance', 'Cheer', 'Wave', 'Fall', 'Point', 'Clap', 'Sit1', 'Sit2', 'Sit3', 'Love'];
 var ANIMATIONS = Array();
 
+var eventMappingName = "io.highfidelity.away"; // restoreAnimation on hand controller button events, too.
+var eventMapping = Controller.newMapping(eventMappingName);
 
 EMOTE_ANIMATIONS.forEach(function (name) {
     var animationURL = Script.resolvePath("assets/animations/" + name + ".fbx");
@@ -31,7 +33,7 @@ EMOTE_ANIMATIONS.forEach(function (name) {
 var EMOTE_APP_BASE = "html/EmoteApp.html";
 var EMOTE_APP_URL = Script.resolvePath(EMOTE_APP_BASE);
 var EMOTE_LABEL = "EMOTE";
-var EMOTE_APP_SORT_ORDER = 11;
+var EMOTE_APP_SORT_ORDER = 12;
 var FPS = 60;
 var MSEC_PER_SEC = 1000;
 var FINISHED = 3; // see ScriptableResource::State
@@ -71,33 +73,78 @@ function onWebEventReceived(event) {
     }
 
     if (event.type === "click") {
+        
         var emoteName = event.data;
-
-        if (ANIMATIONS[emoteName].resource.state == FINISHED) {
-            if (activeTimer !== false) {
-                Script.clearTimeout(activeTimer);
+        
+        if (activeTimer !== false) {
+            Script.clearTimeout(activeTimer);
+        }
+        
+        // if the activeEmote is different from the chosen emote, then play the new emote. Other wise, 
+        // this is a second click on the same emote as the activeEmote, and we will just stop it.
+        if (activeEmote !== emoteName) {
+            activeEmote = emoteName;
+            
+            // Allow for a random sitting animation when a user selects sit
+            var randSit = Math.floor(Math.random() * 3) + 1;
+            if(emoteName == "Sit"){
+                emoteName = event.data + randSit; // "Sit1, Sit2, Sit3"
             }
-
-            // if the activeEmote is different from the chosen emote, then play the new emote. Other wise, 
-            // this is a second click on the same emote as the activeEmote, and we will just stop it.
-            if (activeEmote !== emoteName) {
-                activeEmote = emoteName;
-                var frameCount = ANIMATIONS[emoteName].animation.frames.length;
+            
+            var frameCount = ANIMATIONS[emoteName].animation.frames.length;
+            
+            // Three types of emotes (non-looping end, non-looping return, looping)
+            if(emoteName.match(/^Sit.*$/) || emoteName == "Fall") { // non-looping end
+            
                 MyAvatar.overrideAnimation(ANIMATIONS[emoteName].url, FPS, false, 0, frameCount);
-
+                
+            } else if (emoteName == "Love" || emoteName == "Surprised" || emoteName == "Cry"  || emoteName == "Point"){ // non-looping return
+            
+                MyAvatar.overrideAnimation(ANIMATIONS[emoteName].url, FPS, false, 0, frameCount);
                 var timeOut = MSEC_PER_SEC * frameCount / FPS;
                 activeTimer = Script.setTimeout(function () {
                     MyAvatar.restoreAnimation();
                     activeTimer = false;
                     activeEmote = false;
                 }, timeOut);
-            } else {
-                activeEmote = false;
-                MyAvatar.restoreAnimation();
+                
+            } else { // looping
+            
+                MyAvatar.overrideAnimation(ANIMATIONS[emoteName].url, FPS, true, 0, frameCount);
+                
             }
+            
+        } else {
+            activeEmote = false;
+            MyAvatar.restoreAnimation();
         }
+        
+
+        
     }
 }
+
+function restoreAnimation() {
+        MyAvatar.restoreAnimation();
+}
+
+Controller.keyPressEvent.connect(restoreAnimation)
+// Note peek() so as to not interfere with other mappings.
+eventMapping.from(Controller.Standard.LeftPrimaryThumb).peek().to(restoreAnimation);
+eventMapping.from(Controller.Standard.RightPrimaryThumb).peek().to(restoreAnimation);
+eventMapping.from(Controller.Standard.LeftSecondaryThumb).peek().to(restoreAnimation);
+eventMapping.from(Controller.Standard.RightSecondaryThumb).peek().to(restoreAnimation);
+eventMapping.from(Controller.Standard.LT).peek().to(restoreAnimation);
+eventMapping.from(Controller.Standard.LB).peek().to(restoreAnimation);
+eventMapping.from(Controller.Standard.LS).peek().to(restoreAnimation);
+eventMapping.from(Controller.Standard.LeftGrip).peek().to(restoreAnimation);
+eventMapping.from(Controller.Standard.RT).peek().to(restoreAnimation);
+eventMapping.from(Controller.Standard.RB).peek().to(restoreAnimation);
+eventMapping.from(Controller.Standard.RS).peek().to(restoreAnimation);
+eventMapping.from(Controller.Standard.RightGrip).peek().to(restoreAnimation);
+eventMapping.from(Controller.Standard.Back).peek().to(restoreAnimation);
+eventMapping.from(Controller.Standard.Start).peek().to(restoreAnimation);
+Controller.enableMapping(eventMappingName);
 
 button.clicked.connect(onClicked);
 tablet.screenChanged.connect(onScreenChanged);
