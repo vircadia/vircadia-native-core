21a9525a737e3db268cabdd82c0c60504a54444f
Simon Walton <simon@highfidelity.io>
2018-04-24 17:52:12 -0700

    Version of HMACAuth with exposed lock
    Client is responsible for locking between adding and
    retrieving. Fixes a likely race.

--- libraries/networking/src/NLPacket.cpp
+++ libraries/networking/src/NLPacket.cpp
@@ -157,6 +157,7 @@ QByteArray NLPacket::hashForPacketAndHMAC(const udt::Packet& packet, HMACAuth& h
         + NUM_BYTES_LOCALID + NUM_BYTES_MD5_HASH;
     
     // add the packet payload and the connection UUID
+    QMutexLocker hashLock(&hash.getLock());
     hash.addData(packet.getData() + offset, packet.getDataSize() - offset);
     auto hashResult { hash.result() };
     return QByteArray((const char*) hashResult.data(), (int) hashResult.size());
