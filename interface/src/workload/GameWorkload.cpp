ba29ebaeb0640e787bfe147262b9dff3fd7fe9bf
samcake <samuel.gateau@gmail.com>
2018-04-26 13:08:26 -0700

    Moving the view controller up into workload and exposing timings

--- interface/src/workload/GameWorkload.cpp
+++ interface/src/workload/GameWorkload.cpp
@@ -15,77 +15,6 @@
 
 #include "PhysicsBoundary.h"
 
-ControlViews::ControlViews() {
-    regionBackFronts[0] = glm::vec2(1.0f, 3.0f);
-    regionBackFronts[1] = glm::vec2(1.0f, 5.0f);
-    regionBackFronts[2] = glm::vec2(1.0f, 10.0f);
-    regionRegulators[0] = Regulator(std::chrono::milliseconds(2), regionBackFronts[0], 5.0f * regionBackFronts[0], glm::vec2(0.4f, 0.2f),  0.5f * glm::vec2(0.3f, 0.2f));
-    regionRegulators[1] = Regulator(std::chrono::milliseconds(2), regionBackFronts[1], 8.0f * regionBackFronts[1], glm::vec2(0.4f, 0.2f), 0.5f * glm::vec2(0.3f, 0.2f));
-    regionRegulators[2] = Regulator(std::chrono::milliseconds(2), regionBackFronts[2], 10.0f * regionBackFronts[2], glm::vec2(0.4f, 0.2f), 0.5f * glm::vec2(0.3f, 0.2f));
-}
-
-void ControlViews::configure(const Config& config) {
-    _data = config.data;
-}
-
-void ControlViews::run(const workload::WorkloadContextPointer& runContext, const Input& inputs, Output& outputs) {
-    const auto& inViews = inputs.get0();
-    const auto& inTimings = inputs.get1();
-    auto& outViews = outputs;
-    outViews.clear();
-    outViews = inViews;
-
-    if (_data.regulateViewRanges && inTimings.size()) {
-        regulateViews(outViews, inTimings);
-
-        auto config = std::static_pointer_cast<Config>(runContext->jobConfig);
-        config->dataExport = _dataExport;
-        config->emitDirty();
-    }
-
-
-}
-
-glm::vec2 Regulator::run(const Timing_ns& regulationDuration, const Timing_ns& measured, const glm::vec2& current) {
-    glm::vec2 next = current;
-
-    // Regulate next value based on current moving toward the goal budget
-    float error_ms = std::chrono::duration<float, std::milli>(_budget - measured).count();
-    float coef = error_ms / std::chrono::duration<float, std::milli>(regulationDuration).count();
-    next += coef * (error_ms < 0.0f ? _speedDown : _speedUp);
-
-    // Clamp to min max
-    next = glm::clamp(next, _minRange, _maxRange);
-        
-    return next;
-}
-
-void ControlViews::regulateViews(workload::Views& outViews, const workload::Timings& timings) {
-
-    for (auto& outView : outViews) {
-        for (int r = 0; r < workload::Region::NUM_VIEW_REGIONS; r++) {
-            outView.regionBackFronts[r] = regionBackFronts[r];
-        }
-    }
-
-    auto loopDuration = std::chrono::nanoseconds{ std::chrono::milliseconds(16) };
-    _dataExport.ranges[workload::Region::R1] = regionBackFronts[workload::Region::R1] = regionRegulators[workload::Region::R1].run(loopDuration, timings[0], regionBackFronts[workload::Region::R1]);
-    _dataExport.ranges[workload::Region::R2] = regionBackFronts[workload::Region::R2] = regionRegulators[workload::Region::R2].run(loopDuration, timings[0], regionBackFronts[workload::Region::R2]);
-    _dataExport.ranges[workload::Region::R3] = regionBackFronts[workload::Region::R3] = regionRegulators[workload::Region::R3].run(loopDuration, timings[1], regionBackFronts[workload::Region::R3]);
-
-    _dataExport.timings[workload::Region::R1] = std::chrono::duration<float, std::milli>(timings[0]).count();
-    _dataExport.timings[workload::Region::R2] = _dataExport.timings[workload::Region::R1];
-    _dataExport.timings[workload::Region::R3] = std::chrono::duration<float, std::milli>(timings[1]).count();
-
-    for (auto& outView : outViews) {
-        outView.regionBackFronts[workload::Region::R1] = regionBackFronts[workload::Region::R1];
-        outView.regionBackFronts[workload::Region::R2] = regionBackFronts[workload::Region::R2];
-        outView.regionBackFronts[workload::Region::R3] = regionBackFronts[workload::Region::R3];
-
-        workload::View::updateRegionsFromBackFronts(outView);
-    }
-}
-
 class WorkloadEngineBuilder {
 public:
 public:
@@ -99,8 +28,8 @@ public:
 
         const auto usedViews = model.addJob<workload::SetupViews>("setupViews", inViews);
 
-        const auto controlViewsIn = ControlViews::Input(usedViews, inTimings).asVarying();
-        const auto fixedViews = model.addJob<ControlViews>("controlViews", controlViewsIn);
+        const auto controlViewsIn = workload::ControlViews::Input(usedViews, inTimings).asVarying();
+        const auto fixedViews = model.addJob<workload::ControlViews>("controlViews", controlViewsIn);
 
         const auto regionTrackerOut = model.addJob<workload::SpaceClassifierTask>("spaceClassifier", fixedViews);
 
