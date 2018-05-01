d5f809c3c08730a3016c772a4b710fd8e7b10d85
Zach Fox <fox@highfidelity.io>
2018-05-01 09:54:23 -0700

    Actually fix

--- interface/src/commerce/Wallet.cpp
+++ interface/src/commerce/Wallet.cpp
@@ -615,11 +615,14 @@ void Wallet::updateImageProvider() {
     securityImageProvider->setSecurityImage(_securityImage);
 
     // inform tablet security image provider
-    auto tablet = DependencyManager::get<TabletScriptingInterface>()->getTablet("com.highfidelity.interface.tablet.system");
+    TabletProxy* tablet = DependencyManager::get<TabletScriptingInterface>()->getTablet("com.highfidelity.interface.tablet.system");
     if (tablet) {
-        QQmlEngine* tabletEngine = tablet->getTabletSurface()->getSurfaceContext()->engine();
-        securityImageProvider = reinterpret_cast<SecurityImageProvider*>(tabletEngine->imageProvider(SecurityImageProvider::PROVIDER_NAME));
-        securityImageProvider->setSecurityImage(_securityImage);
+        OffscreenQmlSurface* tabletSurface = tablet->getTabletSurface();
+        if (tabletSurface) {
+            QQmlEngine* tabletEngine = tabletSurface->getSurfaceContext()->engine();
+            securityImageProvider = reinterpret_cast<SecurityImageProvider*>(tabletEngine->imageProvider(SecurityImageProvider::PROVIDER_NAME));
+            securityImageProvider->setSecurityImage(_securityImage);
+        }
     }
 }
 
