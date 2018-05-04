ad60a791803c41ddfac08aef31d7f0ed3c144d46
Clement <clement.brisset@gmail.com>
2018-05-04 14:43:28 -0700

    Fix for android builds

--- interface/src/Application.cpp
+++ interface/src/Application.cpp
@@ -5226,9 +5226,8 @@ void Application::updateSecondaryCameraViewFrustum() {
     auto renderConfig = _renderEngine->getConfiguration();
     assert(renderConfig);
     auto camera = dynamic_cast<SecondaryCameraJobConfig*>(renderConfig->getConfig("SecondaryCamera"));
-    assert(camera);
 
-    if (!camera->isEnabled()) {
+    if (!camera || !camera->isEnabled()) {
         return;
     }
 
