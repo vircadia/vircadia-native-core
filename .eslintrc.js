20fb9034f8bb1727d7df603cd14d812af4c6c5fd
Thijs Wenker <me@thoys.nl>
2018-05-14 20:11:55 +0200

    no magic numbers,  except -1, 0, 1

--- .eslintrc.js
+++ .eslintrc.js
@@ -79,7 +79,7 @@ module.exports = {
         "new-cap": ["error"],
         "no-console": ["off"],
         "no-floating-decimal": ["error"],
-        // "no-magic-numbers": ["error", {"ignore": [0, 1], "ignoreArrayIndexes": true}],
+        "no-magic-numbers": ["error", {"ignore": [-1, 0, 1], "ignoreArrayIndexes": true}],
         "no-multi-spaces": ["error"],
         "no-multiple-empty-lines": ["error"],
         "no-unused-vars": ["error", {"args": "none", "vars": "local"}],
