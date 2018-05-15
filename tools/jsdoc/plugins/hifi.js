1ca6bd2f0bdc7ae704bc8ca50fa7f81379cf9fb7
David Rowe <david@ctrlaltstudio.com>
2018-05-15 17:37:45 +1200

    location getter/setter JSDoc

--- tools/jsdoc/plugins/hifi.js
+++ tools/jsdoc/plugins/hifi.js
@@ -114,8 +114,8 @@ exports.handlers = {
 
             // Append an Available In: table at the end of the namespace description.
             if (rows.length > 0) {
-                var table = "<br><br><table><tr><th>Available in:</th><td>" + rows.join("</td><td>") + "</td></tr></table>";
-                e.doclet.description = (e.doclet.description ? e.doclet.description : "") + table;
+                var table = "<table><tr><th>Available in:</th><td>" + rows.join("</td><td>") + "</td></tr></table><br>";
+                e.doclet.description = table + (e.doclet.description ? e.doclet.description : "");
             }
         }
     }
