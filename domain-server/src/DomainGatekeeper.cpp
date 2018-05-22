666bdb03b72c569fee0f7d0039ded1b6babd1785
Stephen Birarda <commit@birarda.com>
2018-05-22 13:47:31 -0700

    never re-use node ID during domain connection

--- domain-server/src/DomainGatekeeper.cpp
+++ domain-server/src/DomainGatekeeper.cpp
@@ -479,7 +479,7 @@ SharedNodePointer DomainGatekeeper::processAgentConnectRequest(const NodeConnect
         limitedNodeList->killNodeWithUUID(existingNodeID);
     }
 
-    // add the connecting node (or re-use the matched one from eachNodeBreakable above)
+    // add the connecting node
     SharedNodePointer newNode = addVerifiedNodeFromConnectRequest(nodeConnection);
 
     // set the edit rights for this user
@@ -508,25 +508,17 @@ SharedNodePointer DomainGatekeeper::processAgentConnectRequest(const NodeConnect
     return newNode;
 }
 
-SharedNodePointer DomainGatekeeper::addVerifiedNodeFromConnectRequest(const NodeConnectionData& nodeConnection,
-                                                                      QUuid nodeID) {
+SharedNodePointer DomainGatekeeper::addVerifiedNodeFromConnectRequest(const NodeConnectionData& nodeConnection) {
     HifiSockAddr discoveredSocket = nodeConnection.senderSockAddr;
     SharedNetworkPeer connectedPeer = _icePeers.value(nodeConnection.connectUUID);
 
-    if (connectedPeer) {
-        //  this user negotiated a connection with us via ICE, so re-use their ICE client ID
-        nodeID = nodeConnection.connectUUID;
-
-        if (connectedPeer->getActiveSocket()) {
-            // set their discovered socket to whatever the activated socket on the network peer object was
-            discoveredSocket = *connectedPeer->getActiveSocket();
-        }
-    } else {
-        // we got a connectUUID we didn't recognize, either use the hinted node ID or randomly generate a new one
-        if (nodeID.isNull()) {
-            nodeID = QUuid::createUuid();
-        }
+    if (connectedPeer && connectedPeer->getActiveSocket()) {
+        // set their discovered socket to whatever the activated socket on the network peer object was
+        discoveredSocket = *connectedPeer->getActiveSocket();
     }
+
+    // create a new node ID for the verified connecting node
+    auto nodeID = QUuid::createUuid();
 
     auto limitedNodeList = DependencyManager::get<LimitedNodeList>();
 
