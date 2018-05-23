85beca6b6705cf954f0cd3e03870a8b13cfa39b6
Andrew Meadows <andrew@highfidelity.io>
2018-05-23 16:00:27 -0700

    tweak to dynamic workload regions regulation

--- interface/src/workload/GameWorkload.cpp
+++ interface/src/workload/GameWorkload.cpp
@@ -15,6 +15,109 @@
 
 #include "PhysicsBoundary.h"
 
+const glm::vec2 DEFAULT_R1_BACK_FRONT = { 50.0f, 100.0f };
+const glm::vec2 DEFAULT_R2_BACK_FRONT = { 75.0f, 150.0f };
+const glm::vec2 DEFAULT_R3_BACK_FRONT = { 100.0f, 250.0f };
+
+ControlViews::ControlViews() {
+    regionBackFronts[0] = DEFAULT_R1_BACK_FRONT;
+    regionBackFronts[1] = DEFAULT_R2_BACK_FRONT;
+    regionBackFronts[2] = DEFAULT_R3_BACK_FRONT;
+
+    const int32_t TIME_BUDGET_MSEC = 2;
+    const float MIN_BACK_FRONT_SCALE_FACTOR = 0.05f;
+    const float MAX_BACK_FRONT_SCALE_FACTOR = 2.0f;
+    const float MIN_NUM_STEPS_DOWN = 250.0f;
+    const float MIN_NUM_STEPS_UP = 375.0f;
+
+    for (int32_t i = 0; i < workload::Region::NUM_VIEW_REGIONS; ++i) {
+        glm::vec2 minBackFront = MIN_BACK_FRONT_SCALE_FACTOR * regionBackFronts[i];
+        glm::vec2 maxBackFront = MAX_BACK_FRONT_SCALE_FACTOR * regionBackFronts[i];
+        glm::vec2 stepDown = (maxBackFront - minBackFront) / MIN_NUM_STEPS_DOWN;
+        glm::vec2 stepUp = (maxBackFront - minBackFront) / MIN_NUM_STEPS_UP;
+        regionRegulators[i] = Regulator(std::chrono::milliseconds(TIME_BUDGET_MSEC), minBackFront, maxBackFront, stepDown, stepUp);
+    }
+}
+
+void ControlViews::configure(const Config& config) {
+    _data = config.data;
+}
+
+void ControlViews::run(const workload::WorkloadContextPointer& runContext, const Input& inputs, Output& outputs) {
+    const auto& inViews = inputs.get0();
+    const auto& inTimings = inputs.get1();
+    auto& outViews = outputs;
+    outViews.clear();
+    outViews = inViews;
+
+    if (_data.regulateViewRanges && inTimings.size()) {
+        regulateViews(outViews, inTimings);
+
+        auto config = std::static_pointer_cast<Config>(runContext->jobConfig);
+        config->dataExport = _dataExport;
+        config->emitDirty();
+    }
+}
+
+glm::vec2 Regulator::run(const Timing_ns& regulationDuration, const Timing_ns& measured, const glm::vec2& current) {
+    // Regulate next value based on current moving toward the goal budget
+    float error_ms = std::chrono::duration<float, std::milli>(_budget - measured).count();
+    float coef = error_ms / std::chrono::duration<float, std::milli>(regulationDuration).count();
+    return current + coef * (error_ms < 0.0f ? _stepDown : _stepUp);
+}
+
+glm::vec2 Regulator::clamp(const glm::vec2& backFront) const {
+    return glm::clamp(backFront, _minRange, _maxRange);
+}
+
+void ControlViews::regulateViews(workload::Views& outViews, const workload::Timings& timings) {
+
+    for (auto& outView : outViews) {
+        for (int32_t r = 0; r < workload::Region::NUM_VIEW_REGIONS; r++) {
+            outView.regionBackFronts[r] = regionBackFronts[r];
+        }
+    }
+
+    auto loopDuration = std::chrono::nanoseconds{ std::chrono::milliseconds(16) };
+    regionBackFronts[workload::Region::R1] = regionRegulators[workload::Region::R1].run(loopDuration, timings[0], regionBackFronts[workload::Region::R1]);
+    regionBackFronts[workload::Region::R2] = regionRegulators[workload::Region::R2].run(loopDuration, timings[0], regionBackFronts[workload::Region::R2]);
+    regionBackFronts[workload::Region::R3] = regionRegulators[workload::Region::R3].run(loopDuration, timings[1], regionBackFronts[workload::Region::R3]);
+
+    enforceRegionContainment();
+
+    _dataExport.ranges[workload::Region::R1] = regionBackFronts[workload::Region::R1];
+    _dataExport.ranges[workload::Region::R2] = regionBackFronts[workload::Region::R2];
+    _dataExport.ranges[workload::Region::R3] = regionBackFronts[workload::Region::R3];
+
+    _dataExport.timings[workload::Region::R1] = std::chrono::duration<float, std::milli>(timings[0]).count();
+    _dataExport.timings[workload::Region::R2] = _dataExport.timings[workload::Region::R1];
+    _dataExport.timings[workload::Region::R3] = std::chrono::duration<float, std::milli>(timings[1]).count();
+
+    for (auto& outView : outViews) {
+        outView.regionBackFronts[workload::Region::R1] = regionBackFronts[workload::Region::R1];
+        outView.regionBackFronts[workload::Region::R2] = regionBackFronts[workload::Region::R2];
+        outView.regionBackFronts[workload::Region::R3] = regionBackFronts[workload::Region::R3];
+
+        workload::View::updateRegionsFromBackFronts(outView);
+    }
+}
+
+void ControlViews::enforceRegionContainment() {
+    // inner regions should never overreach outer
+    // and each region should never exceed its min/max limits
+    const glm::vec2 MIN_REGION_GAP = { 1.0f, 2.0f };
+    // enforce outside --> in
+    for (int32_t i = workload::Region::NUM_VIEW_REGIONS - 2; i >= 0; --i) {
+        int32_t j = i + 1;
+        regionBackFronts[i] = regionRegulators[i].clamp(glm::min(regionBackFronts[i], regionBackFronts[j] - MIN_REGION_GAP));
+    }
+    // enforce inside --> out
+    for (int32_t i = 1; i < workload::Region::NUM_VIEW_REGIONS; ++i) {
+        int32_t j = i - 1;
+        regionBackFronts[i] = regionRegulators[i].clamp(glm::max(regionBackFronts[i], regionBackFronts[j] + MIN_REGION_GAP));
+    }
+}
+
 class WorkloadEngineBuilder {
 public:
 public:
