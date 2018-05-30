543a721a11f238e6b857ee45d44a64b545f18924
Dante Ruiz <danteruiz102@gmail.com>
2018-05-29 17:55:42 -0700

    fix headers

--- interface/src/AboutUtil.cpp
+++ interface/src/AboutUtil.cpp
@@ -8,16 +8,18 @@
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
+
+#include "AboutUtil.h"
 #include <QDate>
 #include <QLocale>
 
-#include "AboutUtil.h"
-#include "BuildInfo.h"
 #include <ui/TabletScriptingInterface.h>
+#include <OffscreenQmlDialog.h>
+
+#include "BuildInfo.h"
 #include "DependencyManager.h"
 #include "scripting/HMDScriptingInterface.h"
 #include "Application.h"
-#include <OffscreenQmlDialog.h>
 
 AboutUtil::AboutUtil(QObject *parent) : QObject(parent) {
     QLocale locale;
@@ -25,24 +27,20 @@ AboutUtil::AboutUtil(QObject *parent) : QObject(parent) {
             toString(locale.dateFormat(QLocale::ShortFormat));
 }
 
-AboutUtil *AboutUtil::getInstance()
-{
+AboutUtil *AboutUtil::getInstance() {
     static AboutUtil instance;
     return &instance;
 }
 
-QString AboutUtil::getBuildDate() const
-{
+QString AboutUtil::getBuildDate() const {
     return _dateConverted;
 }
 
-QString AboutUtil::getBuildVersion() const
-{
+QString AboutUtil::getBuildVersion() const {
     return BuildInfo::VERSION;
 }
 
-QString AboutUtil::getQtVersion() const
-{
+QString AboutUtil::getQtVersion() const {
     return qVersion();
 }
 
