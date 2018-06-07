7d8e02e714d44cf5d0749935f2b4adfdf53fdba8
Zach Fox <fox@highfidelity.io>
2018-06-07 14:01:52 -0700

    Fix MS15663/15664: Don't set Wallet Status to 'ready' too early

--- interface/src/commerce/Wallet.cpp
+++ interface/src/commerce/Wallet.cpp
@@ -531,7 +531,6 @@ bool Wallet::walletIsAuthenticatedWithPassphrase() {
 
             // be sure to add the public key so we don't do this over and over
             _publicKeys.push_back(publicKey.toBase64());
-            DependencyManager::get<WalletScriptingInterface>()->setWalletStatus((uint)WalletStatus::WALLET_STATUS_READY);
             return true;
         }
     }
