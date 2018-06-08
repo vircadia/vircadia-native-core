d4a19e7bc9f340bf3d88c28f0fc8146ef90230dc
Zach Fox <fox@highfidelity.io>
2018-06-08 10:52:14 -0700

    Fix MS15721: Delete app.json if installed script isn't running

--- interface/src/commerce/QmlCommerce.cpp
+++ interface/src/commerce/QmlCommerce.cpp
@@ -227,10 +227,13 @@ QString QmlCommerce::getInstalledApps() {
             QString scriptURL = appFileJsonObject["scriptURL"].toString();
 
             // If the script .app.json is on the user's local disk but the associated script isn't running
-            // for some reason, start that script again.
+            // for some reason (i.e. the user stopped it from Running Scripts),
+            // delete the .app.json from the user's local disk.
             if (!runningScripts.contains(scriptURL)) {
-                if ((DependencyManager::get<ScriptEngines>()->loadScript(scriptURL.trimmed())).isNull()) {
-                    qCDebug(commerce) << "Couldn't start script while checking installed apps.";
+                if (!appFile.remove()) {
+                    qCWarning(commerce)
+                        << "Couldn't delete local .app.json file (app's script isn't running). App filename is:"
+                        << appFileName;
                 }
             }
         } else {
