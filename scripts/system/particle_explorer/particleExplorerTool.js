2cdda231698a3e31eeeeaed67c8822dd467856af
David Back <davidback@highfidelity.io>
2018-06-27 13:05:39 -0700

    more robustness for optional properties

--- scripts/system/particle_explorer/particleExplorerTool.js
+++ scripts/system/particle_explorer/particleExplorerTool.js
@@ -9,7 +9,8 @@
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
-/* global window, alert, ParticleExplorerTool, EventBridge, dat, listenForSettingsUpdates,createVec3Folder,createQuatFolder,writeVec3ToInterface,writeDataToInterface*/
+/* global window, alert, ParticleExplorerTool, EventBridge, dat, listenForSettingsUpdates, createVec3Folder, 
+   createQuatFolder, writeVec3ToInterface, writeDataToInterface */
 
 
 var PARTICLE_EXPLORER_HTML_URL = Script.resolvePath('particleExplorer.html');
@@ -17,7 +18,7 @@ var PARTICLE_EXPLORER_HTML_URL = Script.resolvePath('particleExplorer.html');
 ParticleExplorerTool = function() {
     var that = {};
     that.activeParticleEntity = 0;
-    that.activeParticleProperties = {};
+    that.updatedActiveParticleProperties = {};
 
     that.createWebView = function() {
         that.webView = Tablet.getTablet("com.highfidelity.interface.tablet.system");
@@ -30,7 +31,7 @@ ParticleExplorerTool = function() {
             return;
         }
         that.activeParticleEntity = 0;
-        that.activeParticleProperties = {};
+        that.updatedActiveParticleProperties = {};
 
         var messageData = {
             messageType: "particle_close"
@@ -38,46 +39,86 @@ ParticleExplorerTool = function() {
         that.webView.emitScriptEvent(JSON.stringify(messageData));
     };
 
-    function sendActiveParticleProperties() {
+    function sendParticleProperties(properties) {
         that.webView.emitScriptEvent(JSON.stringify({
             messageType: "particle_settings",
-            currentProperties: that.activeParticleProperties
+            currentProperties: properties
         }));
+    }
+
+    function sendActiveParticleProperties() {
+        var properties = Entities.getEntityProperties(that.activeParticleEntity);
+        if (properties.emitOrientation) {
+            properties.emitOrientation = Quat.safeEulerAngles(properties.emitOrientation);
+        }
+        // Update uninitialized variables
+        if (isNaN(properties.alphaStart)) {
+            properties.alphaStart = properties.alpha;
+        }
+        if (isNaN(properties.alphaFinish)) {
+            properties.alphaFinish = properties.alpha;
+        }
+        if (isNaN(properties.radiusStart)) {
+            properties.radiusStart = properties.particleRadius;
+        }
+        if (isNaN(properties.radiusFinish)) {
+            properties.radiusFinish = properties.particleRadius;
+        }
+        if (isNaN(properties.colorStart) || isNaN(properties.colorStart.red)) {
+            properties.colorStart = properties.color;
+        }
+        if (isNaN(properties.colorFinish) || isNaN(properties.colorFinish.red)) {
+            properties.colorFinish = properties.color;
+        }
+        sendParticleProperties(properties);
+    }
+    
+    function sendUpdatedActiveParticleProperties() {
+        sendParticleProperties(that.updatedActiveParticleProperties);
+        that.updatedActiveParticleProperties = {};
     }
 
     that.webEventReceived = function(message) {
         var data = JSON.parse(message);
         if (data.messageType === "settings_update") {
-            if (data.updatedSettings.emitOrientation) {
-                data.updatedSettings.emitOrientation = Quat.fromVec3Degrees(data.updatedSettings.emitOrientation);
-            }
-            Entities.editEntity(that.activeParticleEntity, data.updatedSettings);
-
-            for (var key in data.updatedSettings) {
-                if (that.activeParticleProperties.hasOwnProperty(key)) {
-                    that.activeParticleProperties[key] = data.updatedSettings[key];
-                }
-            }
+            var updatedSettings = data.updatedSettings;
 
             var optionalProps = ["alphaStart", "alphaFinish", "radiusStart", "radiusFinish", "colorStart", "colorFinish"];
             var fallbackProps = ["alpha", "particleRadius", "color"];
-            var entityProps = Entities.getEntityProperties(that.activeParticleProperties, optionalProps);
+            for (var i = 0; i < optionalProps.length; i++) {
+                var fallbackProp = fallbackProps[Math.floor(i / 2)];
+                var optionalValue = updatedSettings[optionalProps[i]];
+                var fallbackValue = updatedSettings[fallbackProp];
+                if (optionalValue && fallbackValue) {
+                    delete updatedSettings[optionalProps[i]];
+                }
+            }
+            
+            if (updatedSettings.emitOrientation) {
+                updatedSettings.emitOrientation = Quat.fromVec3Degrees(updatedSettings.emitOrientation);
+            }
+            
+            Entities.editEntity(that.activeParticleEntity, updatedSettings);
+                    
+            var entityProps = Entities.getEntityProperties(that.activeParticleEntity, optionalProps);
+            
             var needsUpdate = false;
             for (var i = 0; i < optionalProps.length; i++) {
-                var fallback = fallbackProps[Math.floor(i / 2)];
-                if (data.updatedSettings[fallback]) {
-                    var prop = optionalProps[i];
-                    if (!that.activeParticleProperties[prop] || (fallback === "color" && !that.activeParticleProperties[prop].red)) {
-                        that.activeParticleProperties[prop] = entityProps[fallback];
+                var fallbackProp = fallbackProps[Math.floor(i / 2)];
+                var fallbackValue = updatedSettings[fallbackProp];
+                if (fallbackValue) {
+                    var optionalProp = optionalProps[i];
+                    if (isNaN(entityProps[optionalProp]) || (fallbackProp === "color" && isNaN(entityProps[optionalProp].red))) {
+                        that.updatedActiveParticleProperties[optionalProp] = fallbackValue;
                         needsUpdate = true;
                     }
                 }
             }
 
             if (needsUpdate) {
-                sendActiveParticleProperties();
+                sendUpdatedActiveParticleProperties();
             }
-
+            
         } else if (data.messageType === "page_loaded") {
             sendActiveParticleProperties();
         }
@@ -85,12 +126,8 @@ ParticleExplorerTool = function() {
 
     that.setActiveParticleEntity = function(id) {
         that.activeParticleEntity = id;
-    };
-
-    that.setActiveParticleProperties = function(properties) {
-        that.activeParticleProperties = properties;
         sendActiveParticleProperties();
     };
-
+    
     return that;
 };
