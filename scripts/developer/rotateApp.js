301658c71a7cd903bbef0d28f4b4e7a49f92f359
amantley <amantley@googlemail.com>
2018-06-29 15:50:08 -0700

    getting the rotate app nearly ready to use

--- scripts/developer/rotateApp.js
+++ scripts/developer/rotateApp.js
@@ -5,7 +5,8 @@ Script.registerValue("ROTATEAPP", true);
 
 var TABLET_BUTTON_NAME = "ROTATE";
 var CHANGE_OF_BASIS_ROTATION = { x: 0, y: 1, z: 0, w: 0 };
-var HEAD_TURN_THRESHOLD = 25.0;
+var HEAD_TURN_THRESHOLD = .5333;
+var HEAD_TURN_FILTER_LENGTH = 4.0;
 var LOADING_DELAY = 500;
 var AVERAGING_RATE = 0.03;
 
@@ -30,7 +31,7 @@ var ikTypes = {
     Unknown: 5
 };
 
-
+/*
 var ANIM_VARS = [
     //"headType",
     "spine2Type",
@@ -57,7 +58,7 @@ var handlerId = MyAvatar.addAnimationStateHandler(function (props) {
     
     return result;
 }, ANIM_VARS);
-
+*/
 // define state readings constructor
 function StateReading(headPose, rhandPose, lhandPose, diffFromAverageEulers) {
     this.headPose = headPose;
@@ -95,8 +96,24 @@ function AppProperty(name, type, eventType, signalType, setFunction, initValue, 
     this.convertToSlider = convertToSlider;
 }
 
-var HTML_URL = Script.resolvePath("file:///c:/dev/high fidelity/hifi/scripts/developer/stepAppExtra.html");
+var HTML_URL = Script.resolvePath("file:///c:/dev/hifi_fork/hifi/scripts/developer/rotateApp.html");
 var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+
+// define the sliders
+var filterLengthProperty = new AppProperty("#filterLength-slider", "slider", "onFilterLengthSlider", "filterSignal",
+    setAnteriorDistance, DEFAULT_ANTERIOR, function (num) {
+        return convertToMeters(num);
+    }, function (num) {
+        return convertToCentimeters(num);
+    },true);
+var angleThresholdProperty = new AppProperty("#angleThreshold-slider", "slider", "onAngleThresholdSlider", "angleSignal",
+    setPosteriorDistance, DEFAULT_POSTERIOR, function (num) {
+        return convertToMeters(num);
+    }, function (num) {
+        return convertToCentimeters(num);
+    }, true);
+
+
 
 function manageClick() {
     if (activated) {
@@ -117,7 +134,7 @@ function onKeyPress(event) {
         // when the sensors are reset, then reset the mode.
     }
 }
-/*
+
 function onWebEventReceived(msg) {
     var message = JSON.parse(msg);
     print(" we have a message from html dialog " + message.type);
@@ -141,16 +158,12 @@ function initAppForm() {
     propArray.forEach(function (prop) {
         print(prop.name);
         tablet.emitScriptEvent(JSON.stringify({
-            "type": "trigger",
-            "id": prop.signalType,
-            "data": { "value": "green" }
-        }));
-        tablet.emitScriptEvent(JSON.stringify({
             "type": "slider",
             "id": prop.name,
             "data": { "value": prop.convertToSlider(prop.value) }
         }));
     });
+    /*
     checkBoxArray.forEach(function(cbox) {
         tablet.emitScriptEvent(JSON.stringify({
             "type": "checkboxtick",
@@ -158,6 +171,7 @@ function initAppForm() {
             "data": { value: cbox.data.value }
         }));
     });
+    */
  
 }
 
@@ -184,83 +198,20 @@ function onScreenChanged(type, url) {
 */
 function update(dt) {
 
-    //  Update head information
-    currentStateReadings.headPose = Controller.getPoseValue(Controller.Standard.Head);
-    currentStateReadings.rhandPose = Controller.getPoseValue(Controller.Standard.RightHand);
-    currentStateReadings.lhandPose = Controller.getPoseValue(Controller.Standard.LeftHand);
-
-    // get the position of the hips and set them for the anim vars callback.
-    var spine2PositionAvatarSpace = MyAvatar.getAbsoluteJointTranslationInObjectFrame(MyAvatar.getJointIndex("Spine2"));
-    var spine2RotationAvatarSpace = MyAvatar.getAbsoluteJointRotationInObjectFrame(MyAvatar.getJointIndex("Spine2"));
-    var hipsPositionAvatarSpace = MyAvatar.getAbsoluteJointTranslationInObjectFrame(MyAvatar.getJointIndex("Hips"));
-    var hipsRotationAvatarSpace = MyAvatar.getAbsoluteJointRotationInObjectFrame(MyAvatar.getJointIndex("Hips"));
-    hipsPositionRigSpace = Vec3.multiplyQbyV(Quat.inverse(CHANGE_OF_BASIS_ROTATION), hipsPositionAvatarSpace);
-    spine2PositionRigSpace = Vec3.multiplyQbyV(Quat.inverse(CHANGE_OF_BASIS_ROTATION), spine2PositionAvatarSpace);
-    hipsRotationRigSpace = Quat.multiply(CHANGE_OF_BASIS_ROTATION, hipsRotationAvatarSpace);
-    spine2RotationRigSpace = Quat.multiply(CHANGE_OF_BASIS_ROTATION, spine2RotationAvatarSpace);
-    //print("hip position rig space" + hipsPositionAvatarSpace.x + " " + hipsPositionAvatarSpace.y + " " + hipsPositionAvatarSpace.z);
-    //print("hip rotation rig space x: " + hipsRotationRigSpace.x + " y: " + hipsRotationRigSpace.y + " z: " + hipsRotationRigSpace.z + " w: " + hipsRotationRigSpace.w);
-    // print("spine2 rotation rig space x: " + spine2RotationRigSpace.x + " y: " + spine2RotationRigSpace.y + " z: " + spine2RotationRigSpace.z + " w: " + spine2RotationRigSpace.w);
-    //print("spine2 position rig space" + spine2PositionRigSpace.x + " " + spine2PositionRigSpace.y + " " + spine2PositionRigSpace.z);
-
-    var headPoseRigSpace = Quat.multiply(CHANGE_OF_BASIS_ROTATION, currentStateReadings.headPose.rotation);
-    headPoseAverageOrientation = Quat.slerp(headPoseAverageOrientation, headPoseRigSpace, AVERAGING_RATE);
-    var headPoseAverageEulers = Quat.safeEulerAngles(headPoseAverageOrientation);
-
-    
-    if (((headPoseAverageEulers.y > HEAD_TURN_THRESHOLD) && (averageAzimuth > HEAD_TURN_THRESHOLD))
-        || ((headPoseAverageEulers.y < -HEAD_TURN_THRESHOLD) && (averageAzimuth < -HEAD_TURN_THRESHOLD))) {
-        print("azimuth " + averageAzimuth);
-        print("headposeAverageEulers " + headPoseAverageEulers.y);
-        //  Turn feet
-        print("rotate feet")
-        MyAvatar.triggerRotationRecenter();
-        headPoseAverageOrientation = { x: 0, y: 0, z: 0, w: 1 };
-    }
-
-    // and the hand to hip vector to determine when to change head rotation. 
-    // leftHandOrientation = Quat.multiply(MyAvatar.orientation, currentStateReadings.lhandPose.rotation);
-    // rightHandOrientation = Quat.multiply(MyAvatar.orientation, currentStateReadings.rhandPose.rotation);
-    var leftHandPositionRigSpace = Vec3.multiplyQbyV(Quat.inverse(CHANGE_OF_BASIS_ROTATION), currentStateReadings.lhandPose.translation);
-    var rightHandPositionRigSpace = Vec3.multiplyQbyV(Quat.inverse(CHANGE_OF_BASIS_ROTATION), currentStateReadings.rhandPose.translation);
-
-    //  Update angle from hips to hand, to be used for turning 
-    var hipToLeftHandAngle = Vec3.orientedAngle({ x: 0, y: 0, z: 1 }, { x: leftHandPositionRigSpace.x, y: 0, z: leftHandPositionRigSpace.z }, { x: 0, y: 1, z: 0 });
-    var hipToRightHandAngle = Vec3.orientedAngle({ x: 0, y: 0, z: 1 }, { x: rightHandPositionRigSpace.x, y: 0, z: rightHandPositionRigSpace.z }, { x: 0, y: 1, z: 0 });
-    var hipToLeftHand = Quat.lookAtSimple({ x: 0, y: 0, z: 0 }, { x: leftHandPositionRigSpace.x, y: 0, z: leftHandPositionRigSpace.z });
-    var hipToRightHand = Quat.lookAtSimple({ x: 0, y: 0, z: 0 }, { x: rightHandPositionRigSpace.x, y: 0, z: rightHandPositionRigSpace.z });
-    if (currentStateReadings.lhandPose.valid) {
-        hipToLeftHandAverage = hipToLeftHandAngle;// hipToLeftHandAverage * (1 - AVERAGING_RATE) + hipToLeftHandAngle * (AVERAGING_RATE);
-    }
-    if (currentStateReadings.rhandPose.valid) {
-        hipToRightHandAverage = hipToRightHandAngle;// hipToRightHandAverage * (1 - AVERAGING_RATE) + hipToRightHandAngle * (AVERAGING_RATE);
-    }
-    var leftRightMidpoint = (hipToLeftHandAverage + hipToRightHandAverage) / 2.0;
-    var FILTER_FACTOR = 0.01;
-    averageAzimuth = leftRightMidpoint; // * (FILTER_FACTOR) + averageAzimuth * (1 - FILTER_FACTOR);
-    spine2Rotation = Quat.angleAxis(averageAzimuth, { x: 0, y: 1, z: 0 });
-    // print("spine 2 orientation x: " + spine2Rotation.x + " y: " + spine2Rotation.y + " z: " + spine2Rotation.z + " w: " + spine2Rotation.w);
-    // print("average azimuth " + averageAzimuth);
-    // print("hip to left hand angle " + hipToLeftHandAngle);
-    // print("hip to right hand angle " + hipToRightHandAngle);
-    // print("left right midpoint " + leftRightMidpoint);
-    // print("left hand position " + leftHandPositionRigSpace.x + " " + leftHandPositionRigSpace.y + " " + leftHandPositionRigSpace.z);
-    // print("right hand position " + rightHandPositionRigSpace.x + " " + rightHandPositionRigSpace.y + " " + rightHandPositionRigSpace.z);
-    // print("hip to left hand average " + hipToLeftHandAverage.x + " " + hipToLeftHandAverage.y + " " + hipToLeftHandAverage.z + " " + hipToLeftHandAverage.w);
 
 }
 
 function shutdownTabletApp() {
     tablet.removeButton(tabletButton);
     if (activated) {
-        // tablet.webEventReceived.disconnect(onWebEventReceived);
+        tablet.webEventReceived.disconnect(onWebEventReceived);
         tablet.gotoHomeScreen();
     }
-    // tablet.screenChanged.disconnect(onScreenChanged);
+    tablet.screenChanged.disconnect(onScreenChanged);
 }
 
 tabletButton.clicked.connect(manageClick);
-// tablet.screenChanged.connect(onScreenChanged);
+tablet.screenChanged.connect(onScreenChanged);
 
 Script.update.connect(update);
 
@@ -269,7 +220,7 @@ Controller.keyPressEvent.connect(onKeyPress);
 Script.scriptEnding.connect(function () {
     if (handlerId) {
         print("removing animation state handler");
-        handlerId = MyAvatar.removeAnimationStateHandler(handlerId);
+        // handlerId = MyAvatar.removeAnimationStateHandler(handlerId);
     }
     MyAvatar.hmdLeanRecenterEnabled = true;
     Script.update.disconnect(update);
