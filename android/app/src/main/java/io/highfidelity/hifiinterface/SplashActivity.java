021905be9bebc70ee705a049f288e8ea2301a0c2
Cristian Luis Duarte <cristian.duarte.mb@gmail.com>
2018-07-19 21:27:10 -0300

    remove unneeded import

--- android/app/src/main/java/io/highfidelity/hifiinterface/SplashActivity.java
+++ android/app/src/main/java/io/highfidelity/hifiinterface/SplashActivity.java
@@ -1,11 +1,9 @@
 package io.highfidelity.hifiinterface;
 
 import android.app.Activity;
-import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.View;
 
 public class SplashActivity extends Activity {
