8642edd144242f061b493e843ed68d2805c5ddcf
Anthony Thibault <tony@highfidelity.io>
2018-09-13 18:18:23 -0700

    Added acceleration limit filter to controller system

---
+++ libraries/controllers/src/controllers/impl/filters/AccelerationLimiterFilter.h
@@ -0,0 +1,40 @@
+//
+//  Created by Anthony Thibault 2018/11/09
+//  Copyright 2018 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_Controllers_Filters_Acceleration_Limiter_h
+#define hifi_Controllers_Filters_Acceleration_Limiter_h
+
+#include "../Filter.h"
+
+namespace controller {
+
+    class AccelerationLimiterFilter : public Filter {
+        REGISTER_FILTER_CLASS(AccelerationLimiterFilter);
+
+    public:
+        AccelerationLimiterFilter() {}
+        AccelerationLimiterFilter(float rotationLimit, float translationLimit) :
+            _rotationLimit(rotationLimit),
+            _translationLimit(translationLimit) {}
+
+        float apply(float value) const override { return value; }
+        Pose apply(Pose value) const override;
+        bool parseParameters(const QJsonValue& parameters) override;
+
+    private:
+        float _rotationLimit { FLT_MAX };
+        float _translationLimit { FLT_MAX };
+
+        mutable glm::vec3 _prevPos[3];  // sensor space
+        mutable glm::quat _prevRot[3];  // sensor space
+        mutable bool _prevValid { false };
+    };
+
+}
+
+#endif
