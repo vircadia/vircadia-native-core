6fdae83f1b5865893dc6692b72ab5a4de5ab1c08
Howard Stearns <howard@highfidelity.io>
2018-09-26 14:59:53 -0700

    fix recent refactoring bug

--- scripts/modules/appUi.js
+++ scripts/modules/appUi.js
@@ -42,7 +42,7 @@ function AppUi(properties) {
     that.additionalAppScreens = [];
     that.checkIsOpen = function checkIsOpen(type, tabletUrl) { // Are we active? Value used to set isOpen.
         // Actual url may have prefix or suffix.
-        return (type === that.currentVisibleScreenType) &&
+        return
             that.currentVisibleUrl &&
             ((that.home.indexOf(that.currentVisibleUrl) > -1) ||
             (that.additionalAppScreens.indexOf(that.currentVisibleUrl) > -1));
