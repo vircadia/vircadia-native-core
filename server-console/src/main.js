6393f907b87afadaec07c42b3c7d00cfb19e526b
Roxanne Skelly <roxanne@highfidelity.io>
2018-09-28 12:33:38 -0700

    Add temporary code to test for interface paths

--- server-console/src/main.js
+++ server-console/src/main.js
@@ -885,6 +885,9 @@ function onContentLoaded() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
+    var paths = pathFinder.searchPaths("interface", binaryType, buildInfo.releaseType);
+    dialog.showMessageBox({type: 'question', buttons: ['Yes', 'No'], title: "Paths", message: paths.join(", ")}, function () {});
+    
 
     if (app.dock) {
         // hide the dock icon on OS X
@@ -902,4 +905,5 @@ app.on('ready', function() {
     updateTrayMenu(ProcessGroupStates.STOPPED);
 
     maybeInstallDefaultContentSet(onContentLoaded);
+
 });
