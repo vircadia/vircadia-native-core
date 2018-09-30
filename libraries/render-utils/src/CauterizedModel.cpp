e30497595ca2fec35841d6be293cd87faa827da7
Seth Alves <seth.alves@gmail.com>
2018-09-30 15:49:42 -0700

    static_cast can be used here rather than dynamic

--- libraries/render-utils/src/CauterizedModel.cpp
+++ libraries/render-utils/src/CauterizedModel.cpp
@@ -228,7 +228,7 @@ void CauterizedModel::updateRenderItems() {
 
                 transaction.updateItem<ModelMeshPartPayload>(itemID, [modelTransform, meshState, useDualQuaternionSkinning, cauterizedMeshState, invalidatePayloadShapeKey,
                 isWireframe, renderItemKeyGlobalFlags, enableCauterization](ModelMeshPartPayload& mmppData) {
-                    CauterizedMeshPartPayload& data = dynamic_cast<CauterizedMeshPartPayload&>(mmppData);
+                    CauterizedMeshPartPayload& data = static_cast<CauterizedMeshPartPayload&>(mmppData);
                     if (useDualQuaternionSkinning) {
                         data.updateClusterBuffer(meshState.clusterDualQuaternions,
                                                  cauterizedMeshState.clusterDualQuaternions);
