611881e0024a10638aadd8c78555f4ba2800a697
Gabriel Calero <gcalero1984@gmail.com>
2018-11-05 18:35:57 -0300

    Mute when entering a domain

--- android/app/src/main/cpp/native.cpp
+++ android/app/src/main/cpp/native.cpp
@@ -21,7 +21,6 @@
 
 #include <AddressManager.h>
 #include "AndroidHelper.h"
-#include "../../../../../libraries/audio-client/src/AudioClient.h"
 #include <udt/PacketHeaders.h>
 #include <SettingHandle.h>
 
@@ -211,15 +210,13 @@ JNIEXPORT void Java_io_highfidelity_hifiinterface_InterfaceActivity_nativeOnDest
 JNIEXPORT void Java_io_highfidelity_hifiinterface_InterfaceActivity_nativeGotoUrl(JNIEnv* env, jobject obj, jstring url) {
     QAndroidJniObject jniUrl("java/lang/String", "(Ljava/lang/String;)V", url);
     DependencyManager::get<AddressManager>()->loadSettings(jniUrl.toString());
-    auto audioClient = DependencyManager::get<AudioClient>();
-    audioClient->setMuted(true, true);
+    AndroidHelper::instance().muteMic();
 }
 
 JNIEXPORT void Java_io_highfidelity_hifiinterface_InterfaceActivity_nativeGoToUser(JNIEnv* env, jobject obj, jstring username) {
     QAndroidJniObject jniUsername("java/lang/String", "(Ljava/lang/String;)V", username);
     DependencyManager::get<AddressManager>()->goToUser(jniUsername.toString(), false);
-    auto audioClient = DependencyManager::get<AudioClient>();
-    audioClient->setMuted(true, true);
+    AndroidHelper::instance().muteMic();
 }
 
 JNIEXPORT void Java_io_highfidelity_hifiinterface_InterfaceActivity_nativeOnPause(JNIEnv* env, jobject obj) {
