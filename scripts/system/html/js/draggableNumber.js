4eab623fd8a22484c0d833bc1272aca79a6b990e
unknown <davidback3@gmail.com>
2018-11-09 17:25:48 -0800

    remove debugPrint function

--- scripts/system/html/js/draggableNumber.js
+++ scripts/system/html/js/draggableNumber.js
@@ -6,10 +6,6 @@
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 
-debugPrint = function (message) {
-    console.log(message);
-};
-
 function DraggableNumber(min, max, step) {
     this.min = min;
     this.max = max;
