7ffa70d0ac3558c31811877dc6af0b66b66dbea2
David Rowe <david@ctrlaltstudio.com>
2018-11-20 09:36:58 +1300

    Fix memory accumulating while Interface is minimized

--- interface/src/Application.cpp
+++ interface/src/Application.cpp
@@ -6222,6 +6222,11 @@ void Application::update(float deltaTime) {
         PerformanceTimer perfTimer("enqueueFrame");
         getMain3DScene()->enqueueFrame();
     }
+
+    // If the display plugin is inactive then the frames won't be processed so process them here.
+    if (!getActiveDisplayPlugin()->isActive()) {
+        getMain3DScene()->processTransactionQueue();
+    }
 }
 
 void Application::updateRenderArgs(float deltaTime) {
