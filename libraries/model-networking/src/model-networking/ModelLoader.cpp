39d1a2be6fab230de00e51b7518cc9eb1de764d0
sabrina-shanman <sabrina@highfidelity.io>
2018-11-30 16:14:14 -0800

    Simplify ModelLoader to utilize new model format registry

--- libraries/model-networking/src/model-networking/ModelLoader.cpp
+++ libraries/model-networking/src/model-networking/ModelLoader.cpp
@@ -11,64 +11,14 @@
 
 #include "ModelLoader.h"
 
-#include "FBXSerializer.h"
-#include "OBJSerializer.h"
-#include "GLTFSerializer.h"
+#include <DependencyManager.h>
+#include "ModelFormatRegistry.h"
 
-ModelLoader::ModelLoader() {
-    // Add supported model formats
-
-    MIMEType fbxMIMEType("fbx");
-    fbxMIMEType.extensions.push_back("fbx");
-    fbxMIMEType.fileSignatures.emplace_back("Kaydara FBX Binary  \x00", 0);
-    addSupportedFormat<FBXSerializer>(fbxMIMEType);
-
-    MIMEType objMIMEType("obj");
-    objMIMEType.extensions.push_back("obj");
-    addSupportedFormat<OBJSerializer>(objMIMEType);
-
-    MIMEType gltfMIMEType("gltf");
-    gltfMIMEType.extensions.push_back("gltf");
-    gltfMIMEType.webMediaTypes.push_back("model/gltf+json");
-    addSupportedFormat<GLTFSerializer>(gltfMIMEType);
-}
 
 hfm::Model::Pointer ModelLoader::load(const hifi::ByteArray& data, const hifi::VariantHash& mapping, const hifi::URL& url, const std::string& webMediaType) const {
-    // Check file contents
-    for (auto& supportedFormat : supportedFormats) {
-        for (auto& fileSignature : supportedFormat.mimeType.fileSignatures) {
-            auto testBytes = data.mid(fileSignature.byteOffset, (int)fileSignature.bytes.size()).toStdString();
-            if (testBytes == fileSignature.bytes) {
-                return supportedFormat.loader(data, mapping, url);
-            }
-        }
+    auto factory = DependencyManager::get<ModelFormatRegistry>()->getFactoryForMIMEType(data, mapping, url, webMediaType);
+    if (!factory) {
+        return hfm::Model::Pointer();
     }
-
-    // Check file extension
-    std::string urlString = url.path().toStdString();
-    std::size_t extensionSeparator = urlString.rfind('.');
-    if (extensionSeparator != std::string::npos) {
-        std::string detectedExtension = urlString.substr(extensionSeparator + 1);
-        for (auto& supportedFormat : supportedFormats) {
-            for (auto& extension : supportedFormat.mimeType.extensions) {
-                if (extension == detectedExtension) {
-                    return supportedFormat.loader(data, mapping, url);
-                }
-            }
-        }
-    }
-
-    // Check web media type
-    if (webMediaType != "") {
-        for (auto& supportedFormat : supportedFormats) {
-            for (auto& candidateWebMediaType : supportedFormat.mimeType.webMediaTypes) {
-                if (candidateWebMediaType == webMediaType) {
-                    return supportedFormat.loader(data, mapping, url);
-                }
-            }
-        }
-    }
-
-    // Supported file type not found. Abort loading.
-    return hfm::Model::Pointer();
+    return factory->get()->read(data, mapping, url);
 }
