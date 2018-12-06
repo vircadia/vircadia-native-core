645508de042037f15dbd25212e08649db1340038
SamGondelman <samuel_gondelman@alumni.brown.edu>
2018-12-06 09:59:24 -0800

    add jsdocs for BillboardMode

--- libraries/shared/src/BillboardMode.h
+++ libraries/shared/src/BillboardMode.h
@@ -11,6 +11,21 @@
 
 #include "QString"
 
+/**jsdoc
+ * <p>How an entity is billboarded.</p>
+ * <table>
+ *   <thead>
+ *     <tr><th>Value</th><th>Description</th></tr>
+ *   </thead>
+ *   <tbody>
+ *     <tr><td><code>none</code></td><td>The entity will not be billboarded.</td></tr>
+ *     <tr><td><code>yaw</code></td><td>The entity will yaw, but not pitch, to face the camera.  Its actual rotation will be ignored.</td></tr>
+ *     <tr><td><code>full</code></td><td>The entity will be billboarded to face the camera.  Its actual rotation will be ignored.</td></tr>
+ *   </tbody>
+ * </table>
+ * @typedef {string} BillboardMode
+ */
+
 enum class BillboardMode {
     NONE = 0,
     YAW,
