b021c3c46df12b051c35f6f85fd5e3aba0d6d66d
sabrina-shanman <sabrina@highfidelity.io>
2018-12-07 15:49:59 -0800

    Add model-baker logging category

---
+++ libraries/model-baker/src/model-baker/ModelBakerLogging.h
@@ -0,0 +1,19 @@
+//
+//  ModelBakerLogging.h
+//  libraries/baker/src/baker
+//
+//  Created by Sabrina Shanman on 2018/12/06.
+//  Copyright 2018 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_ModelBakerLogging_h
+#define hifi_ModelBakerLogging_h
+
+#include <QLoggingCategory>
+
+Q_DECLARE_LOGGING_CATEGORY(model_baker)
+
+#endif // hifi_ModelBakerLogging_h
