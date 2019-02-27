95628dfc111e771842e60cd43ddf08ed70019005
amer cerkic <amer@highfidelity.io>
2019-02-27 15:36:49 -0800

    removing unused imports

--- android/apps/questInterface/src/main/java/io/highfidelity/questInterface/InterfaceActivity.java
+++ android/apps/questInterface/src/main/java/io/highfidelity/questInterface/InterfaceActivity.java
@@ -1,16 +1,10 @@
 package io.highfidelity.questInterface;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-
 import io.highfidelity.oculus.OculusMobileActivity;
 import io.highfidelity.utils.HifiUtils;
 
 public class InterfaceActivity extends OculusMobileActivity {
-    public static final String DOMAIN_URL = "url";
-    private static final String TAG = "Interface";
-    public static final String EXTRA_ARGS = "args";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
