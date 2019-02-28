3be44e0a8ea30fc2500c2d3306e1487a4625e82f
Wayne Chen <chen.wayne@outlook.com>
2019-02-27 18:04:09 -0800

    adding privacy shield files

---
+++ interface/src/ui/PrivacyShield.h
@@ -0,0 +1,12 @@
+//
+//  PrivacyShield.h
+//  interface/src/ui
+//
+//  Created by Wayne Chen on 2/27/19.
+//  Copyright 2019 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#pragma once
