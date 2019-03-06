15d49fd9a923ff3ddde21b15d2f1b9fe2e2ece65
r3tk0n <r3tk0n@gmail.com>
2019-03-05 17:09:54 -0800

    Initial implementation (deadlocks still occurring in Audio.cpp)

--- interface/src/Application.cpp
+++ interface/src/Application.cpp
@@ -1435,6 +1435,8 @@ Application::Application(int& argc, char** argv, QElapsedTimer& startupTimer, bo
         });
         connect(this, &Application::activeDisplayPluginChanged,
             reinterpret_cast<scripting::Audio*>(audioScriptingInterface.data()), &scripting::Audio::onContextChanged);
+        connect(this, &Application::pushedToTalk,
+            reinterpret_cast<scripting::Audio*>(audioScriptingInterface.data()), &scripting::Audio::handlePushedToTalk);
     }
 
     // Create the rendering engine.  This can be slow on some machines due to lots of
@@ -4205,6 +4207,10 @@ void Application::keyPressEvent(QKeyEvent* event) {
                 }
                 break;
 
+            case Qt::Key_T:
+                emit pushedToTalk(true);
+                break;
+
             case Qt::Key_P: {
                 if (!isShifted && !isMeta && !isOption && !event->isAutoRepeat()) {
                     AudioInjectorOptions options;
@@ -4309,6 +4315,12 @@ void Application::keyReleaseEvent(QKeyEvent* event) {
 
     if (_keyboardMouseDevice->isActive()) {
         _keyboardMouseDevice->keyReleaseEvent(event);
+    }
+
+    switch (event->key()) {
+    case Qt::Key_T:
+        emit pushedToTalk(false);
+        break;
     }
 }
 
@@ -5241,6 +5253,9 @@ void Application::loadSettings() {
         }
     }
 
+    auto audioScriptingInterface = reinterpret_cast<scripting::Audio*>(DependencyManager::get<AudioScriptingInterface>().data());
+    audioScriptingInterface->loadData();
+
     getMyAvatar()->loadData();
     _settingsLoaded = true;
 }
@@ -5249,6 +5264,9 @@ void Application::saveSettings() const {
     sessionRunTime.set(_sessionRunTimer.elapsed() / MSECS_PER_SECOND);
     DependencyManager::get<AudioClient>()->saveSettings();
     DependencyManager::get<LODManager>()->saveSettings();
+
+    auto audioScriptingInterface = reinterpret_cast<scripting::Audio*>(DependencyManager::get<AudioScriptingInterface>().data());
+    audioScriptingInterface->saveData();
 
     Menu::getInstance()->saveSettings();
     getMyAvatar()->saveData();
--- interface/src/Application.h
+++ interface/src/Application.h
@@ -358,6 +358,8 @@ signals:
 
     void miniTabletEnabledChanged(bool enabled);
 
+    void pushedToTalk(bool enabled);
+
 public slots:
     QVector<EntityItemID> pasteEntities(float x, float y, float z);
     bool exportEntities(const QString& filename, const QVector<QUuid>& entityIDs, const glm::vec3* givenOffset = nullptr);
