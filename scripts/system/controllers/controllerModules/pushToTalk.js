ec0cf3ee3ace28f9c2bc39fa350190238f15a46c
r3tk0n <r3tk0n@gmail.com>
2019-03-11 09:58:20 -0700

    Fix typo.

--- scripts/system/controllers/controllerModules/pushToTalk.js
+++ scripts/system/controllers/controllerModules/pushToTalk.js
@@ -11,65 +11,66 @@
 Script.include("/~/system/libraries/controllerDispatcherUtils.js");
 Script.include("/~/system/libraries/controllers.js");
 
-(function() {		// BEGIN LOCAL_SCOPE
-	function PushToTalkHandler() {
-		var _this = this;
-		this.active = false;
-		//var pttMapping, mappingName;
-		
-		this.setup = function() {
-			//mappingName = 'Hifi-PTT-Dev-' + Math.random();
-			//pttMapping = Controller.newMapping(mappingName);
-			//pttMapping.enable();
-		};
-		
-		this.shouldTalk = function (controllerData) {
-			// Set up test against controllerData here...
-			var gripVal = controllerData.secondaryValues[LEFT_HAND] && controllerData.secondaryValues[RIGHT_HAND];
-			return (gripVal) ? true : false;
-		};
-		
-		this.shouldStopTalking = function (controllerData) {
-		    var gripVal = controllerData.secondaryValues[LEFT_HAND] && controllerData.secondaryValues[RIGHT_HAND];
-			return (gripVal) ? false : true;			
-		};
-		
-		this.isReady = function (controllerData, deltaTime) {
-			if (HMD.active && Audio.pushToTalk && this.shouldTalk(controllerData)) {
-				Audio.pushingToTalk = true;
-				returnMakeRunningValues(true, [], []);
-			}
-			
-			return makeRunningValues(false, [], []);
-		};
-		
-		this.run = function (controllerData, deltaTime) {
-			if (this.shouldStopTalking(controllerData) || !Audio.pushToTalk) {
-				Audio.pushingToTalk = false;
-				return makeRunningValues(false, [], []);
-			}
-			
-			return makeRunningValues(true, [], []);
-		};
-		
-		this.cleanup = function () {
-			//pttMapping.disable();
-		};
-		
-		this.parameters = makeDispatcherModuleParameters(
-			950,
-			["head"],
-			[],
-			100);
-	}
-	
-	var pushToTalk = new PushToTalkHandler();
-	enableDispatcherModule("PushToTalk", pushToTalk);
-	
-	function cleanup () {
-		pushToTalk.cleanup();
-		disableDispatcherModule("PushToTalk");
-	};
-	
-	Script.scriptEnding.connect(cleanup);
+(function () {		// BEGIN LOCAL_SCOPE
+    function PushToTalkHandler() {
+        var _this = this;
+        this.active = false;
+        //var pttMapping, mappingName;
+
+        this.setup = function () {
+            //mappingName = 'Hifi-PTT-Dev-' + Math.random();
+            //pttMapping = Controller.newMapping(mappingName);
+            //pttMapping.enable();
+        };
+
+        this.shouldTalk = function (controllerData) {
+            // Set up test against controllerData here...
+            var gripVal = controllerData.secondaryValues[LEFT_HAND] && controllerData.secondaryValues[RIGHT_HAND];
+            return (gripVal) ? true : false;
+        };
+
+        this.shouldStopTalking = function (controllerData) {
+            var gripVal = controllerData.secondaryValues[LEFT_HAND] && controllerData.secondaryValues[RIGHT_HAND];
+            return (gripVal) ? false : true;
+        };
+
+        this.isReady = function (controllerData, deltaTime) {
+            if (HMD.active && Audio.pushToTalk && this.shouldTalk(controllerData)) {
+                Audio.pushingToTalk = true;
+                return makeRunningValues(true, [], []);
+            }
+
+            return makeRunningValues(false, [], []);
+        };
+
+        this.run = function (controllerData, deltaTime) {
+            if (this.shouldStopTalking(controllerData) || !Audio.pushToTalk) {
+                Audio.pushingToTalk = false;
+                print("Stop pushing to talk.");
+                return makeRunningValues(false, [], []);
+            }
+
+            return makeRunningValues(true, [], []);
+        };
+
+        this.cleanup = function () {
+            //pttMapping.disable();
+        };
+
+        this.parameters = makeDispatcherModuleParameters(
+            950,
+            ["head"],
+            [],
+            100);
+    }
+
+    var pushToTalk = new PushToTalkHandler();
+    enableDispatcherModule("PushToTalk", pushToTalk);
+
+    function cleanup() {
+        pushToTalk.cleanup();
+        disableDispatcherModule("PushToTalk");
+    };
+
+    Script.scriptEnding.connect(cleanup);
 }());				// END LOCAL_SCOPE
