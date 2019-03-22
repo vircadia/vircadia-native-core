95b4f954a6a1ed2912a6c47ac2c5c6d86a29d8ea
Ken Cooke <ken@highfidelity.io>
2019-03-22 10:12:31 -0700

    Add AudioClient mixing gains for local injectors and system sounds

--- libraries/audio-client/src/AudioClient.cpp
+++ libraries/audio-client/src/AudioClient.cpp
@@ -1368,7 +1368,9 @@ bool AudioClient::mixLocalAudioInjectors(float* mixBuffer) {
             memset(_localScratchBuffer, 0, bytesToRead);
             if (0 < injectorBuffer->readData((char*)_localScratchBuffer, bytesToRead)) {
 
-                float gain = options.volume;
+                bool isSystemSound = !injector->isPositionSet() && !injector->isAmbisonic();
+
+                float gain = injector->getVolume() * (isSystemSound ? _systemInjectorGain : _localInjectorGain);
 
                 if (options.ambisonic) {
 
