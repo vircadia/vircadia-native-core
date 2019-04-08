4b7274f02fc0c2a98009e70e5a35f206e7d5f2b6
luiscuenca <luiscuenca@outboxcode.com>
2019-04-08 11:28:10 -0700

    Fix comparison warning

--- libraries/hfm/src/hfm/HFM.cpp
+++ libraries/hfm/src/hfm/HFM.cpp
@@ -166,7 +166,7 @@ void HFMModel::computeKdops() {
         glm::vec3(INV_SQRT_3,  INV_SQRT_3, -INV_SQRT_3),
         glm::vec3(INV_SQRT_3, -INV_SQRT_3, -INV_SQRT_3)
     };
-    if (joints.size() != shapeVertices.size()) {
+    if (joints.size() != (int)shapeVertices.size()) {
         return;
     }
     // now that all joints have been scanned compute a k-Dop bounding volume of mesh
