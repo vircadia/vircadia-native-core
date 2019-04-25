9b22614135cee1a22c36f1e00c0fef8486e9e0c7
NissimHadar <nissim.hadar@gmail.com>
2019-04-25 10:16:15 -0700

    Removed test code.

--- cmake/macros/SetupQt.cmake
+++ cmake/macros/SetupQt.cmake
@@ -43,7 +43,5 @@ macro(setup_qt)
     if (WIN32)
         add_paths_to_fixup_libs("${QT_DIR}/bin")
     endif ()
-message("=========================================================================SetupQtMake")
-message(${QT_CMAKE_PREFIX_PATH})
 
 endmacro()
