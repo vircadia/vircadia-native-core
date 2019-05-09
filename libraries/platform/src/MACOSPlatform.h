8b1cdb03cce38cc752b8c0bb16bc6e0affd0f3fe
amerhifi <amer@highfidelity.io>
2019-05-09 14:35:05 -0700

    new mac os stubs

---
+++ libraries/platform/src/MACOSPlatform.h
@@ -0,0 +1,33 @@
+//
+//  Created by Amer Cerkic 05/02/2019
+//  Copyright 2019 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#ifndef hifi_WinPlatform_h
+#define hifi_WinPlatform_h
+
+#include "platform.h"
+#include <nlohmann/json.hpp>
+
+namespace platform {
+
+    using namespace nlohmann;
+
+    class MACOSInstance : public Instance {
+    
+    public:
+        bool enumeratePlatform();
+
+    private:
+        unsigned int getNumLogicalCores();
+        void enumerateCpu();
+        void enumerateRam();
+        void enumerateGpu();
+    };
+
+}  // namespace platform
+
+#endif //hifi_winplatform_h
