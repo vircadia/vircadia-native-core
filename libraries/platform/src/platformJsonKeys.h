803631a8212e68273385e6f6cf06ad9f0fbf7653
amerhifi <amer@highfidelity.io>
2019-05-15 11:58:41 -0700

    adding pragma once to see if it will help with gcc warnings. Its complaining that every file that includes the platformkeys is not using the variables

--- libraries/platform/src/platformJsonKeys.h
+++ libraries/platform/src/platformJsonKeys.h
@@ -5,7 +5,7 @@
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
-
+#pragma once
 #ifndef hifi_PlatformJsonKeys_h
 #define hifi_PlatformJsonKeys_h
 #include <QtGlobal>
