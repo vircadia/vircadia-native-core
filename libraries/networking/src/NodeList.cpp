f1e8343b305c5025e511e9b6753c50c77220419c
Howard Stearns <howard@highfidelity.io>
2019-05-17 14:30:30 -0700

    put DomainServerCheckIn on NodeList thread

--- libraries/networking/src/NodeList.cpp
+++ libraries/networking/src/NodeList.cpp
@@ -292,12 +292,6 @@ void NodeList::addSetOfNodeTypesToNodeInterestSet(const NodeSet& setOfNodeTypes)
 
 void NodeList::sendDomainServerCheckIn() {
 
-    // This function is called by the server check-in timer thread
-    // not the NodeList thread.  Calling it on the NodeList thread
-    // resulted in starvation of the server check-in function.
-    // be VERY CAREFUL modifying this code as members of NodeList
-    // may be called by multiple threads.
-
     if (!_sendDomainServerCheckInEnabled) {
         qCDebug(networking_ice) << "Refusing to send a domain-server check in while it is disabled.";
         return;
