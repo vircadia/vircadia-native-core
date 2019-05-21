19a7d91aef13baf04d1dc9b3fb113c6ad894d1f0
amerhifi <amer@highfidelity.io>
2019-05-20 22:37:25 -0700

    testing linux cpu info

--- libraries/platform/src/LinuxPlatform.cpp
+++ libraries/platform/src/LinuxPlatform.cpp
@@ -9,15 +9,51 @@
 #include "LinuxPlatform.h"
 #include "platformJsonKeys.h"
 #include <GPUIdent.h>
-
+#include <thread>
 using namespace platform;
+
+static void getLCpuId( uint32_t* p, uint32_t ax )
+{
+
+#if Q_OS_LINUX
+    __asm __volatile
+    (   "movl %%ebx, %%esi\n\t" 
+     "cpuid\n\t"
+     "xchgl %%ebx, %%esi"
+     : "=a" (p[0]), "=S" (p[1]),
+     "=c" (p[2]), "=d" (p[3])
+     : "0" (ax)
+     );
+#endif
+}
+
 void LinuxInstance::enumerateCpu() {
     json cpu = {};
+
+    uint32_t cpuInfo[4]={0,0,0,0};
+    char CPUBrandString[16];
+    char CPUModelString[16];
+    char CPUClockString[16];
+    uint32_t nExIds;
+    getLCpuId(cpuInfo, 0x80000000);
+    nExIds = cpuInfo[0];
+    
+    for (uint32_t i = 0x80000000; i <= nExIds; ++i) {
+        getLCpuId(cpuInfo, i);
+        // Interpret CPU brand string
+        if (i == 0x80000002) {
+            memcpy(CPUBrandString, cpuInfo, sizeof(cpuInfo));
+        } else if (i == 0x80000003) {
+            memcpy(CPUModelString, cpuInfo, sizeof(cpuInfo));
+        } else if (i == 0x80000004) {
+            memcpy(CPUClockString, cpuInfo, sizeof(cpuInfo));
+        }
+    }
  
-    cpu["cpuBrand"] = "";
-    cpu["cpuModel"] = "";
-    cpu["cpuClockSpeed"] = "";
-    cpu["cpuNumCores"] = "";
+     cpu["cpuBrand"] = CPUBrandString;
+     cpu["cpuModel"] = CPUModelString;
+     cpu["cpuClockSpeed"] = CPUClockString;
+     cpu["cpuNumCores"] = std::thread::hardware_concurrency();
 
     _cpu.push_back(cpu);
 }
