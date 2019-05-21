10db88c07b6914d53cd4dde0afe504570e25147b
Roxanne Skelly <roxanne@highfidelity.io>
2019-05-21 12:32:01 -0700

    Fix ubuntu warning

--- interface/src/octree/SafeLanding.cpp
+++ interface/src/octree/SafeLanding.cpp
@@ -170,7 +170,7 @@ bool SafeLanding::isEntityPhysicsReady(const EntityItemPointer& entity) {
             entity->getAABox(hasAABox);
             if (hasAABox && downloadedCollisionTypes.count(modelEntity->getShapeType()) != 0) {
                 auto space = _entityTreeRenderer->getWorkloadSpace();
-                uint8_t region = space ? space->getRegion(entity->getSpaceIndex()) : workload::Region::INVALID;
+                uint8_t region = space ? space->getRegion(entity->getSpaceIndex()) : (uint8_t)workload::Region::INVALID;
                 bool shouldBePhysical = region < workload::Region::R3 && entity->shouldBePhysical();
                 return (!shouldBePhysical || entity->isInPhysicsSimulation() || modelEntity->computeShapeFailedToLoad());
             }
