971d563f498d2702a0ecf7f8ce8d1afa4b5a89fa
sabrina-shanman <sabrina@highfidelity.io>
2019-06-06 13:23:35 -0700

    Add debug icons for transitions

--- libraries/render/src/render/DrawStatus.cpp
+++ libraries/render/src/render/DrawStatus.cpp
@@ -162,15 +162,19 @@ void DrawStatus::run(const RenderContextPointer& renderContext, const Input& inp
                             auto& transition = transitionStage->getTransition(transitionID);
                             switch (transition.eventType) {
                             case Transition::Type::USER_ENTER_DOMAIN:
-                            case Transition::ELEMENT_ENTER_DOMAIN:
-                                status.setIcon((unsigned char)Item::Status::Icon::PACKET_RECEIVED);
+                                status.setIcon((unsigned char)Item::Status::Icon::USER_TRANSITION_IN);
                                 break;
                             case Transition::Type::USER_LEAVE_DOMAIN:
+                                status.setIcon((unsigned char)Item::Status::Icon::USER_TRANSITION_OUT);
+                                break;
+                            case Transition::ELEMENT_ENTER_DOMAIN:
+                                status.setIcon((unsigned char)Item::Status::Icon::GENERIC_TRANSITION_IN);
+                                break;
                             case Transition::ELEMENT_LEAVE_DOMAIN:
-                                status.setIcon((unsigned char)Item::Status::Icon::PACKET_SENT);
+                                status.setIcon((unsigned char)Item::Status::Icon::GENERIC_TRANSITION_OUT);
                                 break;
                             default:
-                                status.setIcon((unsigned char)Item::Status::Icon::HAS_ACTIONS);
+                                status.setIcon((unsigned char)Item::Status::Icon::GENERIC_TRANSITION);
                                 break;
                             }
                         } else {
