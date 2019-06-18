6196c6b746b48346a573daa64af17495144788b8
Milad Nazeri <milad@miladnazeri.com>
2019-06-18 14:32:11 -0700

    more stable nametag updates

--- scripts/simplifiedUI/ui/simplifiedNametag/simplifiedNametag.js
+++ scripts/simplifiedUI/ui/simplifiedNametag/simplifiedNametag.js
@@ -8,7 +8,7 @@
 //
 //    Click on someone to get a nametag for them
 //    
-var PickRayController = Script.require('./resources/modules/pickRayController.js?' + Date.now());
+var PickRayController = Script.require('./resources/modules/pickRayController.js');
 var NameTagListManager = Script.require('./resources/modules/nameTagListManager.js?' + Date.now());
 var pickRayController = new PickRayController();
 var nameTagListManager = new NameTagListManager();
@@ -31,6 +31,7 @@ function selectAvatar(uuid, intersection) {
 // Handles reset of list if you change domains
 function onDomainChange() {
     nameTagListManager.reset();
+    nameTagListManager.handleAvatarNametagMode(avatarNametagMode);
 }
 
 
