94002436e917a4325fe5d45a8816e423c84cd366
luiscuenca <luiscuenca@outboxcode.com>
2019-07-10 16:05:24 -0700

    Override default script

--- launchers/win32/LauncherManager.cpp
+++ launchers/win32/LauncherManager.cpp
@@ -296,8 +296,8 @@ HWND LauncherManager::launchApplication() {
     LauncherManager::getAndCreatePaths(PathType::Interface_Directory, installDir);
     CString interfaceExe = installDir + _T("\\interface.exe");
     CString urlParam = _T("--url \"") + _domainURL + ("\" ");
-    CString scriptsURL = installDir + _T("\\scripts\\simplifiedUI");
-    CString scriptsParam = _T("--scripts \"") + scriptsURL + ("\" ");
+    CString scriptsURL = installDir + _T("\\scripts\\simplifiedUIBootstrapper.js");
+    CString scriptsParam = _T("--defaultScriptsOverride \"") + scriptsURL + ("\" ");
     CString cacheDir;
     LauncherManager::getAndCreatePaths(PathType::Content_Directory, cacheDir);
     CString cacheParam = _T("--cache \"") + cacheDir + ("\" ");
