a6c569dd7861d69ee4b44336fbc22bf4701efc0f
Anna <anna@highfidelity.io>
2019-07-23 14:45:12 -0700

    fixed index out of bounds checking when getting cpu, gpu or display in PlatformInstance.cpp

--- libraries/platform/src/platform/backend/PlatformInstance.cpp
+++ libraries/platform/src/platform/backend/PlatformInstance.cpp
@@ -42,7 +42,8 @@ void Instance::enumerateNics() {
 
 json Instance::getCPU(int index) {
     assert(index <(int) _cpus.size());
-    if (index >= (int)_cpus.size())
+
+    if (index <= 0 || (int) _cpus.size() <= index)
         return json();
 
     return _cpus.at(index);
@@ -51,7 +52,7 @@ json Instance::getCPU(int index) {
 json Instance::getGPU(int index) {
     assert(index <(int) _gpus.size());
 
-    if (index >=(int) _gpus.size())
+    if (index <= 0 || (int) _gpus.size() <= index)
         return json();
     
     return _gpus.at(index);
@@ -60,7 +61,7 @@ json Instance::getGPU(int index) {
 json Instance::getDisplay(int index) {
     assert(index <(int) _displays.size());
     
-    if (index >=(int) _displays.size())
+    if (index <= 0 || (int) _displays.size() <= index)
         return json();
 
     return _displays.at(index);
