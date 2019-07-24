5afae43051d7673f99341d70c3c4ab71f904ef99
Roxanne Skelly <roxanne@highfidelity.io>
2019-07-24 16:05:34 -0700

    CR fix

--- domain-server/src/DomainServer.h
+++ domain-server/src/DomainServer.h
@@ -194,7 +194,6 @@ private:
     QUrl oauthRedirectURL();
     QUrl oauthAuthorizationURL(const QUuid& stateUUID = QUuid::createUuid());
 
-    QString getWebSessionUsername(HTTPConnection* connection);
     bool isAuthenticatedRequest(HTTPConnection* connection, const QUrl& url);
 
     QNetworkReply* profileRequestGivenTokenReply(QNetworkReply* tokenReply);
