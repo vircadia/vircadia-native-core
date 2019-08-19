083470e4fe7d43eacae3e2c9e3e9cf03eeeb49f9
Zach Fox <fox@highfidelity.io>
2019-08-19 16:48:40 -0700

    Ensure that geometryChanged is emitted after docked widget is resized

--- libraries/ui/src/DockWidget.h
+++ libraries/ui/src/DockWidget.h
@@ -19,6 +19,7 @@
 class QQuickView;
 class QQuickItem;
 class DockWidget : public QDockWidget {
+Q_OBJECT
 public:
     DockWidget(const QString& title, QWidget* parent = nullptr);
     ~DockWidget() = default;
@@ -26,6 +27,13 @@ public:
     void setSource(const QUrl& url);
     QQuickItem* getRootItem() const;
     std::shared_ptr<QQuickView> getQuickView() const;
+
+signals:
+    void onResizeEvent();
+
+protected:
+    void resizeEvent(QResizeEvent* event) override;
+
 private:
     std::shared_ptr<QQuickView> _quickView;
 };
