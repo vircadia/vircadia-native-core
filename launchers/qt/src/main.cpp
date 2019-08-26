0a2b5460173a781cdcbd8c03bdd748aeaceb83a2
danteruiz <danteruiz102@gmail.com>
2019-08-26 16:26:23 -0700

    working on windows cmake version

--- launchers/qt/src/main.cpp
+++ launchers/qt/src/main.cpp
@@ -1,7 +1,26 @@
-//main.cpp
-//#include <iostream>
+#include <iostream>
+#include <QGuiApplication>
+#include <QQuickView>
+#include <QString>
+#include <QtPlugin>
+/*Q_IMPORT_PLUGIN(QCocoaIntegrationPlugin);
+Q_IMPORT_PLUGIN(QtQuick2Plugin);
+Q_IMPORT_PLUGIN(QtQuickControls2Plugin);
+Q_IMPORT_PLUGIN(QtQuickTemplates2Plugin);*/
+int main(int argc, char *argv[])
+{
+    QString name { "QtExamples" };
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setOrganizationName(name);
 
-int main(int argc, char *argv[]) {
-    //std::cout << "Hello World \n";
-    return 0;
+    QGuiApplication app(argc, argv);
+
+    QQuickView view;
+    view.setFlags(Qt::FramelessWindowHint);
+    view.setSource(QUrl("/Users/danteruiz/github/test/qml/root.qml"));
+    if (view.status() == QQuickView::Error)
+        return -1;
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+    view.show();
+    return app.exec();
 }
