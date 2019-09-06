7a5cb0d55580b66703b05d54aaee37d09ef3bede
Andrew Meadows <andrew@highfidelity.io>
2019-09-06 12:28:15 -0700

    fix typo

--- libraries/networking/src/udt/PacketHeaders.h
+++ libraries/networking/src/udt/PacketHeaders.h
@@ -274,7 +274,7 @@ enum class EntityVersion : PacketVersion {
     TextUnlit,
     ShadowBiasAndDistance,
     TextEntityFonts,
-    ScriptServerKinematicMotion
+    ScriptServerKinematicMotion,
 
     // Add new versions above here
     NUM_PACKET_TYPE,
