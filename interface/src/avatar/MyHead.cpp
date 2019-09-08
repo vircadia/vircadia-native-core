943348ba5171b5c19c27df09911b4be3ed207beb
Seth Alves <seth.alves@gmail.com>
2019-09-07 18:11:04 -0700

    input action channels for eyes and eyelids and fingers.  connect eyelid input actions to blendshapes.

--- interface/src/avatar/MyHead.cpp
+++ interface/src/avatar/MyHead.cpp
@@ -15,7 +15,6 @@
 #include <recording/Deck.h>
 #include <Rig.h>
 #include <trackers/FaceTracker.h>
-#include <trackers/EyeTracker.h>
 
 #include "devices/DdeFaceTracker.h"
 #include "Application.h"
@@ -46,18 +45,32 @@ void MyHead::simulate(float deltaTime) {
     auto player = DependencyManager::get<recording::Deck>();
     // Only use face trackers when not playing back a recording.
     if (!player->isPlaying()) {
-        auto faceTracker = qApp->getActiveFaceTracker();
-        const bool hasActualFaceTrackerConnected = faceTracker && !faceTracker->isMuted();
-        _isFaceTrackerConnected = hasActualFaceTrackerConnected || _owningAvatar->getHasScriptedBlendshapes();
-        if (_isFaceTrackerConnected) {
-            if (hasActualFaceTrackerConnected) {
-                _blendshapeCoefficients = faceTracker->getBlendshapeCoefficients();
-            }
-        }
+        // auto faceTracker = qApp->getActiveFaceTracker();
+        // const bool hasActualFaceTrackerConnected = faceTracker && !faceTracker->isMuted();
+        // _isFaceTrackerConnected = hasActualFaceTrackerConnected || _owningAvatar->getHasScriptedBlendshapes();
+        // if (_isFaceTrackerConnected) {
+        //     if (hasActualFaceTrackerConnected) {
+        //         _blendshapeCoefficients = faceTracker->getBlendshapeCoefficients();
+        //     }
+        // }
 
-        auto eyeTracker = DependencyManager::get<EyeTracker>();
-        _isEyeTrackerConnected = eyeTracker->isTracking();
-        // if eye tracker is connected we should get the data here.
+        auto userInputMapper = DependencyManager::get<UserInputMapper>();
+        bool eyeLidsTracked =
+            userInputMapper->getActionStateValid(controller::Action::LEFT_EYE_BLINK) &&
+            userInputMapper->getActionStateValid(controller::Action::RIGHT_EYE_BLINK);
+        setFaceTrackerConnected(eyeLidsTracked);
+        if (eyeLidsTracked) {
+            float leftEyeBlink = userInputMapper->getActionState(controller::Action::LEFT_EYE_BLINK);
+            float rightEyeBlink = userInputMapper->getActionState(controller::Action::RIGHT_EYE_BLINK);
+            _blendshapeCoefficients.resize(std::max(_blendshapeCoefficients.size(), 2));
+            _blendshapeCoefficients[0] = leftEyeBlink;
+            _blendshapeCoefficients[1] = rightEyeBlink;
+        } else {
+            const float FULLY_OPEN = 0.0f;
+            _blendshapeCoefficients.resize(std::max(_blendshapeCoefficients.size(), 2));
+            _blendshapeCoefficients[0] = FULLY_OPEN;
+            _blendshapeCoefficients[1] = FULLY_OPEN;
+        }
     }
     Parent::simulate(deltaTime);
 }
