0de7628d48c27a7395c87ad93ea3032482f06bc0
amerhifi <amer@highfidelity.io>
2019-09-13 13:19:32 -0700

    fix typo

--- libraries/audio-client/src/HifiAudioDeviceinfo.cpp
+++ libraries/audio-client/src/HifiAudioDeviceinfo.cpp
@@ -11,7 +11,7 @@ void HifiAudioDeviceInfo::setIsDefault(bool isDefault) {
 }
 
  void HifiAudioDeviceInfo::setDeviceName() {
-  if (isDefault) {
+  if (_isDefault) {
          if (_mode == QAudio::Mode::AudioInput) {
              _deviceName = "Default microphone (recommended)";
          } else {
