05a8c7853c8cc8b7065acca31377103c3801fd91
amer cerkic <amer@highfidelity.io>
2019-09-17 10:03:54 -0700

    added file info. moved pragma

--- libraries/audio-client/src/HifiAudioDeviceInfo.h
+++ libraries/audio-client/src/HifiAudioDeviceInfo.h
@@ -1,6 +1,18 @@
+//
+//  HifiAudioDeviceInfo.h
+//  libraries/audio-client/src
+//
+//  Created by Amer Cerkic on 9/14/19.
+//  Copyright 2019 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+#pragma once
 #ifndef hifi_audiodeviceinfo_h
 #define hifi_audiodeviceinfo_h
-#pragma once
+
 
 #include <QObject>
 #include <QAudioDeviceInfo>
--- libraries/audio-client/src/HifiAudioDeviceinfo.cpp
+++ libraries/audio-client/src/HifiAudioDeviceinfo.cpp
@@ -1,3 +1,15 @@
+//
+//  HifiAudioDeviceInfo.cpp
+//  libraries/audio-client/src
+//
+//  Created by Amer Cerkic on 9/14/19.
+//  Copyright 2019 High Fidelity, Inc.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+
 #include "HifiAudioDeviceInfo.h"
 #include <QCryptographicHash>
 
