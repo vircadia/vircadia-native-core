764b6a9adf56756ab0dddec6b4f8fe0316475707
Anthony J. Thibault <tony@highfidelity.io>
2019-11-26 10:52:21 -0800

    code review feedback

--- tools/animedit/deploy.bat
+++
@@ -1,3 +0,0 @@
-mkdir C:\Users\ajthy\Documents\animedit\bin
-copy  C:\Users\ajthy\Documents\build-animedit-Desktop_Qt_5_12_3_MSVC2015_64bit-Release\release\animedit.exe C:\Users\ajthy\Documents\animedit\bin
-C:\Qt\5.12.3\msvc2015_64\bin\windeployqt.exe -qmldir C:\Users\ajthy\Documents\animedit\qml C:\Users\ajthy\Documents\animedit\bin\animedit.exe
--- tools/animedit/main.cpp
+++ tools/animedit/main.cpp
@@ -26,9 +26,8 @@ int main(int argc, char *argv[])
     const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl) {
+            // failure loading main.qml
             QCoreApplication::exit(-1);
-        } else {
-            ;
         }
     }, Qt::QueuedConnection);
     engine.load(url);
