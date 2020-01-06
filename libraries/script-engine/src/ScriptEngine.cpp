fb5dbfc52a2fcc5ebee3da39d43f13f4533e220a
Kasen IO <kasenvr@gmail.com>
2020-01-05 23:32:28 -0500

    Disable whitelist on entity_server scripts.

--- libraries/script-engine/src/ScriptEngine.cpp
+++ libraries/script-engine/src/ScriptEngine.cpp
@@ -2377,15 +2377,21 @@ void ScriptEngine::entityScriptContentAvailable(const EntityItemID& entityID, co
         // END PULL SAFEURLS FROM INTERFACE.JSON Settings
         
         bool isInWhitelist = false;  // assume unsafe
-        for (const auto& str : safeURLS) {
-            qCDebug(scriptengine) << whitelistPrefix << "Script URL: " << scriptOrURL << "TESTING AGAINST" << str << "RESULTS IN"
-                     << scriptOrURL.startsWith(str);
-            if (!str.isEmpty() && scriptOrURL.startsWith(str)) {
-                isInWhitelist = true;
-                qCDebug(scriptengine) << whitelistPrefix << "Script approved.";
-                break;  // bail early since we found a match
-            }
-        }
+		
+		if(ScriptEngine::getContext() == "entity_server") {
+			isInWhitelist = true;
+		} else {
+			for (const auto& str : safeURLS) {
+				qCDebug(scriptengine) << whitelistPrefix << "Script URL: " << scriptOrURL << "TESTING AGAINST" << str << "RESULTS IN"
+						 << scriptOrURL.startsWith(str);
+				if (!str.isEmpty() && scriptOrURL.startsWith(str)) {
+					isInWhitelist = true;
+					qCDebug(scriptengine) << whitelistPrefix << "Script approved.";
+					break;  // bail early since we found a match
+				}
+			}
+		}
+
         if (!isInWhitelist) {
             qCDebug(scriptengine) << whitelistPrefix << "(disabled entity script)" << entityID.toString() << scriptOrURL;
             exception = makeError("UNSAFE_ENTITY_SCRIPTS == 0");
