d8693d4d43cc4c5de8d5a103f291ec9607339b97
Kasen IO <kasenvr@gmail.com>
2020-04-03 02:12:27 -0400

    License headers.

--- scripts/system/inventory/inventory.js
+++ scripts/system/inventory/inventory.js
@@ -1,3 +1,13 @@
+//
+//  inventory.js
+//
+//  Created by kasenvr@gmail.com on 2 Mar 2020
+//  Copyright 2020 Vircadia Contributors
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
 (function () { // BEGIN LOCAL_SCOPE
 var AppUi = Script.require('appUi');
 var ui;
@@ -17,6 +27,18 @@ tablet.webEventReceived.connect(onWebAppEventReceived);
 // END APP EVENT ROUTING
 
 function saveInventory() {
+    
+}
+
+function loadInventory() {
+    
+}
+
+function receivingItem() {
+    
+}
+
+function shareItem() {
     
 }
 
