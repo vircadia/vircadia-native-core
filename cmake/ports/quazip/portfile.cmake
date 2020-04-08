53bb62756f6bef68db7e9a9e0b4f9a6e2025fbba
Thijs Wenker <me@thoys.nl>
2020-04-08 16:10:33 +0200

    fix more links

--- cmake/ports/quazip/portfile.cmake
+++ cmake/ports/quazip/portfile.cmake
@@ -1,10 +1,11 @@
 include(vcpkg_common_functions)
 
 file(READ "${VCPKG_ROOT_DIR}/_env/QT_CMAKE_PREFIX_PATH.txt" QT_CMAKE_PREFIX_PATH)
+file(READ "${VCPKG_ROOT_DIR}/_env/EXTERNAL_BUILD_ASSETS.txt" EXTERNAL_BUILD_ASSETS)
 
 vcpkg_download_distfile(
     SOURCE_ARCHIVE
-    URLS https://athena-public.s3.amazonaws.com/dependencies/quazip-0.7.3.zip
+    URLS ${EXTERNAL_BUILD_ASSETS}/dependencies/quazip-0.7.3.zip
     SHA512 b2d812b6346317fd6d8f4f1344ad48b721d697c429acc8b7e7cb776ce5cba15a59efd64b2c5ae1f31b5a3c928014f084aa1379fd55d8a452a6cf4fd510b3afcc
     FILENAME quazip.zip
 )
