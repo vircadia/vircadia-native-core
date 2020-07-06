96ae7097200898db1c947a51d67fab435e5a6781
David Rowe <david@ctrlaltstudio.com>
2020-07-06 20:40:55 +1200

    Update copyrights

--- plugins/openvr/src/OpenVrHelpers.h
+++ plugins/openvr/src/OpenVrHelpers.h
@@ -1,6 +1,7 @@
 //
 //  Created by Bradley Austin Davis on 2015/06/12
 //  Copyright 2015 High Fidelity, Inc.
+//  Copyright 2020 Vircadia contributors.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
