9395caa7d0f812a1fabb3b4bdb8da9d1b5900f9c
David Rowe <david@ctrlaltstudio.com>
2020-09-17 09:01:27 +1200

    Rework GitHub Actions builds for merges to master

--- tools/ci-scripts/upload.py
+++ tools/ci-scripts/upload.py
@@ -6,9 +6,8 @@ import boto3
 import glob
 from github import Github
 
-
 def main():
-    bucket_name = os.environ['BUCKET_NAME']
+    bucket_name = os.environ['UPLOAD_BUCKET']
     upload_prefix = os.environ['UPLOAD_PREFIX']
     release_number = os.environ['RELEASE_NUMBER']
     full_prefix = upload_prefix + '/' + release_number[0:-2] + '/' + release_number
