c6b947e5f9d5426ded7c45b6b9702afab1e537e5
Kalila L <kasenvr@gmail.com>
2020-09-19 23:46:34 -0400

    Update sandbox starter content URL

--- server-console/src/main.js
+++ server-console/src/main.js
@@ -56,7 +56,7 @@ const menuNotificationIcon = path.join(__dirname, '../resources/tray-menu-notifi
 const DELETE_LOG_FILES_OLDER_THAN_X_SECONDS = 60 * 60 * 24 * 7; // 7 Days
 const LOG_FILE_REGEX = /(domain-server|ac-monitor|ac)-.*-std(out|err).txt/;
 
-const HOME_CONTENT_URL = "http://cdn.highfidelity.com/content-sets/home-tutorial-RC40.tar.gz";
+const HOME_CONTENT_URL = "https://vircadia-content-public.s3.amazonaws.com/DomainContent/Sandbox/Basic_sandbox_no_external_links.tar.gz";
 
 const buildInfo = GetBuildInfo();
 
