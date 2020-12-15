4e18382089508a89266360a9456dc35479f9d575
Julian Groß <firedefender1@googlemail.com>
2020-12-15 13:53:17 +0100

    Work around android not building with set glad32es.zip filename

--- cmake/ports/glad/portfile.cmake
+++ cmake/ports/glad/portfile.cmake
@@ -4,7 +4,13 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 file(READ "${VCPKG_ROOT_DIR}/_env/EXTERNAL_BUILD_ASSETS.txt" EXTERNAL_BUILD_ASSETS)
 file(READ "${VCPKG_ROOT_DIR}/_env/USE_GLES.txt" USE_GLES)
 
-if (ANDROID OR USE_GLES)
+if (ANDROID)
+    vcpkg_download_distfile(
+        SOURCE_ARCHIVE
+        URLS ${EXTERNAL_BUILD_ASSETS}/dependencies/glad/glad32es.zip
+        SHA512 2e02ac633eed8f2ba2adbf96ea85d08998f48dd2e9ec9a88ec3c25f48eaf1405371d258066327c783772fcb3793bdb82bd7375fdabb2ba5e2ce0835468b17f65
+    )
+elseif (USE_GLES)
     vcpkg_download_distfile(
         SOURCE_ARCHIVE
         URLS ${EXTERNAL_BUILD_ASSETS}/dependencies/glad/glad32es.zip
