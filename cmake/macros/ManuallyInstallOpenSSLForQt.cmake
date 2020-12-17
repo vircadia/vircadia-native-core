fd1ea3cf04aba638706972fa20280e87dba354d7
David Rowe <david@ctrlaltstudio.com>
2020-12-18 10:05:21 +1300

    Update Windows builds to OpenSSL 1.1.1h

--- cmake/macros/ManuallyInstallOpenSSLForQt.cmake
+++ cmake/macros/ManuallyInstallOpenSSLForQt.cmake
@@ -3,6 +3,7 @@
 #
 #  Created by Stephen Birarda on 1/15/16.
 #  Copyright 2014 High Fidelity, Inc.
+#  Copyright 2020 Vircadia contributors.
 #
 #  Distributed under the Apache License, Version 2.0.
 #  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
@@ -19,13 +20,13 @@ macro(manually_install_openssl_for_qt)
     find_package(OpenSSL REQUIRED)
 
     install(
-      FILES "${VCPKG_INSTALL_ROOT}/bin/ssleay32.dll"
+      FILES "${VCPKG_INSTALL_ROOT}/bin/libcrypto-1_1-x64.dll"
       DESTINATION ${TARGET_INSTALL_DIR}
       COMPONENT ${TARGET_INSTALL_COMPONENT}
     )
 
     install(
-      FILES "${VCPKG_INSTALL_ROOT}/bin/libeay32.dll"
+      FILES "${VCPKG_INSTALL_ROOT}/bin/libssl-1_1-x64.dll"
       DESTINATION ${TARGET_INSTALL_DIR}
       COMPONENT ${TARGET_INSTALL_COMPONENT}
     )
