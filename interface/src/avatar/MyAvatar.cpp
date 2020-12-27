vircadia/vircadia-native-core

commit 8b839fe71b9a2d412100d61671fbd1d71cb6d8c9

VR fixes for different user heights and avatar scales.
- The user's real-world height is now taken into account in MyAvatar::deriveBodyFromHMDSensor.  Therefore, for any user height, the floor stays correctly positioned in all modes of 'Allow my avatar to stand'.
- Whenever the user's real-world height is changed, centerBodyInternal is now called to position the body accordingly.  The floor therefore stays correctly positioned in all modes of 'Allow my avatar to stand'.  (MyAvatar::setUserHeight)
- Fix for walk speeds in VR being too fast at large avatar scale and too slow at small avatar scale.  The action motor velocity was being scaled once too many by the sensor-to-world scale.  The bug existed before this branch.  (MyAvatar::scaleMotorSpeed)

interface/src/avatar/MyAvatar.cpp
@@ -3758,15 +3758,15 @@ glm::vec3 MyAvatar::scaleMotorSpeed(const glm::vec3 forward, const glm::vec3 rig
                     if (length > EPSILON) {
                         direction /= length;
                     }
-                    return getSensorToWorldScale() * direction * getSprintSpeed() * _walkSpeedScalar;
+                    return direction * getSprintSpeed() * _walkSpeedScalar;
                 } else {
                     return Vectors::ZERO;
                 }
             case LocomotionControlsMode::CONTROLS_ANALOG:
             case LocomotionControlsMode::CONTROLS_ANALOG_PLUS:
                 if (zSpeed || xSpeed) {
-                    glm::vec3 scaledForward = getSensorToWorldScale() * calculateGearedSpeed(zSpeed) * _walkSpeedScalar * ((zSpeed >= stickFullOn) ? getSprintSpeed() : getWalkSpeed()) * forward;
-                    glm::vec3 scaledRight = getSensorToWorldScale() * calculateGearedSpeed(xSpeed) * _walkSpeedScalar * ((xSpeed > stickFullOn) ? getSprintSpeed() : getWalkSpeed()) * right;
+                    glm::vec3 scaledForward = calculateGearedSpeed(zSpeed) * _walkSpeedScalar * ((zSpeed >= stickFullOn) ? getSprintSpeed() : getWalkSpeed()) * forward;
+                    glm::vec3 scaledRight = calculateGearedSpeed(xSpeed) * _walkSpeedScalar * ((xSpeed > stickFullOn) ? getSprintSpeed() : getWalkSpeed()) * right;
                     direction = scaledForward + scaledRight;
                     return direction;
                 } else {
@@ -4785,6 +4785,7 @@ void MyAvatar::triggerRotationRecenter() {
     _follow.setForceActivateRotation(true);
 }
 
+// Derive the sensor-space matrix for the body, based on the pose of the HMD and hips tracker.
 // old school meat hook style
 // forceFollowYPos (default false): true to force the body matrix to be affected by the HMD's
 // vertical position, even if crouch recentering is disabled.
@@ -4822,8 +4823,8 @@ glm::mat4 MyAvatar::deriveBodyFromHMDSensor(const bool forceFollowYPos) const {
     glm::vec3 headToNeck = headOrientation * Quaternions::Y_180 * (localNeck - localHead);
     glm::vec3 neckToRoot = headOrientationYawOnly  * Quaternions::Y_180 * -localNeck;
 
-    float invSensorToWorldScale = getUserEyeHeight() / getEyeHeight();
-    glm::vec3 bodyPos = headPosition + invSensorToWorldScale * (headToNeck + neckToRoot);
+    const float worldToSensorScale = getUserEyeHeight() / getEyeHeight();
+    glm::vec3 bodyPos = headPosition + worldToSensorScale * (headToNeck + neckToRoot);
 	glm::quat bodyQuat;
 
     const controller::Pose hipsControllerPose = getControllerPoseInSensorFrame(controller::Action::HIPS);
@@ -4842,7 +4843,7 @@ glm::mat4 MyAvatar::deriveBodyFromHMDSensor(const bool forceFollowYPos) const {
 
     if (!forceFollowYPos && !getHMDCrouchRecenterEnabled()) {
         // Set the body's vertical position as if it were standing in its T-pose.
-        bodyPos.y = rig.getUnscaledHipsHeight();
+        bodyPos.y = worldToSensorScale * rig.getUnscaledHipsHeight();
     }
 
     glm::mat4 bodyMat = createMatFromQuatAndPos(bodyQuat, bodyPos);
@@ -5230,6 +5231,7 @@ float MyAvatar::getUserHeight() const {
 
 void MyAvatar::setUserHeight(float value) {
     _userHeight.set(value);
+    centerBodyInternal(false);
 
     float sensorToWorldScale = getEyeHeight() / getUserEyeHeight();
     emit sensorToWorldScaleChanged(sensorToWorldScale);

commit dec9e9d3380c4c03e4997d254170a16273f5d7fb

VR fix for different avatar scales.
Previously, if the avatar scale wasn't 1, the body and viewpoint would given the wrong vertical position in some situations, eg: getting up from click-to-sit; changing the 'Allow my avatar to stand' setting.
The avatar scale was being taken into account where it shouldn't have in MyAvatar::deriveBodyFromHMDSensor.
The bug started in 8b839fe71b9a2d412100d61671fbd1d71cb6d8c9 ("VR fixes for different user heights and avatar scales".
Repro:
1. Set 'Allow my avatar to stand: Always'
2. Reduce the avatar scale to the minumum.
3. Set 'Allow my avatar to stand: When I stand'
4. Previously, the viewpoint would suddenly be below the floor.

interface/src/avatar/MyAvatar.cpp
@@ -4845,7 +4845,8 @@ glm::mat4 MyAvatar::deriveBodyFromHMDSensor(const bool forceFollowYPos) const {
 
     if (!forceFollowYPos && !getHMDCrouchRecenterEnabled()) {
         // Set the body's vertical position as if it were standing in its T-pose.
-        bodyPos.y = worldToSensorScale * rig.getUnscaledHipsHeight();
+        const float userToRigScale = getUserEyeHeight() / getUnscaledEyeHeight();
+        bodyPos.y = userToRigScale * rig.getUnscaledHipsHeight();
     }
 
     glm::mat4 bodyMat = createMatFromQuatAndPos(bodyQuat, bodyPos);