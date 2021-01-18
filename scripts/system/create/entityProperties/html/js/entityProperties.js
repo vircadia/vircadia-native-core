3a821c2b7448175d32d17cfc321fca7f8b9e91f3
HifiExperiments <thingsandstuffblog@gmail.com>
2021-01-18 11:23:44 -0800

    update create

--- scripts/system/create/entityProperties/html/js/entityProperties.js
+++ scripts/system/create/entityProperties/html/js/entityProperties.js
@@ -117,6 +117,16 @@ const GROUPS = [
                 propertyID: "primitiveMode",
             },
             {
+                label: "Billboard Mode",
+                type: "dropdown",
+                options: {
+                    none: "None",
+                    yaw: "Yaw",
+                    full: "Full"
+                },
+                propertyID: "billboardMode",
+            },
+            {
                 label: "Render With Zones",
                 type: "multipleZonesSelection",
                 propertyID: "renderWithZones",
@@ -228,13 +238,6 @@ const GROUPS = [
                 step: 0.01,
                 decimals: 2,
                 propertyID: "textEffectThickness",
-            },
-            {
-                label: "Billboard Mode",
-                type: "dropdown",
-                options: { none: "None", yaw: "Yaw", full: "Full"},
-                propertyID: "textBillboardMode",
-                propertyName: "billboardMode", // actual entity property name
             },
             {
                 label: "Top Margin",
@@ -719,13 +722,6 @@ const GROUPS = [
                 propertyID: "subImage",
             },
             {
-                label: "Billboard Mode",
-                type: "dropdown",
-                options: { none: "None", yaw: "Yaw", full: "Full"},
-                propertyID: "imageBillboardMode",
-                propertyName: "billboardMode", // actual entity property name
-            },
-            {
                 label: "Keep Aspect Ratio",
                 type: "bool",
                 propertyID: "keepAspectRatio",
@@ -773,13 +769,6 @@ const GROUPS = [
                 step: 1,
                 decimals: 0,
                 propertyID: "maxFPS",
-            },
-            {
-                label: "Billboard Mode",
-                type: "dropdown",
-                options: { none: "None", yaw: "Yaw", full: "Full"},
-                propertyID: "webBillboardMode",
-                propertyName: "billboardMode", // actual entity property name
             },
             {
                 label: "Input Mode",
