973f4a63b187d42e039a7139abb7a6f48d08aa14
Kalila L <somnilibertas@gmail.com>
2021-02-18 03:17:37 -0500

    Update examples.

--- scripts/system/ui/nyx/public/examples/dynamicTextEntity.js
+++ scripts/system/ui/nyx/public/examples/dynamicTextEntity.js
@@ -1,10 +1,10 @@
 (function () {
-    var NyxAlpha1 = Script.require('../nyx-helpers.js?12dsadsddsadssdassaddseras3');
+    var NyxAlpha1 = Script.require('../nyx-helpers.js?12dsadsddsadsdfafdasdassaddseras3');
 
     var _entityID;
     
     function onEntityMenuTriggered(triggeredEntityID, command, data) {
-        if (data.name === ':)' && triggeredEntityID === _entityID) {
+        if (data.name === 'TESTFIELD' && triggeredEntityID === _entityID) {
             Entities.editEntity(_entityID, { 
                 text: data.value
             });
@@ -16,19 +16,19 @@
 
         var initialProps = Entities.getEntityProperties(_entityID, ['text']);
         
-        NyxAlpha1.registerWithEntityMenu(entityID, [
+        NyxAlpha1.registerWithEntityMenu(_entityID, [
             {
                 type: 'textField',
-                name: ':)',
+                name: 'TESTFIELD',
                 hint: 'just smile!',
                 initialValue: initialProps.text
             }
         ]);
-        NyxAlpha1.entityMenuTriggered.connect(onEntityMenuTriggered);
+        NyxAlpha1.entityMenuTriggered.connect(_entityID, onEntityMenuTriggered);
     };
 
     this.unload = function () {
-        NyxAlpha1.entityMenuTriggered.disconnect(onEntityMenuTriggered);
+        NyxAlpha1.entityMenuTriggered.connect(_entityID, onEntityMenuTriggered);
     };
 
 });
