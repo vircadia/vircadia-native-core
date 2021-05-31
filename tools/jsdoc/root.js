7ccdbdea6433221fd356f0451a63449d286b9b4e
Kalila L <somnilibertas@gmail.com>
2021-05-31 05:43:12 -0400

    Some APIDocs metadata fixes.

--- tools/jsdoc/root.js
+++ tools/jsdoc/root.js
@@ -2,10 +2,11 @@
 //  root.js
 //
 //  Copyright 2016 High Fidelity, Inc.
+//  Copyright 2021 Vircaida contributors.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
 //
-//  Root of High Fidelity generated java script documentation
+//  Root of Vircadia JSDoc generated documentation
 //
 
