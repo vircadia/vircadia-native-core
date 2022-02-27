e06a5930b5142608b4df527c2a9b89cd8907b6d6
namark <nshan.nnnn@gmail.com>
2022-02-28 01:55:09 +0400

    Renamed vircadia-sdk library to vircadia-client.

--- libraries/vircadia-sdk/src/version.cpp
+++ libraries/vircadia-sdk/src/version.cpp
@@ -1,6 +1,6 @@
 //
 //  version.cpp
-//  libraries/vircadia_sdk/src
+//  libraries/client/src
 //
 //  Created by Nshan G. on 18 Feb 2022.
 //  Copyright 2022 Vircadia contributors.
@@ -11,15 +11,15 @@
 #define STRINGIFY2( x) #x
 #define STRINGIFY(x) STRINGIFY2(x)
 
-VIRCADIA_SDK_DYN_API
-const vircadia_sdk_version_data* vircadia_sdk_version() {
-    static const vircadia_sdk_version_data data {
-        VIRCADIA_SDK_VERSION_MAJOR,
-        VIRCADIA_SDK_VERSION_MINOR,
-        VIRCADIA_SDK_VERSION_TWEAK,
-        STRINGIFY(VIRCADIA_SDK_GIT_COMMIT_SHA_SHORT),
-        STRINGIFY(VIRCADIA_SDK_VERSION),
-        STRINGIFY(VIRCADIA_SDK_VERSION_FULL)
+VIRCADIA_CLIENT_DYN_API
+const vircadia_client_version_data* vircadia_client_version() {
+    static const vircadia_client_version_data data {
+        VIRCADIA_CLIENT_VERSION_MAJOR,
+        VIRCADIA_CLIENT_VERSION_MINOR,
+        VIRCADIA_CLIENT_VERSION_TWEAK,
+        STRINGIFY(VIRCADIA_CLIENT_GIT_COMMIT_SHA_SHORT),
+        STRINGIFY(VIRCADIA_CLIENT_VERSION),
+        STRINGIFY(VIRCADIA_CLIENT_VERSION_FULL)
     };
     return &data;
 }
--- libraries/vircadia-sdk/src/version.h
+++ libraries/vircadia-sdk/src/version.h
@@ -1,27 +1,27 @@
 //
 //  version.h
-//  libraries/vircadia_sdk/src
+//  libraries/client/src
 //
 //  Created by Nshan G. on 18 Feb 2022.
 //  Copyright 2022 Vircadia contributors.
 //
 
-#ifndef VIRCADIA_LIBRARIES_CLIENT_SDK_SRC_VERSION_H
-#define VIRCADIA_LIBRARIES_CLIENT_SDK_SRC_VERSION_H
+#ifndef VIRCADIA_LIBRARIES_CLIENT_SRC_VERSION_H
+#define VIRCADIA_LIBRARIES_CLIENT_SRC_VERSION_H
 
 #if defined(_MSC_VER)
-#ifdef VIRCADIA_SDK_DLL_EXPORT
+#ifdef VIRCADIA_CLIENT_DLL_EXPORT
 /* We are building this library */
-#   define VIRCADIA_SDK_DYN_API extern "C" __declspec(dllexport)
+#   define VIRCADIA_CLIENT_DYN_API extern "C" __declspec(dllexport)
 #else
 /* We are using this library */
-#   define VIRCADIA_SDK_DYN_API  extern "C" __declspec(dllimport)
+#   define VIRCADIA_CLIENT_DYN_API  extern "C" __declspec(dllimport)
 #endif
 #else
-    #define VIRCADIA_SDK_DYN_API extern "C"
+    #define VIRCADIA_CLIENT_DYN_API extern "C"
 #endif
 
-struct vircadia_sdk_version_data {
+struct vircadia_client_version_data {
     int major;
     int minor;
     int tweak;
@@ -30,7 +30,7 @@ struct vircadia_sdk_version_data {
     const char* full;
 };
 
-VIRCADIA_SDK_DYN_API
-const vircadia_sdk_version_data* vircadia_sdk_version();
+VIRCADIA_CLIENT_DYN_API
+const vircadia_client_version_data* vircadia_client_version();
 
 #endif /* end of include guard */
