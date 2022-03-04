021ecff4a5ec0a35fa90d0b0adbb6af5745a208e
namark <nshan.nnnn@gmail.com>
2022-03-05 02:42:35 +0400

    Force catch2 to be built as a static library,
    trying to fix the windows build.

--- cmake/ports/catch2/portfile.cmake
+++ cmake/ports/catch2/portfile.cmake
@@ -8,6 +8,8 @@ vcpkg_from_github(
         fix-install-path.patch
 )
 
+set(VCPKG_LIBRARY_LINKAGE static)
+
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" BUILD_STATIC)
 
 vcpkg_configure_cmake(
