d93c4a7306bb49da2d64ddd7b347e702fe540915
namark <nshan.nnnn@gmail.com>
2022-03-10 18:37:21 +0400

    Fixed node list initialization in assignment client monitor.

--- assignment-client/src/AssignmentClientMonitor.cpp
+++ assignment-client/src/AssignmentClientMonitor.cpp
@@ -72,7 +72,8 @@ AssignmentClientMonitor::AssignmentClientMonitor(const unsigned int numAssignmen
     // create a NodeList so we can receive stats from children
     DependencyManager::registerInheritance<LimitedNodeList, NodeList>();
     auto addressManager = DependencyManager::set<AddressManager>();
-    auto nodeList = DependencyManager::set<LimitedNodeList>(LimitedNodeList::Ports{listenPort});
+    auto nodeList = DependencyManager::set<NodeList>(NodeList::Params{NodeType::Unassigned, {listenPort}});
+
 
     auto& packetReceiver = DependencyManager::get<NodeList>()->getPacketReceiver();
     packetReceiver.registerListener(PacketType::AssignmentClientStatus,
