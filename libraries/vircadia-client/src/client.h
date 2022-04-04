960aa63020284a581175156493f7cd0048f3c4de
namark <nshan.nnnn@gmail.com>
2022-04-04 20:10:50 +0400

    Main client library header.

---
+++ libraries/vircadia-client/src/client.h
@@ -0,0 +1,25 @@
+//
+//  client.h
+//  libraries/vircadia-client/src
+//
+//  Created by Nshan G. on 31 March 2022.
+//  Copyright 2022 Vircadia contributors.
+//
+//  Distributed under the Apache License, Version 2.0.
+//  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
+//
+
+/// @file
+/// The main header of the library that includes all components.
+
+#ifndef LIBRARIES_VIRCADIA_CLIENT_SRC_CLIENT_H
+#define LIBRARIES_VIRCADIA_CLIENT_SRC_CLIENT_H
+
+#include "version.h"
+#include "error.h"
+#include "context.h"
+#include "node_list.h"
+#include "messages.h"
+#include "message_types.h"
+
+#endif /* end of include guard */
