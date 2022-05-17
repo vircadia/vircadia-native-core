38f9272d792eef66df80f109f979ffda6715142c
namark <nshan.nnnn@gmail.com>
2022-05-18 01:18:04 +0400

    Updated some copyright statements.

--- libraries/avatars-core/src/AvatarTraits.cpp
+++ libraries/avatars-core/src/AvatarTraits.cpp
@@ -5,6 +5,7 @@
 //  Created by Nshan G. on 21 Apr 2022.
 //  Copyright 2019 High Fidelity, Inc.
 //  Copyright 2022 Vircadia contributors.
+//  Copyright 2022 DigiSomni LLC.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
