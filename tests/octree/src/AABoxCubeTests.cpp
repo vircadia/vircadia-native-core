cdf15e40acef504967b9174242ec999c4e4e8fad
Dale Glass <dale@daleglass.net>
2022-06-06 23:25:30 +0200

    Add missing findRayIntersection arguments

--- tests/octree/src/AABoxCubeTests.cpp
+++ tests/octree/src/AABoxCubeTests.cpp
@@ -20,16 +20,17 @@ void AABoxCubeTests::raycastOutHitsXMinFace() {
     // Raycast inside out
     glm::vec3 corner(0.0f, 0.0f, 0.0f);
     float size = 1.0f;
-    
+
     AABox box(corner, size);
     glm::vec3 origin(0.5f, 0.5f, 0.5f);
     glm::vec3 direction(-1.0f, 0.0f, 0.0f);
     float distance;
     BoxFace face;
     glm::vec3 surfaceNormal;
+    glm::vec3 invDirection; //FIXME -- what should this be set to?
 
-    bool intersects = box.findRayIntersection(origin, direction, distance, face, surfaceNormal);
-    
+    bool intersects = box.findRayIntersection(origin, direction, invDirection, distance, face, surfaceNormal);
+
     QCOMPARE(intersects, true);
     QCOMPARE(distance, 0.5f);
     QCOMPARE(face, MIN_X_FACE);
@@ -39,15 +40,17 @@ void AABoxCubeTests::raycastOutHitsXMaxFace () {
     // Raycast inside out
     glm::vec3 corner(0.0f, 0.0f, 0.0f);
     float size = 1.0f;
-    
+
     AABox box(corner, size);
     glm::vec3 origin(0.5f, 0.5f, 0.5f);
     glm::vec3 direction(1.0f, 0.0f, 0.0f);
     float distance;
     BoxFace face;
     glm::vec3 surfaceNormal;
+    glm::vec3 invDirection; //FIXME -- what should this be set to?
 
-    bool intersects = box.findRayIntersection(origin, direction, distance, face, surfaceNormal);
+
+    bool intersects = box.findRayIntersection(origin, direction, invDirection, distance, face, surfaceNormal);
 
     QCOMPARE(intersects, true);
     QCOMPARE(distance, 0.5f);
@@ -57,15 +60,16 @@ void AABoxCubeTests::raycastInHitsXMinFace () {
     // Raycast outside in
     glm::vec3 corner(0.5f, 0.0f, 0.0f);
     float size = 0.5f;
-    
+
     AABox box(corner, size);
     glm::vec3 origin(0.25f, 0.25f, 0.25f);
     glm::vec3 direction(1.0f, 0.0f, 0.0f);
     float distance;
     BoxFace face;
     glm::vec3 surfaceNormal;
+    glm::vec3 invDirection; //FIXME -- what should this be set to?
 
-    bool intersects = box.findRayIntersection(origin, direction, distance, face, surfaceNormal);
+    bool intersects = box.findRayIntersection(origin, direction, invDirection, distance, face, surfaceNormal);
 
     QCOMPARE(intersects, true);
     QCOMPARE(distance, 0.25f);
