737edcd19ec491fb6c6c814bdcbe30beb63a7bbe
Dale Glass <51060919+daleglass@users.noreply.github.com>
2022-06-24 16:19:35 +0200

    Update libraries/shared/src/WarningsSuppression.h
    Co-authored-by: Kalila <69767640+digisomni@users.noreply.github.com>

--- libraries/shared/src/WarningsSuppression.h
+++ libraries/shared/src/WarningsSuppression.h
@@ -3,7 +3,7 @@
 //
 //
 //  Created by Dale Glass on 5/6/2022
-//  Copyright 2022 Dale Glass
+//  Copyright 2022 Vircadia contributors.
 //
 //  Distributed under the Apache License, Version 2.0.
 //  See the accompanying file LICENSE or http://www.apache.org/licenses/LICENSE-2.0.html
