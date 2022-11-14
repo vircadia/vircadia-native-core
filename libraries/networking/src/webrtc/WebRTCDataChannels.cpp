0b97e72524ef519df0bca7745fb9c25bb4241837
namark <nshan.nnnn@gmail.com>
2022-11-14 15:36:07 +0400

    Reverted copyright date update.

--- libraries/networking/src/webrtc/WebRTCDataChannels.cpp
+++ libraries/networking/src/webrtc/WebRTCDataChannels.cpp
@@ -4,7 +4,7 @@
 //
 //  Created by David Rowe on 21 May 2021.
 //  Copyright 2021 Vircadia contributors.
-//  Copyright 2022 DigiSomni LLC.
+//  Copyright 2021 DigiSomni LLC.
 //
 
 #include "WebRTCDataChannels.h"
