9b22626e891aab3e4754cc6ba234e0eb6be0b187
Eugene <luorixo@gmail.com>
2022-11-24 16:58:26 +1300

    new feat: added UI in the Networking page for Metaverse Account state

--- domain-server/resources/web/web-new/src/pages/Settings/Networking.vue
+++ domain-server/resources/web/web-new/src/pages/Settings/Networking.vue
@@ -1,11 +1,32 @@
 <template>
-    <h1>Networking Test</h1>
+    <div class="q-pa-md">
+        <q-card class="my-card">
+            <q-card-section>
+                <div class="text-h5 text-center text-weight-bold">Your Metaverse Account</div>
+                <div v-if="isUserConnected" class="text-overline text-positive text-center">Metaverse Account Connected</div>
+                <div v-else class="text-overline text-negative text-center">Account Not Connected</div>
+                <q-separator inset />
+
+                <!-- METAVERSE LOGIN/LOGOUT (connect/disconnect) BUTTON -->
+                <q-card-actions align="center">
+                    <q-btn v-if="!isUserConnected" class="q-mt-sm" padding="0.5em 2em" push color="positive" label="Connect Metaverse Account" />
+                    <q-btn v-else class="q-mt-sm" padding="0.5em 1.5em" push color="negative" label="Disconnect Account" />
+                </q-card-actions>
+            </q-card-section>
+        </q-card>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-    name: "Index"
+    name: "Index",
+
+    data () {
+        return {
+            isUserConnected: false
+        };
+    }
 });
 </script>
